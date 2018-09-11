d4fc54769e636f2b9705f556f3b8a21d439116d8
Michael Macias <zaeleus@gmail.com>
2018-09-11 17:41:54 -0500

    bam: Add cigar constructor for raw bytes

--- src/formats/bam/cigar.rs
+++ src/formats/bam/cigar.rs
@@ -1,5 +1,8 @@
 use std::fmt;
+use std::mem;
 use std::ops::Deref;
+
+use byteorder::{ByteOrder, LittleEndian};
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Op {
@@ -123,6 +126,14 @@ pub struct Cigar {
 }
 
 impl Cigar {
+    pub fn from_bytes(bytes: &[u8]) -> Cigar {
+        let cigar = bytes.chunks(mem::size_of::<u32>())
+            .map(|c| LittleEndian::read_u32(c))
+            .collect();
+
+        Cigar::new(cigar)
+    }
+
     pub fn new(cigar: Vec<u32>) -> Cigar {
         Cigar { cigar }
     }
@@ -157,5 +168,19 @@ impl<'a, I: Iterator<Item = &'a u32>> Iterator for Ops<I> {
 
     fn next(&mut self) -> Option<Op> {
         self.0.next().map(|&u| Op::from_u32(u))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_bytes() {
+        let bytes = [0x40, 0x02, 0x00, 0x00, 0x62, 0x03, 0x00, 0x00];
+        let cigar =  Cigar::from_bytes(&bytes);
+        assert_eq!(cigar.cigar.len(), 2);
+        assert_eq!(&cigar.cigar[0], &0x240);
+        assert_eq!(&cigar.cigar[1], &0x362);
     }
 }
