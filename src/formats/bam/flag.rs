61d30963f023903932e4d911ea9b939d06cfdb79
Michael Macias <zaeleus@gmail.com>
2019-02-01 17:30:18 -0600

    bam: Always consume self with Flag methods
    So says clippy: https://rust-lang.github.io/rust-clippy/master/index.html#trivially_copy_pass_by_ref

--- src/formats/bam/flag.rs
+++ src/formats/bam/flag.rs
@@ -6,55 +6,55 @@ impl Flag {
         Flag(flag)
     }
 
-    pub fn inner(&self) -> u16 {
+    pub fn inner(self) -> u16 {
         self.0
     }
 
-    pub fn is_paired(&self) -> bool {
+    pub fn is_paired(self) -> bool {
         self.0 & 0x01 != 0
     }
 
-    pub fn is_proper_pair(&self) -> bool {
+    pub fn is_proper_pair(self) -> bool {
         self.0 & 0x02 != 0
     }
 
-    pub fn is_unmapped(&self) -> bool {
+    pub fn is_unmapped(self) -> bool {
         self.0 & 0x04 != 0
     }
 
-    pub fn is_mate_unmapped(&self) -> bool {
+    pub fn is_mate_unmapped(self) -> bool {
         self.0 & 0x08 != 0
     }
 
-    pub fn is_reverse(&self) -> bool {
+    pub fn is_reverse(self) -> bool {
         self.0 & 0x10 != 0
     }
 
-    pub fn is_mate_reverse(&self) -> bool {
+    pub fn is_mate_reverse(self) -> bool {
         self.0 & 0x20 != 0
     }
 
-    pub fn is_read_1(&self) -> bool {
+    pub fn is_read_1(self) -> bool {
         self.0 & 0x40 != 0
     }
 
-    pub fn is_read_2(&self) -> bool {
+    pub fn is_read_2(self) -> bool {
         self.0 & 0x80 != 0
     }
 
-    pub fn is_secondary(&self) -> bool {
+    pub fn is_secondary(self) -> bool {
         self.0 & 0x0100 != 0
     }
 
-    pub fn is_qc_fail(&self) -> bool {
+    pub fn is_qc_fail(self) -> bool {
         self.0 & 0x0200 != 0
     }
 
-    pub fn is_dup(&self) -> bool {
+    pub fn is_dup(self) -> bool {
         self.0 & 0x0400 != 0
     }
 
-    pub fn is_supplementary(&self) -> bool {
+    pub fn is_supplementary(self) -> bool {
         self.0 & 0x0800 != 0
     }
 }
