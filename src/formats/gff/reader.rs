1effd822972b794b3dfcb5852a1520a990ef7bb4
Michael Macias <zaeleus@gmail.com>
2019-05-08 13:37:45 -0500

    gff: Remove Reader::open
    Use `gff::open` to read from files instead.

--- src/formats/gff/reader.rs
+++ src/formats/gff/reader.rs
@@ -1,22 +1,10 @@
-use std::{
-    fs::File,
-    io::{self, Read},
-    path::Path,
-};
+use std::io::Read;
 
 pub struct Reader<R: Read> {
     inner: csv::Reader<R>,
 }
 
 impl<R: Read> Reader<R> {
-    pub fn open<P>(src: P) -> io::Result<Reader<File>>
-    where
-        P: AsRef<Path>,
-    {
-        let file = File::open(src)?;
-        Ok(Reader::new(file))
-    }
-
     pub fn new(reader: R) -> Reader<R> {
         let inner = csv::ReaderBuilder::new()
             .delimiter(b'\t')
