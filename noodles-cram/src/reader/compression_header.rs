b67f954864773fae26003c463042a40adb7cb289
Michael Macias <zaeleus@gmail.com>
2020-02-02 01:21:59 -0600

    cram/reader/compression_header: Compose compression header reader

--- noodles-cram/src/reader/compression_header.rs
+++ noodles-cram/src/reader/compression_header.rs
@@ -1,3 +1,20 @@
 mod data_series_encodings;
 mod preservation_map;
 mod tag_encodings;
+
+use std::io::{self, Read};
+
+use self::{
+    data_series_encodings::read_data_series_encodings, preservation_map::read_preservation_map,
+    tag_encodings::read_tag_encodings,
+};
+
+pub fn read_compression_header<R>(reader: &mut R) -> io::Result<()>
+where
+    R: Read,
+{
+    read_preservation_map(reader)?;
+    read_data_series_encodings(reader)?;
+    read_tag_encodings(reader)?;
+    Ok(())
+}
