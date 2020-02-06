5d80ed7c0938edd5feb1b256fa01c95f9a5025a6
Michael Macias <zaeleus@gmail.com>
2020-02-05 18:13:37 -0600

    bam/examples: Add count example

---
+++ noodles-bam/examples/bam_count.rs
@@ -0,0 +1,20 @@
+//! Counts the number of records in a BAM file.
+//!
+//! The result matches the output of `samtools view -c <src>`.
+
+use std::{env, fs::File};
+
+use noodles_bam as bam;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let src = env::args().nth(1).expect("missing src");
+
+    let file = File::open(src)?;
+    let mut reader = bam::Reader::new(file);
+    reader.meta()?;
+
+    let count = reader.records().count();
+    println!("{}", count);
+
+    Ok(())
+}
