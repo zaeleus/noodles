9c43dead6c77b56bbd3b971da85be703065252de
Michael Macias <zaeleus@gmail.com>
2020-02-05 20:01:03 -0600

    cram/reader: Read compression header as a block

--- noodles-cram/src/reader/compression_header.rs
+++ noodles-cram/src/reader/compression_header.rs
@@ -9,15 +9,23 @@ use self::{
     tag_encodings::read_tag_encodings,
 };
 
-use crate::CompressionHeader;
+use crate::{Block, CompressionHeader};
+
+use super::block::read_block;
 
 pub fn read_compression_header<R>(reader: &mut R) -> io::Result<CompressionHeader>
 where
     R: Read,
 {
-    let preservation_map = read_preservation_map(reader)?;
-    let data_series_encodings = read_data_series_encodings(reader)?;
-    let tag_encodings = read_tag_encodings(reader)?;
+    let mut block = Block::default();
+    read_block(reader, &mut block)?;
+
+    let data = block.decompressed_data();
+    let mut data_reader = &data[..];
+
+    let preservation_map = read_preservation_map(&mut data_reader)?;
+    let data_series_encodings = read_data_series_encodings(&mut data_reader)?;
+    let tag_encodings = read_tag_encodings(&mut data_reader)?;
 
     Ok(CompressionHeader::new(
         preservation_map,
