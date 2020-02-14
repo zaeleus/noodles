5ea06e460a48e20a2085d4652225fac8b215086e
Michael Macias <zaeleus@gmail.com>
2020-02-13 22:06:11 -0600

    cram/reader: Collapse single line readers

--- noodles-cram/src/reader/block.rs
+++ noodles-cram/src/reader/block.rs
@@ -2,68 +2,30 @@ use std::io::{self, Read};
 
 use byteorder::ReadBytesExt;
 
-use crate::{
-    num::{read_itf8, Itf8},
-    Block,
-};
+use crate::{num::read_itf8, Block};
 
 pub fn read_block<R>(reader: &mut R, block: &mut Block) -> io::Result<()>
 where
     R: Read,
 {
-    let method = read_method(reader)?;
-    let content_type_id = read_block_content_type_id(reader)?;
-    let content_id = read_block_content_id(reader)?;
-    let compressed_len = read_size_in_bytes(reader)?;
-    let uncompressed_len = read_raw_size_in_bytes(reader)?;
+    let method = reader.read_u8()?;
+    let block_content_type_id = reader.read_u8()?;
+    let block_content_id = read_itf8(reader)?;
+    let size_in_bytes = read_itf8(reader)?;
+    let raw_size_in_bytes = read_itf8(reader)?;
 
     *block.compression_method_mut() = method;
-    *block.content_type_mut() = content_type_id;
-    *block.content_id_mut() = content_id;
-    *block.uncompressed_len_mut() = uncompressed_len;
+    *block.content_type_mut() = block_content_type_id;
+    *block.content_id_mut() = block_content_id;
+    *block.uncompressed_len_mut() = raw_size_in_bytes;
 
     let data = block.data_mut();
-    data.resize(compressed_len as usize, Default::default());
+    data.resize(size_in_bytes as usize, Default::default());
     reader.read_exact(data)?;
 
     let _crc32 = read_crc32(reader)?;
 
     Ok(())
-}
-
-fn read_method<R>(reader: &mut R) -> io::Result<u8>
-where
-    R: Read,
-{
-    reader.read_u8()
-}
-
-fn read_block_content_type_id<R>(reader: &mut R) -> io::Result<u8>
-where
-    R: Read,
-{
-    reader.read_u8()
-}
-
-fn read_block_content_id<R>(reader: &mut R) -> io::Result<Itf8>
-where
-    R: Read,
-{
-    read_itf8(reader)
-}
-
-fn read_size_in_bytes<R>(reader: &mut R) -> io::Result<Itf8>
-where
-    R: Read,
-{
-    read_itf8(reader)
-}
-
-fn read_raw_size_in_bytes<R>(reader: &mut R) -> io::Result<Itf8>
-where
-    R: Read,
-{
-    read_itf8(reader)
 }
 
 fn read_crc32<R>(reader: &mut R) -> io::Result<[u8; 4]>
--- noodles-cram/src/reader/container.rs
+++ noodles-cram/src/reader/container.rs
@@ -11,84 +11,28 @@ pub fn read_header<R>(reader: &mut R) -> io::Result<Header>
 where
     R: Read,
 {
-    let length = read_length(reader)?;
-    let reference_sequence_id = read_reference_sequence_id(reader)?;
-    let start_pos = read_starting_position_on_the_reference(reader)?;
-    let alignment_span = read_alignment_span(reader)?;
-    let n_records = read_number_of_records(reader)?;
-    let record_counter = read_record_counter(reader)?;
-    let bases = read_bases(reader)?;
-    let n_blocks = read_number_of_blocks(reader)?;
+    let length = reader.read_i32::<LittleEndian>()?;
+    let reference_sequence_id = read_itf8(reader)?;
+    let starting_position_on_the_reference = read_itf8(reader)?;
+    let alignment_span = read_itf8(reader)?;
+    let number_of_records = read_itf8(reader)?;
+    let record_counter = read_ltf8(reader)?;
+    let bases = read_ltf8(reader)?;
+    let number_of_blocks = read_itf8(reader)?;
     let landmarks = read_landmarks(reader)?;
     let _crc = read_crc32(reader)?;
 
     Ok(Header::new(
         length,
         reference_sequence_id,
-        start_pos,
+        starting_position_on_the_reference,
         alignment_span,
-        n_records,
+        number_of_records,
         record_counter,
         bases,
-        n_blocks,
+        number_of_blocks,
         landmarks,
     ))
-}
-
-fn read_length<R>(reader: &mut R) -> io::Result<i32>
-where
-    R: Read,
-{
-    reader.read_i32::<LittleEndian>()
-}
-
-fn read_reference_sequence_id<R>(reader: &mut R) -> io::Result<Itf8>
-where
-    R: Read,
-{
-    read_itf8(reader)
-}
-
-fn read_starting_position_on_the_reference<R>(reader: &mut R) -> io::Result<Itf8>
-where
-    R: Read,
-{
-    read_itf8(reader)
-}
-
-fn read_alignment_span<R>(reader: &mut R) -> io::Result<Itf8>
-where
-    R: Read,
-{
-    read_itf8(reader)
-}
-
-fn read_number_of_records<R>(reader: &mut R) -> io::Result<Itf8>
-where
-    R: Read,
-{
-    read_itf8(reader)
-}
-
-fn read_record_counter<R>(reader: &mut R) -> io::Result<Ltf8>
-where
-    R: Read,
-{
-    read_ltf8(reader)
-}
-
-fn read_bases<R>(reader: &mut R) -> io::Result<Ltf8>
-where
-    R: Read,
-{
-    read_ltf8(reader)
-}
-
-fn read_number_of_blocks<R>(reader: &mut R) -> io::Result<Itf8>
-where
-    R: Read,
-{
-    read_itf8(reader)
 }
 
 fn read_landmarks<R>(reader: &mut R) -> io::Result<Vec<Itf8>>
