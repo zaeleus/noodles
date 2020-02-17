d72f717d1404a3207bd40ad0f16a1ac951e4c39f
Michael Macias <zaeleus@gmail.com>
2020-02-16 18:02:45 -0600

    cram/encoding: Add kind parse error

--- noodles-cram/src/encoding.rs
+++ noodles-cram/src/encoding.rs
@@ -1,40 +1,6 @@
-use std::convert::TryFrom;
+mod kind;
 
-use crate::num::Itf8;
-
-#[derive(Clone, Copy, Debug)]
-pub enum Kind {
-    Null,
-    External,
-    Golomb,
-    Huffman,
-    ByteArrayLen,
-    ByteArrayStop,
-    Beta,
-    Subexp,
-    GolombRice,
-    Gamma,
-}
-
-impl TryFrom<Itf8> for Kind {
-    type Error = ();
-
-    fn try_from(id: Itf8) -> Result<Self, Self::Error> {
-        match id {
-            0 => Ok(Self::Null),
-            1 => Ok(Self::External),
-            2 => Ok(Self::Golomb),
-            3 => Ok(Self::Huffman),
-            4 => Ok(Self::ByteArrayLen),
-            5 => Ok(Self::ByteArrayStop),
-            6 => Ok(Self::Beta),
-            7 => Ok(Self::Subexp),
-            8 => Ok(Self::GolombRice),
-            9 => Ok(Self::Gamma),
-            _ => Err(()),
-        }
-    }
-}
+pub use self::kind::Kind;
 
 #[derive(Debug)]
 pub struct Encoding {
