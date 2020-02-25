f01fcad7bd347eb3ef412d957b3e756d808d5777
Michael Macias <zaeleus@gmail.com>
2020-02-24 20:45:18 -0600

    cram/data_series: Add parse error

--- noodles-cram/src/data_series.rs
+++ noodles-cram/src/data_series.rs
@@ -1,4 +1,15 @@
-use std::convert::TryFrom;
+use std::{convert::TryFrom, error, fmt};
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct TryFromByteSliceError(Vec<u8>);
+
+impl fmt::Display for TryFromByteSliceError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "invalid data series: {:#x?}", self.0)
+    }
+}
+
+impl error::Error for TryFromByteSliceError {}
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub enum DataSeries {
@@ -33,7 +44,7 @@ pub enum DataSeries {
 }
 
 impl TryFrom<&[u8]> for DataSeries {
-    type Error = ();
+    type Error = TryFromByteSliceError;
 
     fn try_from(b: &[u8]) -> Result<Self, Self::Error> {
         match b {
@@ -65,7 +76,30 @@ impl TryFrom<&[u8]> for DataSeries {
             b"MQ" => Ok(Self::MappingQualities),
             b"BA" => Ok(Self::Bases),
             b"QS" => Ok(Self::QualityScores),
-            _ => Err(()),
+            _ => Err(TryFromByteSliceError(b.to_vec())),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::convert::TryFrom;
+
+    use super::*;
+
+    #[test]
+    fn test_try_from() {
+        assert_eq!(
+            DataSeries::try_from(&b"BF"[..]),
+            Ok(DataSeries::BamBitFlags)
+        );
+        assert_eq!(
+            DataSeries::try_from(&b"CF"[..]),
+            Ok(DataSeries::CramBitFlags)
+        );
+        assert_eq!(
+            DataSeries::try_from(&b"XY"[..]),
+            Err(TryFromByteSliceError(vec![0x58, 0x59]))
+        );
     }
 }
