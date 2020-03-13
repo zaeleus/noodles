6769737b53e1795f40c9adf047fcf73cce1cccc7
Michael Macias <zaeleus@gmail.com>
2020-03-12 19:02:43 -0500

    sam/header/read_group: Require id field

--- noodles-sam/src/header/read_group.rs
+++ noodles-sam/src/header/read_group.rs
@@ -6,17 +6,27 @@ use std::{collections::HashMap, convert::TryFrom};
 pub use self::{platform::Platform, tag::Tag};
 
 #[derive(Debug)]
-pub struct ReadGroup(HashMap<Tag, String>);
+pub struct ReadGroup {
+    id: String,
+    fields: HashMap<Tag, String>,
+}
 
 impl ReadGroup {
+    pub fn id(&self) -> &str {
+        &self.id
+    }
+
     pub fn get(&self, tag: &Tag) -> Option<&String> {
-        self.0.get(tag)
+        self.fields.get(tag)
     }
 }
 
 impl Default for ReadGroup {
     fn default() -> Self {
-        Self(HashMap::new())
+        Self {
+            id: String::new(),
+            fields: HashMap::new(),
+        }
     }
 }
 
@@ -24,13 +34,36 @@ impl TryFrom<&[(String, String)]> for ReadGroup {
     type Error = ();
 
     fn try_from(raw_fields: &[(String, String)]) -> Result<Self, Self::Error> {
-        let mut fields = HashMap::new();
+        let mut read_group = ReadGroup::default();
+
+        let mut has_id = false;
 
         for (raw_tag, value) in raw_fields {
             let tag = raw_tag.parse()?;
-            fields.insert(tag, value.into());
+
+            if let Tag::Id = tag {
+                read_group.id = value.into();
+                has_id = true;
+            }
+
+            read_group.fields.insert(tag, value.into());
         }
 
-        Ok(Self(fields))
+        if !has_id {
+            return Err(());
+        }
+
+        Ok(read_group)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_str_with_no_version() {
+        let fields = [(String::from("DS"), String::from("noodles"))];
+        assert!(ReadGroup::try_from(&fields[..]).is_err());
     }
 }
