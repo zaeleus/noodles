06adc275bf8388d287530608454f90463f598547
Michael Macias <zaeleus@gmail.com>
2020-03-12 19:10:54 -0500

    sam/header/program: Require id field

--- noodles-sam/src/header/program.rs
+++ noodles-sam/src/header/program.rs
@@ -5,17 +5,27 @@ use std::{collections::HashMap, convert::TryFrom};
 pub use self::tag::Tag;
 
 #[derive(Debug)]
-pub struct Program(HashMap<Tag, String>);
+pub struct Program {
+    id: String,
+    fields: HashMap<Tag, String>,
+}
 
 impl Program {
+    pub fn id(&self) -> &str {
+        &self.id
+    }
+
     pub fn get(&self, tag: &Tag) -> Option<&String> {
-        self.0.get(tag)
+        self.fields.get(tag)
     }
 }
 
 impl Default for Program {
     fn default() -> Self {
-        Self(HashMap::new())
+        Self {
+            id: String::new(),
+            fields: HashMap::new(),
+        }
     }
 }
 
@@ -23,13 +33,36 @@ impl TryFrom<&[(String, String)]> for Program {
     type Error = ();
 
     fn try_from(raw_fields: &[(String, String)]) -> Result<Self, Self::Error> {
-        let mut fields = HashMap::new();
+        let mut program = Program::default();
+
+        let mut has_id = false;
 
         for (raw_tag, value) in raw_fields {
             let tag = raw_tag.parse()?;
-            fields.insert(tag, value.into());
+
+            if let Tag::Id = tag {
+                program.id = value.into();
+                has_id = true;
+            }
+
+            program.fields.insert(tag, value.into());
         }
 
-        Ok(Self(fields))
+        if !has_id {
+            return Err(());
+        }
+
+        Ok(program)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_str_with_no_id() {
+        let fields = [(String::from("DS"), String::from("noodles"))];
+        assert!(Program::try_from(&fields[..]).is_err());
     }
 }
