0fc3c8ad4e775cce34477cf794b3a79040cb5b5e
Michael Macias <zaeleus@gmail.com>
2020-03-17 17:42:05 -0500

    bam/reader: Remove deprecated header readers

--- noodles-bam/examples/bam_count.rs
+++ noodles-bam/examples/bam_count.rs
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let file = File::open(src)?;
     let mut reader = bam::Reader::new(file);
-    reader.header()?;
+    reader.read_header()?;
 
     let count = reader.records().count();
     println!("{}", count);
