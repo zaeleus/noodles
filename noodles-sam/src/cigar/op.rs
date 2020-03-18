445b1a18582cfa776ead7931215f0b9269a6a1a2
Michael Macias <zaeleus@gmail.com>
2020-03-17 20:16:18 -0500

    sam/cigar/op: Handle parsing empty inputs

--- noodles-sam/src/cigar/op.rs
+++ noodles-sam/src/cigar/op.rs
@@ -27,6 +27,7 @@ impl Op {
 
 #[derive(Debug)]
 pub enum ParseError {
+    Empty,
     InvalidLength(num::ParseIntError),
     InvalidKind(kind::ParseError),
 }
@@ -36,6 +37,7 @@ impl error::Error for ParseError {}
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            Self::Empty => write!(f, "cannot parse op from empty string"),
             Self::InvalidLength(e) => write!(f, "invalid op length: {}", e),
             Self::InvalidKind(e) => write!(f, "invalid op kind: {}", e),
         }
@@ -46,9 +48,15 @@ impl FromStr for Op {
     type Err = ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.is_empty() {
+            return Err(ParseError::Empty);
+        }
+
         let (raw_len, raw_kind) = s.split_at(s.len() - 1);
+
         let len = raw_len.parse().map_err(ParseError::InvalidLength)?;
         let kind = raw_kind.parse().map_err(ParseError::InvalidKind)?;
+
         Ok(Self::new(kind, len))
     }
 }
@@ -58,9 +66,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_from_str() {
-        assert_eq!("1M".parse::<Op>().unwrap(), Op::new(Kind::Match, 1));
-        assert_eq!("13N".parse::<Op>().unwrap(), Op::new(Kind::Skip, 13));
-        assert_eq!("144S".parse::<Op>().unwrap(), Op::new(Kind::SoftClip, 144));
+    fn test_from_str() -> Result<(), ParseError> {
+        assert_eq!("1M".parse::<Op>()?, Op::new(Kind::Match, 1));
+        assert_eq!("13N".parse::<Op>()?, Op::new(Kind::Skip, 13));
+        assert_eq!("144S".parse::<Op>()?, Op::new(Kind::SoftClip, 144));
+
+        assert!("".parse::<Op>().is_err());
+        assert!("Z".parse::<Op>().is_err());
+        assert!("21".parse::<Op>().is_err());
+
+        Ok(())
     }
 }
