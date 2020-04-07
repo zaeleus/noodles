14c7a2408175791d79ecfba4b70c26ab4dcc57ed
Michael Macias <zaeleus@gmail.com>
2020-04-07 18:22:13 -0500

    sam/reader: Add record reader

--- noodles-sam/src/reader.rs
+++ noodles-sam/src/reader.rs
@@ -41,21 +41,33 @@ where
 
         Ok(header_buf)
     }
+
+    pub fn read_record(&mut self, buf: &mut String) -> io::Result<usize> {
+        let result = self.inner.read_line(buf);
+
+        if result.is_ok() {
+            // Remove new line.
+            buf.pop();
+        }
+
+        result
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    static SAM: &[u8] = b"\
+    static DATA: &[u8] = b"\
 @HD\tVN:1.6\tSO:coordinate
 @SQ\tSN:ref0\tLN:13
 r001
+r002
 ";
 
     #[test]
     fn test_read_header() -> io::Result<()> {
-        let mut reader = Reader::new(&SAM[..]);
+        let mut reader = Reader::new(&DATA[..]);
 
         let actual = reader.read_header()?;
         let expected = b"\
@@ -63,6 +75,26 @@ r001
 @SQ\tSN:ref0\tLN:13
 ";
         assert_eq!(actual, &expected[..]);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_read_record() -> io::Result<()> {
+        let mut reader = Reader::new(&DATA[..]);
+        reader.read_header()?;
+
+        let mut buf = String::new();
+        reader.read_record(&mut buf)?;
+        assert_eq!(buf, "r001");
+
+        buf.clear();
+        reader.read_record(&mut buf)?;
+        assert_eq!(buf, "r002");
+
+        buf.clear();
+        let len = reader.read_record(&mut buf)?;
+        assert_eq!(len, 0);
 
         Ok(())
     }
