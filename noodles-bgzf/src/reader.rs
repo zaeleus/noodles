2487374ee93547588f965c107f87b1b02312028e
Michael Macias <zaeleus@gmail.com>
2020-04-14 18:53:45 -0500

    bgzf/reader: Implement Read

--- noodles-bgzf/src/reader.rs
+++ noodles-bgzf/src/reader.rs
@@ -9,6 +9,7 @@ pub struct Reader<R: Read> {
     inner: R,
     position: u64,
     cdata: Vec<u8>,
+    block: Block,
 }
 
 impl<R: Read> Reader<R> {
@@ -17,6 +18,7 @@ impl<R: Read> Reader<R> {
             inner,
             position: 0,
             cdata: Vec::new(),
+            block: Block::default(),
         }
     }
 
@@ -25,38 +27,15 @@ impl<R: Read> Reader<R> {
     }
 
     pub fn read_block(&mut self, block: &mut Block) -> io::Result<usize> {
-        // gzip header
-        let mut header = [0; BGZF_HEADER_SIZE];
-
-        if self.inner.read_exact(&mut header).is_err() {
-            return Ok(0);
+        match read_block(&mut self.inner, &mut self.cdata, block) {
+            Ok(0) => Ok(0),
+            Ok(bs) => {
+                block.set_c_offset(self.position);
+                self.position += bs as u64;
+                Ok(bs)
+            }
+            Err(e) => Err(e),
         }
-
-        let bsize = &header[16..18];
-        // Add 1 because BSIZE is "total Block SIZE minus 1".
-        let block_size = (LittleEndian::read_u16(bsize) + 1) as usize;
-
-        let cdata_len = block_size - BGZF_HEADER_SIZE - gz::TRAILER_SIZE;
-
-        self.cdata.resize(cdata_len, Default::default());
-        self.inner.read_exact(&mut self.cdata)?;
-
-        let mut trailer = [0; gz::TRAILER_SIZE];
-        self.inner.read_exact(&mut trailer)?;
-
-        let mut decoder = DeflateDecoder::new(&self.cdata[..]);
-
-        let block_buf = block.get_mut();
-        block_buf.clear();
-
-        decoder.read_to_end(block_buf)?;
-
-        block.set_c_offset(self.position);
-        block.set_position(0);
-
-        self.position += block_size as u64;
-
-        Ok(block_size)
     }
 }
 
@@ -73,6 +52,85 @@ impl<R: Read + Seek> Reader<R> {
 
         Ok(pos)
     }
+}
+
+impl<R> Read for Reader<R>
+where
+    R: Read,
+{
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        match self.block.read(buf) {
+            Ok(0) => match read_block(&mut self.inner, &mut self.cdata, &mut self.block) {
+                Ok(0) => Ok(0),
+                Ok(bs) => {
+                    self.block.set_c_offset(self.position);
+                    self.position += bs as u64;
+                    Err(io::Error::from(io::ErrorKind::Interrupted))
+                }
+                Err(e) => Err(e),
+            },
+            Ok(n) => Ok(n),
+            Err(e) => Err(e),
+        }
+    }
+}
+
+fn read_block_size<R>(reader: &mut R) -> io::Result<u16>
+where
+    R: Read,
+{
+    let mut header = [0; BGZF_HEADER_SIZE];
+
+    if reader.read_exact(&mut header).is_err() {
+        return Ok(0);
+    }
+
+    let bsize = &header[16..18];
+
+    // Add 1 because BSIZE is "total Block SIZE minus 1".
+    Ok(LittleEndian::read_u16(bsize) + 1)
+}
+
+fn read_trailer<R>(reader: &mut R) -> io::Result<()>
+where
+    R: Read,
+{
+    let mut trailer = [0; gz::TRAILER_SIZE];
+    reader.read_exact(&mut trailer)
+}
+
+fn inflate_data<R>(reader: R, writer: &mut Vec<u8>) -> io::Result<usize>
+where
+    R: Read,
+{
+    let mut decoder = DeflateDecoder::new(reader);
+    decoder.read_to_end(writer)
+}
+
+fn read_block<R>(reader: &mut R, cdata: &mut Vec<u8>, block: &mut Block) -> io::Result<usize>
+where
+    R: Read,
+{
+    let block_size = match read_block_size(reader).map(usize::from) {
+        Ok(0) => return Ok(0),
+        Ok(bs) => bs,
+        Err(e) => return Err(e),
+    };
+
+    let cdata_len = block_size - BGZF_HEADER_SIZE - gz::TRAILER_SIZE;
+    cdata.resize(cdata_len, Default::default());
+    reader.read_exact(cdata)?;
+
+    read_trailer(reader)?;
+
+    let block_buf = block.get_mut();
+    block_buf.clear();
+
+    inflate_data(&cdata[..], block_buf)?;
+
+    block.set_position(0);
+
+    Ok(block_size)
 }
 
 fn compressed_offset(offset: u64) -> u64 {
