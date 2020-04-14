51846a1a99373813bb0d5aac5880b507a0e5b2f4
Michael Macias <zaeleus@gmail.com>
2020-04-14 18:53:53 -0500

    bgzf/tests: Add a self test

--- noodles-bgzf/src/lib.rs
+++ noodles-bgzf/src/lib.rs
@@ -8,3 +8,30 @@ mod writer;
 const GZIP_XLEN_SIZE: usize = 2;
 const BGZF_XLEN: usize = 6;
 pub(crate) const BGZF_HEADER_SIZE: usize = gz::HEADER_SIZE + GZIP_XLEN_SIZE + BGZF_XLEN;
+
+#[cfg(test)]
+mod tests {
+    use std::io::{self, Read, Write};
+
+    use super::*;
+
+    #[test]
+    fn test_self() -> io::Result<()> {
+        let mut writer = Writer::new(Vec::new());
+
+        writer.write_all(b"noodles")?;
+        writer.flush()?;
+        writer.write_all(b"bgzf")?;
+        writer.flush()?;
+
+        let data = writer.get_ref();
+        let mut reader = Reader::new(&data[..]);
+
+        let mut buf = Vec::new();
+        reader.read_to_end(&mut buf)?;
+
+        assert_eq!(buf, b"noodlesbgzf");
+
+        Ok(())
+    }
+}
