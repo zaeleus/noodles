ddc1d8f0cab13ac4fd543d220bcefdf0d0eb5bc7
Michael Macias <zaeleus@gmail.com>
2020-04-15 17:43:52 -0500

    bgzf/gz: Annotate header and trailer sizes

--- noodles-bgzf/src/gz.rs
+++ noodles-bgzf/src/gz.rs
@@ -3,7 +3,10 @@ pub(crate) const MAGIC_NUMBER: [u8; 2] = [0x1f, 0x8b];
 
 pub(crate) const MTIME_NONE: u32 = 0;
 
+// ID1 (1) + ID2 (1) + CM (1) + FLG (1) + MTIME (4) + XLF (1) + OS (1)
 pub(crate) const HEADER_SIZE: usize = 10;
+
+// CRC32 (4) + ISIZE (4)
 pub(crate) const TRAILER_SIZE: usize = 8;
 
 #[non_exhaustive]
