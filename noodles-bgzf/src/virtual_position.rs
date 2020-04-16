8828f32c6f762eb3153217e0ff8909851a7190b7
Michael Macias <zaeleus@gmail.com>
2020-04-15 19:39:28 -0500

    bzgf/virtual_position: Implement Default

--- noodles-bgzf/src/virtual_position.rs
+++ noodles-bgzf/src/virtual_position.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
 pub struct VirtualPosition(u64);
 
 impl VirtualPosition {
