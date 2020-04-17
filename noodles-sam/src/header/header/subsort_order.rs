bbbf443768d97db71e59bbe371ea9ae95892ad02
Michael Macias <zaeleus@gmail.com>
2020-04-17 18:50:39 -0500

    sam/header/header/subsort_order: Add parse error

--- noodles-sam/src/header/header/subsort_order.rs
+++ noodles-sam/src/header/header/subsort_order.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{error, fmt, str::FromStr};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum SubsortOrder {
@@ -7,20 +7,47 @@ pub enum SubsortOrder {
     Coordinate(String),
 }
 
+#[derive(Debug)]
+pub enum ParseError {
+    MissingOrder,
+    InvalidOrder(String),
+    MissingSubsort,
+}
+
+impl error::Error for ParseError {}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "invalid subsort order: ")?;
+
+        match self {
+            Self::MissingOrder => write!(f, "missing order"),
+            Self::InvalidOrder(s) => {
+                write!(f, "expected {{unsorted, queryname, coordinate}}, got {}", s)
+            }
+            Self::MissingSubsort => write!(f, "missing subsort"),
+        }
+    }
+}
+
 impl FromStr for SubsortOrder {
-    type Err = ();
+    type Err = ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut pieces = s.splitn(2, ':');
 
-        let order = pieces.next().ok_or_else(|| ())?;
-        let subsort = pieces.next().map(|s| s.into()).ok_or_else(|| ())?;
+        let order = pieces.next().ok_or_else(|| ParseError::MissingOrder)?;
+
+        let subsort = pieces
+            .next()
+            .map(|s| s.into())
+            .ok_or_else(|| ParseError::MissingSubsort)?;
 
         match order {
             "unsorted" => Ok(Self::Unsorted(subsort)),
             "queryname" => Ok(Self::QueryName(subsort)),
             "coordinate" => Ok(Self::Coordinate(subsort)),
-            _ => Err(()),
+            _ => Err(ParseError::InvalidOrder(s.into())),
         }
     }
 }
@@ -30,7 +57,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_from_str() -> Result<(), ()> {
+    fn test_from_str() -> Result<(), ParseError> {
         assert_eq!(
             "unsorted:MI".parse::<SubsortOrder>()?,
             SubsortOrder::Unsorted(String::from("MI"))
