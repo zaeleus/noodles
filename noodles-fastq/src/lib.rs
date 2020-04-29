9eac1ef49385dcf0d36efb26c179e5dbac49c169
Michael Macias <zaeleus@gmail.com>
2020-04-28 19:06:59 -0500

    fastq: Move open to top-level

--- noodles-fastq/src/lib.rs
+++ noodles-fastq/src/lib.rs
@@ -5,3 +5,29 @@ pub use self::writer::Writer;
 pub mod reader;
 pub mod record;
 pub mod writer;
+
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
+
+use flate2::bufread::MultiGzDecoder;
+
+pub fn open<P>(src: P) -> io::Result<Reader<Box<dyn BufRead>>>
+where
+    P: AsRef<Path>,
+{
+    let path = src.as_ref();
+    let extenstion = path.extension();
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    match extenstion.and_then(|ext| ext.to_str()) {
+        Some("gz") => {
+            let decoder = MultiGzDecoder::new(reader);
+            Ok(Reader::new(Box::new(BufReader::new(decoder))))
+        }
+        _ => Ok(Reader::new(Box::new(reader))),
+    }
+}
--- noodles-fastq/src/reader.rs
+++ noodles-fastq/src/reader.rs
@@ -1,10 +1,4 @@
-use std::{
-    fs::File,
-    io::{self, BufRead, BufReader},
-    path::Path,
-};
-
-use flate2::bufread::MultiGzDecoder;
+use std::io::{self, BufRead};
 
 use super::Record;
 
@@ -29,24 +23,6 @@ impl<R: BufRead> Reader<R> {
         }
 
         Ok(len)
-    }
-}
-
-pub fn open<P>(src: P) -> io::Result<Reader<Box<dyn BufRead>>>
-where
-    P: AsRef<Path>,
-{
-    let path = src.as_ref();
-    let extenstion = path.extension();
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    match extenstion.and_then(|ext| ext.to_str()) {
-        Some("gz") => {
-            let decoder = MultiGzDecoder::new(reader);
-            Ok(Reader::new(Box::new(BufReader::new(decoder))))
-        }
-        _ => Ok(Reader::new(Box::new(reader))),
     }
 }
 
