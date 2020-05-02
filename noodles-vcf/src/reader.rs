cc8ead2ac6be885b27758cde372bbb2b9b57a803
Michael Macias <zaeleus@gmail.com>
2020-05-01 21:50:30 -0500

    vcf: Add raw meta reader

---
+++ noodles-vcf/src/reader.rs
@@ -0,0 +1,73 @@
+use std::io::{self, BufRead};
+
+const NEWLINE: u8 = b'\n';
+const META_PREFIX: &[u8] = b"##";
+
+#[derive(Debug)]
+pub struct Reader<R> {
+    inner: R,
+}
+
+impl<R> Reader<R>
+where
+    R: BufRead,
+{
+    pub fn new(inner: R) -> Self {
+        Self { inner }
+    }
+
+    pub fn read_meta(&mut self) -> io::Result<Vec<u8>> {
+        let mut meta_buf = Vec::new();
+        let mut eol = false;
+
+        loop {
+            let buf = self.inner.fill_buf()?;
+
+            if eol && buf.len() > 2 && &buf[..2] != META_PREFIX {
+                break;
+            }
+
+            let (read_eol, len) = match buf.iter().position(|&b| b == NEWLINE) {
+                Some(i) => {
+                    meta_buf.extend(&buf[..=i]);
+                    (true, i + 1)
+                }
+                None => {
+                    meta_buf.extend(buf);
+                    (false, buf.len())
+                }
+            };
+
+            eol = read_eol;
+            self.inner.consume(len);
+        }
+
+        Ok(meta_buf)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_meta() -> io::Result<()> {
+        let data = b"\
+##fileformat=VCFv4.3
+##fileDate=20200501
+#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO
+";
+
+        let mut reader = Reader::new(&data[..]);
+
+        let actual = reader.read_meta()?;
+        let expected = b"\
+##fileformat=VCFv4.3
+##fileDate=20200501
+";
+
+        assert_eq!(actual, &expected[..]);
+
+        Ok(())
+    }
+}
