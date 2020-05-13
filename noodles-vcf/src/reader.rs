1c44959416e84be4f949650facdeab5d4f997c7d
Michael Macias <zaeleus@gmail.com>
2020-05-12 20:34:33 -0500

    vcf/reader: Add raw header reader

--- noodles-vcf/src/reader.rs
+++ noodles-vcf/src/reader.rs
@@ -2,6 +2,7 @@ use std::io::{self, BufRead};
 
 const NEWLINE: u8 = b'\n';
 const META_PREFIX: &[u8] = b"##";
+const HEADER_PREFIX: char = '#';
 
 #[derive(Debug)]
 pub struct Reader<R> {
@@ -44,21 +45,36 @@ where
 
         String::from_utf8(meta_buf).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
     }
+
+    pub fn read_header(&mut self) -> io::Result<String> {
+        let mut buf = String::new();
+        self.inner.read_line(&mut buf)?;
+
+        if !buf.starts_with(HEADER_PREFIX) {
+            Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "header missing prefix",
+            ))
+        } else {
+            buf.pop();
+            Ok(buf)
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_read_meta() -> io::Result<()> {
-        let data = b"\
+    static DATA: &[u8] = b"\
 ##fileformat=VCFv4.3
 ##fileDate=20200501
 #CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO
 ";
 
-        let mut reader = Reader::new(&data[..]);
+    #[test]
+    fn test_read_meta() -> io::Result<()> {
+        let mut reader = Reader::new(DATA);
 
         let actual = reader.read_meta()?;
         let expected = "\
@@ -69,5 +85,24 @@ mod tests {
         assert_eq!(actual, expected);
 
         Ok(())
+    }
+
+    #[test]
+    fn test_read_header() -> io::Result<()> {
+        let mut reader = Reader::new(DATA);
+        reader.read_meta()?;
+
+        let actual = reader.read_header()?;
+        let expected = "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO";
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_read_header_with_invalid_prefix() {
+        let data = b"CHROM";
+        let mut reader = Reader::new(&data[..]);
+        assert!(reader.read_header().is_err());
     }
 }
