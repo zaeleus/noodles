750cf35d7797e10c000a078677076cc0064e68e3
Michael Macias <zaeleus@gmail.com>
2020-05-13 20:16:59 -0500

    sam/examples: Add read header example

---
+++ noodles-sam/examples/sam_read_header.rs
@@ -0,0 +1,21 @@
+//! Prints the header of a SAM file.
+//!
+//! The result matches the output of `samtools view --no-PG -H <src>`.
+
+use std::{
+    env,
+    fs::File,
+    io::{self, BufReader},
+};
+
+use noodles_sam as sam;
+
+fn main() -> io::Result<()> {
+    let src = env::args().nth(1).expect("missing src");
+    let mut reader = File::open(src).map(BufReader::new).map(sam::Reader::new)?;
+
+    let header = reader.read_header()?;
+    print!("{}", header);
+
+    Ok(())
+}
