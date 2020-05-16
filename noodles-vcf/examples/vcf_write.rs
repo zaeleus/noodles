5ef521f9cf87d6b0d68bd491dc5b9ef193c35771
Michael Macias <zaeleus@gmail.com>
2020-05-16 15:45:33 -0500

    vcf/examples: Add write example

---
+++ noodles-vcf/examples/vcf_write.rs
@@ -0,0 +1,31 @@
+//! Creates a new VCF file.
+//!
+//! This writes a VCF header and one (sparse) record to stdout.
+//!
+//! Verify the output by piping to `bcftools view --no-version`.
+
+use std::io;
+
+use noodles_vcf::{self as vcf, header::Contig};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let stdout = io::stdout();
+    let handle = stdout.lock();
+    let mut writer = vcf::Writer::new(handle);
+
+    let header = vcf::Header::builder()
+        .add_contig(Contig::new(String::from("sq0")))
+        .build();
+
+    writer.write_header(&header)?;
+
+    let record = vcf::Record::builder()
+        .set_chromosome("sq0".parse()?)
+        .set_position(1)
+        .set_reference_bases("A".parse()?)
+        .build()?;
+
+    writer.write_record(&record)?;
+
+    Ok(())
+}
