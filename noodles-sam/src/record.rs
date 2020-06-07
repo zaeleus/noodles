666b912ee0c86bb587c170c177adf46ca9fe4022
Michael Macias <zaeleus@gmail.com>
2020-06-07 15:41:09 -0500

    sam/record: Add documentation

--- noodles-sam/src/record.rs
+++ noodles-sam/src/record.rs
@@ -1,3 +1,5 @@
+//! SAM record and fields
+
 mod builder;
 pub mod cigar;
 pub mod data;
@@ -24,6 +26,23 @@ pub(crate) const NULL_FIELD: &str = "*";
 const FIELD_DELIMITER: char = '\t';
 const MAX_FIELDS: usize = 12;
 
+/// A SAM record.
+///
+/// A SAM record has 11 required fields:
+///
+///   1. read name (QNAME),
+///   2. flags (FLAG),
+///   3. reference sequence name (RNAME),
+///   4. position (POS),
+///   5. mapping quality (MAPQ),
+///   6. CIGAR string (CIGAR),
+///   7. mate reference sequence name (RNEXT),
+///   8. mate position (PNEXT),
+///   9. template length (TLEN),
+///   10. sequence (SEQ), and
+///   11. quality scores (QUAL).
+///
+/// Additionally, optional data fields can be included with any record.
 #[derive(Debug)]
 pub struct Record {
     qname: ReadName,
@@ -41,54 +60,281 @@ pub struct Record {
 }
 
 impl Record {
+    /// Returns a builder to create a record from each of its fields.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Flags};
+    ///
+    /// let record = sam::Record::builder()
+    ///     .set_read_name("r0".parse()?)
+    ///     .set_flags(Flags::UNMAPPED)
+    ///     .build();
+    ///
+    /// assert_eq!(record.read_name().as_ref(), "r0");
+    /// assert_eq!(record.flags(), Flags::UNMAPPED);
+    /// assert!(record.reference_sequence_name().is_none());
+    /// assert!(record.position().is_none());
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn builder() -> Builder {
         Builder::new()
     }
 
+    /// Returns the read name of this record.
+    ///
+    /// This is also called the query name.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    ///
+    /// let record = sam::Record::default();
+    /// assert!(record.read_name().is_empty());
+    /// assert_eq!(record.read_name().as_ref(), "*");
+    ///
+    /// let record = sam::Record::builder().set_read_name("r0".parse()?).build();
+    /// assert_eq!(record.read_name().as_ref(), "r0");
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn read_name(&self) -> &ReadName {
         &self.qname
     }
 
+    /// Returns the SAM flags of this record.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Flags};
+    ///
+    /// let record = sam::Record::default();
+    /// assert!(record.flags().is_empty());
+    /// assert_eq!(u16::from(record.flags()), 0);
+    ///
+    /// let record = sam::Record::builder().set_flags(Flags::PAIRED | Flags::READ_1).build();
+    /// assert_eq!(record.flags(), Flags::PAIRED | Flags::READ_1);
+    /// ```
     pub fn flags(&self) -> Flags {
         self.flag
     }
 
+    /// Returns the reference sequence name of this record.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    ///
+    /// let record = sam::Record::default();
+    /// assert!(record.reference_sequence_name().is_empty());
+    /// assert_eq!(record.reference_sequence_name().as_ref(), "*");
+    ///
+    /// let record = sam::Record::builder().set_reference_sequence_name("sq0".parse()?).build();
+    /// assert_eq!(record.reference_sequence_name().as_ref(), "sq0");
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn reference_sequence_name(&self) -> &ReferenceSequenceName {
         &self.rname
     }
 
+    /// Returns the start position of this record.
+    ///
+    /// This value is 1-based. A position value of 0 is possibly an unmapped read.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Position};
+    ///
+    /// let record = sam::Record::default();
+    /// assert!(record.position().is_none());
+    /// assert_eq!(i32::from(record.position()), 0);
+    ///
+    /// let record = sam::Record::builder().set_position(Position::from(13)).build();
+    /// assert_eq!(*record.position(), Some(13));
+    /// ```
     pub fn position(&self) -> Position {
         self.pos
     }
 
+    /// Returns the mapping quality of this record.
+    ///
+    /// Mapping quality ranges from 0 to 254, inclusive. A value of 255 means no mapping quality is
+    /// set.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::MappingQuality};
+    ///
+    /// let record = sam::Record::default();
+    /// assert!(record.mapping_quality().is_none());
+    /// assert_eq!(u8::from(record.mapping_quality()), 255);
+    ///
+    /// let record = sam::Record::builder().set_mapping_quality(MappingQuality::from(8)).build();
+    /// assert_eq!(*record.mapping_quality(), Some(8));
+    /// ```
     pub fn mapping_quality(&self) -> MappingQuality {
         self.mapq
     }
 
+    /// Returns the CIGAR operations that describe how the read as mapped.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::cigar::{op, Op}};
+    ///
+    /// let record = sam::Record::default();
+    /// assert!(record.cigar().ops().is_empty());
+    /// assert_eq!(record.cigar().to_string(), "*");
+    ///
+    /// let record = sam::Record::builder().set_cigar("34M2S".parse()?).build();
+    /// assert_eq!(record.cigar().to_string(), "34M2S");
+    /// let mut ops = record.cigar().ops().iter();
+    /// assert_eq!(ops.next(), Some(&Op::new(op::Kind::Match, 34)));
+    /// assert_eq!(ops.next(), Some(&Op::new(op::Kind::SoftClip, 2)));
+    /// assert_eq!(ops.next(), None);
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn cigar(&self) -> &Cigar {
         &self.cigar
     }
 
+    /// Returns the reference sequence name of the mate of this record.
+    ///
+    /// The mate reference sequence name can be empty ("*"), the same as the reference sequence
+    /// name ("="), or some other non-empty name.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    ///
+    /// let record = sam::Record::default();
+    /// assert!(record.mate_reference_sequence_name().is_empty());
+    /// assert_eq!(record.mate_reference_sequence_name().as_ref(), "*");
+    ///
+    /// let record = sam::Record::builder()
+    ///     .set_mate_reference_sequence_name("=".parse()?)
+    ///     .build();
+    /// assert!(record.mate_reference_sequence_name().is_eq());
+    /// assert_eq!(record.mate_reference_sequence_name().as_ref(), "=");
+    ///
+    /// let record = sam::Record::builder()
+    ///     .set_mate_reference_sequence_name("sq0".parse()?)
+    ///     .build();
+    /// assert!(record.mate_reference_sequence_name().is_some());
+    /// assert_eq!(record.mate_reference_sequence_name().as_ref(), "sq0");
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn mate_reference_sequence_name(&self) -> &MateReferenceSequenceName {
         &self.rnext
     }
 
+    /// Returns the start position of the mate of this record.
+    ///
+    /// This value is 1-based. A mate position value of 0 is possibly an unmapped mapped.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Position};
+    ///
+    /// let record = sam::Record::default();
+    /// assert!(record.mate_position().is_none());
+    /// assert_eq!(i32::from(record.mate_position()), 0);
+    ///
+    /// let record = sam::Record::builder().set_mate_position(Position::from(21)).build();
+    /// assert_eq!(*record.mate_position(), Some(21));
+    /// ```
     pub fn mate_position(&self) -> Position {
         self.pnext
     }
 
+    /// Returns the template length of this record.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    ///
+    /// let record = sam::Record::default();
+    /// assert_eq!(record.template_len(), 0);
+    ///
+    /// let record = sam::Record::builder().set_template_len(101).build();
+    /// assert_eq!(record.template_len(), 101);
+    /// ```
     pub fn template_len(&self) -> i32 {
         self.tlen
     }
 
+    /// Returns the bases in the sequence of this record.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::sequence::Base};
+    ///
+    /// let record = sam::Record::default();
+    /// assert!(record.sequence().is_empty());
+    /// assert_eq!(record.sequence().to_string(), "*");
+    ///
+    /// let record = sam::Record::builder().set_sequence("AT".parse()?).build();
+    /// assert_eq!(record.sequence().to_string(), "AT");
+    /// let mut bases = record.sequence().bases().iter();
+    /// assert_eq!(bases.next(), Some(&Base::A));
+    /// assert_eq!(bases.next(), Some(&Base::T));
+    /// assert_eq!(bases.next(), None);
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn sequence(&self) -> &Sequence {
         &self.seq
     }
 
+    /// Returns the quality score for each base in the sequence.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::quality_scores::Score};
+    ///
+    /// let record = sam::Record::default();
+    /// assert!(record.quality_scores().is_empty());
+    /// assert_eq!(record.quality_scores().to_string(), "*");
+    ///
+    /// let record = sam::Record::builder().set_quality_scores("ND".parse()?).build();
+    /// assert_eq!(record.quality_scores().to_string(), "ND");
+    /// let mut scores = record.quality_scores().scores().iter().copied().map(u8::from);
+    /// assert_eq!(scores.next(), Some(45));
+    /// assert_eq!(scores.next(), Some(35));
+    /// assert_eq!(scores.next(), None);
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn quality_scores(&self) -> &QualityScores {
         &self.qual
     }
 
+    /// Returns the optional data fields for this record.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::{data, Data}};
+    ///
+    /// let record = sam::Record::default();
+    /// assert!(record.data().is_empty());
+    ///
+    /// let data = Data::new(vec![data::Field::new(
+    ///     data::field::Tag::AlignmentHitCount,
+    ///     data::field::Value::Int32(1),
+    /// )]);
+    /// let record = sam::Record::builder().set_data(data).build();
+    /// assert_eq!(record.data().to_string(), "NH:i:1");
+    /// ```
     pub fn data(&self) -> &Data {
         &self.data
     }
