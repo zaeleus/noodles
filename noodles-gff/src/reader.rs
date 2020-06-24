4141886212becc76ffe0ffb5d0dfccce0057ae6c
Michael Macias <zaeleus@gmail.com>
2020-06-23 20:00:58 -0500

    gff/reader: Remove csv reader

--- noodles-gff/src/reader.rs
+++ noodles-gff/src/reader.rs
@@ -1,21 +1,14 @@
-use std::io::Read;
+use std::io::BufRead;
 
-pub struct Reader<R: Read> {
-    inner: csv::Reader<R>,
+pub struct Reader<R> {
+    inner: R,
 }
 
-impl<R: Read> Reader<R> {
-    pub fn new(reader: R) -> Self {
-        let inner = csv::ReaderBuilder::new()
-            .delimiter(b'\t')
-            .has_headers(false)
-            .comment(Some(b'#'))
-            .from_reader(reader);
-
+impl<R> Reader<R>
+where
+    R: BufRead,
+{
+    pub fn new(inner: R) -> Self {
         Self { inner }
-    }
-
-    pub fn records(&mut self) -> csv::StringRecordsIter<'_, R> {
-        self.inner.records()
     }
 }
