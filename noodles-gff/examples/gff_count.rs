754a9a2384f4bc01473c5c21799bef030038e1c6
Michael Macias <zaeleus@gmail.com>
2020-06-24 19:13:54 -0500

    gff/reader: Add line reader

--- noodles-gff/examples/gff_count.rs
+++ noodles-gff/examples/gff_count.rs
@@ -1,18 +1,13 @@
 //! Counts the number of records in a GFF file.
 
-use std::{
-    env,
-    fs::File,
-    io::{self, BufReader},
-};
+use std::{env, fs::File, io::BufReader};
 
 use noodles_gff as gff;
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let src = env::args().nth(1).expect("missing src");
 
     let mut reader = File::open(src).map(BufReader::new).map(gff::Reader::new)?;
-    reader.read_header()?;
 
     let mut n = 0;
     let mut buf = String::new();
@@ -20,10 +15,22 @@ fn main() -> io::Result<()> {
     loop {
         buf.clear();
 
-        match reader.read_record(&mut buf) {
+        match reader.read_line(&mut buf) {
             Ok(0) => break,
-            Ok(_) => n += 1,
-            Err(e) => return Err(e),
+            Ok(_) => {}
+            Err(e) => return Err(e.into()),
+        }
+
+        let line: gff::Line = buf.parse()?;
+
+        match line {
+            gff::Line::Record(_) => n += 1,
+            gff::Line::Directive(d) => {
+                if let gff::Directive::StartOfFasta = d {
+                    break;
+                }
+            }
+            _ => {}
         }
     }
 
--- noodles-gff/src/reader.rs
+++ noodles-gff/src/reader.rs
@@ -15,6 +15,39 @@ where
         Self { inner }
     }
 
+    /// Reads a raw GFF line.
+    ///
+    /// This reads from the underlying stream until a newline is reached and appends it to the
+    /// given buffer, sans the final newline character. The buffer can subsequently be parsed as a
+    /// [`gff::Line`].
+    ///
+    /// If successful, the number of bytes read is returned. If the number of bytes read is 0, the
+    /// stream reached EOF.
+    ///
+    /// [`gff::Line`]: struct.Line.html
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::io;
+    /// use noodles_gff as gff;
+    ///
+    /// let data = b"##gff-version 3
+    /// sq0\tNOODLES\tgene\t8\t13\t.\t+\t.\tgene_id=ndls0;gene_name=gene0
+    /// ";
+    /// let mut reader = gff::Reader::new(&data[..]);
+    ///
+    /// let mut buf = String::new();
+    /// reader.read_line(&mut buf)?;
+    /// assert_eq!(buf, "##gff-version 3");
+    /// # Ok::<_, io::Error>(())
+    /// ```
+    pub fn read_line(&mut self, buf: &mut String) -> io::Result<usize> {
+        let result = self.inner.read_line(buf);
+        buf.pop();
+        result
+    }
+
     /// Reads the raw GFF header.
     ///
     /// The GFF header is all the comments (lines starting with `#`) preceding the list of records.
