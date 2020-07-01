c356c28404608b22c30a33def2d3fa7983a64772
Michael Macias <zaeleus@gmail.com>
2020-07-01 18:44:42 -0500

    tabix: Add convenience read function

--- noodles-tabix/src/lib.rs
+++ noodles-tabix/src/lib.rs
@@ -4,4 +4,27 @@ mod writer;
 
 pub use self::{index::Index, reader::Reader, writer::Writer};
 
+use std::{fs::File, io, path::Path};
+
 static MAGIC_NUMBER: &[u8] = b"TBI\x01";
+
+/// Reads the entire contents of a tabix index.
+///
+/// This is a convenience function and is equivalent to opening the file at the given path and
+/// reading the index.
+///
+/// # Examples
+///
+/// ```no_run
+/// # use std::io;
+/// use noodles_tabix as tabix;
+/// let index = tabix::read("sample.vcf.gz.tbi")?;
+/// # Ok::<(), io::Error>(())
+/// ```
+pub fn read<P>(src: P) -> io::Result<Index>
+where
+    P: AsRef<Path>,
+{
+    let mut reader = File::open(src).map(Reader::new)?;
+    reader.read_index()
+}
