e0f4c8db005cfae74bb9f7a61730554503788296
Michael Macias <zaeleus@gmail.com>
2020-07-03 10:49:56 -0500

    fastq/writer: Add documentation

--- noodles-fastq/src/writer.rs
+++ noodles-fastq/src/writer.rs
@@ -2,6 +2,7 @@ use std::io::{self, Write};
 
 use super::Record;
 
+/// A FASTQ writer.
 pub struct Writer<W> {
     inner: W,
 }
@@ -10,14 +11,47 @@ impl<W> Writer<W>
 where
     W: Write,
 {
+    /// Creates a FASTQ writer.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_fastq as fastq;
+    /// let writer = fastq::Writer::new(Vec::new());
+    /// ```
     pub fn new(inner: W) -> Self {
         Self { inner }
     }
 
+    /// Returns a reference to the underlying writer.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_fastq as fastq;
+    /// let writer = fastq::Writer::new(Vec::new());
+    /// assert!(writer.get_ref().is_empty());
+    /// ```
     pub fn get_ref(&self) -> &W {
         &self.inner
     }
 
+    /// Writes a FASTQ record.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::io;
+    /// use noodles_fastq as fastq;
+    ///
+    /// let mut writer = fastq::Writer::new(Vec::new());
+    ///
+    /// let record = fastq::Record::new("@r0", "ATCG", "NDLS");
+    /// writer.write_record(&record)?;
+    ///
+    /// assert_eq!(writer.get_ref(), b"@r0\nATCG\n+\nNDLS\n");
+    /// # Ok::<(), io::Error>(())
+    /// ```
     pub fn write_record(&mut self, record: &Record) -> io::Result<()> {
         self.inner.write_all(record.read_name())?;
         self.inner.write_all(b"\n")?;
@@ -25,24 +59,6 @@ where
         self.inner.write_all(b"\n+\n")?;
         self.inner.write_all(record.quality_scores())?;
         self.inner.write_all(b"\n")?;
-
-        Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_write_record() -> io::Result<()> {
-        let mut writer = Writer::new(Vec::new());
-
-        let record = Record::new("r0", "ATCG", "NDLS");
-        writer.write_record(&record)?;
-
-        let expected = b"r0\nATCG\n+\nNDLS\n";
-        assert_eq!(writer.get_ref(), expected);
 
         Ok(())
     }
