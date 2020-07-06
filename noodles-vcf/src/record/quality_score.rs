99ee9bc55aa0cb550cad4c11faefeb8bb17c731c
Michael Macias <zaeleus@gmail.com>
2020-07-06 18:45:52 -0500

    vcf/record/quality_score: Disallow negative values
    A Phred quality score cannot be negative.

--- noodles-vcf/src/record/quality_score.rs
+++ noodles-vcf/src/record/quality_score.rs
@@ -1,7 +1,10 @@
-use std::{error, fmt, ops::Deref, str::FromStr};
+use std::{convert::TryFrom, error, fmt, num, ops::Deref, str::FromStr};
 
 use super::MISSING_FIELD;
 
+const MIN: f32 = 0.0;
+
+/// A VCF record quality score.
 #[derive(Clone, Debug, Default, PartialEq)]
 pub struct QualityScore(Option<f32>);
 
@@ -22,14 +25,55 @@ impl fmt::Display for QualityScore {
     }
 }
 
-#[derive(Debug)]
-pub struct ParseError(String);
+/// An error returned when a raw float fails to convert to a quality score.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum TryFromFloatError {
+    /// The input is negative.
+    Negative,
+}
+
+impl error::Error for TryFromFloatError {}
+
+impl fmt::Display for TryFromFloatError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Negative => f.write_str("negative value"),
+        }
+    }
+}
+
+impl TryFrom<f32> for QualityScore {
+    type Error = TryFromFloatError;
+
+    fn try_from(value: f32) -> Result<Self, Self::Error> {
+        if value < MIN {
+            Err(TryFromFloatError::Negative)
+        } else {
+            Ok(Self(Some(value)))
+        }
+    }
+}
+
+/// An error returned when a raw VCF record quality score cannot be parsed.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum ParseError {
+    /// The input is empty.
+    Empty,
+    /// The input is invalid.
+    Invalid(num::ParseFloatError),
+    /// The quality score is invalid.
+    InvalidValue(TryFromFloatError),
+}
 
 impl error::Error for ParseError {}
 
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "invalid quality score: {}", self.0)
+        match self {
+            Self::Empty => f.write_str("empty input"),
+            Self::Invalid(e) => write!(f, "invalid input: {}", e),
+            Self::InvalidValue(e) => write!(f, "invalid value: {}", e),
+        }
     }
 }
 
@@ -38,12 +82,12 @@ impl FromStr for QualityScore {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "" => Err(ParseError(s.into())),
+            "" => Err(ParseError::Empty),
             MISSING_FIELD => Ok(Self(None)),
             _ => s
-                .parse()
-                .map(|value| Self(Some(value)))
-                .map_err(|_| ParseError(s.into())),
+                .parse::<f32>()
+                .map_err(ParseError::Invalid)
+                .and_then(|value| Self::try_from(value).map_err(ParseError::InvalidValue)),
         }
     }
 }
@@ -62,14 +106,29 @@ mod tests {
     }
 
     #[test]
-    fn test_from_str() -> Result<(), ParseError> {
-        assert!(".".parse::<QualityScore>()?.is_none());
+    fn test_try_from_float_for_quality_score() {
+        assert_eq!(QualityScore::try_from(0.0), Ok(QualityScore(Some(0.0))));
+        assert_eq!(QualityScore::try_from(13.0), Ok(QualityScore(Some(13.0))));
 
-        assert_eq!("5.8".parse::<QualityScore>()?, QualityScore(Some(5.8)));
+        assert_eq!(
+            QualityScore::try_from(-8.0),
+            Err(TryFromFloatError::Negative)
+        );
+    }
 
-        assert!("".parse::<QualityScore>().is_err());
-        assert!("ndls".parse::<QualityScore>().is_err());
+    #[test]
+    fn test_from_str() {
+        assert_eq!(".".parse(), Ok(QualityScore(None)));
+        assert_eq!("5.8".parse(), Ok(QualityScore(Some(5.8))));
 
-        Ok(())
+        assert_eq!("".parse::<QualityScore>(), Err(ParseError::Empty));
+        assert!(matches!(
+            "ndls".parse::<QualityScore>(),
+            Err(ParseError::Invalid(_))
+        ));
+        assert!(matches!(
+            "-8.5".parse::<QualityScore>(),
+            Err(ParseError::InvalidValue(_))
+        ));
     }
 }
