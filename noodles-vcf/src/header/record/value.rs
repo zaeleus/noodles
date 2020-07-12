93d3023519683bd32b3934dfd5c1e6f0b26a53a6
Michael Macias <zaeleus@gmail.com>
2020-07-12 12:15:55 -0500

    vcf/header/record/value: Implement Display

--- noodles-vcf/src/header/record/value.rs
+++ noodles-vcf/src/header/record/value.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 /// A VCF header record value.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Value {
@@ -5,4 +7,50 @@ pub enum Value {
     String(String),
     /// A structure.
     Struct(Vec<(String, String)>),
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::String(value) => f.write_str(value),
+            Self::Struct(fields) => {
+                for (i, (key, value)) in fields.iter().enumerate() {
+                    if i > 0 {
+                        f.write_str(",")?;
+                    }
+
+                    write!(f, "{}={}", key, value)?;
+                }
+
+                Ok(())
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fmt() {
+        assert_eq!(
+            Value::String(String::from("VCFv4.3")).to_string(),
+            "VCFv4.3"
+        );
+
+        assert_eq!(
+            Value::Struct(vec![(String::from("ID"), String::from("sq0"))]).to_string(),
+            "ID=sq0"
+        );
+
+        assert_eq!(
+            Value::Struct(vec![
+                (String::from("ID"), String::from("sq0")),
+                (String::from("length"), String::from("13"))
+            ])
+            .to_string(),
+            "ID=sq0,length=13"
+        );
+    }
 }
