7e1d81b07e75ec0a189ce5853b30dc8c3cce6e08
Michael Macias <zaeleus@gmail.com>
2020-07-20 18:04:47 -0500

    cram/container/block/compression_method: Implement Default

--- noodles-cram/src/container/block/compression_method.rs
+++ noodles-cram/src/container/block/compression_method.rs
@@ -1,7 +1,25 @@
 use std::{convert::TryFrom, error, fmt};
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[non_exhaustive]
+pub enum CompressionMethod {
+    None,
+    Gzip,
+    Bzip2,
+    Lzma,
+    Rans,
+}
+
+impl Default for CompressionMethod {
+    fn default() -> Self {
+        Self::None
+    }
+}
+
 #[derive(Debug, Eq, PartialEq)]
 pub struct TryFromByteError(u8);
+
+impl error::Error for TryFromByteError {}
 
 impl fmt::Display for TryFromByteError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -11,18 +29,6 @@ impl fmt::Display for TryFromByteError {
             self.0
         )
     }
-}
-
-impl error::Error for TryFromByteError {}
-
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-#[non_exhaustive]
-pub enum CompressionMethod {
-    None,
-    Gzip,
-    Bzip2,
-    Lzma,
-    Rans,
 }
 
 impl TryFrom<u8> for CompressionMethod {
@@ -43,6 +49,11 @@ impl TryFrom<u8> for CompressionMethod {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_default() {
+        assert_eq!(CompressionMethod::default(), CompressionMethod::None);
+    }
 
     #[test]
     fn test_try_from() {
