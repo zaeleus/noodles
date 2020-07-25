c8b56e1b3ec21b89295ee251f522f68a0b8517a9
Michael Macias <zaeleus@gmail.com>
2020-07-24 22:37:41 -0500

    cram/num/itf8: Add writer

--- noodles-cram/src/num.rs
+++ noodles-cram/src/num.rs
@@ -1,7 +1,10 @@
 mod itf8;
 mod ltf8;
 
-pub use self::{itf8::read_itf8, ltf8::read_ltf8};
+pub use self::{
+    itf8::{read_itf8, write_itf8},
+    ltf8::read_ltf8,
+};
 
 pub type Itf8 = i32;
 pub type Ltf8 = i64;
--- noodles-cram/src/num/itf8.rs
+++ noodles-cram/src/num/itf8.rs
@@ -1,6 +1,8 @@
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
 
-use byteorder::ReadBytesExt;
+use byteorder::{ReadBytesExt, WriteBytesExt};
+
+use super::Itf8;
 
 fn read_u8_as_i32<R>(reader: &mut R) -> io::Result<i32>
 where
@@ -38,6 +40,38 @@ where
     };
 
     Ok(value)
+}
+
+#[allow(dead_code)]
+pub fn write_itf8<W>(writer: &mut W, value: Itf8) -> io::Result<()>
+where
+    W: Write,
+{
+    if value >> (8 - 1) == 0 {
+        writer.write_u8(value as u8)?;
+    } else if value >> (16 - 2) == 0 {
+        writer.write_u8((value >> 8 | 0x80) as u8)?;
+        writer.write_u8(value as u8)?;
+    } else if value >> (24 - 3) == 0 {
+        writer.write_u8((value >> 16 | 0xc0) as u8)?;
+        writer.write_u8((value >> 8) as u8)?;
+        writer.write_u8(value as u8)?;
+    } else if value >> (32 - 4) == 0 {
+        writer.write_u8((value >> 24 | 0xe0) as u8)?;
+        writer.write_u8((value >> 16) as u8)?;
+        writer.write_u8((value >> 8) as u8)?;
+        writer.write_u8(value as u8)?;
+    } else {
+        writer.write_u8((value >> 28 | 0xf0) as u8)?;
+        writer.write_u8((value >> 20) as u8)?;
+        writer.write_u8((value >> 12) as u8)?;
+        writer.write_u8((value >> 4) as u8)?;
+        // § 2.3 Writing bytes to a byte stream: "only [the] 4 lower bits [are] used in the last
+        // byte 5."
+        writer.write_u8((value & 0x0f) as u8)?;
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -97,6 +131,35 @@ mod tests {
         let mut reader = BufReader::new(&data[..]);
         let i = read_itf8(&mut reader)?;
         assert_eq!(i, -1);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_write_itf8() -> io::Result<()> {
+        let mut buf = Vec::new();
+        write_itf8(&mut buf, 0)?;
+        assert_eq!(buf, [0x00]);
+
+        let mut buf = Vec::new();
+        write_itf8(&mut buf, 1877)?;
+        assert_eq!(buf, [0x87, 0x55]);
+
+        let mut buf = Vec::new();
+        write_itf8(&mut buf, 480665)?;
+        assert_eq!(buf, [0xc7, 0x55, 0x99]);
+
+        let mut buf = Vec::new();
+        write_itf8(&mut buf, 123050342)?;
+        assert_eq!(buf, [0xe7, 0x55, 0x99, 0x66]);
+
+        let mut buf = Vec::new();
+        write_itf8(&mut buf, 1968805474)?;
+        assert_eq!(buf, [0xf7, 0x55, 0x99, 0x66, 0x02]);
+
+        let mut buf = Vec::new();
+        write_itf8(&mut buf, -1)?;
+        assert_eq!(buf, [0xff, 0xff, 0xff, 0xff, 0x0f]);
 
         Ok(())
     }
