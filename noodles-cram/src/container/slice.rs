f4c355b3d1a49a1ba6c7dd7867c6a5e5666a9f51
Michael Macias <zaeleus@gmail.com>
2020-08-09 11:44:22 -0500

    cram/container/slice: Add builder from records
    A lot of the values are currently just placeholders; this is mainly to
    bootstrap the record writer.

--- noodles-cram/src/container/slice.rs
+++ noodles-cram/src/container/slice.rs
@@ -3,15 +3,21 @@ mod header;
 pub use self::header::Header;
 
 use std::{
+    collections::HashMap,
     convert::TryFrom,
     io::{self, Cursor},
 };
 
 use noodles_sam as sam;
 
-use crate::{reader, BitReader, Record};
+use crate::{reader, BitReader, BitWriter, Record};
 
-use super::{Block, CompressionHeader};
+use super::{
+    block, compression_header::data_series_encoding_map::DataSeries, Block, CompressionHeader,
+    ReferenceSequenceId,
+};
+
+const CORE_DATA_BLOCK_CONTENT_ID: i32 = 0;
 
 #[derive(Debug)]
 pub struct Slice {
@@ -21,6 +27,73 @@ pub struct Slice {
 }
 
 impl Slice {
+    pub fn from_records(
+        compression_header: &CompressionHeader,
+        records: &[Record],
+    ) -> io::Result<Self> {
+        let core_data_writer = BitWriter::new(Vec::new());
+
+        let mut external_data_writers = HashMap::new();
+
+        for i in 0..DataSeries::LEN {
+            let block_content_id = (i + 1) as i32;
+            external_data_writers.insert(block_content_id, Vec::new());
+        }
+
+        for &block_content_id in compression_header.tag_encoding_map().keys() {
+            external_data_writers.insert(block_content_id, Vec::new());
+        }
+
+        // TODO: write records
+
+        let core_data_block = core_data_writer.finish().map(|buf| {
+            Block::new(
+                block::CompressionMethod::None,
+                block::ContentType::CoreData,
+                CORE_DATA_BLOCK_CONTENT_ID,
+                buf.len() as i32,
+                buf,
+                0,
+            )
+        })?;
+
+        let mut block_content_ids = vec![CORE_DATA_BLOCK_CONTENT_ID];
+
+        for &block_content_id in external_data_writers.keys() {
+            block_content_ids.push(block_content_id);
+        }
+
+        let external_blocks: Vec<_> = external_data_writers
+            .into_iter()
+            .map(|(block_content_id, buf)| {
+                Block::new(
+                    block::CompressionMethod::None,
+                    block::ContentType::ExternalData,
+                    block_content_id,
+                    buf.len() as i32,
+                    buf,
+                    0,
+                )
+            })
+            .collect();
+
+        // TODO
+        let header = Header::new(
+            ReferenceSequenceId::None,
+            1,
+            0,
+            records.len() as i32,
+            0,
+            (external_blocks.len() + 1) as i32,
+            block_content_ids,
+            0,
+            Default::default(),
+            Vec::new(),
+        );
+
+        Ok(Self::new(header, core_data_block, external_blocks))
+    }
+
     pub fn new(header: Header, core_data_block: Block, external_blocks: Vec<Block>) -> Self {
         Self {
             header,
