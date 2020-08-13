07037734209a62e3cddce8b2170afb671aa8e54f
Michael Macias <zaeleus@gmail.com>
2020-08-13 18:53:38 -0500

    cram/container/compression_header/data_series_encoding_map/data_series: Implement From<DataSeries> for [u8; 2]

--- noodles-cram/src/container/compression_header/data_series_encoding_map/data_series.rs
+++ noodles-cram/src/container/compression_header/data_series_encoding_map/data_series.rs
@@ -86,6 +86,41 @@ impl TryFrom<&[u8]> for DataSeries {
     }
 }
 
+impl From<DataSeries> for [u8; 2] {
+    fn from(data_series: DataSeries) -> Self {
+        match data_series {
+            DataSeries::BamBitFlags => [b'B', b'F'],
+            DataSeries::CramBitFlags => [b'C', b'F'],
+            DataSeries::ReferenceId => [b'R', b'I'],
+            DataSeries::ReadLengths => [b'R', b'L'],
+            DataSeries::InSeqPositions => [b'A', b'P'],
+            DataSeries::ReadGroups => [b'R', b'G'],
+            DataSeries::ReadNames => [b'R', b'N'],
+            DataSeries::NextMateBitFlags => [b'M', b'F'],
+            DataSeries::NextFragmentReferenceSequenceId => [b'N', b'S'],
+            DataSeries::NextMateAlignmentStart => [b'N', b'P'],
+            DataSeries::TemplateSize => [b'T', b'S'],
+            DataSeries::DistanceToNextFragment => [b'N', b'F'],
+            DataSeries::TagIds => [b'T', b'L'],
+            DataSeries::NumberOfReadFeatures => [b'F', b'N'],
+            DataSeries::ReadFeaturesCodes => [b'F', b'C'],
+            DataSeries::InReadPositions => [b'F', b'P'],
+            DataSeries::DeletionLengths => [b'D', b'L'],
+            DataSeries::StretchesOfBases => [b'B', b'B'],
+            DataSeries::StretchesOfQualityScores => [b'Q', b'Q'],
+            DataSeries::BaseSubstitutionCodes => [b'B', b'S'],
+            DataSeries::Insertion => [b'I', b'N'],
+            DataSeries::ReferenceSkipLength => [b'R', b'S'],
+            DataSeries::Padding => [b'P', b'D'],
+            DataSeries::HardClip => [b'H', b'C'],
+            DataSeries::SoftClip => [b'S', b'C'],
+            DataSeries::MappingQualities => [b'M', b'Q'],
+            DataSeries::Bases => [b'B', b'A'],
+            DataSeries::QualityScores => [b'Q', b'S'],
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -104,5 +139,58 @@ mod tests {
             DataSeries::try_from(&b"XY"[..]),
             Err(TryFromByteSliceError(vec![0x58, 0x59]))
         );
+    }
+
+    #[test]
+    fn test_from_data_series_for_u8_array() {
+        assert_eq!(<[u8; 2]>::from(DataSeries::BamBitFlags), [b'B', b'F']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::CramBitFlags), [b'C', b'F']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::ReferenceId), [b'R', b'I']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::ReadLengths), [b'R', b'L']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::InSeqPositions), [b'A', b'P']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::ReadGroups), [b'R', b'G']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::ReadNames), [b'R', b'N']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::NextMateBitFlags), [b'M', b'F']);
+        assert_eq!(
+            <[u8; 2]>::from(DataSeries::NextFragmentReferenceSequenceId),
+            [b'N', b'S']
+        );
+        assert_eq!(
+            <[u8; 2]>::from(DataSeries::NextMateAlignmentStart),
+            [b'N', b'P']
+        );
+        assert_eq!(<[u8; 2]>::from(DataSeries::TemplateSize), [b'T', b'S']);
+        assert_eq!(
+            <[u8; 2]>::from(DataSeries::DistanceToNextFragment),
+            [b'N', b'F']
+        );
+        assert_eq!(<[u8; 2]>::from(DataSeries::TagIds), [b'T', b'L']);
+        assert_eq!(
+            <[u8; 2]>::from(DataSeries::NumberOfReadFeatures),
+            [b'F', b'N']
+        );
+        assert_eq!(<[u8; 2]>::from(DataSeries::ReadFeaturesCodes), [b'F', b'C']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::InReadPositions), [b'F', b'P']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::DeletionLengths), [b'D', b'L']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::StretchesOfBases), [b'B', b'B']);
+        assert_eq!(
+            <[u8; 2]>::from(DataSeries::StretchesOfQualityScores),
+            [b'Q', b'Q']
+        );
+        assert_eq!(
+            <[u8; 2]>::from(DataSeries::BaseSubstitutionCodes),
+            [b'B', b'S']
+        );
+        assert_eq!(<[u8; 2]>::from(DataSeries::Insertion), [b'I', b'N']);
+        assert_eq!(
+            <[u8; 2]>::from(DataSeries::ReferenceSkipLength),
+            [b'R', b'S']
+        );
+        assert_eq!(<[u8; 2]>::from(DataSeries::Padding), [b'P', b'D']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::HardClip), [b'H', b'C']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::SoftClip), [b'S', b'C']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::MappingQualities), [b'M', b'Q']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::Bases), [b'B', b'A']);
+        assert_eq!(<[u8; 2]>::from(DataSeries::QualityScores), [b'Q', b'S']);
     }
 }
