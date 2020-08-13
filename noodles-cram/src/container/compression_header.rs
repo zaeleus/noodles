68cba12e434d0186d1b30c9e42db65614031ee08
Michael Macias <zaeleus@gmail.com>
2020-08-12 19:26:57 -0500

    cram/container/compression_header: Replace from_records with builder

--- noodles-cram/src/container/compression_header.rs
+++ noodles-cram/src/container/compression_header.rs
@@ -1,9 +1,11 @@
+mod builder;
 pub mod data_series_encoding_map;
 pub mod encoding;
 pub mod preservation_map;
 mod tag_encoding_map;
 
 pub use self::{
+    builder::Builder,
     data_series_encoding_map::DataSeriesEncodingMap,
     encoding::Encoding,
     preservation_map::{PreservationMap, SubstitutionMatrix, TagIdsDictionary},
@@ -12,7 +14,7 @@ pub use self::{
 
 use std::{convert::TryFrom, io};
 
-use crate::{reader::compression_header::read_compression_header, Record};
+use crate::reader::compression_header::read_compression_header;
 
 use super::Block;
 
@@ -24,64 +26,8 @@ pub struct CompressionHeader {
 }
 
 impl CompressionHeader {
-    pub fn from_records(reference_sequence: &[u8], records: &[Record]) -> Self {
-        use data_series_encoding_map::DataSeries;
-
-        const DATA_SERIES: [DataSeries; DataSeries::LEN] = [
-            DataSeries::BamBitFlags,
-            DataSeries::CramBitFlags,
-            DataSeries::ReferenceId,
-            DataSeries::ReadLengths,
-            DataSeries::InSeqPositions,
-            DataSeries::ReadGroups,
-            DataSeries::ReadNames,
-            DataSeries::NextMateBitFlags,
-            DataSeries::NextFragmentReferenceSequenceId,
-            DataSeries::NextMateAlignmentStart,
-            DataSeries::TemplateSize,
-            DataSeries::DistanceToNextFragment,
-            DataSeries::TagIds,
-            DataSeries::NumberOfReadFeatures,
-            DataSeries::ReadFeaturesCodes,
-            DataSeries::InReadPositions,
-            DataSeries::DeletionLengths,
-            DataSeries::StretchesOfBases,
-            DataSeries::StretchesOfQualityScores,
-            DataSeries::BaseSubstitutionCodes,
-            DataSeries::Insertion,
-            DataSeries::ReferenceSkipLength,
-            DataSeries::Padding,
-            DataSeries::HardClip,
-            DataSeries::SoftClip,
-            DataSeries::MappingQualities,
-            DataSeries::Bases,
-            DataSeries::QualityScores,
-        ];
-
-        let mut preservation_map_builder = PreservationMap::builder(reference_sequence);
-        let mut tag_encoding_map_builder = TagEncodingMap::builder();
-
-        for record in records {
-            preservation_map_builder.update(record);
-            tag_encoding_map_builder.update(record);
-        }
-
-        let preservation_map = preservation_map_builder.build();
-
-        let mut data_series_encoding_map = DataSeriesEncodingMap::default();
-
-        for (i, &data_series) in DATA_SERIES.iter().enumerate() {
-            let block_content_id = (i + 1) as i32;
-            // TODO: Select encoding depending on the type of data.
-            let encoding = Encoding::External(block_content_id);
-            data_series_encoding_map.insert(data_series, encoding);
-        }
-
-        data_series_encoding_map.insert(DataSeries::ReadNames, Encoding::ByteArrayStop(0x00, 7));
-
-        let tag_encoding_map = tag_encoding_map_builder.build();
-
-        Self::new(preservation_map, data_series_encoding_map, tag_encoding_map)
+    pub fn builder(reference_sequence: &[u8]) -> Builder {
+        Builder::new(reference_sequence)
     }
 
     pub fn new(
---
+++ noodles-cram/src/container/compression_header/builder.rs
@@ -0,0 +1,76 @@
+use crate::Record;
+
+use super::{
+    data_series_encoding_map::{DataSeries, DataSeriesEncodingMap},
+    encoding::Encoding,
+    preservation_map, tag_encoding_map, CompressionHeader, PreservationMap, TagEncodingMap,
+};
+
+const DATA_SERIES: [DataSeries; DataSeries::LEN] = [
+    DataSeries::BamBitFlags,
+    DataSeries::CramBitFlags,
+    DataSeries::ReferenceId,
+    DataSeries::ReadLengths,
+    DataSeries::InSeqPositions,
+    DataSeries::ReadGroups,
+    DataSeries::ReadNames,
+    DataSeries::NextMateBitFlags,
+    DataSeries::NextFragmentReferenceSequenceId,
+    DataSeries::NextMateAlignmentStart,
+    DataSeries::TemplateSize,
+    DataSeries::DistanceToNextFragment,
+    DataSeries::TagIds,
+    DataSeries::NumberOfReadFeatures,
+    DataSeries::ReadFeaturesCodes,
+    DataSeries::InReadPositions,
+    DataSeries::DeletionLengths,
+    DataSeries::StretchesOfBases,
+    DataSeries::StretchesOfQualityScores,
+    DataSeries::BaseSubstitutionCodes,
+    DataSeries::Insertion,
+    DataSeries::ReferenceSkipLength,
+    DataSeries::Padding,
+    DataSeries::HardClip,
+    DataSeries::SoftClip,
+    DataSeries::MappingQualities,
+    DataSeries::Bases,
+    DataSeries::QualityScores,
+];
+
+pub struct Builder<'a> {
+    preservation_map_builder: preservation_map::Builder<'a>,
+    tag_encoding_map_builder: tag_encoding_map::Builder,
+}
+
+impl<'a> Builder<'a> {
+    pub fn new(reference_sequence: &'a [u8]) -> Self {
+        Self {
+            preservation_map_builder: PreservationMap::builder(reference_sequence),
+            tag_encoding_map_builder: TagEncodingMap::builder(),
+        }
+    }
+
+    pub fn update(&mut self, record: &Record) {
+        self.preservation_map_builder.update(record);
+        self.tag_encoding_map_builder.update(record);
+    }
+
+    pub fn build(self) -> CompressionHeader {
+        let preservation_map = self.preservation_map_builder.build();
+
+        let mut data_series_encoding_map = DataSeriesEncodingMap::default();
+
+        for (i, &data_series) in DATA_SERIES.iter().enumerate() {
+            let block_content_id = (i + 1) as i32;
+            // TODO: Select encoding depending on the type of data.
+            let encoding = Encoding::External(block_content_id);
+            data_series_encoding_map.insert(data_series, encoding);
+        }
+
+        data_series_encoding_map.insert(DataSeries::ReadNames, Encoding::ByteArrayStop(0x00, 7));
+
+        let tag_encoding_map = self.tag_encoding_map_builder.build();
+
+        CompressionHeader::new(preservation_map, data_series_encoding_map, tag_encoding_map)
+    }
+}
