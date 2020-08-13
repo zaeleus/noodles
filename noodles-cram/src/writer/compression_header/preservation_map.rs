2872e4e9b0292ac7ef918a1b7bc76c4f77a294b3
Michael Macias <zaeleus@gmail.com>
2020-08-13 17:47:47 -0500

    cram/writer/compression_header: Add preservation map writer

--- noodles-cram/src/writer/compression_header/preservation_map.rs
+++ noodles-cram/src/writer/compression_header/preservation_map.rs
@@ -1,13 +1,74 @@
 use std::io::{self, Write};
 
+use byteorder::WriteBytesExt;
+
 use crate::{
-    container::compression_header::preservation_map::{SubstitutionMatrix, TagIdsDictionary},
+    container::compression_header::{
+        preservation_map::{Key, SubstitutionMatrix, TagIdsDictionary},
+        PreservationMap,
+    },
     num::{write_itf8, Itf8},
 };
+
+const MAP_LENGTH: Itf8 = 5;
+
+const FALSE: u8 = 0x00;
+const TRUE: u8 = 0x01;
 
 const NUL: u8 = 0x00;
 
 #[allow(dead_code)]
+pub fn write_preservation_map<W>(
+    writer: &mut W,
+    preservation_map: &PreservationMap,
+) -> io::Result<()>
+where
+    W: Write,
+{
+    let mut buf = Vec::new();
+
+    write_itf8(&mut buf, MAP_LENGTH)?;
+
+    write_key(&mut buf, Key::ReadNamesIncluded)?;
+    write_bool(&mut buf, preservation_map.read_names_included())?;
+
+    write_key(&mut buf, Key::ApDataSeriesDelta)?;
+    write_bool(&mut buf, preservation_map.ap_data_series_delta())?;
+
+    write_key(&mut buf, Key::ReferenceRequired)?;
+    write_bool(&mut buf, preservation_map.reference_required())?;
+
+    write_key(&mut buf, Key::SubstitutionMatrix)?;
+    write_substituion_matrix(&mut buf, preservation_map.substitution_matrix())?;
+
+    write_key(&mut buf, Key::TagIdsDictionary)?;
+    write_tag_ids_dictionary(&mut buf, preservation_map.tag_ids_dictionary())?;
+
+    let data_len = buf.len() as Itf8;
+    write_itf8(writer, data_len)?;
+
+    writer.write_all(&buf)
+}
+
+fn write_key<W>(writer: &mut W, key: Key) -> io::Result<()>
+where
+    W: Write,
+{
+    let data = <[u8; 2]>::from(key);
+    writer.write_all(&data)
+}
+
+fn write_bool<W>(writer: &mut W, value: bool) -> io::Result<()>
+where
+    W: Write,
+{
+    if value {
+        writer.write_u8(TRUE)
+    } else {
+        writer.write_u8(FALSE)
+    }
+}
+
 fn write_substituion_matrix<W>(
     writer: &mut W,
     substitution_matrix: &SubstitutionMatrix,
@@ -20,7 +81,6 @@ where
     writer.write_all(&buf)
 }
 
-#[allow(dead_code)]
 fn write_tag_ids_dictionary<W>(
     writer: &mut W,
     tag_ids_dictionary: &TagIdsDictionary,
