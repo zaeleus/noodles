96c23fd04670eee4c35a6dffa14c8e65daae99b6
Michael Macias <zaeleus@gmail.com>
2020-09-23 22:40:34 -0500

    cram/container/block/content_type: Add documentation

--- noodles-cram/src/container/block/content_type.rs
+++ noodles-cram/src/container/block/content_type.rs
@@ -1,16 +1,41 @@
+//! CRAM container block content type.
+
 use std::{convert::TryFrom, error, fmt};
 
+/// A CRAM container block content type.
+///
+/// The content type is associated with a block to identify its data.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 #[non_exhaustive]
 pub enum ContentType {
+    /// CRAM file header.
+    ///
+    /// The CRAM file header contains the SAM header.
     FileHeader,
+    /// Compression header.
+    ///
+    /// There is a single compression header associated with each data container. It contains the
+    /// preservation map, data series encoding map, and tag encoding map.
     CompressionHeader,
+    /// Slice header.
+    ///
+    /// A slice typically has a contiguous region of alignments records.
     SliceHeader,
+    /// Reserved.
     Reserved,
+    /// External data.
+    ///
+    /// External data is used in slices. There is at least one external block, typically
+    /// corresponding to a byte stream of external data.
     ExternalData,
+    /// Core data.
+    ///
+    /// Core data is used in slices. It is a single block with a bit stream of any non-external
+    /// encoding.
     CoreData,
 }
 
+/// An error returned when a raw content type fails to convert.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct TryFromByteError(u8);
 
