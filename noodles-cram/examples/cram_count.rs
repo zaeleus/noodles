623c0955d0a3f8ccf7e8622da44fa485862c5aa6
Michael Macias <zaeleus@gmail.com>
2020-10-21 17:46:37 -0500

    cram/examples/count: Use result from records

--- noodles-cram/examples/cram_count.rs
+++ noodles-cram/examples/cram_count.rs
@@ -16,8 +16,14 @@ fn main() -> io::Result<()> {
     reader.read_file_definition()?;
     reader.read_file_header()?;
 
-    let count = reader.records().count();
-    println!("{}", count);
+    let mut n = 0;
+
+    for result in reader.records() {
+        let _ = result?;
+        n += 1;
+    }
+
+    println!("{}", n);
 
     Ok(())
 }
