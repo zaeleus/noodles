8073f034087f1c5730a0715351f72e7c340eeea7
Michael Macias <zaeleus@gmail.com>
2020-10-31 12:41:45 -0500

    sam/examples/count: Use result from records

--- noodles-sam/examples/sam_count.rs
+++ noodles-sam/examples/sam_count.rs
@@ -2,18 +2,28 @@
 //!
 //! The result matches the output of `samtools view -c <src>`.
 
-use std::{env, fs::File, io::BufReader};
+use std::{
+    env,
+    fs::File,
+    io::{self, BufReader},
+};
 
 use noodles_sam as sam;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> io::Result<()> {
     let src = env::args().nth(1).expect("missing src");
 
     let mut reader = File::open(src).map(BufReader::new).map(sam::Reader::new)?;
     reader.read_header()?;
 
-    let count = reader.records().count();
-    println!("{}", count);
+    let mut n = 0;
+
+    for result in reader.records() {
+        let _ = result?;
+        n += 1;
+    }
+
+    println!("{}", n);
 
     Ok(())
 }
