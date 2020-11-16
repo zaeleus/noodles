882c7da69ab1bf8af587afe87a5d85f8a4d62541
Michael Macias <zaeleus@gmail.com>
2020-11-15 19:29:53 -0600

    cram/file_definition: Add documentation

--- noodles-cram/src/file_definition.rs
+++ noodles-cram/src/file_definition.rs
@@ -1,7 +1,13 @@
+//! CRAM file definition and fields.
+
 mod version;
 
 pub use self::version::Version;
 
+/// A CRAM file definition.
+///
+/// The CRAM file definition holds the format version and file ID. See § 6 File definition
+/// (2020-06-22).
 #[derive(Clone, Default, Debug, Eq, PartialEq)]
 pub struct FileDefinition {
     version: Version,
@@ -9,14 +15,46 @@ pub struct FileDefinition {
 }
 
 impl FileDefinition {
+    /// Creates a file definition.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_cram::{file_definition::Version, FileDefinition};
+    /// let file_definition = FileDefinition::new(Version::new(3, 0), [0; 20]);
+    /// assert_eq!(file_definition.version(), Version::new(3, 0));
+    /// ```
     pub fn new(version: Version, file_id: [u8; 20]) -> Self {
         Self { version, file_id }
     }
 
+    /// Returns the file version.
+    ///
+    /// This is also called the (major and minor) format number.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_cram::{file_definition::Version, FileDefinition};
+    /// let file_definition = FileDefinition::new(Version::new(3, 0), [0; 20]);
+    /// assert_eq!(file_definition.version(), Version::new(3, 0));
+    /// ```
     pub fn version(&self) -> Version {
         self.version
     }
 
+    /// Returns the file ID.
+    ///
+    /// The file ID has a fixed length of 20 bytes. It can be any arbitrary identifier, e.g., the
+    /// file name or a 160-bit checksum.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_cram::{file_definition::Version, FileDefinition};
+    /// let file_definition = FileDefinition::new(Version::new(3, 0), [0; 20]);
+    /// assert_eq!(file_definition.file_id(), [0; 20]);
+    /// ```
     pub fn file_id(&self) -> &[u8] {
         &self.file_id
     }
--- noodles-cram/src/file_definition/version.rs
+++ noodles-cram/src/file_definition/version.rs
@@ -1,3 +1,6 @@
+/// A CRAM file definition version.
+///
+/// This is also called the format number.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct Version {
     major: u8,
@@ -5,14 +8,40 @@ pub struct Version {
 }
 
 impl Version {
+    /// Creates a file definition version.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_cram::file_definition::Version;
+    /// let version = Version::new(3, 0);
+    /// ```
     pub fn new(major: u8, minor: u8) -> Self {
         Self { major, minor }
     }
 
+    /// Returns the major version.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_cram::file_definition::Version;
+    /// let version = Version::new(3, 0);
+    /// assert_eq!(version.major(), 3);
+    /// ```
     pub fn major(&self) -> u8 {
         self.major
     }
 
+    /// Returns the minor version.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_cram::file_definition::Version;
+    /// let version = Version::new(3, 0);
+    /// assert_eq!(version.minor(), 0);
+    /// ```
     pub fn minor(&self) -> u8 {
         self.minor
     }
