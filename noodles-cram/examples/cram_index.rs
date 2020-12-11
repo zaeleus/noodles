4cdaa4ead18cc270f3b448f5f68f1a2f8d178d38
Michael Macias <zaeleus@gmail.com>
2020-12-10 19:15:57 -0600

    cram/examples: Add index example

---
+++ noodles-cram/examples/cram_index.rs
@@ -0,0 +1,25 @@
+//! Builds and writes a CRAM index from a CRAM file.
+//!
+//! This writes the output to stdout rather than `<src>.crai`.
+//!
+//! The output is similar to the output of `samtools index <src>`.
+
+use std::{env, io};
+
+use noodles_cram::{self as cram, crai};
+
+fn main() -> io::Result<()> {
+    let src = env::args().nth(1).expect("missing src");
+
+    let index = cram::index(src)?;
+
+    let stdout = io::stdout();
+    let handle = stdout.lock();
+    let mut writer = crai::Writer::new(handle);
+
+    for record in &index {
+        writer.write_record(record)?;
+    }
+
+    Ok(())
+}
