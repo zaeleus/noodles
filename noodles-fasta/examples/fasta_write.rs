8851781bf2a0cc2d28b50a3cf9307b1c821c6cc9
Michael Macias <zaeleus@gmail.com>
2021-01-09 19:08:41 -0600

    fasta/examples: Add write example

---
+++ noodles-fasta/examples/fasta_write.rs
@@ -0,0 +1,21 @@
+//! Creates a new FASTA file.
+//!
+//! This writes a single FASTA record to stdout.
+
+use std::io;
+
+use noodles_fasta as fasta;
+
+fn main() -> io::Result<()> {
+    let stdout = io::stdout();
+    let handle = stdout.lock();
+    let mut writer = fasta::Writer::new(handle);
+
+    let definition = fasta::record::Definition::new(String::from("sq0"), None);
+    let sequence = b"ACGT".iter().cycle().take(256).copied().collect();
+    let record = fasta::Record::new(definition, sequence);
+
+    writer.write_record(&record)?;
+
+    Ok(())
+}
