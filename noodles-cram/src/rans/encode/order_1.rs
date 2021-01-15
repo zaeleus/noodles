f27923c07698a5805c619f037809bdfe9995c625
Michael Macias <zaeleus@gmail.com>
2021-01-14 18:42:21 -0600

    cram/rans/encode/order_1: Add frequency table writer

--- noodles-cram/src/rans/encode/order_1.rs
+++ noodles-cram/src/rans/encode/order_1.rs
@@ -1,9 +1,13 @@
-use std::{io, mem};
+use std::{
+    io::{self, Write},
+    mem,
+};
 
 use byteorder::{LittleEndian, WriteBytesExt};
 
 use super::{
-    build_cumulative_frequencies, normalize, normalize_frequencies, update, BASE, LOWER_BOUND,
+    build_cumulative_frequencies, normalize, normalize_frequencies, order_0, update, BASE,
+    LOWER_BOUND,
 };
 
 #[allow(dead_code)]
@@ -79,6 +83,42 @@ pub fn encode(data: &[u8]) -> io::Result<(Vec<Vec<u32>>, Vec<u8>)> {
     Ok((freq, writer))
 }
 
+#[allow(dead_code)]
+pub fn write_contexts<W>(writer: &mut W, contexts: &[Vec<u32>]) -> io::Result<()>
+where
+    W: Write,
+{
+    let sums: Vec<u32> = contexts
+        .iter()
+        .map(|frequencies| frequencies.iter().sum())
+        .collect();
+
+    let mut rle = 0;
+
+    for (sym, (frequencies, &sum)) in contexts.iter().zip(sums.iter()).enumerate() {
+        if sum == 0 {
+            continue;
+        }
+
+        if rle > 0 {
+            rle -= 1;
+        } else {
+            writer.write_u8(sym as u8)?;
+
+            if sym > 0 && sums[sym - 1] > 0 {
+                rle = sums[sym + 1..].iter().position(|&s| s == 0).unwrap_or(255);
+                writer.write_u8(rle as u8)?;
+            }
+        }
+
+        order_0::write_frequencies(writer, frequencies)?;
+    }
+
+    writer.write_u8(0x00)?;
+
+    Ok(())
+}
+
 fn build_contexts(data: &[u8], bin_count: usize) -> Vec<Vec<u32>> {
     let mut frequencies = vec![vec![0; bin_count]; bin_count];
 
@@ -142,6 +182,54 @@ mod tests {
         ];
 
         assert_eq!(actual_compressed_data, expected_compressed_data);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_write_contexts() -> io::Result<()> {
+        let data = b"abracadabraabracadabraabracadabraabracadabra";
+        let contexts = build_contexts(data, BASE);
+        let normalized_contexts = normalize_contexts(&contexts);
+
+        let mut writer = Vec::new();
+        write_contexts(&mut writer, &normalized_contexts)?;
+
+        let expected = [
+            0x00, // syms[0] = '\0' {
+            0x61, // syms[1] = 'a'
+            0x8f, 0xff, // f[0]['a'] = 4095
+            0x00, // }
+            0x61, // syms[0] = 'a' {
+            0x61, // syms[1] = 'a'
+            0x82, 0x86, // f['a']['a'] = 646
+            0x62, // syms[1] = 'b'
+            0x02, // rle = 2
+            0x86, 0xbd, // f['a']['b'] = 1725
+            0x83, 0x5e, // f['a']['c'] = 862
+            0x83, 0x5e, // f['a']['d'] = 862
+            0x00, // }
+            0x62, // syms[0] = 'b' {
+            0x02, // rle = 2
+            0x72, // syms[1] = 'r'
+            0x8f, 0xff, // f['b']['r'] = 4095
+            0x00, // }
+            // syms[0] = 'c' {
+            0x61, // 'a'
+            0x8f, 0xff, // f['c']['a'] = 4095
+            0x00, // }
+            // syms[0] = 'd' {
+            0x61, // syms[1] = 'a'
+            0x8f, 0xff, // f['d']['a'] = 4095
+            0x00, // }
+            0x72, // syms[0] = 'r' {
+            0x61, // syms[1] = 'a'
+            0x8f, 0xff, // f['r']['a'] = 4095
+            0x00, // }
+            0x00, // end
+        ];
+
+        assert_eq!(writer, expected);
 
         Ok(())
     }
