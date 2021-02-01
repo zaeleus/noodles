5cbc59798389604ddfcd3d57ce024ae550763f01
Michael Macias <zaeleus@gmail.com>
2021-02-01 17:50:31 -0600

    cram/reader/slice: Add read_header test

--- noodles-cram/src/reader/slice.rs
+++ noodles-cram/src/reader/slice.rs
@@ -82,3 +82,48 @@ where
     reader.read_exact(&mut buf)?;
     Ok(buf)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_header() -> Result<(), Box<dyn std::error::Error>> {
+        let data = [
+            0x02, // reference sequence ID = 2
+            0x03, // alignment start = 3
+            0x05, // alignment span = 5
+            0x08, // number of records = 8
+            0x0d, // record counter = 13
+            0x01, // number of blocks = 1
+            0x01, // block content ID count = 1
+            0x15, // block content IDs[0] = 21
+            0xff, 0xff, 0xff, 0xff, 0x0f, // embedded reference bases block content ID = -1
+            0x57, 0xb2, 0x96, 0xa3, 0x16, 0x0a, 0x2c, 0xac, 0x9c, 0x83, 0x33, 0x12, 0x6f, 0xf2,
+            0x7e, 0xf7, // reference MD5 (b"ACGTA")
+        ];
+        let mut reader = &data[..];
+        let actual = read_header(&mut reader)?;
+
+        let expected = slice::Header::builder()
+            .set_reference_sequence_id(ReferenceSequenceId::try_from(2)?)
+            .set_alignment_start(3)
+            .set_alignment_span(5)
+            .set_record_count(8)
+            .set_record_counter(13)
+            .set_block_count(1)
+            .set_block_content_ids(vec![21])
+            .set_embedded_reference_bases_block_content_id(
+                EmbeddedReferenceBasesBlockContentId::try_from(-1)?,
+            )
+            .set_reference_md5([
+                0x57, 0xb2, 0x96, 0xa3, 0x16, 0x0a, 0x2c, 0xac, 0x9c, 0x83, 0x33, 0x12, 0x6f, 0xf2,
+                0x7e, 0xf7,
+            ])
+            .build();
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+}
