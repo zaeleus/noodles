5f82ba559d7302f4aa4498b99dabfb49b0fba508
Michael Macias <zaeleus@gmail.com>
2021-04-18 12:42:23 -0500

    bcf/examples: Add read header example

---
+++ noodles-bcf/examples/bcf_read_header.rs
@@ -0,0 +1,16 @@
+//! Prints the header of a BCF file.
+
+use std::{env, fs::File, io};
+
+use noodles_bcf as bcf;
+
+fn main() -> io::Result<()> {
+    let src = env::args().nth(1).expect("missing src");
+    let mut reader = File::open(src).map(bcf::Reader::new)?;
+    reader.read_file_format()?;
+
+    let header = reader.read_header()?;
+    print!("{}", header);
+
+    Ok(())
+}
