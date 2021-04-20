2f7d78e3c2c0c079a83792fc82514b6084b908e4
Michael Macias <zaeleus@gmail.com>
2021-04-19 20:56:15 -0500

    bcf/examples: Add count example

---
+++ noodles-bcf/examples/bcf_count.rs
@@ -0,0 +1,30 @@
+//! Counts the number of records in a BCF file.
+//!
+//! The result matches the output of `bcftools view --no-header <src> | wc -l`.
+
+use std::{env, fs::File, io};
+
+use noodles_bcf as bcf;
+
+fn main() -> io::Result<()> {
+    let src = env::args().nth(1).expect("missing src");
+
+    let mut reader = File::open(src).map(bcf::Reader::new)?;
+    reader.read_file_format()?;
+    reader.read_header()?;
+
+    let mut buf = Vec::new();
+    let mut n = 0;
+
+    loop {
+        match reader.read_record(&mut buf) {
+            Ok(0) => break,
+            Ok(_) => n += 1,
+            Err(e) => return Err(e),
+        }
+    }
+
+    println!("{}", n);
+
+    Ok(())
+}
