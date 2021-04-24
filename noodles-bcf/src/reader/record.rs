932b70d2ff5a4ec672f42c1d768248f790b3477a
Michael Macias <zaeleus@gmail.com>
2021-04-24 10:58:14 -0500

    bcf/reader/record: Extract field readers

--- noodles-bcf/src/reader/record.rs
+++ noodles-bcf/src/reader/record.rs
@@ -23,33 +23,47 @@ where
     })?;
 
     let n_allele_info = reader.read_i32::<LittleEndian>()?;
-    let allele_count = n_allele_info >> 16;
-    let info_count = n_allele_info & 0xffff;
+    let allele_count = (n_allele_info >> 16) as u16;
+    let info_count = (n_allele_info & 0xffff) as u16;
 
     let n_fmt_sample = reader.read_i32::<LittleEndian>()?;
-    let format_count = n_fmt_sample >> 24;
+    let format_count = (n_fmt_sample >> 24) as u8;
     let sample_count = n_fmt_sample & 0xffffff;
 
-    let id = match read_value(reader) {
+    let id = read_id(reader)?;
+    let ref_alt = read_ref_alt(reader, usize::from(allele_count))?;
+    let filter = read_filter(reader)?;
+
+    Ok(())
+}
+
+fn read_id<R>(reader: &mut R) -> io::Result<Ids>
+where
+    R: Read,
+{
+    match read_value(reader) {
         Ok(Value::String(Some(id))) => id
             .parse()
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?,
-        Ok(Value::String(None)) => Ids::default(),
-        Ok(v) => {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidData,
-                format!("expected string, got {:?}", v),
-            ))
-        }
-        Err(e) => return Err(e),
-    };
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e)),
+        Ok(Value::String(None)) => Ok(Ids::default()),
+        Ok(v) => Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!("expected string, got {:?}", v),
+        )),
+        Err(e) => Err(e),
+    }
+}
 
-    let mut ref_alt = Vec::with_capacity(allele_count as usize);
+fn read_ref_alt<R>(reader: &mut R, len: usize) -> io::Result<Vec<String>>
+where
+    R: Read,
+{
+    let mut alleles = Vec::with_capacity(len);
 
-    for _ in 0..allele_count {
+    for _ in 0..len {
         match read_value(reader) {
-            Ok(Value::String(Some(s))) => ref_alt.push(s),
-            Ok(Value::String(None)) => ref_alt.push(String::from(".")),
+            Ok(Value::String(Some(s))) => alleles.push(s),
+            Ok(Value::String(None)) => alleles.push(String::from(".")),
             Ok(v) => {
                 return Err(io::Error::new(
                     io::ErrorKind::InvalidData,
@@ -60,20 +74,23 @@ where
         }
     }
 
-    let filter_indices = match read_value(reader) {
-        Ok(Value::Int8(None)) => Vec::new(),
-        Ok(Value::Int8(Some(i))) => vec![i],
-        Ok(Value::Int8Array(indicies)) => indicies,
-        Ok(v) => {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidData,
-                format!("expected i8, got {:?}", v),
-            ))
-        }
-        Err(e) => return Err(e),
-    };
+    Ok(alleles)
+}
 
-    Ok(())
+fn read_filter<R>(reader: &mut R) -> io::Result<Vec<i8>>
+where
+    R: Read,
+{
+    match read_value(reader) {
+        Ok(Value::Int8(None)) => Ok(Vec::new()),
+        Ok(Value::Int8(Some(i))) => Ok(vec![i]),
+        Ok(Value::Int8Array(indicies)) => Ok(indicies),
+        Ok(v) => Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!("expected i8, got {:?}", v),
+        )),
+        Err(e) => Err(e),
+    }
 }
 
 #[cfg(test)]
