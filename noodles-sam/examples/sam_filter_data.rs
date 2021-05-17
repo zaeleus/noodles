00a9019ccd48bfaaa5952a02151b4534b86e0766
Michael Macias <zaeleus@gmail.com>
2021-05-17 18:15:02 -0500

    sam/examples: Add filter data example

---
+++ noodles-sam/examples/sam_filter_data.rs
@@ -0,0 +1,54 @@
+//! Filters records in a SAM that have a unique alignment.
+//!
+//! That is, there is a single alignment hit count (SAM record data tag `NM` = 1).
+
+use std::{
+    env,
+    fs::File,
+    io::{self, BufReader},
+};
+
+use noodles_sam::{
+    self as sam,
+    record::data::field::{Tag, Value},
+};
+
+fn is_unique_record(record: &sam::Record) -> io::Result<bool> {
+    let value = record
+        .data()
+        .get(&Tag::AlignmentHitCount)
+        .map(|field| field.value());
+
+    match value {
+        Some(Value::Int32(n)) => Ok(*n == 1),
+        Some(v) => Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!("expected Int32, got {:?}", v),
+        )),
+        None => Ok(false),
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = env::args().skip(1);
+    let src = args.next().expect("missing src");
+
+    let mut reader = File::open(src).map(BufReader::new).map(sam::Reader::new)?;
+    let header: sam::Header = reader.read_header()?.parse()?;
+
+    let stdout = io::stdout();
+    let handle = stdout.lock();
+    let mut writer = sam::Writer::new(handle);
+
+    writer.write_header(&header)?;
+
+    for result in reader.records() {
+        let record = result?;
+
+        if is_unique_record(&record)? {
+            writer.write_record(&record)?;
+        }
+    }
+
+    Ok(())
+}
