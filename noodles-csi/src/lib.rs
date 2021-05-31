2d37a1f8dbcb7c3f1f1bca9a99f43c2967fc1f98
Michael Macias <zaeleus@gmail.com>
2021-05-31 12:21:24 -0500

    csi: Add convenience read function

--- noodles-csi/src/lib.rs
+++ noodles-csi/src/lib.rs
@@ -6,4 +6,27 @@ mod writer;
 
 pub use self::{index::Index, reader::Reader, writer::Writer};
 
+use std::{fs::File, io, path::Path};
+
 static MAGIC_NUMBER: &[u8] = b"CSI\x01";
+
+/// Reads the entire contents of an coordinate-sorted index (CSI).
+///
+/// This is a convenience function and is equivalent to opening the file at the given path and
+/// reading the index.
+///
+/// # Examples
+///
+/// ```no_run
+/// # use std::io;
+/// use noodles_csi as csi;
+/// let index = csi::read("sample.bcf.csi")?;
+/// # Ok::<(), io::Error>(())
+/// ```
+pub fn read<P>(src: P) -> io::Result<Index>
+where
+    P: AsRef<Path>,
+{
+    let mut reader = File::open(src).map(Reader::new)?;
+    reader.read_index()
+}
