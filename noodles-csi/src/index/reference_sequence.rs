50c6a4632b4b07e9d28c532a5593b282ce43699d
Michael Macias <zaeleus@gmail.com>
2021-06-01 20:25:18 -0500

    csi/index/reference_sequence: Add method to query region bins

--- noodles-csi/src/index/reference_sequence.rs
+++ noodles-csi/src/index/reference_sequence.rs
@@ -4,6 +4,8 @@ mod bin;
 
 pub use self::bin::Bin;
 
+use bit_vec::BitVec;
+
 /// A CSI reference sequence.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct ReferenceSequence {
@@ -35,5 +37,38 @@ impl ReferenceSequence {
     /// ```
     pub fn bins(&self) -> &[Bin] {
         &self.bins
+    }
+
+    pub fn query(&self, min_shift: i32, depth: i32, start: i64, end: i64) -> Vec<&Bin> {
+        let max_bin_id = Bin::max_id(depth);
+        let mut region_bins = BitVec::from_elem(max_bin_id as usize, false);
+
+        reg2bins(start, end, min_shift, depth, &mut region_bins);
+
+        self.bins()
+            .iter()
+            .filter(|b| region_bins[b.id() as usize])
+            .collect()
+    }
+}
+
+// `CSIv1.pdf` (2020-07-21)
+#[allow(clippy::many_single_char_names)]
+fn reg2bins(beg: i64, end: i64, min_shift: i32, depth: i32, bins: &mut BitVec) {
+    let mut l = 0;
+    let mut t = 0;
+    let mut s = min_shift + depth * 3;
+
+    while l <= depth {
+        let b = t + (beg >> s);
+        let e = t + (end >> s);
+
+        for i in b..=e {
+            bins.set(i as usize, true);
+        }
+
+        s -= 3;
+        t += 1 << (l * 3);
+        l += 1;
     }
 }
