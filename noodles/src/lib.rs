cc0a0b703da7c54d0be5964648c7894c912fd6fe
Michael Macias <zaeleus@gmail.com>
2021-06-02 20:49:12 -0500

    noodles: Add CSI to module documentation

--- noodles/src/lib.rs
+++ noodles/src/lib.rs
@@ -1,8 +1,8 @@
 #![deny(missing_docs)]
 
 //! **noodles** is a library for handling various bioinformatics file formats. It currently
-//! includes readers and writers for BAM 1.6, BCF 2.2, BGZF, CRAM 3.0, FASTA, FASTQ, GFF3, SAM 1.6,
-//! tabix, and VCF 4.3.
+//! includes readers and writers for BAM 1.6, BCF 2.2, BGZF, CRAM 3.0, CSI, FASTA, FASTQ, GFF3, SAM
+//! 1.6, tabix, and VCF 4.3.
 
 #[cfg(feature = "bam")]
 pub use noodles_bam as bam;
