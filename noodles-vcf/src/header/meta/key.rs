5985dc271d11b0e46fcefb9b6b8738334b1a55f0
Michael Macias <zaeleus@gmail.com>
2021-06-14 11:22:08 -0400

    vcf/header/meta/key: Add parse errors

--- noodles-vcf/src/header/meta/key.rs
+++ noodles-vcf/src/header/meta/key.rs
@@ -34,21 +34,21 @@ impl fmt::Display for Key {
 
 /// An error returned when a raw VCF header meta record key fails to parse.
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct ParseError(String);
+pub enum ParseError {
+    /// The input is empty.
+    Empty,
+    /// The input is invalid.
+    Invalid,
+}
 
 impl error::Error for ParseError {}
 
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "invalid meta key: expected {{{}, {}, {}, {}}}, got {}",
-            Key::Id,
-            Key::Type,
-            Key::Number,
-            Key::Values,
-            self.0
-        )
+        match self {
+            Self::Empty => f.write_str("empty input"),
+            Self::Invalid => f.write_str("invalid input"),
+        }
     }
 }
 
@@ -57,11 +57,12 @@ impl FromStr for Key {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
+            "" => Err(ParseError::Empty),
             "ID" => Ok(Self::Id),
             "Type" => Ok(Self::Type),
             "Number" => Ok(Self::Number),
             "Values" => Ok(Self::Values),
-            _ => Err(ParseError(s.into())),
+            _ => Err(ParseError::Invalid),
         }
     }
 }
@@ -85,10 +86,7 @@ mod tests {
         assert_eq!("Number".parse(), Ok(Key::Number));
         assert_eq!("Values".parse(), Ok(Key::Values));
 
-        assert_eq!("".parse::<Key>(), Err(ParseError(String::from(""))));
-        assert_eq!(
-            "Noodles".parse::<Key>(),
-            Err(ParseError(String::from("Noodles")))
-        );
+        assert_eq!("".parse::<Key>(), Err(ParseError::Empty));
+        assert_eq!("Noodles".parse::<Key>(), Err(ParseError::Invalid));
     }
 }
