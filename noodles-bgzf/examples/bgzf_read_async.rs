7c736d13cc1ed05da0545637f22ded7c9a291f81
Michael Macias <zaeleus@gmail.com>
2021-07-05 14:13:44 -0500

    bgzf/examples: Add async read example

---
+++ noodles-bgzf/examples/bgzf_read_async.rs
@@ -0,0 +1,22 @@
+//! Decompresses a BGZF file using an asynchronous reader.
+//!
+//! The result matches the ouptput of `bgzip --decompress --stdout <src>`.
+
+use std::env;
+
+use noodles_bgzf as bgzf;
+use tokio::{fs::File, io};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let src = env::args().nth(1).expect("missing src");
+
+    let mut reader = File::open(src).await.map(bgzf::AsyncReader::new)?;
+
+    let stdout = io::stdout();
+    let mut writer = io::BufWriter::new(stdout);
+
+    io::copy_buf(&mut reader, &mut writer).await?;
+
+    Ok(())
+}
