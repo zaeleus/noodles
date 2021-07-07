43e71f8db09299392db58494de67a6b0477a47bb
Michael Macias <zaeleus@gmail.com>
2021-07-06 22:02:51 -0500

    cram/crai: Add convenience read function

--- noodles-cram/src/crai.rs
+++ noodles-cram/src/crai.rs
@@ -4,5 +4,38 @@ mod writer;
 
 pub use self::{reader::Reader, record::Record, writer::Writer};
 
+use std::{
+    fs::File,
+    io::{self, BufReader},
+    path::Path,
+};
+
+use flate2::read::GzDecoder;
+
 /// A CRAM index.
 pub type Index = Vec<Record>;
+
+/// Reads the entire contents of a CRAM index.
+///
+/// This is a convenience function and is equivalent to opening the file at the given path and
+/// reading the index.
+///
+/// # Examples
+///
+/// ```no_run
+/// # use std::io;
+/// use noodles_cram::crai;
+/// let index = crai::read("sample.cram.crai")?;
+/// # Ok::<(), io::Error>(())
+/// ```
+pub fn read<P>(src: P) -> io::Result<Index>
+where
+    P: AsRef<Path>,
+{
+    let mut reader = File::open(src)
+        .map(GzDecoder::new)
+        .map(BufReader::new)
+        .map(Reader::new)?;
+
+    reader.read_index()
+}
