7ca1f1a222f0685d2b233f9cf6c2e701453b807f
Michael Macias <zaeleus@gmail.com>
2021-07-09 18:10:09 -0500

    bam/examples: Add async read header example

---
+++ noodles-bam/examples/bam_read_header_async.rs
@@ -0,0 +1,33 @@
+//! Prints the header of a BAM file.
+//!
+//! A BAM file header is a SAM header.
+//!
+//! The result matches the output of `samtools view -H --no-PG <src>`.
+
+use std::env;
+
+use noodles_bam as bam;
+use noodles_sam as sam;
+use tokio::{fs::File, io};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let src = env::args().nth(1).expect("missing src");
+
+    let mut reader = File::open(src).await.map(bam::AsyncReader::new)?;
+    let raw_header = reader.read_header().await?;
+
+    if raw_header.is_empty() {
+        let reference_sequences = reader.read_reference_sequences().await?;
+
+        let header = sam::Header::builder()
+            .set_reference_sequences(reference_sequences)
+            .build();
+
+        print!("{}", header);
+    } else {
+        print!("{}", raw_header);
+    }
+
+    Ok(())
+}
