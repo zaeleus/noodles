9a74536ee13e94b0bef9dc8106e5f1b91e5c8c41
Michael Macias <zaeleus@gmail.com>
2021-07-14 17:04:57 -0500

    noodles: Add module docs inline

--- noodles/src/lib.rs
+++ noodles/src/lib.rs
@@ -5,37 +5,49 @@
 //! 1.6, tabix, and VCF 4.3.
 
 #[cfg(feature = "bam")]
+#[doc(inline)]
 pub use noodles_bam as bam;
 
 #[cfg(feature = "bcf")]
+#[doc(inline)]
 pub use noodles_bcf as bcf;
 
 #[cfg(feature = "bgzf")]
+#[doc(inline)]
 pub use noodles_bgzf as bgzf;
 
 #[cfg(feature = "core")]
+#[doc(inline)]
 pub use noodles_core as core;
 
 #[cfg(feature = "cram")]
+#[doc(inline)]
 pub use noodles_cram as cram;
 
 #[cfg(feature = "csi")]
+#[doc(inline)]
 pub use noodles_csi as csi;
 
 #[cfg(feature = "fasta")]
+#[doc(inline)]
 pub use noodles_fasta as fasta;
 
 #[cfg(feature = "fastq")]
+#[doc(inline)]
 pub use noodles_fastq as fastq;
 
 #[cfg(feature = "gff")]
+#[doc(inline)]
 pub use noodles_gff as gff;
 
 #[cfg(feature = "sam")]
+#[doc(inline)]
 pub use noodles_sam as sam;
 
 #[cfg(feature = "tabix")]
+#[doc(inline)]
 pub use noodles_tabix as tabix;
 
 #[cfg(feature = "vcf")]
+#[doc(inline)]
 pub use noodles_vcf as vcf;
