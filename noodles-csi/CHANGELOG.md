c53ec8e731bb99f1a76a59ff1ca4a3dd03ff081d
Michael Macias <zaeleus@gmail.com>
2021-07-15 17:48:51 -0500

    csi: Add convenience write function

--- noodles-csi/CHANGELOG.md
+++ noodles-csi/CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## Unreleased
+
+### Added
+
+  * Add convenience function to write an entire index to a file: `csi::write`.
+
 ## 0.1.0 - 2021-07-14
 
   * Initial release.
--- noodles-csi/src/lib.rs
+++ noodles-csi/src/lib.rs
@@ -32,3 +32,25 @@ where
     let mut reader = File::open(src).map(Reader::new)?;
     reader.read_index()
 }
+
+/// Writes a coordinate-sorted index (CSI) to a file.
+///
+/// This is a convenience function and is equivalent to creating a file at the given path and
+/// writing the index.
+///
+/// # Examples
+///
+/// ```no_run
+/// # use std::io;
+/// use noodles_csi as csi;
+/// let index = csi::Index::default();
+/// csi::write("sample.bcf.csi", &index)?;
+/// # Ok::<(), io::Error>(())
+/// ```
+pub fn write<P>(dst: P, index: &Index) -> io::Result<()>
+where
+    P: AsRef<Path>,
+{
+    let mut writer = File::create(dst).map(Writer::new)?;
+    writer.write_index(index)
+}
