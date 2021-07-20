352f29b96bbb2fe78033cc4bae51333e46ce9cf3
Michael Macias <zaeleus@gmail.com>
2021-07-20 18:39:01 -0500

    vcf/async/reader: Add async record reader

--- noodles-vcf/src/async/reader.rs
+++ noodles-vcf/src/async/reader.rs
@@ -1,8 +1,11 @@
 mod header;
 
-use tokio::io::AsyncBufRead;
+use tokio::io::{self, AsyncBufRead, AsyncBufReadExt};
 
 use self::header::ReadHeader;
+
+const LINE_FEED: char = '\n';
+const CARRIAGE_RETURN: char = '\r';
 
 /// An async VCF reader.
 pub struct Reader<R> {
@@ -53,5 +56,79 @@ where
     /// ```
     pub fn read_header(&mut self) -> ReadHeader<'_, R> {
         ReadHeader::new(&mut self.inner)
+    }
+
+    /// Reads a single raw VCF record.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::io;
+    /// #
+    /// # #[tokio::main]
+    /// # async fn main() -> io::Result<()> {
+    /// use noodles_vcf as vcf;
+    ///
+    /// let data = b"##fileformat=VCFv4.3
+    /// #CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO
+    /// sq0\t1\t.\tA\t.\t.\tPASS\t.
+    /// ";
+    ///
+    /// let mut reader = vcf::AsyncReader::new(&data[..]);
+    /// reader.read_header().await?;
+    ///
+    /// let mut buf = String::new();
+    /// reader.read_record(&mut buf).await?;
+    ///
+    /// assert_eq!(buf, "sq0\t1\t.\tA\t.\t.\tPASS\t.");
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub async fn read_record(&mut self, buf: &mut String) -> io::Result<usize> {
+        read_line(&mut self.inner, buf).await
+    }
+}
+
+async fn read_line<R>(reader: &mut R, buf: &mut String) -> io::Result<usize>
+where
+    R: AsyncBufRead + Unpin,
+{
+    match reader.read_line(buf).await {
+        Ok(0) => Ok(0),
+        Ok(n) => {
+            if buf.ends_with(LINE_FEED) {
+                buf.pop();
+
+                if buf.ends_with(CARRIAGE_RETURN) {
+                    buf.pop();
+                }
+            }
+
+            Ok(n)
+        }
+        Err(e) => Err(e),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_read_line() -> io::Result<()> {
+        async fn t(buf: &mut String, mut data: &[u8], expected: &str) -> io::Result<()> {
+            buf.clear();
+            read_line(&mut data, buf).await?;
+            assert_eq!(buf, expected);
+            Ok(())
+        }
+
+        let mut buf = String::new();
+
+        t(&mut buf, b"noodles\n", "noodles").await?;
+        t(&mut buf, b"noodles\r\n", "noodles").await?;
+        t(&mut buf, b"noodles", "noodles").await?;
+
+        Ok(())
     }
 }
