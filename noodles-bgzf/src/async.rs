93c182f93af54052b817c3f7d5e145c3a7dfbbc0
Michael Macias <zaeleus@gmail.com>
2021-07-31 15:12:51 -0500

    bgzf/async: Add self test
    This was previously the read test, but it tests both the writer and
    reader.

--- noodles-bgzf/src/async.rs
+++ noodles-bgzf/src/async.rs
@@ -5,3 +5,27 @@ mod writer;
 pub use self::{reader::Reader, writer::Writer};
 
 use self::block_codec::BlockCodec;
+
+#[cfg(test)]
+mod tests {
+    use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_self() -> io::Result<()> {
+        let mut writer = Writer::new(Vec::new());
+        writer.write_all(b"noodles").await?;
+        writer.shutdown().await?;
+
+        let data = writer.into_inner();
+        let mut reader = Reader::new(&data[..]);
+
+        let mut buf = Vec::new();
+        reader.read_to_end(&mut buf).await?;
+
+        assert_eq!(buf, b"noodles");
+
+        Ok(())
+    }
+}
--- noodles-bgzf/src/async/reader.rs
+++ noodles-bgzf/src/async/reader.rs
@@ -150,28 +150,3 @@ where
         this.block.set_upos(upos);
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use tokio::io::{AsyncReadExt, AsyncWriteExt};
-
-    use super::*;
-    use crate::AsyncWriter;
-
-    #[tokio::test]
-    async fn test_read() -> io::Result<()> {
-        let mut writer = AsyncWriter::new(Vec::new());
-        writer.write_all(b"noodles").await?;
-        writer.shutdown().await?;
-
-        let data = writer.into_inner();
-        let mut reader = Reader::new(&data[..]);
-
-        let mut buf = Vec::new();
-        reader.read_to_end(&mut buf).await?;
-
-        assert_eq!(buf, b"noodles");
-
-        Ok(())
-    }
-}
