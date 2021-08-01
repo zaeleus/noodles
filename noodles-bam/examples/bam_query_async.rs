a4f1ab0f2450c84641873cff78305ab5cd6927bf
Michael Macias <zaeleus@gmail.com>
2021-08-01 14:41:37 -0500

    bam/examples: Add async query example

---
+++ noodles-bam/examples/bam_query_async.rs
@@ -0,0 +1,49 @@
+//! Queries a BAM file with a given region.
+//!
+//! The input BAM must have an index in the same directory.
+//!
+//! The result matches the output of `samtools view <src> <region>`.
+
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+
+use futures::StreamExt;
+use noodles_bam::{self as bam, bai};
+use noodles_core::Region;
+use noodles_sam as sam;
+use tokio::{fs::File, io};
+
+async fn read_bai<P>(src: P) -> io::Result<bai::Index>
+where
+    P: AsRef<Path>,
+{
+    let mut reader = File::open(src).await.map(bai::AsyncReader::new)?;
+    reader.read_header().await?;
+    reader.read_index().await
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = env::args();
+
+    let src = args.nth(1).map(PathBuf::from).expect("missing src");
+    let raw_region = args.next().expect("missing region");
+
+    let mut reader = File::open(&src).await.map(bam::AsyncReader::new)?;
+    let header: sam::Header = reader.read_header().await?.parse()?;
+
+    let reference_sequences = header.reference_sequences();
+    let index = read_bai(src.with_extension("bam.bai")).await?;
+    let region = Region::from_str_reference_sequences(&raw_region, reference_sequences)?;
+    let mut query = reader.query(reference_sequences, &index, &region)?;
+
+    while let Some(result) = query.next().await {
+        let record = result?;
+        let sam_record = record.try_into_sam_record(reference_sequences)?;
+        println!("{}", sam_record);
+    }
+
+    Ok(())
+}
