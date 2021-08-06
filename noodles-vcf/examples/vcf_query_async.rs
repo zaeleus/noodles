84e7e60584076ffc29d7e0257b562074ac6fb7e9
Michael Macias <zaeleus@gmail.com>
2021-08-06 18:10:30 -0500

    vcf/examples: Add async query example

---
+++ noodles-vcf/examples/vcf_query_async.rs
@@ -0,0 +1,35 @@
+//! Queries a VCF file with a given region.
+//!
+//! The input VCF must have an associated index in the same directory.
+//!
+//! The result matches the output `bcftools view --no-header <src> <region>`.
+
+use std::{env, path::PathBuf};
+
+use futures::TryStreamExt;
+use noodles_bgzf as bgzf;
+use noodles_tabix as tabix;
+use noodles_vcf as vcf;
+use tokio::fs::File;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = env::args();
+
+    let src = args.nth(1).map(PathBuf::from).expect("missing src");
+    let region = args.next().map(|s| s.parse()).expect("missing region")?;
+
+    let mut reader = File::open(&src)
+        .await
+        .map(bgzf::AsyncReader::new)
+        .map(vcf::AsyncReader::new)?;
+
+    let index = tabix::read(src.with_extension("gz.tbi"))?;
+    let mut query = reader.query(&index, &region)?;
+
+    while let Some(record) = query.try_next().await? {
+        println!("{}", record);
+    }
+
+    Ok(())
+}
