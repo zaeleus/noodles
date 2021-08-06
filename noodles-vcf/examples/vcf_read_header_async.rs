768c6d7f407fa8e976a5e3ec9d5937a9ef92632e
Michael Macias <zaeleus@gmail.com>
2021-08-06 18:25:46 -0500

    vcf/examples: Add async read header example

---
+++ noodles-vcf/examples/vcf_read_header_async.rs
@@ -0,0 +1,29 @@
+//! Prints the header of a VCF file.
+//!
+//! The header is defined to be the meta lines (`##` prefix) and header line (`#`` prefix).
+//!
+//! The result is similar to or matches the output of `bcftools view --header-only --no-version
+//! <src>`. bcftools may add a PASS FILTER to the meta if it is missing.
+
+use std::env;
+
+use noodles_vcf as vcf;
+use tokio::{
+    fs::File,
+    io::{self, BufReader},
+};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let src = env::args().nth(1).expect("missing src");
+
+    let mut reader = File::open(src)
+        .await
+        .map(BufReader::new)
+        .map(vcf::AsyncReader::new)?;
+
+    let header = reader.read_header().await?;
+    print!("{}", header);
+
+    Ok(())
+}
