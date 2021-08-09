20025e283e471e049899d789fedfe35cf4657d2d
Michael Macias <zaeleus@gmail.com>
2021-08-09 18:11:21 -0500

    bcf/examples: Add async view example

---
+++ noodles-bcf/examples/bcf_view_async.rs
@@ -0,0 +1,30 @@
+//! Prints a BCF file in the VCF format.
+//!
+//! The result matches the output of `bcftools view --no-header <src>`.
+
+use std::env;
+
+use futures::TryStreamExt;
+use noodles_bcf as bcf;
+use tokio::fs::File;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let src = env::args().nth(1).expect("missing src");
+
+    let mut reader = File::open(src).await.map(bcf::AsyncReader::new)?;
+    reader.read_file_format().await?;
+
+    let raw_header = reader.read_header().await?;
+    let header = raw_header.parse()?;
+    let string_map = raw_header.parse()?;
+
+    let mut records = reader.records();
+
+    while let Some(record) = records.try_next().await? {
+        let vcf_record = record.try_into_vcf_record(&header, &string_map)?;
+        println!("{}", vcf_record);
+    }
+
+    Ok(())
+}
