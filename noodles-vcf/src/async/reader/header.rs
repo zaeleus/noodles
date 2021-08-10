1967bc9fa26fc2bc87f974267cada42915105ef0
Michael Macias <zaeleus@gmail.com>
2021-08-09 19:12:48 -0500

    vcf/async/reader/header: Track state

--- noodles-vcf/src/async/reader/header.rs
+++ noodles-vcf/src/async/reader/header.rs
@@ -1,18 +1,26 @@
 use std::{
     future::Future,
     pin::Pin,
+    str,
     task::{Context, Poll},
 };
 
 use futures::ready;
+use pin_project_lite::pin_project;
 use tokio::io::{self, AsyncBufRead};
 
 const LINE_FEED: u8 = b'\n';
 
 const HEADER_PREFIX: u8 = b'#';
 
-pub struct ReadHeader<'a, R> {
-    reader: &'a mut R,
+pin_project! {
+    pub struct ReadHeader<'a, R> {
+        #[pin]
+        reader: &'a mut R,
+        buf: Vec<u8>,
+        i: usize,
+        is_eol: bool,
+    }
 }
 
 impl<'a, R> ReadHeader<'a, R>
@@ -20,7 +28,12 @@ where
     R: AsyncBufRead + Unpin,
 {
     pub(crate) fn new(reader: &'a mut R) -> Self {
-        Self { reader }
+        Self {
+            reader,
+            buf: Vec::new(),
+            i: 0,
+            is_eol: false,
+        }
     }
 }
 
@@ -31,39 +44,34 @@ where
     type Output = io::Result<String>;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let reader = Pin::new(&mut self.reader);
-        read_header(reader, cx)
-    }
-}
+        let mut this = self.as_mut().project();
 
-fn read_header<R>(mut reader: Pin<&mut R>, cx: &mut Context<'_>) -> Poll<io::Result<String>>
-where
-    R: AsyncBufRead,
-{
-    let mut header_buf = Vec::new();
-    let mut is_eol = false;
+        for i in (*this.i).. {
+            let buf = ready!(this.reader.as_mut().poll_fill_buf(cx))?;
 
-    for i in 0.. {
-        let buf = ready!(reader.as_mut().poll_fill_buf(cx))?;
+            if (i == 0 || *this.is_eol) && buf.first().map(|&b| b != HEADER_PREFIX).unwrap_or(true)
+            {
+                break;
+            }
 
-        if (i == 0 || is_eol) && buf.first().map(|&b| b != HEADER_PREFIX).unwrap_or(true) {
-            break;
+            let (read_eol, len) = if let Some(i) = buf.iter().position(|&b| b == LINE_FEED) {
+                this.buf.extend(&buf[..=i]);
+                (true, i + 1)
+            } else {
+                this.buf.extend(buf);
+                (false, buf.len())
+            };
+
+            *this.i = i;
+            *this.is_eol = read_eol;
+
+            this.reader.as_mut().consume(len);
         }
 
-        let (read_eol, len) = if let Some(i) = buf.iter().position(|&b| b == LINE_FEED) {
-            header_buf.extend(&buf[..=i]);
-            (true, i + 1)
-        } else {
-            header_buf.extend(buf);
-            (false, buf.len())
-        };
+        let result = str::from_utf8(this.buf)
+            .map(|s| s.into())
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e));
 
-        is_eol = read_eol;
-
-        reader.as_mut().consume(len);
+        Poll::Ready(result)
     }
-
-    Poll::Ready(
-        String::from_utf8(header_buf).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e)),
-    )
 }
