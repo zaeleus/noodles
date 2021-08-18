5001278d0aea4ed62aa6650263e1f6fd2dfca368
Michael Macias <zaeleus@gmail.com>
2021-08-17 20:40:35 -0500

    cram/async/reader/compression_header: Add async compression header reader

--- noodles-cram/src/async/reader/compression_header.rs
+++ noodles-cram/src/async/reader/compression_header.rs
@@ -3,4 +3,25 @@ mod encoding;
 mod preservation_map;
 mod tag_encoding_map;
 
-use self::encoding::read_encoding;
+use tokio::io::{self, AsyncRead};
+
+use self::{
+    data_series_encoding_map::read_data_series_encoding_map, encoding::read_encoding,
+    preservation_map::read_preservation_map, tag_encoding_map::read_tag_encoding_map,
+};
+use crate::container::CompressionHeader;
+
+pub async fn read_compression_header<R>(reader: &mut R) -> io::Result<CompressionHeader>
+where
+    R: AsyncRead + Unpin,
+{
+    let preservation_map = read_preservation_map(reader).await?;
+    let data_series_encoding_map = read_data_series_encoding_map(reader).await?;
+    let tag_encoding_map = read_tag_encoding_map(reader).await?;
+
+    Ok(CompressionHeader::new(
+        preservation_map,
+        data_series_encoding_map,
+        tag_encoding_map,
+    ))
+}
