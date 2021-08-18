38a71b7e112ce196952b2d133570a986b563c436
Michael Macias <zaeleus@gmail.com>
2021-08-17 21:14:25 -0500

    cram/async/reader/slice: Add async slice reader

--- noodles-cram/src/async/reader/slice.rs
+++ noodles-cram/src/async/reader/slice.rs
@@ -1,3 +1,48 @@
 mod header;
 
 pub use self::header::read_header;
+
+use std::convert::TryFrom;
+
+use tokio::io::{self, AsyncRead};
+
+use super::block::read_block;
+use crate::container::{slice, Block, Slice};
+
+pub async fn read_slice<R>(reader: &mut R) -> io::Result<Slice>
+where
+    R: AsyncRead + Unpin,
+{
+    let header = read_header_from_block(reader).await?;
+    let core_data_block = read_block(reader).await?;
+
+    let external_block_count = usize::try_from(header.block_count() - 1)
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+    let external_blocks = read_external_blocks(reader, external_block_count).await?;
+
+    Ok(Slice::new(header, core_data_block, external_blocks))
+}
+
+async fn read_header_from_block<R>(reader: &mut R) -> io::Result<slice::Header>
+where
+    R: AsyncRead + Unpin,
+{
+    let header_block = read_block(reader).await?;
+    let data = header_block.decompressed_data()?;
+    let mut data_reader = &data[..];
+    read_header(&mut data_reader).await
+}
+
+async fn read_external_blocks<R>(reader: &mut R, len: usize) -> io::Result<Vec<Block>>
+where
+    R: AsyncRead + Unpin,
+{
+    let mut external_blocks = Vec::with_capacity(len);
+
+    for _ in 0..len {
+        let block = read_block(reader).await?;
+        external_blocks.push(block);
+    }
+
+    Ok(external_blocks)
+}
