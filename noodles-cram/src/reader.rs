66c96717a98b5a0f73a21b0b251857e0e2f4010e
Michael Macias <zaeleus@gmail.com>
2021-08-23 17:13:47 -0500

    cram/reader: Remove useless conversion

--- noodles-cram/src/reader.rs
+++ noodles-cram/src/reader.rs
@@ -130,7 +130,7 @@ where
     pub(crate) fn read_container(&mut self) -> io::Result<Container> {
         let header = container::read_header(&mut self.inner)?;
 
-        let blocks_len = header.block_count() as usize;
+        let blocks_len = header.block_count();
         let mut blocks = Vec::with_capacity(blocks_len);
 
         for _ in 0..blocks_len {
