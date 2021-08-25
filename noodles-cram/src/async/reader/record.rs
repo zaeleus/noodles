d95b428559ec2db09b61c29833a7a2dc9a382184
Michael Macias <zaeleus@gmail.com>
2021-08-24 19:28:35 -0500

    cram/async/reader/record: Add async positional data reader

--- noodles-cram/src/async/reader/record.rs
+++ noodles-cram/src/async/reader/record.rs
@@ -4,15 +4,20 @@ use std::{
     io::{self, Read},
 };
 
+use noodles_bam as bam;
 use noodles_sam as sam;
 use tokio::io::{AsyncBufRead, AsyncRead};
 
 use crate::{
     container::ReferenceSequenceId,
-    data_container::{compression_header::Encoding, CompressionHeader},
+    data_container::{
+        compression_header::{data_series_encoding_map::DataSeries, Encoding},
+        CompressionHeader,
+    },
     num::Itf8,
     r#async::reader::num::read_itf8,
-    record::Flags,
+    reader::record::ReadRecordError,
+    record::{Builder, Flags, ReadGroupId},
     BitReader, Record,
 };
 
@@ -58,7 +63,13 @@ where
         let cram_bit_flags = self.read_cram_bit_flags().await?;
         builder = builder.set_flags(cram_bit_flags);
 
-        Ok(builder.build())
+        builder = self.read_positional_data(builder).await?;
+
+        let record = builder.build();
+
+        self.prev_alignment_start = record.alignment_start();
+
+        Ok(record)
     }
 
     async fn read_bam_bit_flags(&mut self) -> io::Result<sam::record::Flags> {
@@ -91,6 +102,105 @@ where
         .await
         .and_then(|n| u8::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e)))
         .map(Flags::from)
+    }
+
+    async fn read_positional_data(&mut self, mut builder: Builder) -> io::Result<Builder> {
+        let reference_id = if self.reference_sequence_id.is_many() {
+            self.read_reference_id().await?
+        } else {
+            i32::from(self.reference_sequence_id)
+        };
+
+        if reference_id != bam::record::reference_sequence_id::UNMAPPED {
+            let reference_sequence_id = bam::record::ReferenceSequenceId::try_from(reference_id)
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+            builder = builder.set_reference_sequence_id(reference_sequence_id);
+        }
+
+        let read_length = self.read_read_length().await?;
+        builder = builder.set_read_length(read_length);
+
+        let alignment_start = self.read_alignment_start().await?;
+        builder = builder.set_alignment_start(alignment_start);
+
+        let read_group = self.read_read_group().await?;
+        builder = builder.set_read_group_id(read_group);
+
+        Ok(builder)
+    }
+
+    async fn read_reference_id(&mut self) -> io::Result<Itf8> {
+        let encoding = self
+            .compression_header
+            .data_series_encoding_map()
+            .reference_id_encoding()
+            .ok_or_else(|| {
+                io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    ReadRecordError::MissingDataSeriesEncoding(DataSeries::ReferenceId),
+                )
+            })?;
+
+        decode_itf8(
+            encoding,
+            &mut self.core_data_reader,
+            &mut self.external_data_readers,
+        )
+        .await
+    }
+
+    async fn read_read_length(&mut self) -> io::Result<Itf8> {
+        let encoding = self
+            .compression_header
+            .data_series_encoding_map()
+            .read_lengths_encoding();
+
+        decode_itf8(
+            encoding,
+            &mut self.core_data_reader,
+            &mut self.external_data_readers,
+        )
+        .await
+    }
+
+    async fn read_alignment_start(&mut self) -> io::Result<Itf8> {
+        let ap_data_series_delta = self
+            .compression_header
+            .preservation_map()
+            .ap_data_series_delta();
+
+        let encoding = self
+            .compression_header
+            .data_series_encoding_map()
+            .in_seq_positions_encoding();
+
+        let alignment_start = decode_itf8(
+            encoding,
+            &mut self.core_data_reader,
+            &mut self.external_data_readers,
+        )
+        .await?;
+
+        if ap_data_series_delta {
+            Ok(self.prev_alignment_start + alignment_start)
+        } else {
+            Ok(alignment_start)
+        }
+    }
+
+    async fn read_read_group(&mut self) -> io::Result<ReadGroupId> {
+        let encoding = self
+            .compression_header
+            .data_series_encoding_map()
+            .read_groups_encoding();
+
+        decode_itf8(
+            encoding,
+            &mut self.core_data_reader,
+            &mut self.external_data_readers,
+        )
+        .await
+        .map(ReadGroupId::from)
     }
 }
 
