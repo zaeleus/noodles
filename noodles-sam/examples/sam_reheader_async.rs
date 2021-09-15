b828b7e681a2b5606954f783283c50d1e5589543
Michael Macias <zaeleus@gmail.com>
2021-09-15 17:49:07 -0500

    sam/examples: Add async reheader example

---
+++ noodles-sam/examples/sam_reheader_async.rs
@@ -0,0 +1,38 @@
+//! Replaces the header of a SAM file.
+//!
+//! This is similar to the functionality of `samtools reheader`.
+//!
+//! Verify the output by piping to `samtools view --no-PG --with-header`.
+
+use std::env;
+
+use futures::TryStreamExt;
+use noodles_sam as sam;
+use tokio::{
+    fs::File,
+    io::{self, BufReader},
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let src = env::args().nth(1).expect("missing src");
+
+    let mut reader = File::open(src)
+        .await
+        .map(BufReader::new)
+        .map(sam::AsyncReader::new)?;
+
+    let mut header: sam::Header = reader.read_header().await?.parse()?;
+    header.add_comment("a comment added by noodles-sam");
+
+    let mut writer = sam::AsyncWriter::new(io::stdout());
+    writer.write_header(&header).await?;
+
+    let mut records = reader.records();
+
+    while let Some(record) = records.try_next().await? {
+        writer.write_record(&record).await?;
+    }
+
+    Ok(())
+}
