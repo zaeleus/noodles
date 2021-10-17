9181c4f47e59c8a672a0713a068d1dda6fabd5a1
Michael Macias <zaeleus@gmail.com>
2021-10-17 14:39:18 -0500

    gtf/examples: Add view example

---
+++ noodles-gtf/examples/gtf_view.rs
@@ -0,0 +1,24 @@
+//! Validates and prints all records in a GTF file.
+//!
+//! The result matches the output of `grep --invert-match "^#" <src>`.
+
+use std::{
+    env,
+    fs::File,
+    io::{self, BufReader},
+};
+
+use noodles_gtf as gtf;
+
+fn main() -> io::Result<()> {
+    let src = env::args().nth(1).expect("missing src");
+
+    let mut reader = File::open(src).map(BufReader::new).map(gtf::Reader::new)?;
+
+    for result in reader.records() {
+        let record = result?;
+        println!("{}", record);
+    }
+
+    Ok(())
+}
