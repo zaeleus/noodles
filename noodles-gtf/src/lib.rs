befcd3801affd466317d12f6ebc48347555919e9
Michael Macias <zaeleus@gmail.com>
2021-10-17 13:54:39 -0500

    gtf: Add line parser

--- noodles-gtf/src/lib.rs
+++ noodles-gtf/src/lib.rs
@@ -2,7 +2,8 @@
 
 //! **noodles-gtf** handles the reading of the Gene Transfer Format (GTF).
 
+pub mod line;
 mod reader;
 pub mod record;
 
-pub use self::{reader::Reader, record::Record};
+pub use self::{line::Line, reader::Reader, record::Record};
---
+++ noodles-gtf/src/line.rs
@@ -0,0 +1,61 @@
+//! GTF lines.
+
+use std::{error, fmt, str::FromStr};
+
+use super::{record, Record};
+
+/// A GTF line.
+#[derive(Clone, Debug, PartialEq)]
+pub enum Line {
+    /// A comment (`#`).
+    Comment(String),
+    /// A record.
+    Record(Record),
+}
+
+/// An error returns when a raw GFF line fails to parse.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum ParseError {
+    /// The record is invalid.
+    InvalidRecord(record::ParseError),
+}
+
+impl error::Error for ParseError {}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidRecord(e) => write!(f, "invalid record: {}", e),
+        }
+    }
+}
+
+impl FromStr for Line {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some(t) = s.strip_prefix('#') {
+            Ok(Self::Comment(t.into()))
+        } else {
+            s.parse()
+                .map(Self::Record)
+                .map_err(ParseError::InvalidRecord)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_str() {
+        assert_eq!(
+            "##format: gtf".parse(),
+            Ok(Line::Comment(String::from("#format: gtf")))
+        );
+
+        let s = "sq0\tNOODLES\tgene\t8\t13\t.\t+\t.\tgene_id \"ndls0\"; transcript_id \"ndls0\";";
+        assert!(matches!(s.parse(), Ok(Line::Record(_))));
+    }
+}
