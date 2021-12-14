5e84abac7890f4eede712db9d06ecda6f90323d4
Michael Macias <zaeleus@gmail.com>
2021-12-14 15:35:23 -0600

    bcf/writer/vcf_record: Add test

--- noodles-bcf/src/writer/vcf_record.rs
+++ noodles-bcf/src/writer/vcf_record.rs
@@ -42,3 +42,47 @@ where
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_write_vcf_record() -> Result<(), Box<dyn std::error::Error>> {
+        let header = vcf::Header::builder()
+            .add_contig(vcf::header::Contig::new("sq0"))
+            .build();
+
+        let string_map = StringMap::default();
+
+        let record = vcf::Record::builder()
+            .set_chromosome("sq0".parse()?)
+            .set_position(vcf::record::Position::try_from(1)?)
+            .set_reference_bases("A".parse()?)
+            .build()?;
+
+        let mut buf = Vec::new();
+        write_vcf_record(&mut buf, &header, &string_map, &record)?;
+
+        let expected = [
+            0x1d, 0x00, 0x00, 0x00, // l_shared = 29
+            0x00, 0x00, 0x00, 0x00, // l_indiv = 0
+            0x00, 0x00, 0x00, 0x00, // chrom = 0,
+            0x00, 0x00, 0x00, 0x00, // pos = 0 (0-based)
+            0x01, 0x00, 0x00, 0x00, // rlen = 1
+            0x01, 0x00, 0x80, 0x7f, // qual = Float::Missing
+            0x00, 0x00, // n_info = 0
+            0x01, 0x00, // n_allele = 1
+            0x00, // n_fmt = 0
+            0x00, 0x00, 0x00, // n_sample = 0
+            0x07, // id = None
+            0x17, b'A', // ref = [A]
+            0x07, // alt = []
+            0x00, // filter = []
+        ];
+
+        assert_eq!(buf, expected);
+
+        Ok(())
+    }
+}
