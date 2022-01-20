71266ee7287fe0a21eeeba35c8809a026214ce8e
Michael Macias <zaeleus@gmail.com>
2022-01-20 13:44:56 -0600

    bgzf/async/block_codec: Use Buf to read u16

--- noodles-bgzf/src/async/block_codec.rs
+++ noodles-bgzf/src/async/block_codec.rs
@@ -1,7 +1,6 @@
 use std::io;
 
-use byteorder::{ByteOrder, LittleEndian};
-use bytes::{BufMut, BytesMut};
+use bytes::{Buf, BufMut, BytesMut};
 use tokio_util::codec::{Decoder, Encoder};
 
 use crate::{gz, BGZF_HEADER_SIZE};
@@ -21,9 +20,9 @@ impl Decoder for BlockCodec {
         }
 
         let block_size = {
-            let header = &src[..BGZF_HEADER_SIZE];
-            let bsize = LittleEndian::read_u16(&header[16..]);
-            usize::from(bsize) + 1
+            let mut header = &src[..BGZF_HEADER_SIZE];
+            header.advance(16); // [ID1, ..., SLEN]
+            usize::from(header.get_u16_le()) + 1
         };
 
         if src.len() < block_size {
