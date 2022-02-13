702525d03861a30c360ca1f8c3f595fb16ae3783
Michael Macias <zaeleus@gmail.com>
2022-02-13 11:52:19 -0600

    cram/codecs/aac: Add tests

--- noodles-cram/src/codecs/aac.rs
+++ noodles-cram/src/codecs/aac.rs
@@ -232,3 +232,110 @@ where
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_arith_decode_order_0() -> io::Result<()> {
+        let data = [
+            0x00, // flags = {empty}
+            0x07, // uncompressed len = 7
+            0x74, 0x00, 0xf4, 0xe5, 0xb7, 0x4e, 0x50, 0x0f, 0x2e, 0x97, 0x00,
+        ];
+
+        let mut reader = &data[..];
+        assert_eq!(arith_decode(&mut reader, 0)?, b"noodles");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_arith_decode_order_1() -> io::Result<()> {
+        let data = [
+            0x01, // flags = ORDER
+            0x07, // uncompressed len = 7
+            0x74, 0x00, 0xf4, 0xe3, 0x83, 0x41, 0xe2, 0x9a, 0xef, 0x53, 0x50, 0x00,
+        ];
+
+        let mut reader = &data[..];
+        assert_eq!(arith_decode(&mut reader, 0)?, b"noodles");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_arith_decode_stripe() -> io::Result<()> {
+        let data = [
+            0x08, // flags = STRIPE
+            0x07, // uncompressed len = 7
+            0x04, 0x09, 0x09, 0x09, 0x08, 0x00, 0x02, 0x6f, 0x00, 0xff, 0xa7, 0xab, 0x62, 0x00,
+            0x00, 0x02, 0x70, 0x00, 0xff, 0x84, 0x92, 0x1b, 0x00, 0x00, 0x02, 0x74, 0x00, 0xf7,
+            0x27, 0xdb, 0x24, 0x00, 0x00, 0x01, 0x65, 0x00, 0xfd, 0x77, 0x20, 0xb0, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ];
+
+        let mut reader = &data[..];
+        assert_eq!(arith_decode(&mut reader, 0)?, b"noodles");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_arith_decode_uncompressed() -> io::Result<()> {
+        let data = [
+            0x20, // flags = CAT
+            0x07, // uncompressed len = 7
+            0x6e, 0x6f, 0x6f, 0x64, 0x6c, 0x65, 0x73,
+        ];
+
+        let mut reader = &data[..];
+        assert_eq!(arith_decode(&mut reader, 0)?, b"noodles");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_arith_decode_rle_with_order_0() -> io::Result<()> {
+        let data = [
+            0x40, // flags = RLE
+            0x0d, // uncompressed len = 13
+            0x74, 0x00, 0xf3, 0x4b, 0x21, 0x10, 0xa8, 0xe3, 0x84, 0xfe, 0x6b, 0x22, 0x00,
+        ];
+
+        let mut reader = &data[..];
+        assert_eq!(arith_decode(&mut reader, 0)?, b"noooooooodles");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_arith_decode_rle_with_order_1() -> io::Result<()> {
+        let data = [
+            0x41, // flags = ORDER | RLE
+            0x0d, // uncompressed len = 13
+            0x74, 0x00, 0xf3, 0x4a, 0x89, 0x79, 0xc1, 0xe8, 0xc3, 0xc5, 0x62, 0x31, 0x00,
+        ];
+
+        let mut reader = &data[..];
+        assert_eq!(arith_decode(&mut reader, 0)?, b"noooooooodles");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_arith_decode_bit_packing_with_6_symbols() -> io::Result<()> {
+        let data = [
+            0x80, // flags = PACK
+            0x07, // uncompressed len = 7
+            0x06, 0x64, 0x65, 0x6c, 0x6e, 0x6f, 0x73, 0x04, 0x44, 0x00, 0xfc, 0x6e, 0x0c, 0xbf,
+            0x01, 0xf8, 0x00,
+        ];
+
+        let mut reader = &data[..];
+        assert_eq!(arith_decode(&mut reader, 0)?, b"noodles");
+
+        Ok(())
+    }
+}
