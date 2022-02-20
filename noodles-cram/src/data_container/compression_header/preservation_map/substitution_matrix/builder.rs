333817e6a09bc3c44ee1cf4f15ca5a0bb44a1ab3
Michael Macias <zaeleus@gmail.com>
2022-02-20 11:57:47 -0600

    cram/data_container/compression_header/preservation_map/substitution_matrix/builder: Read read base from read bases

--- noodles-cram/src/data_container/compression_header/preservation_map/substitution_matrix/builder.rs
+++ noodles-cram/src/data_container/compression_header/preservation_map/substitution_matrix/builder.rs
@@ -4,19 +4,29 @@ use super::{Base, Histogram, SubstitutionMatrix};
 
 #[derive(Debug, Default)]
 pub struct Builder {
-    substitution_matrix: SubstitutionMatrix,
     histogram: Histogram,
 }
 
 impl Builder {
     pub fn update(&mut self, reference_sequence: &[u8], record: &Record) {
+        let alignment_start = match record.alignment_start() {
+            Some(position) => i32::from(position) - 1,
+            None => return,
+        };
+
+        let read_bases = record.bases();
+
         for feature in record.features() {
-            if let Feature::Substitution(pos, code) = feature {
-                // FIXME: pos = 1-based, position = 0-based
-                let reference_position = (pos - 1) as usize;
-                let base = reference_sequence[reference_position] as char;
+            if let Feature::Substitution(pos, _) = feature {
+                let read_pos = (pos - 1) as usize;
+                let reference_pos = alignment_start as usize + read_pos;
+
+                let base = char::from(reference_sequence[reference_pos]);
                 let reference_base = Base::try_from(base).unwrap_or_default();
-                let read_base = self.substitution_matrix.get(reference_base, *code);
+
+                let base = char::from(read_bases[read_pos]);
+                let read_base = Base::try_from(base).unwrap_or_default();
+
                 self.histogram.hit(reference_base, read_base);
             }
         }
@@ -29,19 +39,26 @@ impl Builder {
 
 #[cfg(test)]
 mod tests {
+    use noodles_sam as sam;
+
     use super::*;
 
     #[test]
-    fn test_build() {
+    fn test_build() -> Result<(), sam::record::position::TryFromIntError> {
         let reference_sequence = b"ACAGGAATAANNNNNN";
+        let bases = b"TCTGGCGTGT";
 
-        let mut record = Record::default();
-        record.add_feature(Feature::Substitution(1, 2)); // A => T
-        record.add_feature(Feature::Substitution(3, 2)); // A => T
-        record.add_feature(Feature::Substitution(6, 0)); // A => C
-        record.add_feature(Feature::Substitution(7, 1)); // A => G
-        record.add_feature(Feature::Substitution(9, 1)); // A => G
-        record.add_feature(Feature::Substitution(10, 2)); // A => T
+        let record = Record::builder()
+            .set_alignment_start(sam::record::Position::try_from(1)?)
+            .set_read_length(bases.len())
+            .set_bases(bases.to_vec())
+            .add_feature(Feature::Substitution(1, 2)) // A => T
+            .add_feature(Feature::Substitution(3, 2)) // A => T
+            .add_feature(Feature::Substitution(6, 0)) // A => C
+            .add_feature(Feature::Substitution(7, 1)) // A => G
+            .add_feature(Feature::Substitution(9, 1)) // A => G
+            .add_feature(Feature::Substitution(10, 2)) // A => T
+            .build();
 
         let mut builder = Builder::default();
         builder.update(reference_sequence, &record);
@@ -57,5 +74,7 @@ mod tests {
                 [Base::A, Base::C, Base::G, Base::T],
             ]
         );
+
+        Ok(())
     }
 }
