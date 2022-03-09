f450998d572785a40eec04a5aca17d8e76921223
Michael Macias <zaeleus@gmail.com>
2022-03-09 13:34:11 -0600

    cram/data_container/compression_header/preservation_map/substitution_matrix/base: Add conversions to and from sam::record::sequence::Base

--- noodles-cram/src/data_container/compression_header/preservation_map/substitution_matrix/base.rs
+++ noodles-cram/src/data_container/compression_header/preservation_map/substitution_matrix/base.rs
@@ -1,5 +1,7 @@
 use std::{error, fmt};
 
+use noodles_sam as sam;
+
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub enum Base {
     A,
@@ -16,22 +18,35 @@ impl Default for Base {
 }
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct TryFromIntError(u8);
+pub struct TryFromError;
 
-impl fmt::Display for TryFromIntError {
+impl fmt::Display for TryFromError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "invalid substitution matrix base: expected {{A, C, G, T, N}}, got {:#04x}",
-            self.0
-        )
+        write!(f, "invalid substitution matrix base",)
     }
 }
 
-impl error::Error for TryFromIntError {}
+impl error::Error for TryFromError {}
+
+impl TryFrom<sam::record::sequence::Base> for Base {
+    type Error = TryFromError;
+
+    fn try_from(base: sam::record::sequence::Base) -> Result<Self, Self::Error> {
+        use sam::record::sequence::Base as SamBase;
+
+        match base {
+            SamBase::A => Ok(Self::A),
+            SamBase::C => Ok(Self::C),
+            SamBase::G => Ok(Self::G),
+            SamBase::T => Ok(Self::T),
+            SamBase::N => Ok(Self::N),
+            _ => Err(TryFromError),
+        }
+    }
+}
 
 impl TryFrom<u8> for Base {
-    type Error = TryFromIntError;
+    type Error = TryFromError;
 
     fn try_from(n: u8) -> Result<Self, Self::Error> {
         match n {
@@ -40,7 +55,21 @@ impl TryFrom<u8> for Base {
             b'G' => Ok(Self::G),
             b'T' => Ok(Self::T),
             b'N' => Ok(Self::N),
-            _ => Err(TryFromIntError(n)),
+            _ => Err(TryFromError),
+        }
+    }
+}
+
+impl From<Base> for sam::record::sequence::Base {
+    fn from(base: Base) -> Self {
+        use sam::record::sequence::Base as SamBase;
+
+        match base {
+            Base::A => SamBase::A,
+            Base::C => SamBase::C,
+            Base::G => SamBase::G,
+            Base::T => SamBase::T,
+            Base::N => SamBase::N,
         }
     }
 }
@@ -59,11 +88,23 @@ impl From<Base> for u8 {
 
 #[cfg(test)]
 mod tests {
+    use sam::record::sequence::Base as SamBase;
+
     use super::*;
 
     #[test]
     fn test_default() {
         assert_eq!(Base::default(), Base::N);
+    }
+
+    #[test]
+    fn test_try_from_sam_record_sequence_base_for_base() {
+        assert_eq!(Base::try_from(SamBase::A), Ok(Base::A));
+        assert_eq!(Base::try_from(SamBase::C), Ok(Base::C));
+        assert_eq!(Base::try_from(SamBase::G), Ok(Base::G));
+        assert_eq!(Base::try_from(SamBase::T), Ok(Base::T));
+        assert_eq!(Base::try_from(SamBase::N), Ok(Base::N));
+        assert_eq!(Base::try_from(SamBase::U), Err(TryFromError));
     }
 
     #[test]
@@ -73,7 +114,16 @@ mod tests {
         assert_eq!(Base::try_from(b'G'), Ok(Base::G));
         assert_eq!(Base::try_from(b'T'), Ok(Base::T));
         assert_eq!(Base::try_from(b'N'), Ok(Base::N));
-        assert_eq!(Base::try_from(b'U'), Err(TryFromIntError(b'U')));
+        assert_eq!(Base::try_from(b'U'), Err(TryFromError));
+    }
+
+    #[test]
+    fn test_from_base_for_sam_record_sequence_base() {
+        assert_eq!(SamBase::from(Base::A), SamBase::A);
+        assert_eq!(SamBase::from(Base::C), SamBase::C);
+        assert_eq!(SamBase::from(Base::G), SamBase::G);
+        assert_eq!(SamBase::from(Base::T), SamBase::T);
+        assert_eq!(SamBase::from(Base::N), SamBase::N);
     }
 
     #[test]
