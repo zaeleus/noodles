951683c1f8f74b5066a35db5d5f48231ae43065f
Michael Macias <zaeleus@gmail.com>
2022-03-18 15:44:54 -0500

    util/examples/alignment_rewrite: Add optional FASTA source parameter

--- noodles-util/examples/util_alignment_rewrite.rs
+++ noodles-util/examples/util_alignment_rewrite.rs
@@ -2,8 +2,15 @@
 //!
 //! The output format is determined from the extension of the destination.
 
-use std::{env, fs::File, io, path::Path};
+use std::{
+    env,
+    ffi::{OsStr, OsString},
+    fs::File,
+    io::{self, BufReader, BufWriter},
+    path::{Path, PathBuf},
+};
 
+use noodles_fasta::{self as fasta, fai, repository::adapters::IndexedReader};
 use noodles_util::alignment::{self, Format};
 
 fn detect_format_from_extension<P>(path: P) -> Option<Format>
@@ -18,22 +25,57 @@ where
     }
 }
 
+fn push_ext<S>(path: PathBuf, ext: S) -> PathBuf
+where
+    S: AsRef<OsStr>,
+{
+    let mut s = OsString::from(path);
+    s.push(".");
+    s.push(ext);
+    PathBuf::from(s)
+}
+
+fn create_reference_sequence_repository<P>(src: P) -> io::Result<fasta::Repository>
+where
+    P: AsRef<Path>,
+{
+    let src = src.as_ref();
+
+    let reader = File::open(src)
+        .map(BufReader::new)
+        .map(fasta::Reader::new)?;
+
+    let fai_src = push_ext(src.to_path_buf(), "fai");
+    let index = fai::read(fai_src)?;
+
+    let adapter = IndexedReader::new(reader, index);
+
+    Ok(fasta::Repository::new(adapter))
+}
+
 fn main() -> io::Result<()> {
     let mut args = env::args().skip(1);
 
     let src = args.next().expect("missing src");
     let dst = args.next().expect("missing dst");
+    let fasta_src = args.next();
 
     let mut reader = File::open(src).and_then(|f| alignment::Reader::builder(f).build())?;
     let header = reader.read_header()?;
 
-    let output_format = detect_format_from_extension(&dst).expect("invalid dst extension");
+    let mut builder = File::create(&dst)
+        .map(BufWriter::new)
+        .map(alignment::Writer::builder)?;
 
-    let mut writer = File::create(dst).map(|f| {
-        alignment::Writer::builder(f)
-            .set_format(output_format)
-            .build()
-    })?;
+    let output_format = detect_format_from_extension(dst).expect("invalid dst extension");
+    builder = builder.set_format(output_format);
+
+    if let Some(fasta_src) = fasta_src {
+        let repository = create_reference_sequence_repository(fasta_src)?;
+        builder = builder.set_reference_sequence_repository(repository);
+    }
+
+    let mut writer = builder.build();
 
     writer.write_header(&header)?;
 
