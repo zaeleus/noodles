5ca95f3bbe9e2e75dd06898a89bac1e8e47f3c07
Michael Macias <zaeleus@gmail.com>
2022-03-21 11:41:06 -0500

    cram/record/features/with_positions: Skip quality score features

--- noodles-cram/src/record/features/with_positions.rs
+++ noodles-cram/src/record/features/with_positions.rs
@@ -38,47 +38,79 @@ where
     type Item = ((Position, Position), I::Item);
 
     fn next(&mut self) -> Option<Self::Item> {
-        let feature = self.iter.next()?;
+        loop {
+            let feature = self.iter.next()?;
 
-        let feature_position = usize::from(feature.position());
-        let match_len = feature_position - usize::from(self.read_position);
+            let (reference_position_delta, read_position_delta) = match feature {
+                Feature::Bases(_, bases) => (bases.len(), bases.len()),
+                Feature::Scores(..) => continue,
+                Feature::ReadBase(..) => (1, 1),
+                Feature::Substitution(..) => (1, 1),
+                Feature::Insertion(_, bases) => (0, bases.len()),
+                Feature::Deletion(_, len) => (*len as usize, 0),
+                Feature::InsertBase(..) => (0, 1),
+                Feature::QualityScore(..) => continue,
+                Feature::ReferenceSkip(_, len) => (*len as usize, 0),
+                Feature::SoftClip(_, bases) => (0, bases.len()),
+                Feature::Padding(..) => (0, 0),
+                Feature::HardClip(..) => (0, 0),
+            };
 
-        self.reference_position = self
-            .reference_position
-            .checked_add(match_len)
-            .expect("attempt to add with overflow");
+            let feature_position = usize::from(feature.position());
+            let match_len = feature_position - usize::from(self.read_position);
 
-        self.read_position = self
-            .read_position
-            .checked_add(match_len)
-            .expect("attempt to add with overflow");
+            self.reference_position = self
+                .reference_position
+                .checked_add(match_len)
+                .expect("attempt to add with overflow");
 
-        let (reference_position_delta, read_position_delta) = match feature {
-            Feature::Bases(_, bases) => (bases.len(), bases.len()),
-            Feature::ReadBase(..) => (1, 1),
-            Feature::Substitution(..) => (1, 1),
-            Feature::Insertion(_, bases) => (0, bases.len()),
-            Feature::Deletion(_, len) => (*len as usize, 0),
-            Feature::InsertBase(..) => (0, 1),
-            Feature::ReferenceSkip(_, len) => (*len as usize, 0),
-            Feature::SoftClip(_, bases) => (0, bases.len()),
-            Feature::Padding(..) => (0, 0),
-            Feature::HardClip(..) => (0, 0),
-            _ => todo!("unhandled feature: {:?}", feature),
-        };
+            self.read_position = self
+                .read_position
+                .checked_add(match_len)
+                .expect("attempt to add with overflow");
 
-        let positions = self.positions();
+            let positions = self.positions();
 
-        self.reference_position = self
-            .reference_position
-            .checked_add(reference_position_delta)
-            .expect("attempt to add with overflow");
+            self.reference_position = self
+                .reference_position
+                .checked_add(reference_position_delta)
+                .expect("attempt to add with overflow");
 
-        self.read_position = self
-            .read_position
-            .checked_add(read_position_delta)
-            .expect("attempt to add with overflow");
+            self.read_position = self
+                .read_position
+                .checked_add(read_position_delta)
+                .expect("attempt to add with overflow");
 
-        Some((positions, feature))
+            return Some((positions, feature));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_next() -> Result<(), Box<dyn std::error::Error>> {
+        use crate::record::Features;
+        use noodles_sam::record::{quality_scores::Score, sequence::Base};
+
+        let features = Features::from(vec![
+            Feature::Bases(Position::MIN, vec![Base::A, Base::C]),
+            Feature::Scores(
+                Position::MIN,
+                vec![Score::try_from(0)?, Score::try_from(0)?],
+            ),
+        ]);
+
+        let mut iter = WithPositions::new(features.iter(), Position::MIN);
+
+        assert_eq!(
+            iter.next(),
+            Some(((Position::MIN, Position::MIN), &features[0]))
+        );
+        assert!(iter.next().is_none());
+
+        Ok(())
     }
 }
