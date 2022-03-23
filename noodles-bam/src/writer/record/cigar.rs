ace3d0d79680653f13e7efe2b5549bc1d13297ca
Michael Macias <zaeleus@gmail.com>
2022-03-23 16:34:59 -0500

    bam/writer/record/cigar: Add tests

--- noodles-bam/src/writer/record/cigar.rs
+++ noodles-bam/src/writer/record/cigar.rs
@@ -31,3 +31,46 @@ fn encode_op(op: Op) -> io::Result<u32> {
         ))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_put_cigar() -> Result<(), Box<dyn std::error::Error>> {
+        fn t(buf: &mut Vec<u8>, cigar: &sam::record::Cigar, expected: &[u8]) -> io::Result<()> {
+            buf.clear();
+            put_cigar(buf, cigar)?;
+            assert_eq!(buf, expected);
+            Ok(())
+        }
+
+        let mut buf = Vec::new();
+
+        t(&mut buf, &sam::record::Cigar::default(), &[])?;
+        t(&mut buf, &"4M".parse()?, &[0x40, 0x00, 0x00, 0x00])?;
+        t(
+            &mut buf,
+            &"4M2H".parse()?,
+            &[0x40, 0x00, 0x00, 0x00, 0x25, 0x00, 0x00, 0x00],
+        )?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_encode_op() -> io::Result<()> {
+        use sam::record::cigar::op::Kind;
+
+        let op = Op::new(Kind::Match, 1);
+        assert_eq!(encode_op(op)?, 0x10);
+
+        let op = Op::new(Kind::Match, 1 << 28);
+        assert!(matches!(
+            encode_op(op),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData,
+        ));
+
+        Ok(())
+    }
+}
