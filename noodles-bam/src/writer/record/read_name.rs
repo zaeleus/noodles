ddd818171deeaa6dd77b224342bf22f418e25794
Michael Macias <zaeleus@gmail.com>
2022-03-24 14:34:09 -0500

    bam/writer/record/read_name: Add tests

--- noodles-bam/src/writer/record/read_name.rs
+++ noodles-bam/src/writer/record/read_name.rs
@@ -17,3 +17,25 @@ where
 
     dst.put_u8(NUL);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_put_read_name() -> Result<(), sam::record::read_name::ParseError> {
+        fn t(buf: &mut Vec<u8>, read_name: Option<&sam::record::ReadName>, expected: &[u8]) {
+            buf.clear();
+            put_read_name(buf, read_name);
+            assert_eq!(buf, expected);
+        }
+
+        let mut buf = Vec::new();
+
+        t(&mut buf, None, &[b'*', 0x00]);
+        t(&mut buf, Some(&"r".parse()?), &[b'r', 0x00]);
+        t(&mut buf, Some(&"r0".parse()?), &[b'r', b'0', 0x00]);
+
+        Ok(())
+    }
+}
