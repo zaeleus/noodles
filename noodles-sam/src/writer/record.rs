dc0a79e6d36c45f8e528e4e0bf086865eb5a557f
Michael Macias <zaeleus@gmail.com>
2022-04-01 11:34:26 -0500

    sam/writer/record: Move sequence writer to a module

--- noodles-sam/src/writer/record.rs
+++ noodles-sam/src/writer/record.rs
@@ -1,29 +1,14 @@
 mod cigar;
 mod data;
+mod sequence;
 
-pub use self::{cigar::write_cigar, data::write_data};
+pub use self::{cigar::write_cigar, data::write_data, sequence::write_sequence};
 
 use std::io::{self, Write};
 
-use crate::record::{QualityScores, Sequence};
+use crate::record::QualityScores;
 
 const MISSING: u8 = b'*';
-
-pub(super) fn write_sequence<W>(writer: &mut W, sequence: &Sequence) -> io::Result<()>
-where
-    W: Write,
-{
-    if sequence.is_empty() {
-        writer.write_all(&[MISSING])?;
-    } else {
-        for &base in sequence.as_ref() {
-            let n = u8::from(base);
-            writer.write_all(&[n])?;
-        }
-    }
-
-    Ok(())
-}
 
 pub(super) fn write_quality_scores<W>(
     writer: &mut W,
@@ -49,22 +34,6 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_write_sequence() -> io::Result<()> {
-        use crate::record::sequence::Base;
-
-        let mut buf = Vec::new();
-        write_sequence(&mut buf, &Sequence::default())?;
-        assert_eq!(buf, b"*");
-
-        buf.clear();
-        let sequence = Sequence::from(vec![Base::A, Base::C, Base::G, Base::T]);
-        write_sequence(&mut buf, &sequence)?;
-        assert_eq!(buf, b"ACGT");
-
-        Ok(())
-    }
 
     #[test]
     fn test_write_quality_scores() -> Result<(), Box<dyn std::error::Error>> {
