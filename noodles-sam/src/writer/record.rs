b5a480ab6f98780d6908b5868a9e16ecd206dd65
Michael Macias <zaeleus@gmail.com>
2022-04-01 11:36:58 -0500

    sam/writer/record: Move quality scores writer to a module

--- noodles-sam/src/writer/record.rs
+++ noodles-sam/src/writer/record.rs
@@ -1,51 +1,11 @@
 mod cigar;
 mod data;
+mod quality_scores;
 mod sequence;
 
-pub use self::{cigar::write_cigar, data::write_data, sequence::write_sequence};
-
-use std::io::{self, Write};
-
-use crate::record::QualityScores;
+pub use self::{
+    cigar::write_cigar, data::write_data, quality_scores::write_quality_scores,
+    sequence::write_sequence,
+};
 
 const MISSING: u8 = b'*';
-
-pub(super) fn write_quality_scores<W>(
-    writer: &mut W,
-    quality_scores: &QualityScores,
-) -> io::Result<()>
-where
-    W: Write,
-{
-    const MIN_VALUE: u8 = b'!';
-
-    if quality_scores.is_empty() {
-        writer.write_all(&[MISSING])?;
-    } else {
-        for &score in quality_scores.as_ref() {
-            let n = u8::from(score) + MIN_VALUE;
-            writer.write_all(&[n])?;
-        }
-    }
-
-    Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_write_quality_scores() -> Result<(), Box<dyn std::error::Error>> {
-        let mut buf = Vec::new();
-        write_quality_scores(&mut buf, &QualityScores::default())?;
-        assert_eq!(buf, b"*");
-
-        buf.clear();
-        let quality_scores = "NDLS".parse()?;
-        write_quality_scores(&mut buf, &quality_scores)?;
-        assert_eq!(buf, b"NDLS");
-
-        Ok(())
-    }
-}
