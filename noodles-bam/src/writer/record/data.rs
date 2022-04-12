fab4a2c11b83685bcb3b316d8642c407818dc0e6
Michael Macias <zaeleus@gmail.com>
2022-04-12 10:35:18 -0500

    bam/writer/record/data: Add tests

--- noodles-bam/src/writer/record/data.rs
+++ noodles-bam/src/writer/record/data.rs
@@ -17,3 +17,33 @@ where
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_put_data() -> Result<(), Box<dyn std::error::Error>> {
+        fn t(buf: &mut Vec<u8>, data: &Data, expected: &[u8]) -> io::Result<()> {
+            buf.clear();
+            put_data(buf, data)?;
+            assert_eq!(buf, expected);
+            Ok(())
+        }
+
+        let mut buf = Vec::new();
+
+        t(&mut buf, &Data::default(), &[])?;
+        t(&mut buf, &"NH:i:1".parse()?, &[b'N', b'H', b'C', 0x01])?;
+        t(
+            &mut buf,
+            &"NH:i:1\tRG:Z:rg0".parse()?,
+            &[
+                b'N', b'H', b'C', 0x01, // NH:C:0
+                b'R', b'G', b'Z', b'r', b'g', b'0', 0x00, // RG:Z:rg0
+            ],
+        )?;
+
+        Ok(())
+    }
+}
