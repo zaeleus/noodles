2250337f145715fb5c756becf9d6f08954db4fdc
Michael Macias <zaeleus@gmail.com>
2022-05-02 13:38:31 -0500

    bam/writer/record/mapping_quality: Add tests

--- noodles-bam/src/writer/record/mapping_quality.rs
+++ noodles-bam/src/writer/record/mapping_quality.rs
@@ -9,3 +9,22 @@ where
     let mapq = mapping_quality.map(u8::from).unwrap_or(MISSING);
     dst.put_u8(mapq);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_put_mapping_quality() {
+        fn t(buf: &mut Vec<u8>, mapping_quality: Option<MappingQuality>, expected: &[u8]) {
+            buf.clear();
+            put_mapping_quality(buf, mapping_quality);
+            assert_eq!(buf, expected);
+        }
+
+        let mut buf = Vec::new();
+
+        t(&mut buf, None, &[0xff]);
+        t(&mut buf, MappingQuality::new(8), &[0x08]);
+    }
+}
