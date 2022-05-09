23067949d0cd5ad1ae78769e5fec80ea100309ed
Michael Macias <zaeleus@gmail.com>
2022-05-09 15:52:32 -0500

    bam/reader/record/mapping_quality: Avoid double-checking the missing value

--- noodles-bam/src/reader/record/mapping_quality.rs
+++ noodles-bam/src/reader/record/mapping_quality.rs
@@ -1,22 +1,17 @@
 use std::{io, mem};
 
 use bytes::Buf;
-use noodles_sam::{self as sam, record::MappingQuality};
+use noodles_sam::record::MappingQuality;
 
 pub fn get_mapping_quality<B>(src: &mut B) -> io::Result<Option<MappingQuality>>
 where
     B: Buf,
 {
-    use sam::record::mapping_quality::MISSING;
-
     if src.remaining() < mem::size_of::<u8>() {
         return Err(io::Error::from(io::ErrorKind::UnexpectedEof));
     }
 
-    match src.get_u8() {
-        MISSING => Ok(None),
-        n => Ok(MappingQuality::new(n)),
-    }
+    Ok(MappingQuality::new(src.get_u8()))
 }
 
 #[cfg(test)]
