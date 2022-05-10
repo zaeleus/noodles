70779ffa9c4fc47b7483ff0066a577324fbe3ac0
Michael Macias <zaeleus@gmail.com>
2022-05-10 10:05:28 -0500

    bam/examples/view: Use SAM writer

--- noodles-bam/examples/bam_view.rs
+++ noodles-bam/examples/bam_view.rs
@@ -2,10 +2,10 @@
 //!
 //! The result matches the output of `samtools view <src>`.
 
-use std::{env, fs::File};
+use std::{env, fs::File, io};
 
 use noodles_bam as bam;
-use noodles_sam as sam;
+use noodles_sam::{self as sam, AlignmentWriter};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let src = env::args().nth(1).expect("missing src");
@@ -14,11 +14,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let header: sam::Header = reader.read_header()?.parse()?;
     reader.read_reference_sequences()?;
 
+    let stdout = io::stdout();
+    let handle = stdout.lock();
+    let mut writer = sam::Writer::new(handle);
+
     for result in reader.records() {
         let record = result?;
-        let sam_record = record.try_into_sam_record(header.reference_sequences())?;
-        println!("{}", sam_record);
+        writer.write_alignment_record(&header, &record)?;
     }
+
+    writer.finish(&header)?;
 
     Ok(())
 }
