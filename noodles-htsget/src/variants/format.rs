948c0a33d54f7309cc4851f36cf58010323f2f75
Michael Macias <zaeleus@gmail.com>
2022-05-20 15:11:21 -0500

    htsget/variants/format: Add tests

--- noodles-htsget/src/variants/format.rs
+++ noodles-htsget/src/variants/format.rs
@@ -15,3 +15,14 @@ impl From<Format> for crate::Format {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_format_for_crate_format() {
+        assert_eq!(crate::Format::from(Format::Vcf), crate::Format::Vcf);
+        assert_eq!(crate::Format::from(Format::Bcf), crate::Format::Bcf);
+    }
+}
