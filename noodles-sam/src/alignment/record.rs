3afd1d2354cbae3bbd06c6dfd86b5e7d64646fe2
Michael Macias <zaeleus@gmail.com>
2022-05-29 20:41:28 -0500

    sam/alignment: Implement AlignmentRecord

--- noodles-sam/src/alignment/record.rs
+++ noodles-sam/src/alignment/record.rs
@@ -1,6 +1,12 @@
+use std::io;
+
 use noodles_core::Position;
 
-use crate::record::{Cigar, Data, Flags, MappingQuality, QualityScores, ReadName, Sequence};
+use crate::{
+    header::{ReferenceSequence, ReferenceSequences},
+    record::{Cigar, Data, Flags, MappingQuality, QualityScores, ReadName, Sequence},
+    AlignmentRecord,
+};
 
 /// An alignment record.
 #[derive(Clone, Debug, PartialEq)]
@@ -20,19 +26,9 @@ pub struct Record {
 }
 
 impl Record {
-    /// Returns the read name.
-    pub fn read_name(&self) -> Option<&ReadName> {
-        self.read_name.as_ref()
-    }
-
     /// Returns a mutable reference to the read name.
     pub fn read_name_mut(&mut self) -> &mut Option<ReadName> {
         &mut self.read_name
-    }
-
-    /// Returns the flags.
-    pub fn flags(&self) -> Flags {
-        self.flags
     }
 
     /// Returns a mutable reference to the flags.
@@ -50,29 +46,14 @@ impl Record {
         &mut self.reference_sequence_id
     }
 
-    /// Returns the alignment start.
-    pub fn alignment_start(&self) -> Option<Position> {
-        self.alignment_start
-    }
-
     /// Returns a mutable reference to the alignment start.
     pub fn alignment_start_mut(&mut self) -> &mut Option<Position> {
         &mut self.alignment_start
     }
 
-    /// Returns the mapping quality.
-    pub fn mapping_quality(&self) -> Option<MappingQuality> {
-        self.mapping_quality
-    }
-
     /// Returns a mutable reference to the mapping quality.
     pub fn mapping_quality_mut(&mut self) -> &mut Option<MappingQuality> {
         &mut self.mapping_quality
-    }
-
-    /// Returns the CIGAR operations.
-    pub fn cigar(&self) -> &Cigar {
-        &self.cigar
     }
 
     /// Returns a mutable reference to the CIGAR operations.
@@ -90,19 +71,9 @@ impl Record {
         &mut self.mate_reference_sequence_id
     }
 
-    /// Returns the mate alignment start.
-    pub fn mate_alignment_start(&self) -> Option<Position> {
-        self.mate_alignment_start
-    }
-
     /// Returns a mutable reference to the mate alignment start.
     pub fn mate_alignment_start_mut(&mut self) -> &mut Option<Position> {
         &mut self.mate_alignment_start
-    }
-
-    /// Returns the template length.
-    pub fn template_length(&self) -> i32 {
-        self.template_length
     }
 
     /// Returns a mutable reference to template length.
@@ -110,19 +81,9 @@ impl Record {
         &mut self.template_length
     }
 
-    /// Returns the sequence.
-    pub fn sequence(&self) -> &Sequence {
-        &self.sequence
-    }
-
     /// Returns a mutable reference to sequence.
     pub fn sequence_mut(&mut self) -> &mut Sequence {
         &mut self.sequence
-    }
-
-    /// Returns the quality scores.
-    pub fn quality_scores(&self) -> &QualityScores {
-        &self.quality_scores
     }
 
     /// Returns a mutable reference to quality scores.
@@ -130,13 +91,82 @@ impl Record {
         &mut self.quality_scores
     }
 
-    /// Returns the data.
-    pub fn data(&self) -> &Data {
-        &self.data
-    }
-
     /// Returns a mutable reference to the data.
     pub fn data_mut(&mut self) -> &mut Data {
         &mut self.data
     }
+}
+
+impl AlignmentRecord for Record {
+    fn read_name(&self) -> Option<&ReadName> {
+        self.read_name.as_ref()
+    }
+
+    fn reference_sequence<'rs>(
+        &self,
+        reference_sequences: &'rs ReferenceSequences,
+    ) -> Option<std::io::Result<&'rs ReferenceSequence>> {
+        get_reference_sequence(reference_sequences, self.reference_sequence_id())
+    }
+
+    fn flags(&self) -> Flags {
+        self.flags
+    }
+
+    fn alignment_start(&self) -> Option<Position> {
+        self.alignment_start
+    }
+
+    fn alignment_span(&self) -> usize {
+        self.cigar().reference_len()
+    }
+
+    fn mapping_quality(&self) -> Option<MappingQuality> {
+        self.mapping_quality
+    }
+
+    fn cigar(&self) -> &Cigar {
+        &self.cigar
+    }
+
+    fn mate_reference_sequence<'rs>(
+        &self,
+        reference_sequences: &'rs ReferenceSequences,
+    ) -> Option<std::io::Result<&'rs ReferenceSequence>> {
+        get_reference_sequence(reference_sequences, self.mate_reference_sequence_id())
+    }
+
+    fn mate_alignment_start(&self) -> Option<Position> {
+        self.mate_alignment_start
+    }
+
+    fn template_length(&self) -> i32 {
+        self.template_length
+    }
+
+    fn sequence(&self) -> &Sequence {
+        &self.sequence
+    }
+
+    fn quality_scores(&self) -> &QualityScores {
+        &self.quality_scores
+    }
+
+    fn data(&self) -> &Data {
+        &self.data
+    }
+}
+
+fn get_reference_sequence(
+    reference_sequences: &ReferenceSequences,
+    reference_sequence_id: Option<usize>,
+) -> Option<io::Result<&ReferenceSequence>> {
+    reference_sequence_id.map(|id| {
+        reference_sequences
+            .get_index(id)
+            .map(|(_, rs)| rs)
+            .ok_or_else(|| {
+                io::Error::new(io::ErrorKind::InvalidData, "invalid reference sequence ID")
+            })
+    })
 }
