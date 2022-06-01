67f90d106535323fd8b8da18aad834b8aecfd674
Michael Macias <zaeleus@gmail.com>
2022-06-01 09:45:47 -0500

    bam/async/reader/record: Extract block size reader

--- noodles-bam/src/async/reader/record.rs
+++ noodles-bam/src/async/reader/record.rs
@@ -14,10 +14,9 @@ where
 {
     use crate::reader::record::decode_record;
 
-    let block_size = match reader.read_u32_le().await {
-        Ok(bs) => usize::try_from(bs).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?,
-        Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => return Ok(0),
-        Err(e) => return Err(e),
+    let block_size = match read_block_size(reader).await? {
+        0 => return Ok(0),
+        n => n,
     };
 
     buf.resize(block_size, Default::default());
@@ -28,9 +27,33 @@ where
     Ok(block_size)
 }
 
+async fn read_block_size<R>(reader: &mut R) -> io::Result<usize>
+where
+    R: AsyncRead + Unpin,
+{
+    match reader.read_u32_le().await {
+        Ok(bs) => usize::try_from(bs).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e)),
+        Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => Ok(0),
+        Err(e) => Err(e),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[tokio::test]
+    async fn test_read_block_size() -> io::Result<()> {
+        let data = [0x08, 0x00, 0x00, 0x00];
+        let mut reader = &data[..];
+        assert_eq!(read_block_size(&mut reader).await?, 8);
+
+        let data = [];
+        let mut reader = &data[..];
+        assert_eq!(read_block_size(&mut reader).await?, 0);
+
+        Ok(())
+    }
 
     #[tokio::test]
     async fn test_read_record() -> io::Result<()> {
