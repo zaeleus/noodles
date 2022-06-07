08ec7701e59f4231b21b840a1aabbfadbae69a9f
Michael Macias <zaeleus@gmail.com>
2022-06-07 10:10:29 -0500

    core/region/interval: Represent endpoints with Option
    `Some` is included; and `None`, unbounded.

--- noodles-core/src/region/interval.rs
+++ noodles-core/src/region/interval.rs
@@ -2,17 +2,11 @@
 
 use std::{
     error, fmt,
-    ops::{RangeBounds, RangeFrom, RangeFull, RangeInclusive, RangeToInclusive},
+    ops::{Bound, RangeBounds, RangeFrom, RangeFull, RangeInclusive, RangeToInclusive},
     str::FromStr,
 };
 
 use crate::{position, Position};
-
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-enum Bound {
-    Included(Position),
-    Unbounded,
-}
 
 /// An interval.
 ///
@@ -20,8 +14,8 @@ enum Bound {
 /// and right-closed ((-∞, b]), or unbounded ((-∞, ∞)).
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct Interval {
-    start: Bound,
-    end: Bound,
+    start: Option<Position>,
+    end: Option<Position>,
 }
 
 impl Interval {
@@ -30,21 +24,20 @@ impl Interval {
     /// # Examples
     ///
     /// ```
-    /// use std::ops::{Bound, RangeBounds};
     /// use noodles_core::{region::Interval, Position};
     ///
     /// let start = Position::try_from(8)?;
     /// let end = Position::try_from(13)?;
     ///
     /// let interval = Interval::new(start, end);
-    /// assert_eq!(interval.start_bound().cloned(), Bound::Included(start));
-    /// assert_eq!(interval.end_bound().cloned(), Bound::Included(end));
+    /// assert_eq!(interval.start(), Some(start));
+    /// assert_eq!(interval.end(), Some(end));
     /// # Ok::<_, noodles_core::position::TryFromIntError>(())
     /// ```
     pub fn new(start: Position, end: Position) -> Self {
         Self {
-            start: Bound::Included(start),
-            end: Bound::Included(end),
+            start: Some(start),
+            end: Some(end),
         }
     }
 
@@ -65,10 +58,7 @@ impl Interval {
     /// # Ok::<_, noodles_core::position::TryFromIntError>(())
     /// ```
     pub fn start(&self) -> Option<Position> {
-        match self.start {
-            Bound::Included(start) => Some(start),
-            Bound::Unbounded => None,
-        }
+        self.start
     }
 
     /// Returns the end.
@@ -88,10 +78,7 @@ impl Interval {
     /// # Ok::<_, noodles_core::position::TryFromIntError>(())
     /// ```
     pub fn end(&self) -> Option<Position> {
-        match self.end {
-            Bound::Included(end) => Some(end),
-            Bound::Unbounded => None,
-        }
+        self.end
     }
 
     /// Returns whether the given interval intersects this interval.
@@ -127,28 +114,27 @@ impl Interval {
 impl fmt::Display for Interval {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match (self.start, self.end) {
-            (Bound::Unbounded, Bound::Unbounded) => Ok(()),
-            (Bound::Unbounded, Bound::Included(e)) => write!(f, "{}-{}", Position::MIN, e),
-            (Bound::Included(s), Bound::Unbounded) => s.fmt(f),
-            (Bound::Included(s), Bound::Included(e)) => write!(f, "{}-{}", s, e),
+            (Some(s), Some(e)) => write!(f, "{}-{}", s, e),
+            (Some(s), None) => s.fmt(f),
+            (None, Some(e)) => write!(f, "{}-{}", Position::MIN, e),
+            (None, None) => Ok(()),
         }
     }
 }
 
 impl RangeBounds<Position> for Interval {
-    fn start_bound(&self) -> std::ops::Bound<&Position> {
-        bound_to_std_ops_bound(&self.start)
+    fn start_bound(&self) -> Bound<&Position> {
+        self.start
+            .as_ref()
+            .map(Bound::Included)
+            .unwrap_or(Bound::Unbounded)
     }
 
-    fn end_bound(&self) -> std::ops::Bound<&Position> {
-        bound_to_std_ops_bound(&self.end)
-    }
-}
-
-fn bound_to_std_ops_bound(bound: &Bound) -> std::ops::Bound<&Position> {
-    match bound {
-        Bound::Included(ref value) => std::ops::Bound::Included(value),
-        Bound::Unbounded => std::ops::Bound::Unbounded,
+    fn end_bound(&self) -> Bound<&Position> {
+        self.end
+            .as_ref()
+            .map(Bound::Included)
+            .unwrap_or(Bound::Unbounded)
     }
 }
 
@@ -185,17 +171,17 @@ impl FromStr for Interval {
         let start = match components.next() {
             Some(t) => t
                 .parse()
-                .map(Bound::Included)
+                .map(Some)
                 .map_err(ParseError::InvalidStartPosition)?,
-            None => Bound::Unbounded,
+            None => None,
         };
 
         let end = match components.next() {
             Some(t) => t
                 .parse()
-                .map(Bound::Included)
+                .map(Some)
                 .map_err(ParseError::InvalidEndPosition)?,
-            None => Bound::Unbounded,
+            None => None,
         };
 
         Ok(Self { start, end })
@@ -205,8 +191,8 @@ impl FromStr for Interval {
 impl From<RangeFrom<Position>> for Interval {
     fn from(range: RangeFrom<Position>) -> Self {
         Self {
-            start: Bound::Included(range.start),
-            end: Bound::Unbounded,
+            start: Some(range.start),
+            end: None,
         }
     }
 }
@@ -214,8 +200,8 @@ impl From<RangeFrom<Position>> for Interval {
 impl From<RangeFull> for Interval {
     fn from(_: RangeFull) -> Self {
         Self {
-            start: Bound::Unbounded,
-            end: Bound::Unbounded,
+            start: None,
+            end: None,
         }
     }
 }
@@ -225,8 +211,8 @@ impl From<RangeInclusive<Position>> for Interval {
         let (start, end) = range.into_inner();
 
         Self {
-            start: Bound::Included(start),
-            end: Bound::Included(end),
+            start: Some(start),
+            end: Some(end),
         }
     }
 }
@@ -234,8 +220,8 @@ impl From<RangeInclusive<Position>> for Interval {
 impl From<RangeToInclusive<Position>> for Interval {
     fn from(range: RangeToInclusive<Position>) -> Self {
         Self {
-            start: Bound::Unbounded,
-            end: Bound::Included(range.end),
+            start: None,
+            end: Some(range.end),
         }
     }
 }
@@ -265,7 +251,7 @@ mod tests {
         // n                 [-> [9, ∞)
         // o <-]                 (-∞, 2]
 
-        let a = Interval::new(Position::try_from(5)?, Position::try_from(8)?);
+        let a = Interval::from(Position::try_from(5)?..=Position::try_from(8)?);
         assert!(a.intersects(a));
 
         let b = (Position::try_from(4)?..=Position::try_from(6)?).into();
@@ -320,25 +306,17 @@ mod tests {
         let start = Position::try_from(8)?;
         let end = Position::try_from(13)?;
 
-        let interval = Interval {
-            start: Bound::Unbounded,
-            end: Bound::Unbounded,
-        };
+        let interval = Interval::from(..);
         assert_eq!(interval.to_string(), "");
 
-        let interval = Interval {
-            start: Bound::Unbounded,
-            end: Bound::Included(end),
-        };
+        let interval = Interval::from(..=end);
         assert_eq!(interval.to_string(), "1-13");
 
-        let interval = Interval {
-            start: Bound::Included(start),
-            end: Bound::Unbounded,
-        };
+        let interval = Interval::from(start..);
         assert_eq!(interval.to_string(), "8");
 
-        assert_eq!(Interval::new(start, end).to_string(), "8-13");
+        let interval = Interval::from(start..=end);
+        assert_eq!(interval.to_string(), "8-13");
 
         Ok(())
     }
@@ -348,29 +326,9 @@ mod tests {
         let start = Position::try_from(8)?;
         let end = Position::try_from(13)?;
 
-        assert_eq!(
-            "".parse(),
-            Ok(Interval {
-                start: Bound::Unbounded,
-                end: Bound::Unbounded
-            })
-        );
-
-        assert_eq!(
-            "8".parse(),
-            Ok(Interval {
-                start: Bound::Included(start),
-                end: Bound::Unbounded
-            })
-        );
-
-        assert_eq!(
-            "8-13".parse(),
-            Ok(Interval {
-                start: Bound::Included(start),
-                end: Bound::Included(end),
-            })
-        );
+        assert_eq!("".parse(), Ok(Interval::from(..)));
+        assert_eq!("8".parse(), Ok(Interval::from(start..)));
+        assert_eq!("8-13".parse(), Ok(Interval::from(start..=end)));
 
         assert!(matches!(
             "x".parse::<Interval>(),
