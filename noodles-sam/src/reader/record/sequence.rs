6a95a09cb86c28323e2e9677f1e0bdc1a339d367
Michael Macias <zaeleus@gmail.com>
2022-06-23 12:02:27 -0500

    sam/reader/record/sequence: Read field from byte string

--- noodles-sam/src/reader/record/sequence.rs
+++ noodles-sam/src/reader/record/sequence.rs
@@ -1,17 +1,41 @@
-use std::{io, str};
+use std::io;
 
-use crate::record::Sequence;
+use crate::record::{sequence::Base, Sequence};
 
 pub(crate) fn parse_sequence(src: &[u8]) -> io::Result<Sequence> {
     const MISSING: &[u8] = b"*";
 
-    match src {
-        MISSING => Ok(Sequence::default()),
-        _ => str::from_utf8(src)
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-            .and_then(|s| {
-                s.parse()
-                    .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-            }),
+    if src == MISSING {
+        return Ok(Sequence::default());
+    }
+
+    src.iter()
+        .copied()
+        .map(Base::try_from)
+        .collect::<Result<Vec<_>, _>>()
+        .map(Sequence::from)
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_sequence() -> Result<(), Box<dyn std::error::Error>> {
+        let actual = parse_sequence(b"")?;
+        let expected = Sequence::default();
+        assert_eq!(actual, expected);
+
+        let actual = parse_sequence(b"ACGT")?;
+        let expected = Sequence::from(vec![Base::A, Base::C, Base::G, Base::T]);
+        assert_eq!(actual, expected);
+
+        assert!(matches!(
+            parse_sequence(&[0x07]),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+
+        Ok(())
     }
 }
