d266e03126a321c437211c94d0125aa9f22d364a
Michael Macias <zaeleus@gmail.com>
2022-07-08 10:32:40 -0500

    vcf/header/format: Add builder

---
+++ noodles-vcf/src/header/format/builder.rs
@@ -0,0 +1,68 @@
+use indexmap::IndexMap;
+
+use super::{Format, Key, Type};
+use crate::header::Number;
+
+#[derive(Default)]
+pub struct Builder {
+    id: Option<Key>,
+    number: Option<Number>,
+    ty: Option<Type>,
+    description: Option<String>,
+    idx: Option<usize>,
+    other_fields: IndexMap<String, String>,
+}
+
+impl Builder {
+    pub fn set_id(mut self, id: Key) -> Self {
+        self.id = Some(id);
+        self
+    }
+
+    pub fn set_number(mut self, number: Number) -> Self {
+        self.number = Some(number);
+        self
+    }
+
+    pub fn set_type(mut self, ty: Type) -> Self {
+        self.ty = Some(ty);
+        self
+    }
+
+    pub fn set_description<D>(mut self, description: D) -> Self
+    where
+        D: Into<String>,
+    {
+        self.description = Some(description.into());
+        self
+    }
+
+    pub fn set_idx(mut self, idx: usize) -> Self {
+        self.idx = Some(idx);
+        self
+    }
+
+    pub fn insert(mut self, key: String, value: String) -> Self {
+        self.other_fields.insert(key, value);
+        self
+    }
+
+    pub fn build(self) -> Result<Format, BuildError> {
+        use super::{DESCRIPTION, ID, NUMBER, TYPE};
+
+        Ok(Format {
+            id: self.id.ok_or(BuildError::MissingField(ID))?,
+            number: self.number.ok_or(BuildError::MissingField(NUMBER))?,
+            ty: self.ty.ok_or(BuildError::MissingField(TYPE))?,
+            description: self
+                .description
+                .ok_or(BuildError::MissingField(DESCRIPTION))?,
+            idx: self.idx,
+            fields: self.other_fields,
+        })
+    }
+}
+
+pub enum BuildError {
+    MissingField(&'static str),
+}
