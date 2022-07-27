357c579ef38e46d6489c687ce6fb889490e9b714
Marko Malenic <mmalenic1@gmail.com>
2022-07-27 15:56:37 +1000

    Add gzi reader tests.

--- noodles-bgzf/src/gzi.rs
+++ noodles-bgzf/src/gzi.rs
@@ -3,6 +3,7 @@
 mod reader;
 
 /// A GZI Index containing a number of entries, representing pairs of compressed and uncompressed offsets in a BGZF file.
+#[derive(Debug, PartialEq)]
 pub struct Index {
   number_entries: u64,
   offsets: Vec<(u64, u64)>
--- noodles-bgzf/src/gzi/reader.rs
+++ noodles-bgzf/src/gzi/reader.rs
@@ -30,7 +30,7 @@ impl<R> Reader<R>
         }
 
         if self.inner.read(&mut [0; 1])? > 0 {
-            Err(io::Error::new(io::ErrorKind::InvalidData, format!("Trailing data left in read stream, expected {} entries.", number_entries)))
+            Err(io::Error::new(io::ErrorKind::InvalidData, format!("Trailing data remaining in read stream, expected {} entries.", number_entries)))
         } else {
             Ok(Index { number_entries, offsets })
         }
@@ -45,4 +45,70 @@ fn read_little_endian<R>(reader: &mut R) -> io::Result<u64>
     let mut buf = [0; 8];
     reader.read_exact(&mut buf)?;
     Ok(u64::from_le_bytes(buf))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_index() -> Result<(), Box<dyn std::error::Error>> {
+        let data: &[u8] = &[
+            0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // number_entries = 2
+            0x3c, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // compressed_offset = 4668
+            0x2e, 0x53, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // uncompressed_offset = 21294
+            0x02, 0x5d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // compressed_offset = 23810
+            0x01, 0x52, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, // uncompressed_offset = 86529
+        ];
+
+        let mut reader = Reader::new(data);
+
+        let actual = reader.read_index()?;
+        let expected = Index {
+            number_entries: 2, offsets: vec![(4668, 21294), (23810, 86529)]
+        };
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_too_many_entries() -> Result<(), Box<dyn std::error::Error>> {
+        let data: &[u8] = &[
+            0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // number_entries = 3
+            0x3c, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // compressed_offset = 4668
+            0x2e, 0x53, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // uncompressed_offset = 21294
+            0x02, 0x5d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // compressed_offset = 23810
+            0x01, 0x52, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, // uncompressed_offset = 86529
+        ];
+
+        let mut reader = Reader::new(data);
+
+        let actual = reader.read_index().map_err(|err| err.kind());
+        let expected = Err(io::ErrorKind::UnexpectedEof);
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_trailing_data() -> Result<(), Box<dyn std::error::Error>> {
+        let data: &[u8] = &[
+            0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // number_entries = 1
+            0x3c, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // compressed_offset = 4668
+            0x2e, 0x53, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // uncompressed_offset = 21294
+            0x00
+        ];
+
+        let mut reader = Reader::new(data);
+
+        let actual = reader.read_index().map_err(|err| err.kind());
+        let expected = Err(io::ErrorKind::InvalidData);
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
 }
