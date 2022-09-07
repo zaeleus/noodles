ba87aba08866b0e71e2c347db96fd69fcc62535c
Michael Macias <zaeleus@gmail.com>
2022-09-07 13:32:09 -0500

    cram/codecs/rans_nx16/encode: Add stripe encoder

--- noodles-cram/src/codecs/rans_nx16/encode.rs
+++ noodles-cram/src/codecs/rans_nx16/encode.rs
@@ -24,7 +24,9 @@ pub fn rans_encode_nx16(flags: Flags, src: &[u8]) -> io::Result<Vec<u8>> {
     let n = if flags.contains(Flags::N32) { 32 } else { 4 };
 
     if flags.contains(Flags::STRIPE) {
-        todo!("rans_encode_stripe");
+        let buf = rans_encode_stripe(&src, n)?;
+        dst.extend(&buf);
+        return Ok(dst);
     }
 
     let mut pack_header = None;
@@ -172,6 +174,59 @@ where
     }
 
     Ok(r)
+}
+
+fn rans_encode_stripe(src: &[u8], n: usize) -> io::Result<Vec<u8>> {
+    let mut ulens = Vec::with_capacity(n);
+    let mut t = Vec::with_capacity(n);
+
+    for j in 0..n {
+        let mut ulen = src.len() / n;
+
+        if src.len() % n > j {
+            ulen += 1;
+        }
+
+        let chunk = vec![0; ulen];
+
+        ulens.push(ulen);
+        t.push(chunk);
+    }
+
+    let mut x = 0;
+    let mut i = 0;
+
+    while i < src.len() {
+        for j in 0..n {
+            if x < ulens[j] {
+                t[j][x] = src[i + j];
+            }
+        }
+
+        x += 1;
+        i += n;
+    }
+
+    let mut chunks = vec![Vec::new(); n];
+
+    for (chunk, s) in chunks.iter_mut().zip(t.iter()) {
+        *chunk = rans_encode_nx16(Flags::empty(), s)?;
+    }
+
+    let mut dst = Vec::new();
+
+    dst.write_u8(n as u8)?;
+
+    for chunk in &chunks {
+        let clen = chunk.len() as u32;
+        write_uint7(&mut dst, clen)?;
+    }
+
+    for chunk in &chunks {
+        dst.write_all(chunk)?;
+    }
+
+    Ok(dst)
 }
 
 fn encode_pack(src: &[u8]) -> io::Result<(Vec<u8>, Vec<u8>)> {
@@ -354,6 +409,26 @@ mod tests {
             0x04, 0xa0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x90, 0x00, 0x00, 0x02, 0x90, 0x00, 0x00,
             0x00, 0xa0, 0x00, 0x00, 0x05, 0x00, 0x04, 0x02, 0x00, 0x00, 0x08, 0x01, 0x00, 0x00,
             0x08, 0x01, 0x00, 0x00, 0x00, 0x02, 0x00,
+        ];
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_rans_encode_nx16_stripe() -> io::Result<()> {
+        let actual = rans_encode_nx16(Flags::STRIPE, b"noodles")?;
+
+        let expected = [
+            0x08, 0x07, 0x04, 0x19, 0x19, 0x19, 0x16, 0x00, 0x02, 0x6c, 0x6e, 0x00, 0x90, 0x00,
+            0x90, 0x00, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x80, 0x00, 0x00,
+            0x00, 0x80, 0x00, 0x00, 0x00, 0x02, 0x65, 0x6f, 0x00, 0x90, 0x00, 0x90, 0x00, 0x00,
+            0x08, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x80, 0x00,
+            0x00, 0x00, 0x02, 0x6f, 0x73, 0x00, 0x90, 0x00, 0x90, 0x00, 0x00, 0x00, 0x01, 0x00,
+            0x00, 0x08, 0x01, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x01,
+            0x64, 0x00, 0xa0, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x80,
+            0x00, 0x00, 0x00, 0x80, 0x00, 0x00,
         ];
 
         assert_eq!(actual, expected);
