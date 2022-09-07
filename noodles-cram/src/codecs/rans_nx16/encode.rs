382bddc4ed980d3330a0412eb8e11ecfb80a028a
Michael Macias <zaeleus@gmail.com>
2022-09-07 11:19:42 -0500

    cram/codecs/rans_nx16/encode: Add RLE encoder

--- noodles-cram/src/codecs/rans_nx16/encode.rs
+++ noodles-cram/src/codecs/rans_nx16/encode.rs
@@ -35,11 +35,19 @@ pub fn rans_encode_nx16(flags: Flags, src: &[u8]) -> io::Result<Vec<u8>> {
         src = buf;
     }
 
+    let mut rle_header = None;
+
     if flags.contains(Flags::RLE) {
-        todo!("encode_rle_meta");
+        let (header, buf) = encode_rle(&src)?;
+        rle_header = Some(header);
+        src = buf;
     }
 
     if let Some(header) = pack_header {
+        dst.write_all(&header)?;
+    }
+
+    if let Some(header) = rle_header {
         dst.write_all(&header)?;
     }
 
@@ -248,6 +256,72 @@ fn encode_pack(src: &[u8]) -> io::Result<(Vec<u8>, Vec<u8>)> {
     Ok((header, buf))
 }
 
+fn encode_rle(src: &[u8]) -> io::Result<(Vec<u8>, Vec<u8>)> {
+    let mut scores = [0; 256];
+
+    for window in src.windows(2) {
+        let prev_sym = usize::from(window[0]);
+        let curr_sym = usize::from(window[1]);
+
+        if curr_sym == prev_sym {
+            scores[curr_sym] += 1;
+        } else {
+            scores[curr_sym] -= 1;
+        }
+    }
+
+    let n = scores.iter().filter(|&&s| s > 0).count();
+
+    assert!(0 < n && n < 256);
+    let mut meta = vec![n as u8];
+
+    for (i, &score) in scores.iter().enumerate() {
+        if score > 0 {
+            let sym = i as u8;
+            meta.push(sym);
+        }
+    }
+
+    let mut buf = vec![0; src.len()];
+    let mut end = 0;
+
+    let mut i = 0;
+
+    while i < src.len() {
+        buf[end] = src[i];
+        end += 1;
+
+        if scores[src[i] as usize] > 0 {
+            let mut run = 0;
+            let last = src[i];
+
+            while i + run + 1 < src.len() && src[i + run + 1] == last {
+                run += 1;
+            }
+
+            write_uint7(&mut meta, run as u32)?;
+
+            i += run;
+        }
+
+        i += 1;
+    }
+
+    buf.truncate(end);
+
+    let mut header = Vec::new();
+
+    let rle_meta_len = meta.len() as u32;
+    write_uint7(&mut header, (rle_meta_len << 1) | 1)?;
+
+    let len = buf.len() as u32;
+    write_uint7(&mut header, len)?;
+
+    header.write_all(&meta)?;
+
+    Ok((header, buf))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -295,6 +369,21 @@ mod tests {
             0x20, // flags = CAT
             0x07, // uncompressed len = 7
             0x6e, 0x6f, 0x6f, 0x64, 0x6c, 0x65, 0x73,
+        ];
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_rans_encode_nx16_rle() -> io::Result<()> {
+        let actual = rans_encode_nx16(Flags::CAT | Flags::RLE, b"noooooooodles")?;
+
+        let expected = [
+            0x60, // flags = CAT | RLE
+            0x0d, // uncompressed length = 13
+            0x07, 0x06, 0x01, 0x6f, 0x07, 0x6e, 0x6f, 0x64, 0x6c, 0x65, 0x73,
         ];
 
         assert_eq!(actual, expected);
