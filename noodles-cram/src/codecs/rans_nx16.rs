4b7c47b823d787a2458847c09f1660ca426cece8
Michael Macias <zaeleus@gmail.com>
2022-09-09 17:05:54 -0500

    cram/codecs/rans_nx16: Increase visibility of Flags

--- noodles-cram/src/codecs/rans_nx16.rs
+++ noodles-cram/src/codecs/rans_nx16.rs
@@ -2,6 +2,4 @@ pub mod decode;
 mod encode;
 mod flags;
 
-pub use self::{decode::rans_decode_nx16, encode::rans_encode_nx16};
-
-use self::flags::Flags;
+pub use self::{decode::rans_decode_nx16, encode::rans_encode_nx16, flags::Flags};
