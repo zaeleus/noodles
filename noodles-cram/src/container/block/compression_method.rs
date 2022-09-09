de45b90c8f441078289133edadedccf3ed3ac2e3
Michael Macias <zaeleus@gmail.com>
2022-09-09 11:59:06 -0500

    cram/container/block/compression_method: Move serializer to writer

--- noodles-cram/src/container/block/compression_method.rs
+++ noodles-cram/src/container/block/compression_method.rs
@@ -32,22 +32,6 @@ impl Default for CompressionMethod {
     }
 }
 
-impl From<CompressionMethod> for u8 {
-    fn from(compression_method: CompressionMethod) -> Self {
-        match compression_method {
-            CompressionMethod::None => 0,
-            CompressionMethod::Gzip => 1,
-            CompressionMethod::Bzip2 => 2,
-            CompressionMethod::Lzma => 3,
-            CompressionMethod::Rans4x8 => 4,
-            CompressionMethod::RansNx16 => 5,
-            CompressionMethod::AdaptiveArithmeticCoding => 6,
-            CompressionMethod::Fqzcomp => 7,
-            CompressionMethod::NameTokenizer => 8,
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,18 +39,5 @@ mod tests {
     #[test]
     fn test_default() {
         assert_eq!(CompressionMethod::default(), CompressionMethod::None);
-    }
-
-    #[test]
-    fn test_from_compression_method_for_u8() {
-        assert_eq!(u8::from(CompressionMethod::None), 0);
-        assert_eq!(u8::from(CompressionMethod::Gzip), 1);
-        assert_eq!(u8::from(CompressionMethod::Bzip2), 2);
-        assert_eq!(u8::from(CompressionMethod::Lzma), 3);
-        assert_eq!(u8::from(CompressionMethod::Rans4x8), 4);
-        assert_eq!(u8::from(CompressionMethod::RansNx16), 5);
-        assert_eq!(u8::from(CompressionMethod::AdaptiveArithmeticCoding), 6);
-        assert_eq!(u8::from(CompressionMethod::Fqzcomp), 7);
-        assert_eq!(u8::from(CompressionMethod::NameTokenizer), 8);
     }
 }
--- noodles-cram/src/writer/container/block.rs
+++ noodles-cram/src/writer/container/block.rs
@@ -3,7 +3,10 @@ use std::io::{self, Write};
 use byteorder::{LittleEndian, WriteBytesExt};
 use flate2::CrcWriter;
 
-use crate::{container::Block, writer::num::write_itf8};
+use crate::{
+    container::{block::CompressionMethod, Block},
+    writer::num::write_itf8,
+};
 
 pub fn write_block<W>(writer: &mut W, block: &Block) -> io::Result<()>
 where
@@ -11,8 +14,7 @@ where
 {
     let mut crc_writer = CrcWriter::new(writer);
 
-    let method = u8::from(block.compression_method());
-    crc_writer.write_u8(method)?;
+    write_compression_method(&mut crc_writer, block.compression_method())?;
 
     let content_type = u8::from(block.content_type());
     crc_writer.write_u8(content_type)?;
@@ -35,4 +37,63 @@ where
     writer.write_u32::<LittleEndian>(crc32)?;
 
     Ok(())
+}
+
+fn write_compression_method<W>(
+    writer: &mut W,
+    compression_method: CompressionMethod,
+) -> io::Result<()>
+where
+    W: Write,
+{
+    let n = match compression_method {
+        CompressionMethod::None => 0,
+        CompressionMethod::Gzip => 1,
+        CompressionMethod::Bzip2 => 2,
+        CompressionMethod::Lzma => 3,
+        CompressionMethod::Rans4x8 => 4,
+        CompressionMethod::RansNx16 => 5,
+        CompressionMethod::AdaptiveArithmeticCoding => 6,
+        CompressionMethod::Fqzcomp => 7,
+        CompressionMethod::NameTokenizer => 8,
+    };
+
+    writer.write_u8(n)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_write_compression_method() -> io::Result<()> {
+        fn t(
+            buf: &mut Vec<u8>,
+            compression_method: CompressionMethod,
+            expected: &[u8],
+        ) -> io::Result<()> {
+            buf.clear();
+            write_compression_method(buf, compression_method)?;
+            assert_eq!(buf, expected);
+            Ok(())
+        }
+
+        let mut buf = Vec::new();
+
+        t(&mut buf, CompressionMethod::None, &[0x00])?;
+        t(&mut buf, CompressionMethod::Gzip, &[0x01])?;
+        t(&mut buf, CompressionMethod::Bzip2, &[0x02])?;
+        t(&mut buf, CompressionMethod::Lzma, &[0x03])?;
+        t(&mut buf, CompressionMethod::Rans4x8, &[0x04])?;
+        t(&mut buf, CompressionMethod::RansNx16, &[0x05])?;
+        t(
+            &mut buf,
+            CompressionMethod::AdaptiveArithmeticCoding,
+            &[0x06],
+        )?;
+        t(&mut buf, CompressionMethod::Fqzcomp, &[0x07])?;
+        t(&mut buf, CompressionMethod::NameTokenizer, &[0x08])?;
+
+        Ok(())
+    }
 }
