9362c9b36cfa1a362947d6da34bc846bb62be6f1
Michael Macias <zaeleus@gmail.com>
2022-09-19 15:45:42 -0500

    fasta/examples/read: Use reader builder

--- noodles-fasta/examples/fasta_read.rs
+++ noodles-fasta/examples/fasta_read.rs
@@ -1,19 +1,13 @@
 //! Prints the reference sequence names and lengths of all the records in a FASTA file.
 
-use std::{
-    env,
-    fs::File,
-    io::{self, BufReader},
-};
+use std::{env, io};
 
 use noodles_fasta as fasta;
 
 fn main() -> io::Result<()> {
     let src = env::args().nth(1).expect("missing src");
 
-    let mut reader = File::open(src)
-        .map(BufReader::new)
-        .map(fasta::Reader::new)?;
+    let mut reader = fasta::reader::Builder::default().build_from_path(src)?;
 
     for result in reader.records() {
         let record = result?;
