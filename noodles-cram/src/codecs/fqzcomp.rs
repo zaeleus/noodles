ebf7f2577d27f10027d763e3e292bca55aa986d7
Michael Macias <zaeleus@gmail.com>
2022-10-01 15:48:03 -0500

    cram/codecs/fqzcomp: Add self test

--- noodles-cram/src/codecs/fqzcomp.rs
+++ noodles-cram/src/codecs/fqzcomp.rs
@@ -26,3 +26,41 @@ impl Models {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+
+    use super::*;
+
+    #[test]
+    fn test_self() -> io::Result<()> {
+        fn t(data: &[Vec<u8>]) -> io::Result<()> {
+            let lens: Vec<_> = data.iter().map(|scores| scores.len()).collect();
+            let src: Vec<_> = data.iter().flatten().copied().collect();
+
+            let compressed_data = encode(&lens, &src)?;
+
+            let mut reader = &compressed_data[..];
+            let uncompressed_data = decode(&mut reader)?;
+
+            assert_eq!(uncompressed_data, src);
+
+            Ok(())
+        }
+
+        t(&[
+            vec![0, 0, 0, 1, 1, 2, 1, 1, 0, 0],
+            vec![0, 1, 2, 3, 3, 3, 3, 3, 3, 3],
+            vec![2, 1, 1, 0, 0],
+        ])?;
+
+        t(&[
+            vec![0, 0, 0, 1, 1, 2, 1, 1, 0, 0],
+            vec![0, 1, 2, 3, 3, 3, 3, 3, 3, 3],
+            vec![2, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+        ])?;
+
+        Ok(())
+    }
+}
