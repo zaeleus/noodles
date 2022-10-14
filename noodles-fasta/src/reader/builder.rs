bf29d335a7effb7607ac0b76550f160dba5a8f28
Michael Macias <zaeleus@gmail.com>
2022-10-14 11:04:02 -0500

    fasta/reader/builder: Add bgzip handler

--- noodles-fasta/src/reader/builder.rs
+++ noodles-fasta/src/reader/builder.rs
@@ -4,6 +4,8 @@ use std::{
     path::Path,
 };
 
+use noodles_bgzf as bgzf;
+
 use crate::Reader;
 
 /// A FASTA reader builder.
@@ -12,11 +14,19 @@ pub struct Builder;
 
 impl Builder {
     /// Builds a FASTA reader from a path.
-    pub fn build_from_path<P>(self, src: P) -> io::Result<Reader<BufReader<File>>>
+    pub fn build_from_path<P>(self, src: P) -> io::Result<Reader<Box<dyn BufRead>>>
     where
         P: AsRef<Path>,
     {
-        let reader = File::open(&src).map(BufReader::new)?;
+        let src = src.as_ref();
+
+        let file = File::open(src)?;
+
+        let reader: Box<dyn BufRead> = match src.extension().and_then(|ext| ext.to_str()) {
+            Some("gz") => Box::new(bgzf::Reader::new(file)),
+            _ => Box::new(BufReader::new(file)),
+        };
+
         self.build_from_reader(reader)
     }
 
