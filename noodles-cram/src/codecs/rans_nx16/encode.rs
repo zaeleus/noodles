0c9ad7fbb7eddb92417f8ded2e0ef1732792252a
Michael Macias <zaeleus@gmail.com>
2022-10-19 09:46:09 -0500

    cram/codecs/rans_nx16/encode: Move RLE encoder to a module

--- noodles-cram/src/codecs/rans_nx16/encode.rs
+++ noodles-cram/src/codecs/rans_nx16/encode.rs
@@ -1,6 +1,7 @@
 mod order_0;
 mod order_1;
 pub mod pack;
+mod rle;
 
 use std::io::{self, Write};
 
@@ -48,7 +49,7 @@ pub fn encode(mut flags: Flags, src: &[u8]) -> io::Result<Vec<u8>> {
     let mut rle_header = None;
 
     if flags.contains(Flags::RLE) {
-        let (header, buf) = encode_rle(&src)?;
+        let (header, buf) = rle::encode(&src)?;
         rle_header = Some(header);
         src = buf;
     }
@@ -258,76 +259,6 @@ fn rans_encode_stripe(src: &[u8], n: usize) -> io::Result<Vec<u8>> {
     }
 
     Ok(dst)
-}
-
-fn encode_rle(src: &[u8]) -> io::Result<(Vec<u8>, Vec<u8>)> {
-    let mut scores = [0; 256];
-
-    for window in src.windows(2) {
-        let prev_sym = usize::from(window[0]);
-        let curr_sym = usize::from(window[1]);
-
-        if curr_sym == prev_sym {
-            scores[curr_sym] += 1;
-        } else {
-            scores[curr_sym] -= 1;
-        }
-    }
-
-    let mut n = scores.iter().filter(|&&s| s > 0).count();
-
-    if n == 0 {
-        n = 1;
-        scores[0] = 1;
-    }
-
-    let mut meta = vec![n as u8];
-
-    for (i, &score) in scores.iter().enumerate() {
-        if score > 0 {
-            let sym = i as u8;
-            meta.push(sym);
-        }
-    }
-
-    let mut buf = vec![0; src.len()];
-    let mut end = 0;
-
-    let mut i = 0;
-
-    while i < src.len() {
-        buf[end] = src[i];
-        end += 1;
-
-        if scores[src[i] as usize] > 0 {
-            let mut run = 0;
-            let last = src[i];
-
-            while i + run + 1 < src.len() && src[i + run + 1] == last {
-                run += 1;
-            }
-
-            write_uint7(&mut meta, run as u32)?;
-
-            i += run;
-        }
-
-        i += 1;
-    }
-
-    buf.truncate(end);
-
-    let mut header = Vec::new();
-
-    let rle_meta_len = meta.len() as u32;
-    write_uint7(&mut header, (rle_meta_len << 1) | 1)?;
-
-    let len = buf.len() as u32;
-    write_uint7(&mut header, len)?;
-
-    header.write_all(&meta)?;
-
-    Ok((header, buf))
 }
 
 #[cfg(test)]
---
+++ noodles-cram/src/codecs/rans_nx16/encode/rle.rs
@@ -0,0 +1,73 @@
+use std::io::{self, Write};
+
+use crate::writer::num::write_uint7;
+
+pub fn encode(src: &[u8]) -> io::Result<(Vec<u8>, Vec<u8>)> {
+    let mut scores = [0; 256];
+
+    for window in src.windows(2) {
+        let prev_sym = usize::from(window[0]);
+        let curr_sym = usize::from(window[1]);
+
+        if curr_sym == prev_sym {
+            scores[curr_sym] += 1;
+        } else {
+            scores[curr_sym] -= 1;
+        }
+    }
+
+    let mut n = scores.iter().filter(|&&s| s > 0).count();
+
+    if n == 0 {
+        n = 1;
+        scores[0] = 1;
+    }
+
+    let mut meta = vec![n as u8];
+
+    for (i, &score) in scores.iter().enumerate() {
+        if score > 0 {
+            let sym = i as u8;
+            meta.push(sym);
+        }
+    }
+
+    let mut buf = vec![0; src.len()];
+    let mut end = 0;
+
+    let mut i = 0;
+
+    while i < src.len() {
+        buf[end] = src[i];
+        end += 1;
+
+        if scores[src[i] as usize] > 0 {
+            let mut run = 0;
+            let last = src[i];
+
+            while i + run + 1 < src.len() && src[i + run + 1] == last {
+                run += 1;
+            }
+
+            write_uint7(&mut meta, run as u32)?;
+
+            i += run;
+        }
+
+        i += 1;
+    }
+
+    buf.truncate(end);
+
+    let mut header = Vec::new();
+
+    let rle_meta_len = meta.len() as u32;
+    write_uint7(&mut header, (rle_meta_len << 1) | 1)?;
+
+    let len = buf.len() as u32;
+    write_uint7(&mut header, len)?;
+
+    header.write_all(&meta)?;
+
+    Ok((header, buf))
+}
