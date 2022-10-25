53b43c73e0b904778651bea02a5a529ff209688d
Clay McLeod <clay.l.mcleod@gmail.com>
2022-10-24 21:21:51 -0500

    gff/writer: Adds `write_line` method to writer
    Co-authored-by: Michael Macias <zaeleus@gmail.com>

--- noodles-gff/src/writer.rs
+++ noodles-gff/src/writer.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Write};
 
-use super::{Directive, Record};
+use super::{Directive, Line, Record};
 
 /// A GFF writer.
 pub struct Writer<W> {
@@ -34,6 +34,37 @@ where
     /// ```
     pub fn get_ref(&self) -> &W {
         &self.inner
+    }
+
+    /// Writes a [`Line`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::io;
+    /// use noodles_gff as gff;
+    /// use gff::line::Line;
+    ///
+    /// let mut writer = gff::Writer::new(Vec::new());
+    ///
+    /// let version = Line::Directive(gff::Directive::GffVersion(Default::default()));
+    /// writer.write_line(&version)?;
+    ///
+    /// let comment = Line::Comment(String::from("noodles"));
+    /// writer.write_line(&comment)?;
+    ///
+    /// let record = Line::Record(gff::Record::default());
+    /// writer.write_line(&record)?;
+    ///
+    /// let expected = b"##gff-version 3
+    /// #noodles
+    /// .\t.\t.\t1\t1\t.\t.\t.\t.
+    /// ";
+    ///
+    /// assert_eq!(&writer.get_ref()[..], &expected[..]);
+    /// # Ok::<(), io::Error>(())
+    pub fn write_line(&mut self, line: &Line) -> io::Result<()> {
+        writeln!(self.inner, "{}", line)
     }
 
     /// Writes a GFF directive.
