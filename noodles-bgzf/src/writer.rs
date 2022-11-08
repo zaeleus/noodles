e1fe8884fb3e26d936c299aa5736dd9c623952eb
Michael Macias <zaeleus@gmail.com>
2022-11-08 16:04:39 -0600

    bgzf/writer: Use src and dst naming in deflate

--- noodles-bgzf/src/writer.rs
+++ noodles-bgzf/src/writer.rs
@@ -258,45 +258,45 @@ where
 
 #[cfg(feature = "libdeflate")]
 pub(crate) fn deflate_data(
-    data: &[u8],
+    src: &[u8],
     compression_level: libdeflater::CompressionLvl,
 ) -> io::Result<(Vec<u8>, u32, u32)> {
     use libdeflater::Compressor;
 
     let mut encoder = Compressor::new(compression_level);
 
-    let mut compressed_data = Vec::new();
+    let mut dst = Vec::new();
 
-    let max_len = encoder.deflate_compress_bound(data.len());
-    compressed_data.resize(max_len, Default::default());
+    let max_len = encoder.deflate_compress_bound(src.len());
+    dst.resize(max_len, Default::default());
 
     let len = encoder
-        .deflate_compress(data, &mut compressed_data)
+        .deflate_compress(src, &mut dst)
         .map_err(|_| io::Error::from(io::ErrorKind::InvalidInput))?;
 
-    compressed_data.resize(len, Default::default());
+    dst.resize(len, Default::default());
 
     let mut crc = Crc::new();
-    crc.update(data);
+    crc.update(src);
 
-    Ok((compressed_data, crc.sum(), crc.amount()))
+    Ok((dst, crc.sum(), crc.amount()))
 }
 
 #[cfg(not(feature = "libdeflate"))]
 pub(crate) fn deflate_data(
-    data: &[u8],
+    src: &[u8],
     compression_level: flate2::Compression,
 ) -> io::Result<(Vec<u8>, u32, u32)> {
     use flate2::write::DeflateEncoder;
 
     let mut encoder = DeflateEncoder::new(Vec::new(), compression_level);
-    encoder.write_all(data)?;
-    let compressed_data = encoder.finish()?;
+    encoder.write_all(src)?;
+    let dst = encoder.finish()?;
 
     let mut crc = Crc::new();
-    crc.update(data);
+    crc.update(src);
 
-    Ok((compressed_data, crc.sum(), crc.amount()))
+    Ok((dst, crc.sum(), crc.amount()))
 }
 
 #[cfg(test)]
