bbdd003982b458c9d353afab475b742757414ebf
Michael Macias <zaeleus@gmail.com>
2022-12-08 14:35:51 -0600

    sam/alignment/record: Add examples

--- noodles-sam/src/alignment/record.rs
+++ noodles-sam/src/alignment/record.rs
@@ -36,131 +36,378 @@ pub struct Record {
 
 impl Record {
     /// Creates an alignment record builder.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let builder = sam::alignment::Record::builder();
+    /// ```
     pub fn builder() -> Builder {
         Builder::default()
     }
 
     /// Returns the read name.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.read_name().is_none());
+    /// ```
     pub fn read_name(&self) -> Option<&ReadName> {
         self.read_name.as_ref()
     }
 
     /// Returns a mutable reference to the read name.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::ReadName};
+    ///
+    /// let read_name: ReadName = "r1".parse()?;
+    ///
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.read_name_mut() = Some(read_name.clone());
+    ///
+    /// assert_eq!(record.read_name(), Some(&read_name));
+    /// Ok::<_, sam::record::read_name::ParseError>(())
+    /// ```
     pub fn read_name_mut(&mut self) -> &mut Option<ReadName> {
         &mut self.read_name
     }
 
     /// Returns the flags.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Flags};
+    /// let record = sam::alignment::Record::default();
+    /// assert_eq!(record.flags(), Flags::UNMAPPED);
+    /// ```
     pub fn flags(&self) -> Flags {
         self.flags
     }
 
     /// Returns a mutable reference to the flags.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Flags};
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.flags_mut() = Flags::empty();
+    /// assert!(record.flags().is_empty());
+    /// ```
     pub fn flags_mut(&mut self) -> &mut Flags {
         &mut self.flags
     }
 
     /// Returns the reference sequence ID.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.reference_sequence_id().is_none());
+    /// ```
     pub fn reference_sequence_id(&self) -> Option<usize> {
         self.reference_sequence_id
     }
 
     /// Returns a mutable reference to the reference sequence ID.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.reference_sequence_id_mut() = Some(0);
+    /// assert_eq!(record.reference_sequence_id(), Some(0));
+    /// ```
     pub fn reference_sequence_id_mut(&mut self) -> &mut Option<usize> {
         &mut self.reference_sequence_id
     }
 
     /// Returns the alignment start.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.alignment_start().is_none());
+    /// ```
     pub fn alignment_start(&self) -> Option<Position> {
         self.alignment_start
     }
 
     /// Returns a mutable reference to the alignment start.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_core::Position;
+    /// use noodles_sam as sam;
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.alignment_start_mut() = Some(Position::MIN);
+    /// assert_eq!(record.alignment_start(), Some(Position::MIN));
+    /// ```
     pub fn alignment_start_mut(&mut self) -> &mut Option<Position> {
         &mut self.alignment_start
     }
 
     /// Returns the mapping quality.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.mapping_quality().is_none());
+    /// ```
     pub fn mapping_quality(&self) -> Option<MappingQuality> {
         self.mapping_quality
     }
 
     /// Returns a mutable reference to the mapping quality.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::MappingQuality};
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.mapping_quality_mut() = Some(MappingQuality::MIN);
+    /// assert_eq!(record.mapping_quality(), Some(MappingQuality::MIN));
+    /// ```
     pub fn mapping_quality_mut(&mut self) -> &mut Option<MappingQuality> {
         &mut self.mapping_quality
     }
 
     /// Returns the CIGAR operations.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.cigar().is_empty());
+    /// ```
     pub fn cigar(&self) -> &Cigar {
         &self.cigar
     }
 
     /// Returns a mutable reference to the CIGAR operations.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Cigar};
+    ///
+    /// let cigar: Cigar = "4M".parse()?;
+    ///
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.cigar_mut() = cigar.clone();
+    ///
+    /// assert_eq!(record.cigar(), &cigar);
+    /// Ok::<_, sam::record::cigar::ParseError>(())
+    /// ```
     pub fn cigar_mut(&mut self) -> &mut Cigar {
         &mut self.cigar
     }
 
     /// Returns the mate reference sequence ID.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.mate_reference_sequence_id().is_none());
+    /// ```
     pub fn mate_reference_sequence_id(&self) -> Option<usize> {
         self.mate_reference_sequence_id
     }
 
     /// Returns a mutable reference to the mate reference sequence ID.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.mate_reference_sequence_id_mut() = Some(0);
+    /// assert_eq!(record.mate_reference_sequence_id(), Some(0));
+    /// ```
     pub fn mate_reference_sequence_id_mut(&mut self) -> &mut Option<usize> {
         &mut self.mate_reference_sequence_id
     }
 
     /// Returns the mate alignment start.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.mate_alignment_start().is_none());
+    /// ```
     pub fn mate_alignment_start(&self) -> Option<Position> {
         self.mate_alignment_start
     }
 
     /// Returns a mutable reference to the mate alignment start.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_core::Position;
+    /// use noodles_sam as sam;
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.mate_alignment_start_mut() = Some(Position::MIN);
+    /// assert_eq!(record.mate_alignment_start(), Some(Position::MIN));
+    /// ```
     pub fn mate_alignment_start_mut(&mut self) -> &mut Option<Position> {
         &mut self.mate_alignment_start
     }
 
     /// Returns the template length.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert_eq!(record.template_length(), 0);
+    /// ```
     pub fn template_length(&self) -> i32 {
         self.template_length
     }
 
     /// Returns a mutable reference to template length.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.template_length_mut() = 4;
+    /// assert_eq!(record.template_length(), 4);
+    /// ```
     pub fn template_length_mut(&mut self) -> &mut i32 {
         &mut self.template_length
     }
 
     /// Returns the sequence.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.sequence().is_empty());
+    /// ```
     pub fn sequence(&self) -> &Sequence {
         &self.sequence
     }
 
     /// Returns a mutable reference to sequence.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Sequence};
+    ///
+    /// let sequence: Sequence = "ACGT".parse()?;
+    ///
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.sequence_mut() = sequence.clone();
+    ///
+    /// assert_eq!(record.sequence(), &sequence);
+    /// Ok::<_, sam::record::sequence::ParseError>(())
+    /// ```
     pub fn sequence_mut(&mut self) -> &mut Sequence {
         &mut self.sequence
     }
 
     /// Returns the quality scores.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.quality_scores().is_empty());
+    /// ```
     pub fn quality_scores(&self) -> &QualityScores {
         &self.quality_scores
     }
 
     /// Returns a mutable reference to quality scores.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::QualityScores};
+    ///
+    /// let quality_scores: QualityScores = "NDLS".parse()?;
+    ///
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.quality_scores_mut() = quality_scores.clone();
+    ///
+    /// assert_eq!(record.quality_scores(), &quality_scores);
+    /// Ok::<_, sam::record::quality_scores::ParseError>(())
+    /// ```
     pub fn quality_scores_mut(&mut self) -> &mut QualityScores {
         &mut self.quality_scores
     }
 
     /// Returns the data.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.data().is_empty());
+    /// ```
     pub fn data(&self) -> &Data {
         &self.data
     }
 
     /// Returns a mutable reference to the data.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Data};
+    ///
+    /// let data: Data = "NH:i:1".parse()?;
+    ///
+    /// let mut record = sam::alignment::Record::default();
+    /// *record.data_mut() = data.clone();
+    ///
+    /// assert_eq!(record.data_mut(), &data);
+    /// Ok::<_, sam::record::data::ParseError>(())
+    /// ```
     pub fn data_mut(&mut self) -> &mut Data {
         &mut self.data
     }
 
     /// Returns the associated reference sequence.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let header = sam::Header::default();
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.reference_sequence(&header).is_none());
+    /// ```
     pub fn reference_sequence<'a>(
         &self,
         header: &'a Header,
@@ -169,6 +416,15 @@ impl Record {
     }
 
     /// Returns the associated mate reference sequence.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let header = sam::Header::default();
+    /// let record = sam::alignment::Record::default();
+    /// assert!(record.mate_reference_sequence(&header).is_none());
+    /// ```
     pub fn mate_reference_sequence<'a>(
         &self,
         header: &'a Header,
@@ -180,6 +436,14 @@ impl Record {
     }
 
     /// Returns the alignment span.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::default();
+    /// assert_eq!(record.alignment_span(), 0);
+    /// ```
     pub fn alignment_span(&self) -> usize {
         self.cigar().alignment_span()
     }
@@ -190,9 +454,9 @@ impl Record {
     ///
     /// ```
     /// use noodles_core::Position;
-    /// use noodles_sam::{self as sam, alignment::Record};
+    /// use noodles_sam as sam;
     ///
-    /// let record = Record::builder()
+    /// let record = sam::alignment::Record::builder()
     ///     .set_alignment_start(Position::try_from(8)?)
     ///     .set_cigar("5M".parse()?)
     ///     .build();
