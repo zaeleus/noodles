dd8922e58a2fccd6d6fa671992ea40eec80a1930
Michael Macias <zaeleus@gmail.com>
2022-12-10 13:53:37 -0600

    sam/alignment/record/builder: Add examples

--- noodles-sam/src/alignment/record/builder.rs
+++ noodles-sam/src/alignment/record/builder.rs
@@ -22,78 +22,247 @@ pub struct Builder {
 
 impl Builder {
     /// Sets the read name.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::ReadName};
+    ///
+    /// let read_name: ReadName = "r1".parse()?;
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_read_name(read_name.clone())
+    ///     .build();
+    ///
+    /// assert_eq!(record.read_name(), Some(&read_name));
+    /// Ok::<_, sam::record::read_name::ParseError>(())
+    /// ```
     pub fn set_read_name(mut self, read_name: ReadName) -> Self {
         self.read_name = Some(read_name);
         self
     }
 
     /// Sets the flags.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Flags};
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_flags(Flags::empty())
+    ///     .build();
+    ///
+    /// assert_eq!(record.flags(), Flags::empty());
+    /// ```
     pub fn set_flags(mut self, flags: Flags) -> Self {
         self.flags = flags;
         self
     }
 
     /// Sets the reference sequence ID.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_reference_sequence_id(0)
+    ///     .build();
+    ///
+    /// assert_eq!(record.reference_sequence_id(), Some(0));
+    /// ```
     pub fn set_reference_sequence_id(mut self, reference_sequence_id: usize) -> Self {
         self.reference_sequence_id = Some(reference_sequence_id);
         self
     }
 
     /// Sets the alignment start.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_core::Position;
+    /// use noodles_sam as sam;
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_alignment_start(Position::MIN)
+    ///     .build();
+    ///
+    /// assert_eq!(record.alignment_start(), Some(Position::MIN));
+    /// ```
     pub fn set_alignment_start(mut self, alignment_start: Position) -> Self {
         self.alignment_start = Some(alignment_start);
         self
     }
 
     /// Sets the mapping quality.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::MappingQuality};
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_mapping_quality(MappingQuality::MIN)
+    ///     .build();
+    ///
+    /// assert_eq!(record.mapping_quality(), Some(MappingQuality::MIN));
+    /// ```
     pub fn set_mapping_quality(mut self, mapping_quality: MappingQuality) -> Self {
         self.mapping_quality = Some(mapping_quality);
         self
     }
 
     /// Sets the CIGAR operations.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Cigar};
+    ///
+    /// let cigar: Cigar = "4M".parse()?;
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_cigar(cigar.clone())
+    ///     .build();
+    ///
+    /// assert_eq!(record.cigar(), &cigar);
+    /// Ok::<_, sam::record::cigar::ParseError>(())
+    /// ```
     pub fn set_cigar(mut self, cigar: Cigar) -> Self {
         self.cigar = cigar;
         self
     }
 
     /// Sets the mate reference sequence ID.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_mate_reference_sequence_id(0)
+    ///     .build();
+    ///
+    /// assert_eq!(record.mate_reference_sequence_id(), Some(0));
+    /// ```
     pub fn set_mate_reference_sequence_id(mut self, mate_reference_sequence_id: usize) -> Self {
         self.mate_reference_sequence_id = Some(mate_reference_sequence_id);
         self
     }
 
     /// Sets the mate alignment start.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_core::Position;
+    /// use noodles_sam as sam;
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_mate_alignment_start(Position::MIN)
+    ///     .build();
+    ///
+    /// assert_eq!(record.mate_alignment_start(), Some(Position::MIN));
+    /// ```
     pub fn set_mate_alignment_start(mut self, mate_alignment_start: Position) -> Self {
         self.mate_alignment_start = Some(mate_alignment_start);
         self
     }
 
     /// Sets the template length.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_template_length(4)
+    ///     .build();
+    ///
+    /// assert_eq!(record.template_length(), 4);
+    /// ```
     pub fn set_template_length(mut self, template_length: i32) -> Self {
         self.template_length = template_length;
         self
     }
 
     /// Sets the sequence.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Sequence};
+    ///
+    /// let sequence: Sequence = "ACGT".parse()?;
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_sequence(sequence.clone())
+    ///     .build();
+    ///
+    /// assert_eq!(record.sequence(), &sequence);
+    /// Ok::<_, sam::record::sequence::ParseError>(())
+    /// ```
     pub fn set_sequence(mut self, sequence: Sequence) -> Self {
         self.sequence = sequence;
         self
     }
 
     /// Sets the quality scores.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::QualityScores};
+    ///
+    /// let quality_scores: QualityScores = "NDLS".parse()?;
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_quality_scores(quality_scores.clone())
+    ///     .build();
+    ///
+    /// assert_eq!(record.quality_scores(), &quality_scores);
+    /// Ok::<_, sam::record::quality_scores::ParseError>(())
+    /// ```
     pub fn set_quality_scores(mut self, quality_scores: QualityScores) -> Self {
         self.quality_scores = quality_scores;
         self
     }
 
     /// Sets the data.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::{self as sam, record::Data};
+    ///
+    /// let data: Data = "NH:i:1".parse()?;
+    ///
+    /// let record = sam::alignment::Record::builder()
+    ///     .set_data(data.clone())
+    ///     .build();
+    ///
+    /// assert_eq!(record.data(), &data);
+    /// Ok::<_, sam::record::data::ParseError>(())
+    /// ```
     pub fn set_data(mut self, data: Data) -> Self {
         self.data = data;
         self
     }
 
     /// Builds the alignment record.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam as sam;
+    /// let record = sam::alignment::Record::builder().build();
+    /// assert_eq!(record, sam::alignment::Record::default());
+    /// ```
     pub fn build(self) -> Record {
         Record {
             read_name: self.read_name,
