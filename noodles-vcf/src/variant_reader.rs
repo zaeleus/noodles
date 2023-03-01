9b2b8b5fbe06b1039b61e69ef93817d186930aeb
Malthe Sebro Rasmussen <malthesebro@protonmail.com>
2023-03-01 14:41:16 +0100

    vcf: Add VariantReader trait

---
+++ noodles-vcf/src/variant_reader.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+use super::{Header, Record};
+
+/// An variant format reader.
+pub trait VariantReader<R> {
+    /// Reads a VCF header.
+    fn read_variant_header(&mut self) -> io::Result<Header>;
+
+    /// Returns an iterator over records.
+    fn variant_records<'a>(
+        &'a mut self,
+        header: &'a Header,
+    ) -> Box<dyn Iterator<Item = io::Result<Record>> + 'a>;
+}
