e1a40f7a55c487b5461947bc0b2a1b9e25907d65
Michael Macias <zaeleus@gmail.com>
2023-03-02 10:30:09 -0600

    sam/reader: Fix read_record description

--- noodles-sam/src/reader.rs
+++ noodles-sam/src/reader.rs
@@ -146,7 +146,7 @@ where
     /// The stream is expected to be directly after the header or at the start of another record.
     ///
     /// It is more ergonomic to read records using an iterator (see [`Self::records`]), but using
-    /// this method allows control of the line buffer and whether the raw record should be parsed.
+    /// this method allows control of the record buffer.
     ///
     /// If successful, the number of bytes read is returned. If the number of bytes read is 0, the
     /// stream reached EOF.
