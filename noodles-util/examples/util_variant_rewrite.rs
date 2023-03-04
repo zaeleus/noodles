dedfb98d8597d082966409d8aa00a211e0e81203
Malthe Sebro Rasmussen <malthesebro@protonmail.com>
2023-03-04 17:06:40 +0100

    util/examples: Add util_variant_rewrite example

---
+++ noodles-util/examples/util_variant_rewrite.rs
@@ -0,0 +1,29 @@
+//! Rewrites a variant format to another variant format.
+//!
+//! The output format is determined from the extension of the destination.
+
+use std::{env, io};
+
+use noodles_util::variant;
+
+fn main() -> io::Result<()> {
+    let mut args = env::args().skip(1);
+
+    let src = args.next().expect("missing src");
+    let dst = args.next().expect("missing dst");
+
+    let mut reader = variant::reader::Builder::default().build_from_path(src)?;
+
+    let header = reader.read_header()?;
+
+    let mut writer = variant::writer::Builder::default().build_from_path(dst)?;
+
+    writer.write_header(&header)?;
+
+    for result in reader.records(&header) {
+        let record = result?;
+        writer.write_record(&header, &record)?;
+    }
+
+    Ok(())
+}
