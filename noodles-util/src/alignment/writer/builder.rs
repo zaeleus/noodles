a56aea55e2d400935e7515d94deb3b8087a8a99e
Michael Macias <zaeleus@gmail.com>
2023-03-13 22:35:53 -0500

    util/alignment/writer/builder: Buffer file writer

--- noodles-util/src/alignment/writer/builder.rs
+++ noodles-util/src/alignment/writer/builder.rs
@@ -2,7 +2,7 @@
 
 use std::{
     fs::File,
-    io::{self, Write},
+    io::{self, BufWriter, Write},
     path::Path,
 };
 
@@ -106,7 +106,7 @@ impl Builder {
             self.format = detect_format_from_path_extension(src);
         }
 
-        let file = File::create(src)?;
+        let file = File::create(src).map(BufWriter::new)?;
         Ok(self.build_from_writer(file))
     }
 
