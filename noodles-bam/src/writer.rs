2d46fa8829b04f1af42a1c8f22ca563b9f62a83c
Michael Macias <zaeleus@gmail.com>
2023-03-24 09:41:40 -0500

    bam/writer: Move header writers to a module

--- noodles-bam/src/writer.rs
+++ noodles-bam/src/writer.rs
@@ -1,20 +1,13 @@
 //! BAM writer.
 
+mod header;
 pub mod record;
 
-use std::{
-    ffi::CString,
-    io::{self, Write},
-    num::NonZeroUsize,
-};
+use std::io::{self, Write};
 
 use byteorder::{LittleEndian, WriteBytesExt};
 use noodles_bgzf as bgzf;
-use noodles_sam::{
-    self as sam,
-    alignment::Record,
-    header::{record::value::map, ReferenceSequences},
-};
+use noodles_sam::{self as sam, alignment::Record, header::ReferenceSequences};
 
 use self::record::encode_record;
 
@@ -101,6 +94,7 @@ where
     /// # Ok::<(), io::Error>(())
     /// ```
     pub fn write_header(&mut self, header: &sam::Header) -> io::Result<()> {
+        use self::header::write_header;
         write_header(&mut self.inner, header)
     }
 
@@ -138,6 +132,7 @@ where
         &mut self,
         reference_sequences: &ReferenceSequences,
     ) -> io::Result<()> {
+        use self::header::write_reference_sequences;
         write_reference_sequences(&mut self.inner, reference_sequences)
     }
 
@@ -236,123 +231,12 @@ where
     }
 }
 
-fn write_header<W>(writer: &mut W, header: &sam::Header) -> io::Result<()>
-where
-    W: Write,
-{
-    use super::MAGIC_NUMBER;
-
-    writer.write_all(MAGIC_NUMBER)?;
-
-    let text = header.to_string();
-    let l_text =
-        i32::try_from(text.len()).map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-    writer.write_i32::<LittleEndian>(l_text)?;
-
-    writer.write_all(text.as_bytes())?;
-
-    Ok(())
-}
-
-fn write_reference_sequences<W>(
-    writer: &mut W,
-    reference_sequences: &ReferenceSequences,
-) -> io::Result<()>
-where
-    W: Write,
-{
-    let n_ref = i32::try_from(reference_sequences.len())
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-    writer.write_i32::<LittleEndian>(n_ref)?;
-
-    for (name, reference_sequence) in reference_sequences {
-        write_reference_sequence(writer, name, reference_sequence.length())?;
-    }
-
-    Ok(())
-}
-
-fn write_reference_sequence<W>(
-    writer: &mut W,
-    reference_sequence_name: &map::reference_sequence::Name,
-    length: NonZeroUsize,
-) -> io::Result<()>
-where
-    W: Write,
-{
-    let c_name = CString::new(reference_sequence_name.as_bytes())
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-    let name = c_name.as_bytes_with_nul();
-
-    let l_name =
-        u32::try_from(name.len()).map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-    writer.write_u32::<LittleEndian>(l_name)?;
-    writer.write_all(name)?;
-
-    let l_ref = i32::try_from(usize::from(length))
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-    writer.write_i32::<LittleEndian>(l_ref)?;
-
-    Ok(())
-}
-
 #[cfg(test)]
 mod tests {
     use sam::AlignmentWriter;
 
     use super::*;
     use crate::Reader;
-
-    #[test]
-    fn test_write_header() -> Result<(), Box<dyn std::error::Error>> {
-        use sam::header::record::value::{
-            map::{self, header::Version},
-            Map,
-        };
-
-        let header = sam::Header::builder()
-            .set_header(Map::<map::Header>::new(Version::new(1, 6)))
-            .build();
-
-        let mut buf = Vec::new();
-        write_header(&mut buf, &header)?;
-
-        let mut expected = vec![
-            b'B', b'A', b'M', 0x01, // magic
-            0x0b, 0x00, 0x00, 0x00, // l_text = 11
-        ];
-        expected.extend_from_slice(b"@HD\tVN:1.6\n"); // text
-
-        assert_eq!(buf, expected);
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_write_reference_sequences() -> Result<(), Box<dyn std::error::Error>> {
-        use sam::header::record::value::{map::ReferenceSequence, Map};
-
-        let reference_sequences = [(
-            "sq0".parse()?,
-            Map::<ReferenceSequence>::new(NonZeroUsize::try_from(8)?),
-        )]
-        .into_iter()
-        .collect();
-
-        let mut buf = Vec::new();
-        write_reference_sequences(&mut buf, &reference_sequences)?;
-
-        let expected = [
-            0x01, 0x00, 0x00, 0x00, // n_ref = 1
-            0x04, 0x00, 0x00, 0x00, // ref[0].l_name = 4
-            b's', b'q', b'0', 0x00, // ref[0].name = b"sq0\x00"
-            0x08, 0x00, 0x00, 0x00, // ref[0].l_ref = 8
-        ];
-
-        assert_eq!(buf, expected);
-
-        Ok(())
-    }
 
     #[test]
     fn test_write_alignment_record() -> Result<(), Box<dyn std::error::Error>> {
@@ -519,24 +403,6 @@ mod tests {
         );
 
         assert!(fields.next().is_none());
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_write_reference_sequence() -> Result<(), Box<dyn std::error::Error>> {
-        let mut buf = Vec::new();
-        let reference_sequence_name = "sq0".parse()?;
-        let length = NonZeroUsize::try_from(8)?;
-        write_reference_sequence(&mut buf, &reference_sequence_name, length)?;
-
-        let expected = [
-            0x04, 0x00, 0x00, 0x00, // l_name = 4
-            0x73, 0x71, 0x30, 0x00, // name = b"sq0\x00"
-            0x08, 0x00, 0x00, 0x00, // l_ref = 8
-        ];
-
-        assert_eq!(buf, expected);
 
         Ok(())
     }
---
+++ noodles-bam/src/writer/header.rs
@@ -0,0 +1,145 @@
+use std::{
+    ffi::CString,
+    io::{self, Write},
+    num::NonZeroUsize,
+};
+
+use byteorder::{LittleEndian, WriteBytesExt};
+use noodles_sam::{
+    self as sam,
+    header::{record::value::map, ReferenceSequences},
+};
+
+pub(super) fn write_header<W>(writer: &mut W, header: &sam::Header) -> io::Result<()>
+where
+    W: Write,
+{
+    use crate::MAGIC_NUMBER;
+
+    writer.write_all(MAGIC_NUMBER)?;
+
+    let text = header.to_string();
+    let l_text =
+        i32::try_from(text.len()).map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+    writer.write_i32::<LittleEndian>(l_text)?;
+
+    writer.write_all(text.as_bytes())?;
+
+    Ok(())
+}
+
+pub(super) fn write_reference_sequences<W>(
+    writer: &mut W,
+    reference_sequences: &ReferenceSequences,
+) -> io::Result<()>
+where
+    W: Write,
+{
+    let n_ref = i32::try_from(reference_sequences.len())
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+    writer.write_i32::<LittleEndian>(n_ref)?;
+
+    for (name, reference_sequence) in reference_sequences {
+        write_reference_sequence(writer, name, reference_sequence.length())?;
+    }
+
+    Ok(())
+}
+
+fn write_reference_sequence<W>(
+    writer: &mut W,
+    reference_sequence_name: &map::reference_sequence::Name,
+    length: NonZeroUsize,
+) -> io::Result<()>
+where
+    W: Write,
+{
+    let c_name = CString::new(reference_sequence_name.as_bytes())
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+    let name = c_name.as_bytes_with_nul();
+
+    let l_name =
+        u32::try_from(name.len()).map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+    writer.write_u32::<LittleEndian>(l_name)?;
+    writer.write_all(name)?;
+
+    let l_ref = i32::try_from(usize::from(length))
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+    writer.write_i32::<LittleEndian>(l_ref)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_write_header() -> Result<(), Box<dyn std::error::Error>> {
+        use sam::header::record::value::{
+            map::{self, header::Version},
+            Map,
+        };
+
+        let header = sam::Header::builder()
+            .set_header(Map::<map::Header>::new(Version::new(1, 6)))
+            .build();
+
+        let mut buf = Vec::new();
+        write_header(&mut buf, &header)?;
+
+        let mut expected = vec![
+            b'B', b'A', b'M', 0x01, // magic
+            0x0b, 0x00, 0x00, 0x00, // l_text = 11
+        ];
+        expected.extend_from_slice(b"@HD\tVN:1.6\n"); // text
+
+        assert_eq!(buf, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_write_reference_sequences() -> Result<(), Box<dyn std::error::Error>> {
+        use sam::header::record::value::{map::ReferenceSequence, Map};
+
+        let reference_sequences = [(
+            "sq0".parse()?,
+            Map::<ReferenceSequence>::new(NonZeroUsize::try_from(8)?),
+        )]
+        .into_iter()
+        .collect();
+
+        let mut buf = Vec::new();
+        write_reference_sequences(&mut buf, &reference_sequences)?;
+
+        let expected = [
+            0x01, 0x00, 0x00, 0x00, // n_ref = 1
+            0x04, 0x00, 0x00, 0x00, // ref[0].l_name = 4
+            b's', b'q', b'0', 0x00, // ref[0].name = b"sq0\x00"
+            0x08, 0x00, 0x00, 0x00, // ref[0].l_ref = 8
+        ];
+
+        assert_eq!(buf, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_write_reference_sequence() -> Result<(), Box<dyn std::error::Error>> {
+        let mut buf = Vec::new();
+        let reference_sequence_name = "sq0".parse()?;
+        let length = NonZeroUsize::try_from(8)?;
+        write_reference_sequence(&mut buf, &reference_sequence_name, length)?;
+
+        let expected = [
+            0x04, 0x00, 0x00, 0x00, // l_name = 4
+            0x73, 0x71, 0x30, 0x00, // name = b"sq0\x00"
+            0x08, 0x00, 0x00, 0x00, // l_ref = 8
+        ];
+
+        assert_eq!(buf, expected);
+
+        Ok(())
+    }
+}
