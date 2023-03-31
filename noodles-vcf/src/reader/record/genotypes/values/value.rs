54ed9374d21cc6c10f7863bf60759dfdecd726bd
Michael Macias <zaeleus@gmail.com>
2023-03-31 11:50:09 -0500

    vcf/reader/record/genotypes/values/value: Implement From<ParseError> for core::Error

--- noodles-vcf/src/reader/record/genotypes/values/value.rs
+++ noodles-vcf/src/reader/record/genotypes/values/value.rs
@@ -1,5 +1,7 @@
 use std::{error, fmt, num, str};
 
+use noodles_core as core;
+
 use crate::{
     header::{record::value::map::format::Type, Number},
     reader::record::MISSING,
@@ -46,6 +48,12 @@ impl fmt::Display for ParseError {
             ParseError::InvalidCharacter => write!(f, "invalid character"),
             ParseError::InvalidString(_) => write!(f, "invalid string"),
         }
+    }
+}
+
+impl From<ParseError> for core::Error {
+    fn from(e: ParseError) -> Self {
+        Self::new(core::error::Kind::Parse, e)
     }
 }
 
