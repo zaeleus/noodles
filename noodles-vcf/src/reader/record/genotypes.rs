e204ac05c3bc6991ea3062832230a95545a339bf
Michael Macias <zaeleus@gmail.com>
2023-04-05 13:58:34 -0500

    vcf/reader/record/genotypes: Fail parsing if there are no samples in the header but the record has genotype fields

--- noodles-vcf/src/reader/record/genotypes.rs
+++ noodles-vcf/src/reader/record/genotypes.rs
@@ -12,6 +12,10 @@ use crate::{record::Genotypes, Header};
 /// An error when raw VCF record genotypes fail to parse.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum ParseError {
+    /// The input is unexpected.
+    ///
+    /// The header has no samples, but the record has unexpected data.
+    UnexpectedInput,
     /// The keys are invalid.
     InvalidKeys(keys::ParseError),
     /// A list of sample values is invalid.
@@ -21,6 +25,7 @@ pub enum ParseError {
 impl error::Error for ParseError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
+            Self::UnexpectedInput => None,
             Self::InvalidKeys(e) => Some(e),
             Self::InvalidValues(e) => Some(e),
         }
@@ -30,6 +35,7 @@ impl error::Error for ParseError {
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            ParseError::UnexpectedInput => write!(f, "unexpected input"),
             ParseError::InvalidKeys(_) => write!(f, "invalid keys"),
             ParseError::InvalidValues(_) => write!(f, "invalid values"),
         }
@@ -49,20 +55,26 @@ pub(super) fn parse_genotypes(
 ) -> Result<(), ParseError> {
     genotypes.keys.clear();
 
-    for values in &mut genotypes.values {
-        values.clear();
+    let sample_count = header.sample_names().len();
+
+    if sample_count == 0 {
+        genotypes.values.clear();
+
+        if s.is_empty() {
+            return Ok(());
+        } else {
+            return Err(ParseError::UnexpectedInput);
+        }
     }
 
-    if s.is_empty() {
-        return Ok(());
+    for values in &mut genotypes.values {
+        values.clear();
     }
 
     let field = next_field(&mut s);
     parse_keys(header, field, &mut genotypes.keys).map_err(ParseError::InvalidKeys)?;
 
-    genotypes
-        .values
-        .resize(header.sample_names().len(), Vec::new());
+    genotypes.values.resize(sample_count, Vec::new());
 
     for values in &mut genotypes.values {
         let field = next_field(&mut s);
@@ -110,6 +122,12 @@ mod tests {
             ],
         );
         assert_eq!(genotypes, expected);
+
+        let header = Header::default();
+        assert_eq!(
+            parse_genotypes(&header, "GT\t0|0", &mut genotypes),
+            Err(ParseError::UnexpectedInput)
+        );
 
         let header = Header::builder().add_sample_name("sample0").build();
 
