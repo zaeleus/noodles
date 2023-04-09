e715030e6e501478c6bc8e77addad773580467d9
Michael Macias <zaeleus@gmail.com>
2023-04-09 12:32:06 -0500

    sam/reader/record/reference_sequence_id: Add tests

--- noodles-sam/src/reader/record/reference_sequence_id.rs
+++ noodles-sam/src/reader/record/reference_sequence_id.rs
@@ -26,3 +26,37 @@ pub(super) fn parse_reference_sequence_id(
             }),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::num::NonZeroUsize;
+
+    use super::*;
+    use crate::header::record::value::{map::ReferenceSequence, Map};
+
+    #[test]
+    fn test_parse_reference_sequence_id() -> Result<(), Box<dyn std::error::Error>> {
+        let header = Header::builder()
+            .add_reference_sequence(
+                "sq0".parse()?,
+                Map::<ReferenceSequence>::new(NonZeroUsize::try_from(8)?),
+            )
+            .add_reference_sequence(
+                "sq1".parse()?,
+                Map::<ReferenceSequence>::new(NonZeroUsize::try_from(13)?),
+            )
+            .build();
+
+        assert!(parse_reference_sequence_id(&header, b"*")?.is_none());
+
+        assert_eq!(parse_reference_sequence_id(&header, b"sq0")?, Some(0));
+        assert_eq!(parse_reference_sequence_id(&header, b"sq1")?, Some(1));
+
+        assert!(matches!(
+            parse_reference_sequence_id(&header, b"sq2"),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+
+        Ok(())
+    }
+}
