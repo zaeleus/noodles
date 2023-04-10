05f0fe277835c5b563f5756bba7c359f9114dba2
Michael Macias <zaeleus@gmail.com>
2023-04-10 14:53:59 -0500

    bam/reader/record/cigar/op: Move kind decoder to a module

--- noodles-bam/src/reader/record/cigar/op.rs
+++ noodles-bam/src/reader/record/cigar/op.rs
@@ -1,6 +1,10 @@
+mod kind;
+
 use std::io;
 
-use noodles_sam::record::cigar::{op::Kind, Op};
+use noodles_sam::record::cigar::Op;
+
+use self::kind::decode_kind;
 
 pub(crate) fn decode_op(n: u32) -> io::Result<Op> {
     let kind = decode_kind(n)?;
@@ -8,26 +12,10 @@ pub(crate) fn decode_op(n: u32) -> io::Result<Op> {
     Ok(Op::new(kind, len))
 }
 
-fn decode_kind(n: u32) -> io::Result<Kind> {
-    match n & 0x0f {
-        0 => Ok(Kind::Match),
-        1 => Ok(Kind::Insertion),
-        2 => Ok(Kind::Deletion),
-        3 => Ok(Kind::Skip),
-        4 => Ok(Kind::SoftClip),
-        5 => Ok(Kind::HardClip),
-        6 => Ok(Kind::Pad),
-        7 => Ok(Kind::SequenceMatch),
-        8 => Ok(Kind::SequenceMismatch),
-        _ => Err(io::Error::new(
-            io::ErrorKind::InvalidData,
-            "invalid CIGAR op kind",
-        )),
-    }
-}
-
 #[cfg(test)]
 mod tests {
+    use noodles_sam::record::cigar::op::Kind;
+
     use super::*;
 
     #[test]
@@ -36,26 +24,6 @@ mod tests {
 
         assert!(matches!(
             decode_op(0x19),
-            Err(e) if e.kind() == io::ErrorKind::InvalidData
-        ));
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_decode_kind() -> io::Result<()> {
-        assert_eq!(decode_kind(0x00)?, Kind::Match);
-        assert_eq!(decode_kind(0x01)?, Kind::Insertion);
-        assert_eq!(decode_kind(0x02)?, Kind::Deletion);
-        assert_eq!(decode_kind(0x03)?, Kind::Skip);
-        assert_eq!(decode_kind(0x04)?, Kind::SoftClip);
-        assert_eq!(decode_kind(0x05)?, Kind::HardClip);
-        assert_eq!(decode_kind(0x06)?, Kind::Pad);
-        assert_eq!(decode_kind(0x07)?, Kind::SequenceMatch);
-        assert_eq!(decode_kind(0x08)?, Kind::SequenceMismatch);
-
-        assert!(matches!(
-            decode_op(0x09),
             Err(e) if e.kind() == io::ErrorKind::InvalidData
         ));
 
---
+++ noodles-bam/src/reader/record/cigar/op/kind.rs
@@ -0,0 +1,46 @@
+use std::io;
+
+use noodles_sam::record::cigar::op::Kind;
+
+pub(super) fn decode_kind(n: u32) -> io::Result<Kind> {
+    match n & 0x0f {
+        0 => Ok(Kind::Match),
+        1 => Ok(Kind::Insertion),
+        2 => Ok(Kind::Deletion),
+        3 => Ok(Kind::Skip),
+        4 => Ok(Kind::SoftClip),
+        5 => Ok(Kind::HardClip),
+        6 => Ok(Kind::Pad),
+        7 => Ok(Kind::SequenceMatch),
+        8 => Ok(Kind::SequenceMismatch),
+        _ => Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "invalid CIGAR op kind",
+        )),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_decode_kind() -> io::Result<()> {
+        assert_eq!(decode_kind(0x00)?, Kind::Match);
+        assert_eq!(decode_kind(0x01)?, Kind::Insertion);
+        assert_eq!(decode_kind(0x02)?, Kind::Deletion);
+        assert_eq!(decode_kind(0x03)?, Kind::Skip);
+        assert_eq!(decode_kind(0x04)?, Kind::SoftClip);
+        assert_eq!(decode_kind(0x05)?, Kind::HardClip);
+        assert_eq!(decode_kind(0x06)?, Kind::Pad);
+        assert_eq!(decode_kind(0x07)?, Kind::SequenceMatch);
+        assert_eq!(decode_kind(0x08)?, Kind::SequenceMismatch);
+
+        assert!(matches!(
+            decode_kind(0x09),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+
+        Ok(())
+    }
+}
