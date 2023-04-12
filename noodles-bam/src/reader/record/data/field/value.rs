65851ca66f2f7e785f724905a690075fb7f976d1
Michael Macias <zaeleus@gmail.com>
2023-04-11 22:27:06 -0500

    bam/reader/record/data/field/value: Move array decoder to a module

--- noodles-bam/src/reader/record/data/field/value.rs
+++ noodles-bam/src/reader/record/data/field/value.rs
@@ -1,3 +1,4 @@
+mod array;
 mod subtype;
 mod ty;
 
@@ -7,9 +8,11 @@ use std::{error, fmt, mem, num, string};
 
 use bytes::Buf;
 use noodles_sam::record::data::field::{
-    value::{character, hex, Character, Subtype, Type},
+    value::{character, hex, Character, Type},
     Value,
 };
+
+use self::array::get_array_value;
 
 // An error when a raw BAM record data field value fails to parse.
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -208,80 +211,6 @@ where
     get_string(src)
         .and_then(|s| s.parse().map_err(ParseError::InvalidHex))
         .map(Value::Hex)
-}
-
-fn get_array_value<B>(src: &mut B) -> Result<Value, ParseError>
-where
-    B: Buf,
-{
-    let subtype = get_subtype(src).map_err(ParseError::InvalidArraySubtype)?;
-    let len = usize::try_from(src.get_i32_le()).map_err(ParseError::InvalidArrayLength)?;
-
-    match subtype {
-        Subtype::Int8 => {
-            let mut buf = Vec::with_capacity(len);
-
-            for _ in 0..len {
-                buf.push(src.get_i8());
-            }
-
-            Ok(Value::Int8Array(buf))
-        }
-        Subtype::UInt8 => {
-            let mut buf = Vec::with_capacity(len);
-
-            for _ in 0..len {
-                buf.push(src.get_u8());
-            }
-
-            Ok(Value::UInt8Array(buf))
-        }
-        Subtype::Int16 => {
-            let mut buf = Vec::with_capacity(len);
-
-            for _ in 0..len {
-                buf.push(src.get_i16_le());
-            }
-
-            Ok(Value::Int16Array(buf))
-        }
-        Subtype::UInt16 => {
-            let mut buf = Vec::with_capacity(len);
-
-            for _ in 0..len {
-                buf.push(src.get_u16_le());
-            }
-
-            Ok(Value::UInt16Array(buf))
-        }
-        Subtype::Int32 => {
-            let mut buf = Vec::with_capacity(len);
-
-            for _ in 0..len {
-                buf.push(src.get_i32_le());
-            }
-
-            Ok(Value::Int32Array(buf))
-        }
-        Subtype::UInt32 => {
-            let mut buf = Vec::with_capacity(len);
-
-            for _ in 0..len {
-                buf.push(src.get_u32_le());
-            }
-
-            Ok(Value::UInt32Array(buf))
-        }
-        Subtype::Float => {
-            let mut buf = Vec::with_capacity(len);
-
-            for _ in 0..len {
-                buf.push(src.get_f32_le());
-            }
-
-            Ok(Value::FloatArray(buf))
-        }
-    }
 }
 
 #[cfg(test)]
---
+++ noodles-bam/src/reader/record/data/field/value/array.rs
@@ -0,0 +1,78 @@
+use bytes::Buf;
+use noodles_sam::record::data::field::{value::Subtype, Value};
+
+use super::{get_subtype, ParseError};
+
+pub(super) fn get_array_value<B>(src: &mut B) -> Result<Value, ParseError>
+where
+    B: Buf,
+{
+    let subtype = get_subtype(src).map_err(ParseError::InvalidArraySubtype)?;
+    let len = usize::try_from(src.get_i32_le()).map_err(ParseError::InvalidArrayLength)?;
+
+    match subtype {
+        Subtype::Int8 => {
+            let mut buf = Vec::with_capacity(len);
+
+            for _ in 0..len {
+                buf.push(src.get_i8());
+            }
+
+            Ok(Value::Int8Array(buf))
+        }
+        Subtype::UInt8 => {
+            let mut buf = Vec::with_capacity(len);
+
+            for _ in 0..len {
+                buf.push(src.get_u8());
+            }
+
+            Ok(Value::UInt8Array(buf))
+        }
+        Subtype::Int16 => {
+            let mut buf = Vec::with_capacity(len);
+
+            for _ in 0..len {
+                buf.push(src.get_i16_le());
+            }
+
+            Ok(Value::Int16Array(buf))
+        }
+        Subtype::UInt16 => {
+            let mut buf = Vec::with_capacity(len);
+
+            for _ in 0..len {
+                buf.push(src.get_u16_le());
+            }
+
+            Ok(Value::UInt16Array(buf))
+        }
+        Subtype::Int32 => {
+            let mut buf = Vec::with_capacity(len);
+
+            for _ in 0..len {
+                buf.push(src.get_i32_le());
+            }
+
+            Ok(Value::Int32Array(buf))
+        }
+        Subtype::UInt32 => {
+            let mut buf = Vec::with_capacity(len);
+
+            for _ in 0..len {
+                buf.push(src.get_u32_le());
+            }
+
+            Ok(Value::UInt32Array(buf))
+        }
+        Subtype::Float => {
+            let mut buf = Vec::with_capacity(len);
+
+            for _ in 0..len {
+                buf.push(src.get_f32_le());
+            }
+
+            Ok(Value::FloatArray(buf))
+        }
+    }
+}
