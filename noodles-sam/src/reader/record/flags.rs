cc30f663674ac7da1d137e74f7191f1421c8e42d
Michael Macias <zaeleus@gmail.com>
2023-04-13 20:39:53 -0500

    sam/reader/record/flags: Add tests

--- noodles-sam/src/reader/record/flags.rs
+++ noodles-sam/src/reader/record/flags.rs
@@ -7,3 +7,31 @@ pub(crate) fn parse_flags(src: &[u8]) -> io::Result<Flags> {
         .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
         .map(Flags::from)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_flags() -> io::Result<()> {
+        assert_eq!(parse_flags(b"0")?, Flags::empty());
+        assert_eq!(parse_flags(b"4")?, Flags::UNMAPPED);
+
+        assert!(matches!(
+            parse_flags(b""),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+
+        assert!(matches!(
+            parse_flags(b"-4"),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+
+        assert!(matches!(
+            parse_flags(b"n"),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+
+        Ok(())
+    }
+}
