c55a80431d21af3eb8a084e5b84a30d40d5c4fa3
Michael Macias <zaeleus@gmail.com>
2023-04-25 21:35:04 -0500

    vcf/header/record/value/map/info/tag: Organize constants

--- noodles-vcf/src/header/record/value/map/info/tag.rs
+++ noodles-vcf/src/header/record/value/map/info/tag.rs
@@ -8,8 +8,8 @@ pub type Tag = map::tag::Tag<StandardTag>;
 // For some reason, using the `Tag` type alias produces a `nontrivial_structural_match` warning
 // when pattern matching, so it's avoided here.
 pub(super) const ID: Tag = map::tag::Tag::<StandardTag>::Standard(StandardTag::Id);
-pub(super) const TYPE: Tag = map::tag::Tag::<StandardTag>::Standard(StandardTag::Type);
 pub(super) const NUMBER: Tag = map::tag::Tag::<StandardTag>::Standard(StandardTag::Number);
+pub(super) const TYPE: Tag = map::tag::Tag::<StandardTag>::Standard(StandardTag::Type);
 pub(super) const DESCRIPTION: Tag =
     map::tag::Tag::<StandardTag>::Standard(StandardTag::Description);
 pub(super) const IDX: Tag = map::tag::Tag::<StandardTag>::Standard(StandardTag::Idx);
