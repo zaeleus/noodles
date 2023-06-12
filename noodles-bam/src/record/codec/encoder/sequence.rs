457de4e960f0f3eaf343a92676992fb1f733404c
Art Rand <art.rand@nanoporetech.com>
2023-06-12 06:58:29 -0700

    [bam] makes async writer no-op for empty sequence.

--- noodles-bam/src/record/codec/encoder/sequence.rs
+++ noodles-bam/src/record/codec/encoder/sequence.rs
@@ -7,6 +7,10 @@ pub fn put_sequence<B>(dst: &mut B, read_length: usize, sequence: &Sequence) -> 
 where
     B: BufMut,
 {
+    if sequence.is_empty() {
+        return Ok(());
+    }
+
     // § 1.4.10 "`SEQ`" (2022-08-22): "If not a '*', the length of the sequence must equal the sum
     // of lengths of `M`/`I`/`S`/`=`/`X` operations in `CIGAR`."
     if read_length > 0 && sequence.len() != read_length {
