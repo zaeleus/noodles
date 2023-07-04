c2524f8e8a6be3f14419c68c2350194297f69c24
Michael Macias <zaeleus@gmail.com>
2023-07-04 10:47:09 -0500

    sam/record/data/field/value/base_modifications: Add group

--- noodles-sam/src/record/data/field/value/base_modifications/group.rs
+++ noodles-sam/src/record/data/field/value/base_modifications/group.rs
@@ -1,3 +1,39 @@
 mod status;
 mod strand;
 mod unmodified_base;
+
+use self::{status::Status, strand::Strand, unmodified_base::UnmodifiedBase};
+
+/// A base modifications group.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Group {
+    unmodified_base: UnmodifiedBase,
+    strand: Strand,
+    status: Option<Status>,
+}
+
+impl Group {
+    /// Creates a base modifications group.
+    fn new(unmodified_base: UnmodifiedBase, strand: Strand, status: Option<Status>) -> Self {
+        Self {
+            unmodified_base,
+            strand,
+            status,
+        }
+    }
+
+    /// Returns the unmodified base.
+    pub fn unmodified_base(&self) -> UnmodifiedBase {
+        self.unmodified_base
+    }
+
+    /// Returns the strand.
+    pub fn strand(&self) -> Strand {
+        self.strand
+    }
+
+    /// Returns the status.
+    pub fn status(&self) -> Option<Status> {
+        self.status
+    }
+}
