7dbefde7ed12f0fb2a1f99c8edd8f49c488bb6a7
Michael Macias <zaeleus@gmail.com>
2023-07-05 11:00:19 -0500

    sam/record/data/field/value: Add base modifications

--- noodles-sam/src/record/data/field/value/base_modifications.rs
+++ noodles-sam/src/record/data/field/value/base_modifications.rs
@@ -1,3 +1,18 @@
 #![allow(dead_code)]
 
 mod group;
+
+use std::ops::Deref;
+
+use self::group::Group;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct BaseModifications(Vec<Group>);
+
+impl Deref for BaseModifications {
+    type Target = [Group];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
