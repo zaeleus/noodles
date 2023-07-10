2f68f580bdd8e6565e83ab967ac5d7beacf3739c
Michael Macias <zaeleus@gmail.com>
2023-07-10 15:46:15 -0500

    sam/record/data/field/value/base_modifications: Implement AsRef and AsMut

--- noodles-sam/src/record/data/field/value/base_modifications.rs
+++ noodles-sam/src/record/data/field/value/base_modifications.rs
@@ -8,6 +8,18 @@ pub use self::group::Group;
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct BaseModifications(Vec<Group>);
 
+impl AsRef<[Group]> for BaseModifications {
+    fn as_ref(&self) -> &[Group] {
+        &self.0
+    }
+}
+
+impl AsMut<Vec<Group>> for BaseModifications {
+    fn as_mut(&mut self) -> &mut Vec<Group> {
+        &mut self.0
+    }
+}
+
 impl From<Vec<Group>> for BaseModifications {
     fn from(groups: Vec<Group>) -> Self {
         Self(groups)
