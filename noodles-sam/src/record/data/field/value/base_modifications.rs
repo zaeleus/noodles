f79b122d68279a9cd92d34b542c793773d943a5e
Michael Macias <zaeleus@gmail.com>
2023-07-18 11:56:53 -0500

    sam/record/data/field/value/base_modifications: Add parse example

--- noodles-sam/src/record/data/field/value/base_modifications.rs
+++ noodles-sam/src/record/data/field/value/base_modifications.rs
@@ -13,6 +13,37 @@ pub struct BaseModifications(Vec<Group>);
 
 impl BaseModifications {
     /// Parses base modifications from a string.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_sam::record::data::field::value::{
+    ///     base_modifications::{
+    ///         group::{modification, Strand, UnmodifiedBase},
+    ///         Group,
+    ///     },
+    ///     BaseModifications,
+    /// };
+    ///
+    /// let is_reverse_complemented = false;
+    /// let sequence = "CACCCGATGACCGGCT".parse()?;
+    /// let base_modifications = BaseModifications::parse(
+    ///     "C+m,1,3,0;",
+    ///     is_reverse_complemented,
+    ///     &sequence,
+    /// )?;
+    ///
+    /// assert_eq!(base_modifications, BaseModifications::from(vec![
+    ///     Group::new(
+    ///         UnmodifiedBase::C,
+    ///         Strand::Forward,
+    ///         vec![modification::FIVE_METHYLCYTOSINE],
+    ///         None,
+    ///         vec![2, 11, 14],
+    ///     )
+    /// ]));
+    /// # Ok::<_, Box<dyn std::error::Error>>(())
+    /// ```
     pub fn parse(
         s: &str,
         is_reverse_complemented: bool,
