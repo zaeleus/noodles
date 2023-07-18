3c094747a5a4c41d2f498cc13421df02a90532e9
Michael Macias <zaeleus@gmail.com>
2023-07-18 16:39:11 -0500

    refget/sequence/serivce: Define the subsequence limit to be non-zero

--- noodles-refget/src/sequence/service.rs
+++ noodles-refget/src/sequence/service.rs
@@ -2,7 +2,9 @@ mod builder;
 
 pub use self::builder::Builder;
 
-use serde::Deserialize;
+use std::num::NonZeroU32;
+
+use serde::{Deserialize, Deserializer};
 
 /// Service information.
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
@@ -10,7 +12,8 @@ pub struct Service {
     circular_supported: bool,
     algorithms: Vec<String>,
     identifier_types: Vec<String>,
-    subsequence_limit: Option<u32>,
+    #[serde(deserialize_with = "deserialize_subsequence_limit")]
+    subsequence_limit: Option<NonZeroU32>,
 }
 
 impl Service {
@@ -31,8 +34,43 @@ impl Service {
 
     /// Returns the maximum length of an interval.
     ///
-    /// If missing or smaller than 1, there is no limit.
-    pub fn subsequence_limit(&self) -> Option<u32> {
+    /// If missing, there is no limit.
+    pub fn subsequence_limit(&self) -> Option<NonZeroU32> {
         self.subsequence_limit
+    }
+}
+
+fn deserialize_subsequence_limit<'de, D>(deserializer: D) -> Result<Option<NonZeroU32>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value: Option<u32> = Deserialize::deserialize(deserializer)?;
+
+    match value {
+        Some(n) => Ok(NonZeroU32::new(n)),
+        None => Ok(None),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_test::{assert_de_tokens, Token};
+
+    use super::*;
+
+    #[test]
+    fn test_deserialize_subsequence_limit() {
+        #[derive(Debug, Deserialize, Eq, PartialEq)]
+        #[serde(transparent)]
+        struct SubsequenceLimit(
+            #[serde(deserialize_with = "deserialize_subsequence_limit")] Option<NonZeroU32>,
+        );
+
+        assert_de_tokens(
+            &SubsequenceLimit(NonZeroU32::new(8)),
+            &[Token::Some, Token::I64(8)],
+        );
+        assert_de_tokens(&SubsequenceLimit(None), &[Token::Some, Token::I64(0)]);
+        assert_de_tokens(&SubsequenceLimit(None), &[Token::None]);
     }
 }
