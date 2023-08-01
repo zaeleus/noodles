191b4d135d0eae9935cc603d7825b5fce0084d8f
Michael Macias <zaeleus@gmail.com>
2023-08-01 11:31:05 -0500

    util/variant/indexed_reader/builder: Add examples

--- noodles-util/src/variant/indexed_reader/builder.rs
+++ noodles-util/src/variant/indexed_reader/builder.rs
@@ -24,24 +24,71 @@ pub struct Builder {
 
 impl Builder {
     /// Sets the compression of the input.
+    ///
+    /// By default, the compression is autodetected on build. This can be used to override it, but
+    /// note that only bgzip-compressed streams can be indexed.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_util::variant::{self, Compression};
+    /// let builder = variant::indexed_reader::Builder::default()
+    ///     .set_compression(Some(Compression::Bgzf));
+    /// ```
     pub fn set_compression(mut self, compression: Option<Compression>) -> Self {
         self.compression = Some(compression);
         self
     }
 
     /// Sets the format of the input.
+    ///
+    /// By default, the format is autodetected on build. This can be used to override it.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_util::variant::{self, Format};
+    /// let builder = variant::indexed_reader::Builder::default().set_format(Format::Vcf);
+    /// ```
     pub fn set_format(mut self, format: Format) -> Self {
         self.format = Some(format);
         self
     }
 
     /// Sets an index.
+    ///
+    /// When building from a path ([`Self::build_from_path`]), an associated index at `<src>.tbi`
+    /// or `<src>.csi` will attempt to be loaded. This can be used to override it if the index
+    /// cannot be found or when building from a reader (`[Self::build_from_reader]`).
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_csi as csi;
+    /// use noodles_util::variant;
+    ///
+    /// let index = csi::Index::default();
+    /// let builder = variant::indexed_reader::Builder::default().set_index(index);
+    /// ```
     pub fn set_index(mut self, index: csi::Index) -> Self {
         self.index = Some(index);
         self
     }
 
     /// Builds an indexed variant reader from a path.
+    ///
+    /// The compression method and format will be autodetected, if not overridden. If no index is
+    /// set ([`Self::set_index`]), this will attempt to load an associated index at `<src>.tbi` or
+    /// `<src>.csi`.
+    ///
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use noodles_util::variant;
+    /// let reader = variant::indexed_reader::Builder::default().build_from_path("sample.vcf.gz")?;
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn build_from_path<P>(self, src: P) -> io::Result<IndexedReader<File>>
     where
         P: AsRef<Path>,
@@ -85,6 +132,28 @@ impl Builder {
     }
 
     /// Builds an indexed variant reader from a path.
+    ///
+    /// The compression method and format will be autodetected, if not overridden. An index must be
+    /// set (`[Self::set_index]`). The reader must be a bgzip-compressed stream.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::io::{self, Write};
+    /// use noodles_bgzf as bgzf;
+    /// use noodles_csi as csi;
+    /// use noodles_util::variant;
+    ///
+    /// let mut writer = bgzf::Writer::new(Vec::new());
+    /// writer.write_all(b"BCF")?;
+    /// let data = writer.finish()?;
+    ///
+    /// let index = csi::Index::default();
+    /// let reader = variant::indexed_reader::Builder::default()
+    ///     .set_index(index)
+    ///     .build_from_reader(&data[..])?;
+    /// # Ok::<_, std::io::Error>(())
+    /// ```
     pub fn build_from_reader<R>(self, reader: R) -> io::Result<IndexedReader<R>>
     where
         R: Read,
