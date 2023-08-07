a5c8c6855aa7816152c655feae68bfcd19edde5c
Michael Macias <zaeleus@gmail.com>
2023-08-07 09:55:29 -0500

    util/alignment/indexed_reader/builder: Add Builder::build_from_path

--- noodles-util/src/alignment/indexed_reader/builder.rs
+++ noodles-util/src/alignment/indexed_reader/builder.rs
@@ -1,22 +1,44 @@
 use std::{
     fs::File,
-    io::{self, BufReader},
+    io::{self, BufReader, Read},
     path::Path,
 };
 
 use noodles_bam as bam;
-use noodles_cram as cram;
+use noodles_cram::{self as cram, crai};
+use noodles_csi as csi;
 use noodles_fasta as fasta;
 use noodles_sam as sam;
 
 use super::IndexedReader;
-use crate::alignment::Format;
+use crate::alignment::{reader::builder::detect_format, Format};
+
+/// An alignment index.
+pub enum Index {
+    /// CSI.
+    Csi(csi::Index),
+    /// CRAI.
+    Crai(crai::Index),
+}
+
+impl From<csi::Index> for Index {
+    fn from(index: csi::Index) -> Self {
+        Self::Csi(index)
+    }
+}
+
+impl From<crai::Index> for Index {
+    fn from(index: crai::Index) -> Self {
+        Self::Crai(index)
+    }
+}
 
 /// An indexed alignment reader builder.
 #[derive(Default)]
 pub struct Builder {
     format: Option<Format>,
     reference_sequence_repository: fasta::Repository,
+    index: Option<Index>,
 }
 
 impl Builder {
@@ -35,13 +57,20 @@ impl Builder {
         self
     }
 
+    /// Sets an index.
+    pub fn set_index<I>(mut self, index: I) -> Self
+    where
+        I: Into<Index>,
+    {
+        self.index = Some(index.into());
+        self
+    }
+
     /// Builds an indexed alignment reader from a path.
     pub fn build_from_path<P>(self, src: P) -> io::Result<IndexedReader<File>>
     where
         P: AsRef<Path>,
     {
-        use crate::alignment::reader::builder::detect_format;
-
         let mut reader = File::open(src.as_ref()).map(BufReader::new)?;
 
         let format = match self.format {
@@ -64,6 +93,54 @@ impl Builder {
                 .set_reference_sequence_repository(self.reference_sequence_repository)
                 .build_from_path(src)
                 .map(IndexedReader::Cram),
+        }
+    }
+
+    /// Builds an indexed alignment reader from a reader.
+    pub fn build_from_reader<R>(self, reader: R) -> io::Result<IndexedReader<BufReader<R>>>
+    where
+        R: Read,
+    {
+        let mut reader = BufReader::new(reader);
+
+        let format = match self.format {
+            Some(format) => format,
+            None => detect_format(&mut reader)?,
+        };
+
+        match format {
+            Format::Sam => Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "source not bgzip-compressed",
+            )),
+            Format::SamGz => {
+                let mut builder = sam::indexed_reader::Builder::default();
+
+                if let Some(Index::Csi(index)) = self.index {
+                    builder = builder.set_index(index);
+                }
+
+                builder.build_from_reader(reader).map(IndexedReader::Sam)
+            }
+            Format::Bam => {
+                let mut builder = bam::indexed_reader::Builder::default();
+
+                if let Some(Index::Csi(index)) = self.index {
+                    builder = builder.set_index(index);
+                }
+
+                builder.build_from_reader(reader).map(IndexedReader::Bam)
+            }
+            Format::Cram => {
+                let mut builder = cram::indexed_reader::Builder::default()
+                    .set_reference_sequence_repository(self.reference_sequence_repository);
+
+                if let Some(Index::Crai(index)) = self.index {
+                    builder = builder.set_index(index);
+                }
+
+                builder.build_from_reader(reader).map(IndexedReader::Cram)
+            }
         }
     }
 }
