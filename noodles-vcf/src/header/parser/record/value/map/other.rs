34263fa300bb28b79b955cadaaf767f7e154ffd8
Michael Macias <zaeleus@gmail.com>
2023-08-14 12:00:41 -0500

    vcf/header/parser/record/value/map/other: Add ID as parse error context

--- noodles-vcf/src/header/parser/record/value/map/other.rs
+++ noodles-vcf/src/header/parser/record/value/map/other.rs
@@ -10,9 +10,8 @@ use crate::header::record::value::{
     Map,
 };
 
-/// An error returned when a VCF header record other map value fails to parse.
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub enum ParseError {
+pub enum ParseErrorKind {
     InvalidMap(super::ParseError),
     InvalidField(super::field::ParseError),
     InvalidKey(super::field::key::ParseError),
@@ -26,15 +25,32 @@ pub enum ParseError {
     DuplicateTag(Tag),
 }
 
+/// An error returned when a VCF header record other map value fails to parse.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ParseError {
+    id: Option<String>,
+    kind: ParseErrorKind,
+}
+
+impl ParseError {
+    fn new(id: Option<String>, kind: ParseErrorKind) -> Self {
+        Self { id, kind }
+    }
+
+    pub(crate) fn id(&self) -> Option<&str> {
+        self.id.as_deref()
+    }
+}
+
 impl error::Error for ParseError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match self {
-            ParseError::InvalidMap(e) => Some(e),
-            ParseError::InvalidField(e) => Some(e),
-            ParseError::InvalidKey(e) => Some(e),
-            ParseError::InvalidValue(e) => Some(e),
-            ParseError::InvalidValues(e) => Some(e),
-            ParseError::InvalidOther(_, e) => Some(e),
+        match &self.kind {
+            ParseErrorKind::InvalidMap(e) => Some(e),
+            ParseErrorKind::InvalidField(e) => Some(e),
+            ParseErrorKind::InvalidKey(e) => Some(e),
+            ParseErrorKind::InvalidValue(e) => Some(e),
+            ParseErrorKind::InvalidValues(e) => Some(e),
+            ParseErrorKind::InvalidOther(_, e) => Some(e),
             _ => None,
         }
     }
@@ -42,15 +58,15 @@ impl error::Error for ParseError {
 
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::InvalidMap(_) => write!(f, "invalid map"),
-            Self::InvalidField(_) => write!(f, "invalid field"),
-            Self::InvalidKey(_) => write!(f, "invalid key"),
-            Self::InvalidValue(_) => write!(f, "invalid value"),
-            Self::MissingId => write!(f, "missing ID"),
-            Self::InvalidValues(_) => write!(f, "invalid values"),
-            Self::InvalidOther(tag, _) => write!(f, "invalid other: {tag}"),
-            Self::DuplicateTag(tag) => write!(f, "duplicate tag: {tag}"),
+        match &self.kind {
+            ParseErrorKind::InvalidMap(_) => write!(f, "invalid map"),
+            ParseErrorKind::InvalidField(_) => write!(f, "invalid field"),
+            ParseErrorKind::InvalidKey(_) => write!(f, "invalid key"),
+            ParseErrorKind::InvalidValue(_) => write!(f, "invalid value"),
+            ParseErrorKind::MissingId => write!(f, "missing ID"),
+            ParseErrorKind::InvalidValues(_) => write!(f, "invalid values"),
+            ParseErrorKind::InvalidOther(tag, _) => write!(f, "invalid other: {tag}"),
+            ParseErrorKind::DuplicateTag(tag) => write!(f, "duplicate tag: {tag}"),
         }
     }
 }
@@ -58,7 +74,7 @@ impl fmt::Display for ParseError {
 pub fn parse_other(src: &mut &[u8]) -> Result<(String, Map<Other>), ParseError> {
     const VALUES: &str = "Values";
 
-    super::consume_prefix(src).map_err(ParseError::InvalidMap)?;
+    super::consume_prefix(src).map_err(|e| ParseError::new(None, ParseErrorKind::InvalidMap(e)))?;
 
     let mut id = None;
 
@@ -67,29 +83,33 @@ pub fn parse_other(src: &mut &[u8]) -> Result<(String, Map<Other>), ParseError> 
     loop {
         let tag = parse_key(src)
             .map(Tag::from)
-            .map_err(ParseError::InvalidKey)?;
+            .map_err(|e| ParseError::new(id.clone(), ParseErrorKind::InvalidKey(e)))?;
 
         match tag {
-            tag::ID => parse_id(src).and_then(|v| try_replace(&mut id, tag::ID, v))?,
+            tag::ID => parse_id(src, &id).and_then(|v| try_replace(&mut id, &None, tag::ID, v))?,
             Tag::Other(t) => {
                 if t.as_ref() == VALUES {
-                    parse_values(src, &t)
-                        .and_then(|value| try_insert(&mut other_fields, t, value))?;
+                    parse_values(src, &id, &t)
+                        .and_then(|value| try_insert(&mut other_fields, &id, t, value))?;
                 } else {
-                    parse_other_value(src, &t)
-                        .and_then(|value| try_insert(&mut other_fields, t, value))?;
+                    parse_other_value(src, &id, &t)
+                        .and_then(|value| try_insert(&mut other_fields, &id, t, value))?;
                 }
             }
         }
 
-        if !super::field::consume_separator(src).map_err(ParseError::InvalidField)? {
+        let has_separator = super::field::consume_separator(src)
+            .map_err(|e| ParseError::new(id.clone(), ParseErrorKind::InvalidField(e)))?;
+
+        if !has_separator {
             break;
         }
     }
 
-    super::consume_suffix(src).map_err(ParseError::InvalidMap)?;
+    super::consume_suffix(src)
+        .map_err(|e| ParseError::new(id.clone(), ParseErrorKind::InvalidMap(e)))?;
 
-    let id = id.ok_or(ParseError::MissingId)?;
+    let id = id.ok_or_else(|| ParseError::new(None, ParseErrorKind::MissingId))?;
 
     Ok((
         id,
@@ -100,14 +120,15 @@ pub fn parse_other(src: &mut &[u8]) -> Result<(String, Map<Other>), ParseError> 
     ))
 }
 
-fn parse_id(src: &mut &[u8]) -> Result<String, ParseError> {
+fn parse_id(src: &mut &[u8], id: &Option<String>) -> Result<String, ParseError> {
     parse_value(src)
         .map(String::from)
-        .map_err(ParseError::InvalidValue)
+        .map_err(|e| ParseError::new(id.clone(), ParseErrorKind::InvalidValue(e)))
 }
 
 fn parse_values(
     src: &mut &[u8],
+    id: &Option<String>,
     tag: &map::tag::Other<tag::StandardTag>,
 ) -> Result<String, ParseError> {
     const PREFIX: u8 = b'[';
@@ -118,34 +139,48 @@ fn parse_values(
     if is_delimited {
         if let Some(i) = src.iter().position(|&b| b == SUFFIX) {
             let (buf, rest) = src.split_at(i + 1);
-            let s = str::from_utf8(buf).map_err(ParseError::InvalidValues)?;
+
+            let s = str::from_utf8(buf)
+                .map_err(|e| ParseError::new(id.clone(), ParseErrorKind::InvalidValues(e)))?;
+
             *src = rest;
+
             return Ok(s.into());
         }
     }
 
-    parse_other_value(src, tag)
+    parse_other_value(src, id, tag)
 }
 
 fn parse_other_value(
     src: &mut &[u8],
+    id: &Option<String>,
     tag: &map::tag::Other<tag::StandardTag>,
 ) -> Result<String, ParseError> {
     parse_value(src)
         .map(String::from)
-        .map_err(|e| ParseError::InvalidOther(tag.clone(), e))
+        .map_err(|e| ParseError::new(id.clone(), ParseErrorKind::InvalidOther(tag.clone(), e)))
 }
 
-fn try_replace<T>(option: &mut Option<T>, tag: Tag, value: T) -> Result<(), ParseError> {
+fn try_replace<T>(
+    option: &mut Option<T>,
+    id: &Option<String>,
+    tag: Tag,
+    value: T,
+) -> Result<(), ParseError> {
     if option.replace(value).is_none() {
         Ok(())
     } else {
-        Err(ParseError::DuplicateTag(tag))
+        Err(ParseError::new(
+            id.clone(),
+            ParseErrorKind::DuplicateTag(tag),
+        ))
     }
 }
 
 fn try_insert(
     other_fields: &mut OtherFields<tag::StandardTag>,
+    id: &Option<String>,
     tag: map::tag::Other<tag::StandardTag>,
     value: String,
 ) -> Result<(), ParseError> {
@@ -158,7 +193,10 @@ fn try_insert(
         }
         Entry::Occupied(entry) => {
             let (t, _) = entry.remove_entry();
-            Err(ParseError::DuplicateTag(Tag::Other(t)))
+            Err(ParseError::new(
+                id.clone(),
+                ParseErrorKind::DuplicateTag(Tag::Other(t)),
+            ))
         }
     }
 }
