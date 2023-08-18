9d8302e621f1391cc0c7249b70e671be48b89b8c
Michael Macias <zaeleus@gmail.com>
2023-08-18 10:41:23 -0500

    sam/record/record/cigar: Push ops into given container

--- noodles-sam/src/reader/record/cigar.rs
+++ noodles-sam/src/reader/record/cigar.rs
@@ -1,6 +1,6 @@
 mod op;
 
-use std::{error, fmt, mem};
+use std::{error, fmt};
 
 use self::op::parse_op;
 use crate::record::Cigar;
@@ -10,8 +10,6 @@ use crate::record::Cigar;
 pub enum ParseError {
     /// The input is empty.
     Empty,
-    /// The input is invalid.
-    Invalid,
     /// An op is invalid.
     InvalidOp(op::ParseError),
 }
@@ -29,7 +27,6 @@ impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Empty => write!(f, "empty input"),
-            Self::Invalid => write!(f, "invalid input"),
             Self::InvalidOp(_) => write!(f, "invalid op"),
         }
     }
@@ -40,14 +37,12 @@ pub(crate) fn parse_cigar(mut src: &[u8], cigar: &mut Cigar) -> Result<(), Parse
         return Err(ParseError::Empty);
     }
 
-    let mut ops = Vec::from(mem::take(cigar));
+    cigar.clear();
 
     while !src.is_empty() {
         let op = parse_op(&mut src).map_err(ParseError::InvalidOp)?;
-        ops.push(op);
+        cigar.as_mut().push(op);
     }
-
-    *cigar = Cigar::try_from(ops).map_err(|_| ParseError::Invalid)?;
 
     Ok(())
 }
