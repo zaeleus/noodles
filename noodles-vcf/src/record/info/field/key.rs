797e1d6782612046c38d4b50b6f29de875cea829
Michael Macias <zaeleus@gmail.com>
2023-08-22 14:22:29 -0500

    vcf/record/info/field/key: Move Other to a module

--- noodles-vcf/src/record/info/field/key.rs
+++ noodles-vcf/src/record/info/field/key.rs
@@ -1,10 +1,11 @@
 //! VCF record info key.
 
+mod other;
 mod standard;
 
 use std::{borrow::Borrow, error, fmt, str::FromStr};
 
-pub use self::standard::Standard;
+pub use self::{other::Other, standard::Standard};
 
 /// Ancestral allele (`AA`).
 pub const ANCESTRAL_ALLELE: Key = Key::Standard(Standard::AncestralAllele);
@@ -220,51 +221,6 @@ impl fmt::Display for ParseError {
             Self::Invalid => f.write_str("invalid input"),
         }
     }
-}
-
-/// A non-reserved VCF header info key.
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub struct Other(String);
-
-impl AsRef<str> for Other {
-    fn as_ref(&self) -> &str {
-        &self.0
-    }
-}
-
-impl fmt::Display for Other {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.as_ref().fmt(f)
-    }
-}
-
-impl FromStr for Other {
-    type Err = ParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if is_valid_name(s) {
-            Ok(Self(s.into()))
-        } else {
-            Err(ParseError::Invalid)
-        }
-    }
-}
-
-// § 1.6.1 Fixed fields
-fn is_valid_name_char(c: char) -> bool {
-    matches!(c, '0'..='9' | 'A'..='Z' | 'a'..='z' | '_' | '.')
-}
-
-fn is_valid_name(s: &str) -> bool {
-    let mut chars = s.chars();
-
-    if let Some(c) = chars.next() {
-        if !matches!(c, 'A'..='Z' | 'a'..='z' | '_') {
-            return false;
-        }
-    }
-
-    chars.all(is_valid_name_char)
 }
 
 /// A VCF header info key.
