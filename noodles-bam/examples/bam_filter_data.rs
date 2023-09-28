zaeleus/noodles

commit 8bec4e933239e4b29e8c582726ce14d95300abf4

sam/record/data/field/tag: Implement PartialEq<[u8; 2]> for Tag

noodles-bam/examples/bam_filter_data.rs
@@ -8,6 +8,12 @@ use noodles_bam as bam;
 use noodles_sam::{alignment::Record, record::data::field::tag};
 
 fn is_unique_record(record: &Record) -> io::Result<bool> {
+    const NH: [u8; 2] = [b'N', b'H'];
+
+    if tag::ALIGNMENT_HIT_COUNT == NH {
+        panic!();
+    }
+
     match record.data().get(&tag::ALIGNMENT_HIT_COUNT) {
         Some(value) => value.as_int().map(|hits| hits == 1).ok_or_else(|| {
             io::Error::new(

noodles-sam/src/record/data/field/tag.rs
@@ -310,6 +310,12 @@ impl FromStr for Tag {
     }
 }
 
+impl PartialEq<[u8; LENGTH]> for Tag {
+    fn eq(&self, other: &[u8; LENGTH]) -> bool {
+        self.as_ref().eq(other)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;

commit 24b33a62b83225ed6f21039f6dfb69ca57c93231

bam/examples/filter_data: Remove tag assertion
This was accidentally introduced in
8bec4e933239e4b29e8c582726ce14d95300abf4.

noodles-bam/examples/bam_filter_data.rs
@@ -8,12 +8,6 @@ use noodles_bam as bam;
 use noodles_sam::{alignment::Record, record::data::field::tag};
 
 fn is_unique_record(record: &Record) -> io::Result<bool> {
-    const NH: [u8; 2] = [b'N', b'H'];
-
-    if tag::ALIGNMENT_HIT_COUNT == NH {
-        panic!();
-    }
-
     match record.data().get(&tag::ALIGNMENT_HIT_COUNT) {
         Some(value) => value.as_int().map(|hits| hits == 1).ok_or_else(|| {
             io::Error::new(