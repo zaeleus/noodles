f99664b29e6869622dea2fc499cda8aabecc2551
Michael Macias <zaeleus@gmail.com>
2023-12-05 15:34:38 -0600

    bcf/writer/builder: Add compression method setter

--- noodles-bcf/src/writer/builder.rs
+++ noodles-bcf/src/writer/builder.rs
@@ -1,47 +1,69 @@
 use std::{
     fs::File,
-    io::{self, Write},
+    io::{self, BufWriter, Write},
     path::Path,
 };
 
 use noodles_bgzf as bgzf;
 
 use super::Writer;
+use crate::io::CompressionMethod;
 
 /// A BCF writer builder.
-pub struct Builder;
+#[derive(Debug, Default)]
+pub struct Builder {
+    compression_method: Option<CompressionMethod>,
+}
 
 impl Builder {
+    /// Sets the compression method.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_bcf::{io::CompressionMethod, writer::Builder};
+    /// let builder = Builder::default().set_compression_method(CompressionMethod::Bgzf);
+    /// ```
+    pub fn set_compression_method(mut self, compression_method: CompressionMethod) -> Self {
+        self.compression_method = Some(compression_method);
+        self
+    }
+
     /// Builds a BCF writer from a path.
     ///
     /// # Examples
     ///
     /// ```no_run
     /// use noodles_bcf::writer::Builder;
-    /// let writer = Builder.build_from_path("out.bam")?;
+    /// let writer = Builder::default().build_from_path("out.bam")?;
     /// # Ok::<_, std::io::Error>(())
     /// ```
-    pub fn build_from_path<P>(self, dst: P) -> io::Result<Writer<bgzf::Writer<File>>>
+    pub fn build_from_path<P>(self, dst: P) -> io::Result<Writer<Box<dyn Write>>>
     where
         P: AsRef<Path>,
     {
-        File::create(dst).map(Writer::new)
+        let file = File::create(dst)?;
+        Ok(self.build_from_writer(file))
     }
 
     /// Builds a BCF writer from a writer.
     ///
     /// # Examples
     ///
-    /// ```no_run
+    /// ```
     /// # use std::io;
     /// use noodles_bcf::writer::Builder;
-    /// let writer = Builder.build_from_writer(io::sink())?;
-    /// # Ok::<_, io::Error>(())
+    /// let writer = Builder::default().build_from_writer(io::sink());
     /// ```
-    pub fn build_from_writer<W>(self, writer: W) -> io::Result<Writer<bgzf::Writer<W>>>
+    pub fn build_from_writer<'w, W>(self, writer: W) -> Writer<Box<dyn Write + 'w>>
     where
-        W: Write,
+        W: Write + 'w,
     {
-        Ok(Writer::new(writer))
+        let inner: Box<dyn Write> = match self.compression_method {
+            Some(CompressionMethod::Bgzf) | None => Box::new(bgzf::Writer::new(writer)),
+            Some(CompressionMethod::None) => Box::new(BufWriter::new(writer)),
+        };
+
+        Writer::from(inner)
     }
 }
