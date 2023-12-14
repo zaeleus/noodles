4b8c6386630acfb2b9a65fec19f11ded98e71094
Michael Macias <zaeleus@gmail.com>
2023-12-14 15:50:23 -0600

    sam/alignment/any_record: Box trait objects

--- noodles-sam/src/alignment/any_record.rs
+++ noodles-sam/src/alignment/any_record.rs
@@ -7,38 +7,41 @@ use crate::Header;
 /// An alignment record.
 pub trait AnyRecord {
     /// Returns the read name.
-    fn read_name(&self) -> Option<&dyn ReadName>;
+    fn read_name(&self) -> Option<Box<dyn ReadName + '_>>;
 
     /// Returns the flags.
-    fn flags(&self) -> &dyn Flags;
+    fn flags(&self) -> Box<dyn Flags + '_>;
 
     /// Returns the reference sequence ID.
-    fn reference_sequence_id(&self, header: &Header) -> Option<&dyn ReferenceSequenceId>;
+    fn reference_sequence_id(&self, header: &Header) -> Option<Box<dyn ReferenceSequenceId + '_>>;
 
     /// Returns the alignment start.
-    fn alignment_start(&self) -> Option<&dyn Position>;
+    fn alignment_start(&self) -> Option<Box<dyn Position + '_>>;
 
     /// Returns the mapping quality.
-    fn mapping_quality(&self) -> Option<&dyn MappingQuality>;
+    fn mapping_quality(&self) -> Option<Box<dyn MappingQuality + '_>>;
 
     /// Returns the CIGAR operations.
-    fn cigar(&self, header: &Header) -> &dyn Cigar;
+    fn cigar(&self, header: &Header) -> Box<dyn Cigar + '_>;
 
     /// Returns the mate reference sequence ID.
-    fn mate_reference_sequence_id(&self, header: &Header) -> Option<&dyn ReferenceSequenceId>;
+    fn mate_reference_sequence_id(
+        &self,
+        header: &Header,
+    ) -> Option<Box<dyn ReferenceSequenceId + '_>>;
 
     /// Returns the mate alignment start.
-    fn mate_alignment_start(&self) -> Option<&dyn Position>;
+    fn mate_alignment_start(&self) -> Option<Box<dyn Position + '_>>;
 
     /// Returns the template length.
-    fn template_length(&self) -> &dyn TemplateLength;
+    fn template_length(&self) -> Box<dyn TemplateLength + '_>;
 
     /// Returns the sequence.
-    fn sequence(&self) -> &dyn Sequence;
+    fn sequence(&self) -> Box<dyn Sequence + '_>;
 
     /// Returns the quality scores.
-    fn quality_scores(&self) -> &dyn QualityScores;
+    fn quality_scores(&self) -> Box<dyn QualityScores + '_>;
 
     /// Returns the data.
-    fn data(&self) -> &dyn Data;
+    fn data(&self) -> Box<dyn Data + '_>;
 }
