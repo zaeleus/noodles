f251100f6c4885e3dc3735492e05f9c20b4506d4
Michael Macias <zaeleus@gmail.com>
2023-12-14 11:13:13 -0600

    sam/lazy/record/position: Implement crate::alignment::record::Position for Position

--- noodles-sam/src/lazy/record/position.rs
+++ noodles-sam/src/lazy/record/position.rs
@@ -2,6 +2,8 @@ use std::io;
 
 use noodles_core as core;
 
+use crate::alignment::record::Position as _;
+
 /// Raw SAM record position.
 #[derive(Debug, Eq, PartialEq)]
 pub struct Position<'a>(&'a [u8]);
@@ -9,6 +11,13 @@ pub struct Position<'a>(&'a [u8]);
 impl<'a> Position<'a> {
     pub(super) fn new(buf: &'a [u8]) -> Self {
         Self(buf)
+    }
+}
+
+impl<'a> crate::alignment::record::Position for Position<'a> {
+    fn try_to_usize(&self) -> io::Result<usize> {
+        lexical_core::parse(self.as_ref())
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
     }
 }
 
@@ -22,11 +31,8 @@ impl<'a> TryFrom<Position<'a>> for core::Position {
     type Error = io::Error;
 
     fn try_from(raw_position: Position<'a>) -> Result<Self, Self::Error> {
-        lexical_core::parse(raw_position.as_ref())
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-            .and_then(|n: usize| {
-                core::Position::try_from(n)
-                    .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-            })
+        raw_position.try_to_usize().and_then(|n: usize| {
+            core::Position::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+        })
     }
 }
