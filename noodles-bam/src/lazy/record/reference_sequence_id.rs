a33b404e561d8d58bd15ff7324c7b73bfd2aa59c
Michael Macias <zaeleus@gmail.com>
2023-12-18 16:30:05 -0600

    bam/lazy/record/reference_sequence_id: Implement sam::alignment::record::ReferenceSequenceId

--- noodles-bam/src/lazy/record/reference_sequence_id.rs
+++ noodles-bam/src/lazy/record/reference_sequence_id.rs
@@ -1,5 +1,7 @@
 use std::io;
 
+use noodles_sam::{self as sam, alignment::record::ReferenceSequenceId as _};
+
 /// A raw BAM record reference sequence ID.
 #[derive(Debug, Eq, PartialEq)]
 pub struct ReferenceSequenceId(i32);
@@ -7,6 +9,12 @@ pub struct ReferenceSequenceId(i32);
 impl ReferenceSequenceId {
     pub(super) fn new(n: i32) -> Self {
         Self(n)
+    }
+}
+
+impl sam::alignment::record::ReferenceSequenceId for ReferenceSequenceId {
+    fn try_to_usize(&self) -> io::Result<usize> {
+        usize::try_from(self.0).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
     }
 }
 
@@ -20,7 +28,6 @@ impl TryFrom<ReferenceSequenceId> for usize {
     type Error = io::Error;
 
     fn try_from(reference_sequence_id: ReferenceSequenceId) -> Result<Self, Self::Error> {
-        usize::try_from(i32::from(reference_sequence_id))
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+        reference_sequence_id.try_to_usize()
     }
 }
