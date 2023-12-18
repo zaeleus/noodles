d74f706aacf80fc95165d0342cff2b662d428fa3
Michael Macias <zaeleus@gmail.com>
2023-12-17 21:07:43 -0600

    sam/alignment/any_record: Add provided method for alignment end

--- noodles-sam/src/alignment/any_record.rs
+++ noodles-sam/src/alignment/any_record.rs
@@ -1,5 +1,7 @@
 use std::io;
 
+use noodles_core as core;
+
 use super::record::{
     Cigar, Data, Flags, MappingQuality, Position, QualityScores, ReadName, ReferenceSequenceId,
     Sequence, TemplateLength,
@@ -53,5 +55,45 @@ pub trait AnyRecord {
     /// Returns the alignment span.
     fn alignment_span(&self, header: &Header) -> io::Result<usize> {
         self.cigar(header).alignment_span()
+    }
+
+    /// Calculates the end position.
+    fn alignment_end(&self, header: &Header) -> Option<io::Result<core::Position>> {
+        let alignment_start = self.alignment_start()?;
+
+        let start = match core::Position::try_from(alignment_start.as_ref()) {
+            Ok(position) => position,
+            Err(e) => return Some(Err(e)),
+        };
+
+        let span = match self.alignment_span(header) {
+            Ok(span) => span,
+            Err(e) => return Some(Err(e)),
+        };
+
+        let end = usize::from(start) + span - 1;
+        core::Position::new(end).map(Ok)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_alignment_end() -> io::Result<()> {
+        let header = Header::default();
+
+        let src = b"*\t4\t*\t8\t255\t5M\t*\t0\t0\t*\t*";
+        let mut reader = crate::Reader::new(&src[..]);
+
+        let mut record = crate::lazy::Record::default();
+        reader.read_lazy_record(&mut record)?;
+
+        let actual = record.alignment_end(&header).transpose()?;
+        let expected = core::Position::new(12);
+        assert_eq!(actual, expected);
+
+        Ok(())
     }
 }
