03ad0c9d542ca174900b03aec1ca64070207c2e1
Michael Macias <zaeleus@gmail.com>
2023-12-19 11:55:34 -0600

    bam/lazy/record/template_length: Implement sam::alignment::record::TemplateLength

--- noodles-bam/src/lazy/record/template_length.rs
+++ noodles-bam/src/lazy/record/template_length.rs
@@ -1,3 +1,7 @@
+use std::io;
+
+use noodles_sam as sam;
+
 /// A raw BAM record template length.
 #[derive(Debug, Eq, PartialEq)]
 pub struct TemplateLength(i32);
@@ -5,6 +9,12 @@ pub struct TemplateLength(i32);
 impl TemplateLength {
     pub(super) fn new(n: i32) -> Self {
         Self(n)
+    }
+}
+
+impl sam::alignment::record::TemplateLength for TemplateLength {
+    fn try_to_i32(&self) -> io::Result<i32> {
+        Ok(self.0)
     }
 }
 
