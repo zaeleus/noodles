4d3c6976ef02d758de1c6d51a2646e1c794a6173
Michael Macias <zaeleus@gmail.com>
2023-12-19 10:42:37 -0600

    bam/lazy/record/mapping_quality: Implement sam::alignment::record::MappingQuality

--- noodles-bam/src/lazy/record/mapping_quality.rs
+++ noodles-bam/src/lazy/record/mapping_quality.rs
@@ -1,3 +1,7 @@
+use std::io;
+
+use noodles_sam as sam;
+
 /// A raw BAM record mapping quality.
 #[derive(Debug, Eq, PartialEq)]
 pub struct MappingQuality(u8);
@@ -5,6 +9,12 @@ pub struct MappingQuality(u8);
 impl MappingQuality {
     pub(super) fn new(n: u8) -> Self {
         Self(n)
+    }
+}
+
+impl sam::alignment::record::MappingQuality for MappingQuality {
+    fn try_to_u8(&self) -> io::Result<u8> {
+        Ok(self.0)
     }
 }
 
