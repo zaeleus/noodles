56543481cd5eb16eccc03ed1208933e06d852fef
Michael Macias <zaeleus@gmail.com>
2024-01-09 13:18:31 -0600

    sam/record/data/field/ty: Remove parser

--- noodles-cram/src/reader/data_container/compression_header/preservation_map.rs
+++ noodles-cram/src/reader/data_container/compression_header/preservation_map.rs
@@ -140,8 +140,7 @@ fn get_tag_ids_dictionary(src: &mut Bytes) -> io::Result<TagIdsDictionary> {
             let tag = Tag::try_from([t0, t1])
                 .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
 
-            let ty =
-                Type::try_from(ty).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+            let ty = get_type(ty)?;
 
             let key = tag_ids_dictionary::Key::new(tag, ty);
             line.push(key);
@@ -151,6 +150,23 @@ fn get_tag_ids_dictionary(src: &mut Bytes) -> io::Result<TagIdsDictionary> {
     }
 
     Ok(TagIdsDictionary::from(dictionary))
+}
+
+fn get_type(n: u8) -> io::Result<Type> {
+    match n {
+        b'A' => Ok(Type::Character),
+        b'c' => Ok(Type::Int8),
+        b'C' => Ok(Type::UInt8),
+        b's' => Ok(Type::Int16),
+        b'S' => Ok(Type::UInt16),
+        b'i' => Ok(Type::Int32),
+        b'I' => Ok(Type::UInt32),
+        b'f' => Ok(Type::Float),
+        b'Z' => Ok(Type::String),
+        b'H' => Ok(Type::Hex),
+        b'B' => Ok(Type::Array),
+        _ => Err(io::Error::from(io::ErrorKind::InvalidData)),
+    }
 }
 
 #[cfg(test)]
@@ -241,6 +257,28 @@ mod tests {
         assert!(matches!(
             get_bool(&mut reader),
             Err(ref e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_type() -> io::Result<()> {
+        assert_eq!(get_type(b'A')?, Type::Character);
+        assert_eq!(get_type(b'c')?, Type::Int8);
+        assert_eq!(get_type(b'C')?, Type::UInt8);
+        assert_eq!(get_type(b's')?, Type::Int16);
+        assert_eq!(get_type(b'S')?, Type::UInt16);
+        assert_eq!(get_type(b'i')?, Type::Int32);
+        assert_eq!(get_type(b'I')?, Type::UInt32);
+        assert_eq!(get_type(b'f')?, Type::Float);
+        assert_eq!(get_type(b'Z')?, Type::String);
+        assert_eq!(get_type(b'H')?, Type::Hex);
+        assert_eq!(get_type(b'B')?, Type::Array);
+
+        assert!(matches!(
+            get_type(b'n'),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
         ));
 
         Ok(())
--- noodles-sam/src/record/data/field/ty.rs
+++ noodles-sam/src/record/data/field/ty.rs
@@ -1,10 +1,6 @@
 //! SAM record data field value type.
 
-use std::{
-    error,
-    fmt::{self, Write},
-    str::FromStr,
-};
+use std::fmt::{self, Write};
 
 /// A SAM record data field value type.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
@@ -36,65 +32,6 @@ pub enum Type {
 impl fmt::Display for Type {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_char(char::from(*self))
-    }
-}
-
-/// An error returned when a raw SAM record data field value type fails to parse.
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum ParseError {
-    /// The input is invalid.
-    Invalid,
-}
-
-impl error::Error for ParseError {}
-
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Invalid => f.write_str("invalid input"),
-        }
-    }
-}
-
-impl FromStr for Type {
-    type Err = ParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "A" => Ok(Self::Character),
-            "c" => Ok(Self::Int8),
-            "C" => Ok(Self::UInt8),
-            "s" => Ok(Self::Int16),
-            "S" => Ok(Self::UInt16),
-            "i" => Ok(Self::Int32),
-            "I" => Ok(Self::UInt32),
-            "f" => Ok(Self::Float),
-            "Z" => Ok(Self::String),
-            "H" => Ok(Self::Hex),
-            "B" => Ok(Self::Array),
-            _ => Err(ParseError::Invalid),
-        }
-    }
-}
-
-impl TryFrom<u8> for Type {
-    type Error = ParseError;
-
-    fn try_from(n: u8) -> Result<Self, Self::Error> {
-        match n {
-            b'A' => Ok(Self::Character),
-            b'c' => Ok(Self::Int8),
-            b'C' => Ok(Self::UInt8),
-            b's' => Ok(Self::Int16),
-            b'S' => Ok(Self::UInt16),
-            b'i' => Ok(Self::Int32),
-            b'I' => Ok(Self::UInt32),
-            b'f' => Ok(Self::Float),
-            b'Z' => Ok(Self::String),
-            b'H' => Ok(Self::Hex),
-            b'B' => Ok(Self::Array),
-            _ => Err(ParseError::Invalid),
-        }
     }
 }
 
@@ -139,42 +76,6 @@ mod tests {
         assert_eq!(Type::String.to_string(), "Z");
         assert_eq!(Type::Hex.to_string(), "H");
         assert_eq!(Type::Array.to_string(), "B");
-    }
-
-    #[test]
-    fn test_from_str() {
-        assert_eq!("A".parse(), Ok(Type::Character));
-        assert_eq!("c".parse(), Ok(Type::Int8));
-        assert_eq!("C".parse(), Ok(Type::UInt8));
-        assert_eq!("s".parse(), Ok(Type::Int16));
-        assert_eq!("S".parse(), Ok(Type::UInt16));
-        assert_eq!("i".parse(), Ok(Type::Int32));
-        assert_eq!("I".parse(), Ok(Type::UInt32));
-        assert_eq!("f".parse(), Ok(Type::Float));
-        assert_eq!("Z".parse(), Ok(Type::String));
-        assert_eq!("H".parse(), Ok(Type::Hex));
-        assert_eq!("B".parse(), Ok(Type::Array));
-
-        assert_eq!("".parse::<Type>(), Err(ParseError::Invalid));
-        assert_eq!("n".parse::<Type>(), Err(ParseError::Invalid));
-        assert_eq!("noodles".parse::<Type>(), Err(ParseError::Invalid));
-    }
-
-    #[test]
-    fn test_try_from_u8_for_type() {
-        assert_eq!(Type::try_from(b'A'), Ok(Type::Character));
-        assert_eq!(Type::try_from(b'c'), Ok(Type::Int8));
-        assert_eq!(Type::try_from(b'C'), Ok(Type::UInt8));
-        assert_eq!(Type::try_from(b's'), Ok(Type::Int16));
-        assert_eq!(Type::try_from(b'S'), Ok(Type::UInt16));
-        assert_eq!(Type::try_from(b'i'), Ok(Type::Int32));
-        assert_eq!(Type::try_from(b'I'), Ok(Type::UInt32));
-        assert_eq!(Type::try_from(b'f'), Ok(Type::Float));
-        assert_eq!(Type::try_from(b'Z'), Ok(Type::String));
-        assert_eq!(Type::try_from(b'H'), Ok(Type::Hex));
-        assert_eq!(Type::try_from(b'B'), Ok(Type::Array));
-
-        assert_eq!(Type::try_from(b'n'), Err(ParseError::Invalid));
     }
 
     #[test]
