7a934d8f420d715aa68b46b2c46042d664c4b398
Michael Macias <zaeleus@gmail.com>
2024-01-09 14:49:44 -0600

    sam/record/data/field/value/array/subtype: Remove parser

--- noodles-sam/src/record/data/field/value/array/subtype.rs
+++ noodles-sam/src/record/data/field/value/array/subtype.rs
@@ -1,10 +1,6 @@
 //! SAM record data field value subtype.
 
-use std::{
-    error,
-    fmt::{self, Write},
-    str::FromStr,
-};
+use std::fmt::{self, Write};
 
 /// A SAM record data field value subtype.
 ///
@@ -30,57 +26,6 @@ pub enum Subtype {
 impl fmt::Display for Subtype {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_char(char::from(*self))
-    }
-}
-
-/// An error returned when a raw SAM record data field value subtype fails to parse.
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum ParseError {
-    /// The input is invalid.
-    Invalid,
-}
-
-impl error::Error for ParseError {}
-
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Invalid => f.write_str("invalid input"),
-        }
-    }
-}
-
-impl FromStr for Subtype {
-    type Err = ParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "c" => Ok(Self::Int8),
-            "C" => Ok(Self::UInt8),
-            "s" => Ok(Self::Int16),
-            "S" => Ok(Self::UInt16),
-            "i" => Ok(Self::Int32),
-            "I" => Ok(Self::UInt32),
-            "f" => Ok(Self::Float),
-            _ => Err(ParseError::Invalid),
-        }
-    }
-}
-
-impl TryFrom<u8> for Subtype {
-    type Error = ParseError;
-
-    fn try_from(n: u8) -> Result<Self, Self::Error> {
-        match n {
-            b'c' => Ok(Self::Int8),
-            b'C' => Ok(Self::UInt8),
-            b's' => Ok(Self::Int16),
-            b'S' => Ok(Self::UInt16),
-            b'i' => Ok(Self::Int32),
-            b'I' => Ok(Self::UInt32),
-            b'f' => Ok(Self::Float),
-            _ => Err(ParseError::Invalid),
-        }
     }
 }
 
@@ -117,34 +62,6 @@ mod tests {
         assert_eq!(Subtype::Int32.to_string(), "i");
         assert_eq!(Subtype::UInt32.to_string(), "I");
         assert_eq!(Subtype::Float.to_string(), "f");
-    }
-
-    #[test]
-    fn test_from_str() {
-        assert_eq!("c".parse(), Ok(Subtype::Int8));
-        assert_eq!("C".parse(), Ok(Subtype::UInt8));
-        assert_eq!("s".parse(), Ok(Subtype::Int16));
-        assert_eq!("S".parse(), Ok(Subtype::UInt16));
-        assert_eq!("i".parse(), Ok(Subtype::Int32));
-        assert_eq!("I".parse(), Ok(Subtype::UInt32));
-        assert_eq!("f".parse(), Ok(Subtype::Float));
-
-        assert_eq!("".parse::<Subtype>(), Err(ParseError::Invalid));
-        assert_eq!("n".parse::<Subtype>(), Err(ParseError::Invalid));
-        assert_eq!("noodles".parse::<Subtype>(), Err(ParseError::Invalid));
-    }
-
-    #[test]
-    fn test_try_from_u8_for_subtype() {
-        assert_eq!(Subtype::try_from(b'c'), Ok(Subtype::Int8));
-        assert_eq!(Subtype::try_from(b'C'), Ok(Subtype::UInt8));
-        assert_eq!(Subtype::try_from(b's'), Ok(Subtype::Int16));
-        assert_eq!(Subtype::try_from(b'S'), Ok(Subtype::UInt16));
-        assert_eq!(Subtype::try_from(b'i'), Ok(Subtype::Int32));
-        assert_eq!(Subtype::try_from(b'I'), Ok(Subtype::UInt32));
-        assert_eq!(Subtype::try_from(b'f'), Ok(Subtype::Float));
-
-        assert_eq!(Subtype::try_from(b'n'), Err(ParseError::Invalid));
     }
 
     #[test]
