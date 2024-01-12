aa056951bb3e8bea16915abd06800e534e1c4672
Michael Macias <zaeleus@gmail.com>
2024-01-12 10:22:38 -0600

    bam/examples/read_header: Use SAM writer to write header

--- noodles-bam/examples/bam_read_header.rs
+++ noodles-bam/examples/bam_read_header.rs
@@ -7,13 +7,17 @@
 use std::{env, io};
 
 use noodles_bam as bam;
+use noodles_sam as sam;
 
 fn main() -> io::Result<()> {
     let src = env::args().nth(1).expect("missing src");
 
     let mut reader = bam::reader::Builder.build_from_path(src)?;
     let header = reader.read_header()?;
-    print!("{header}");
+
+    let stdout = io::stdout().lock();
+    let mut writer = sam::Writer::new(stdout);
+    writer.write_header(&header)?;
 
     Ok(())
 }
