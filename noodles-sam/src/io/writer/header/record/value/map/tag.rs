476ad0a4ad11d20513b715ac909d4e0a9ec2275b
Michael Macias <zaeleus@gmail.com>
2024-01-17 17:49:56 -0600

    sam/io/writer/header/record/value/map/tag: Add validation

--- noodles-sam/src/io/writer/header/record/value/map/tag.rs
+++ noodles-sam/src/io/writer/header/record/value/map/tag.rs
@@ -4,5 +4,50 @@ pub(super) fn write_tag<W>(writer: &mut W, tag: [u8; 2]) -> io::Result<()>
 where
     W: Write,
 {
-    writer.write_all(&tag)
+    if is_valid(tag) {
+        writer.write_all(&tag)
+    } else {
+        Err(io::Error::new(io::ErrorKind::InvalidInput, "invalid tag"))
+    }
+}
+
+// § 1.3 "The header section" (2023-05-24): "...`[A-Za-z][A-Za-z0-9]`..."
+fn is_valid(tag: [u8; 2]) -> bool {
+    tag[0].is_ascii_alphabetic() && tag[1].is_ascii_alphanumeric()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_write_tag() -> io::Result<()> {
+        let mut buf = Vec::new();
+
+        buf.clear();
+        write_tag(&mut buf, [b'N', b'D'])?;
+        assert_eq!(buf, b"ND");
+
+        buf.clear();
+        assert!(matches!(
+            write_tag(&mut buf, [b'0', b'D']),
+            Err(e) if e.kind() == io::ErrorKind::InvalidInput
+        ));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_is_valid() {
+        assert!(is_valid([b'N', b'D']));
+        assert!(is_valid([b'N', b'd']));
+        assert!(is_valid([b'N', b'0']));
+        assert!(is_valid([b'n', b'D']));
+        assert!(is_valid([b'n', b'd']));
+        assert!(is_valid([b'n', b'0']));
+
+        assert!(!is_valid([b'0', b'D']));
+        assert!(!is_valid([b'*', b'D']));
+        assert!(!is_valid([b'D', b'*']));
+    }
 }
