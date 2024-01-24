8f6e4c009ed452a795cf5bf332d8d4a41851fd96
Michael Macias <zaeleus@gmail.com>
2024-01-24 11:41:44 -0600

    sam/async/io/reader/header: Parse header line by line
    The header parser can now build a `sam::Header` by parsing a raw header
    line by line. This makes it so that it is no longer required to read the
    entire raw header into memory before parsing.

--- noodles-sam/src/async/io/reader/header.rs
+++ noodles-sam/src/async/io/reader/header.rs
@@ -1,46 +1,110 @@
 use tokio::io::{self, AsyncBufRead, AsyncBufReadExt};
 
-use crate::Header;
+use crate::{header, Header};
 
 pub(super) async fn read_header<R>(reader: &mut R) -> io::Result<Header>
 where
     R: AsyncBufRead + Unpin,
 {
-    read_raw_header(reader).await.and_then(|s| {
-        s.parse()
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-    })
+    let mut parser = header::Parser::default();
+    let mut buf = Vec::new();
+
+    while read_header_line(reader, &mut buf).await? != 0 {
+        parser
+            .parse_partial(&buf)
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+    }
+
+    Ok(parser.finish())
 }
 
-async fn read_raw_header<R>(reader: &mut R) -> io::Result<String>
+async fn read_header_line<R>(reader: &mut R, dst: &mut Vec<u8>) -> io::Result<usize>
 where
     R: AsyncBufRead + Unpin,
 {
     const PREFIX: u8 = b'@';
     const LINE_FEED: u8 = b'\n';
+    const CARRIAGE_RETURN: u8 = b'\r';
 
-    let mut buf = Vec::new();
-    let mut is_eol = false;
+    let src = reader.fill_buf().await?;
 
-    for i in 0.. {
-        let src = reader.fill_buf().await?;
-
-        if (i == 0 || is_eol) && src.first().map(|&b| b != PREFIX).unwrap_or(true) {
-            break;
-        }
-
-        let (read_eol, len) = if let Some(i) = src.iter().position(|&b| b == LINE_FEED) {
-            buf.extend(&src[..=i]);
-            (true, i + 1)
-        } else {
-            buf.extend(src);
-            (false, src.len())
-        };
-
-        is_eol = read_eol;
-
-        reader.consume(len);
+    if src.is_empty() || src[0] != PREFIX {
+        return Ok(0);
     }
 
-    String::from_utf8(buf).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+    dst.clear();
+
+    match reader.read_until(LINE_FEED, dst).await? {
+        0 => Ok(0),
+        n => {
+            if dst.ends_with(&[LINE_FEED]) {
+                dst.pop();
+
+                if dst.ends_with(&[CARRIAGE_RETURN]) {
+                    dst.pop();
+                }
+            }
+
+            Ok(n)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::num::NonZeroUsize;
+
+    use super::*;
+    use crate::header::record::value::{
+        map::{self, header::Version, ReferenceSequence},
+        Map,
+    };
+
+    #[tokio::test]
+    async fn test_read_header_with_no_header() -> io::Result<()> {
+        let data = b"*\t4\t*\t0\t255\t*\t*\t0\t0\t*\t*\n";
+        let mut reader = &data[..];
+        assert!(read_header(&mut reader).await?.is_empty());
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_read_header_with_no_records() -> io::Result<()> {
+        let data = "@HD\tVN:1.6\n";
+        let mut reader = data.as_bytes();
+
+        let actual = read_header(&mut reader).await?;
+
+        let expected = crate::Header::builder()
+            .set_header(Map::<map::Header>::new(Version::new(1, 6)))
+            .build();
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_read_header_with_multiple_buffer_fills() -> io::Result<()> {
+        use tokio::io::BufReader;
+
+        const SQ0_LN: NonZeroUsize = match NonZeroUsize::new(8) {
+            Some(length) => length,
+            None => unreachable!(),
+        };
+
+        let data = "@HD\tVN:1.6\n@SQ\tSN:sq0\tLN:8\n";
+        let mut reader = BufReader::with_capacity(16, data.as_bytes());
+
+        let actual = read_header(&mut reader).await?;
+
+        let expected = crate::Header::builder()
+            .set_header(Map::<map::Header>::new(Version::new(1, 6)))
+            .add_reference_sequence("sq0", Map::<ReferenceSequence>::new(SQ0_LN))
+            .build();
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
 }
