34b5787fc335b8dde0b2acf330ebb0cefbb7a963
Michael Macias <zaeleus@gmail.com>
2024-01-24 10:20:46 -0600

    sam/io/writer/record/data/field/tag: Add validation

--- noodles-sam/src/io/writer/record/data/field/tag.rs
+++ noodles-sam/src/io/writer/record/data/field/tag.rs
@@ -6,7 +6,17 @@ pub fn write_tag<W>(writer: &mut W, tag: Tag) -> io::Result<()>
 where
     W: Write,
 {
-    writer.write_all(tag.as_ref())
+    if is_valid(tag) {
+        writer.write_all(tag.as_ref())
+    } else {
+        Err(io::Error::new(io::ErrorKind::InvalidInput, "invalid tag"))
+    }
+}
+
+// § 1.5 "The alignment section: optional fields" (2023-05-24): "`[A-Za-z][A-Za-z0-9]`".
+fn is_valid(tag: Tag) -> bool {
+    let b: [u8; 2] = tag.into();
+    b[0].is_ascii_alphabetic() && b[1].is_ascii_alphanumeric()
 }
 
 #[cfg(test)]
@@ -16,8 +26,27 @@ mod tests {
     #[test]
     fn test_put_tag() -> io::Result<()> {
         let mut buf = Vec::new();
+
+        buf.clear();
         write_tag(&mut buf, Tag::ALIGNMENT_HIT_COUNT)?;
         assert_eq!(buf, b"NH");
+
+        buf.clear();
+        assert!(matches!(
+            write_tag(&mut buf, Tag::new(b'n', b'\t')),
+            Err(e) if e.kind() == io::ErrorKind::InvalidInput
+        ));
+
         Ok(())
+    }
+
+    #[test]
+    fn test_is_valid() {
+        assert!(is_valid(Tag::ALIGNMENT_HIT_COUNT));
+        assert!(is_valid(Tag::new(b'n', b'd')));
+
+        assert!(!is_valid(Tag::new(b'n', b'\t')));
+        assert!(!is_valid(Tag::new(b' ', b'd')));
+        assert!(!is_valid(Tag::new(0x00, 0xe6)));
     }
 }
