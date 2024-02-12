986a05280be097b5baadf148d0bb2a43130059d3
Michael Macias <zaeleus@gmail.com>
2024-02-12 16:08:15 -0600

    vcf/variant/record: Add series trait

--- noodles-vcf/src/variant/record/samples/series.rs
+++ noodles-vcf/src/variant/record/samples/series.rs
@@ -2,4 +2,15 @@
 
 pub mod value;
 
+use std::io;
+
 pub use self::value::Value;
+
+/// A variant record samples series.
+pub trait Series {
+    /// Returns the name.
+    fn name(&self) -> &str;
+
+    /// Returns an iterator over values.
+    fn iter(&self) -> Box<dyn Iterator<Item = io::Result<Value<'_>>> + '_>;
+}
