fd0e7c1b47ebd3ade169407d5f28930fb2c42adb
Michael Macias <zaeleus@gmail.com>
2024-02-13 16:01:41 -0600

    vcf/variant/record/samples: Add trait

--- noodles-vcf/src/variant/record/samples.rs
+++ noodles-vcf/src/variant/record/samples.rs
@@ -3,4 +3,16 @@
 mod sample;
 pub mod series;
 
-pub use self::sample::Sample;
+pub use self::{sample::Sample, series::Series};
+
+/// Variant record samples.
+pub trait Samples {
+    /// Returns whether there are any samples.
+    fn is_empty(&self) -> bool;
+
+    /// Returns an iterator over series.
+    fn series(&self) -> Box<dyn Iterator<Item = Box<dyn Series>> + '_>;
+
+    /// Returns an iterator over samples.
+    fn samples(&self) -> Box<dyn Iterator<Item = Box<dyn Sample>> + '_>;
+}
