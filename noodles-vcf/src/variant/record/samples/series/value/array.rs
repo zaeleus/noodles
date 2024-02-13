5ccd934044e60359930b33598be05af76dddfdb4
Michael Macias <zaeleus@gmail.com>
2024-02-13 15:53:27 -0600

    vcf/variant/record/samples/series/value/array: Implement Debug

--- noodles-vcf/src/variant/record/samples/series/value/array.rs
+++ noodles-vcf/src/variant/record/samples/series/value/array.rs
@@ -2,6 +2,8 @@
 
 mod values;
 
+use std::fmt;
+
 pub use self::values::Values;
 
 /// A variant record samples array value.
@@ -14,4 +16,15 @@ pub enum Array<'a> {
     Character(Box<dyn Values<'a, char> + 'a>),
     /// A string array.
     String(Box<dyn Values<'a, &'a str> + 'a>),
+}
+
+impl<'a> fmt::Debug for Array<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Integer(values) => f.debug_list().entries(values.iter()).finish(),
+            Self::Float(values) => f.debug_list().entries(values.iter()).finish(),
+            Self::Character(values) => f.debug_list().entries(values.iter()).finish(),
+            Self::String(values) => f.debug_list().entries(values.iter()).finish(),
+        }
+    }
 }
