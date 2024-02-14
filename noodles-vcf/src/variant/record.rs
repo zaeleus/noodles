3fce091ed2b2f7ffbee4a552544a594351fa699d
Michael Macias <zaeleus@gmail.com>
2024-02-14 10:58:03 -0600

    vcf/variant/record: Add alternate bases trait

--- noodles-vcf/src/variant/record.rs
+++ noodles-vcf/src/variant/record.rs
@@ -1,7 +1,8 @@
 //! Variant record.
 
+mod alternate_bases;
 mod ids;
 pub mod info;
 pub mod samples;
 
-pub use self::{ids::Ids, info::Info, samples::Samples};
+pub use self::{alternate_bases::AlternateBases, ids::Ids, info::Info, samples::Samples};
---
+++ noodles-vcf/src/variant/record/alternate_bases.rs
@@ -0,0 +1,11 @@
+/// Variant record alternate bases.
+pub trait AlternateBases {
+    /// Returns whether there are any alternate bases.
+    fn is_empty(&self) -> bool;
+
+    /// Returns the number of alternate bases.
+    fn len(&self) -> usize;
+
+    /// Returns an iterator over alternate bases.
+    fn iter(&self) -> Box<dyn Iterator<Item = Option<&str>> + '_>;
+}
