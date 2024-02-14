27d560feb5e5809b8be18beff465fb2bd022b9ac
Michael Macias <zaeleus@gmail.com>
2024-02-14 11:29:42 -0600

    vcf/variant/record: Add filters trait

--- noodles-vcf/src/variant/record.rs
+++ noodles-vcf/src/variant/record.rs
@@ -1,8 +1,11 @@
 //! Variant record.
 
 mod alternate_bases;
+mod filters;
 mod ids;
 pub mod info;
 pub mod samples;
 
-pub use self::{alternate_bases::AlternateBases, ids::Ids, info::Info, samples::Samples};
+pub use self::{
+    alternate_bases::AlternateBases, filters::Filters, ids::Ids, info::Info, samples::Samples,
+};
