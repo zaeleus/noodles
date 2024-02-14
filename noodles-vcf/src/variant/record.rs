402feefe53d2a7e2705d118f747caee054121f1c
Michael Macias <zaeleus@gmail.com>
2024-02-14 11:31:19 -0600

    vcf/variant: Add record trait

--- noodles-vcf/src/variant/record.rs
+++ noodles-vcf/src/variant/record.rs
@@ -6,6 +6,40 @@ mod ids;
 pub mod info;
 pub mod samples;
 
+use std::io;
+
+use noodles_core::Position;
+
 pub use self::{
     alternate_bases::AlternateBases, filters::Filters, ids::Ids, info::Info, samples::Samples,
 };
+
+/// A variant record.
+pub trait Record {
+    /// Returns the reference sequence name.
+    fn reference_sequence_name(&self) -> &str;
+
+    /// Returns the position.
+    fn position(&self) -> Option<io::Result<Position>>;
+
+    /// Returns the IDs.
+    fn ids(&self) -> Box<dyn Ids>;
+
+    /// Returns the reference bases.
+    fn reference_bases(&self) -> &str;
+
+    /// Returns the alternate bases.
+    fn alternate_bases(&self) -> Box<dyn AlternateBases>;
+
+    /// Returns the quality scores.
+    fn quality_score(&self) -> Option<io::Result<f32>>;
+
+    /// Returns the filters.
+    fn filters(&self) -> Box<dyn Filters>;
+
+    /// Return the info fields.
+    fn info(&self) -> Box<dyn Info>;
+
+    /// Returns the samples.
+    fn samples(&self) -> Box<dyn Samples>;
+}
