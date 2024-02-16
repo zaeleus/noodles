3585221719bddbab6c686b017830e7e441b67f07
Michael Macias <zaeleus@gmail.com>
2024-02-16 10:19:19 -0600

    vcf/record/alternate_bases: Add tests

--- noodles-vcf/src/record/alternate_bases.rs
+++ noodles-vcf/src/record/alternate_bases.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{fmt, iter};
 
 use crate::variant::record::AlternateBases as _;
 
@@ -39,6 +39,46 @@ impl<'a> crate::variant::record::AlternateBases for AlternateBases<'a> {
     }
 
     fn iter(&self) -> Box<dyn Iterator<Item = &str> + '_> {
-        Box::new(self.0.split(DELIMITER))
+        if self.is_empty() {
+            Box::new(iter::empty())
+        } else {
+            Box::new(self.0.split(DELIMITER))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_empty() {
+        assert!(AlternateBases::new("").is_empty());
+        assert!(!AlternateBases::new("A").is_empty());
+        assert!(!AlternateBases::new("A,C").is_empty());
+    }
+
+    #[test]
+    fn test_len() {
+        assert_eq!(AlternateBases::new("").len(), 0);
+        assert_eq!(AlternateBases::new("A").len(), 1);
+        assert_eq!(AlternateBases::new("A,C").len(), 2);
+    }
+
+    #[test]
+    fn test_iter() {
+        let alternate_bases = AlternateBases::new("");
+        assert!(alternate_bases.iter().next().is_none());
+
+        let alternate_bases = AlternateBases::new("A");
+        let mut iter = alternate_bases.iter();
+        assert_eq!(iter.next(), Some("A"));
+        assert!(iter.next().is_none());
+
+        let alternate_bases = AlternateBases::new("A,C");
+        let mut iter = alternate_bases.iter();
+        assert_eq!(iter.next(), Some("A"));
+        assert_eq!(iter.next(), Some("C"));
+        assert!(iter.next().is_none());
     }
 }
