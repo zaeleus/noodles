7fe46f41766151e26082ae8bd54d154155d13bc4
Michael Macias <zaeleus@gmail.com>
2024-02-21 13:55:53 -0600

    vcf/variant/record_buf/samples/sample: Remove GenotypeError

--- noodles-vcf/src/variant/record_buf/samples/sample.rs
+++ noodles-vcf/src/variant/record_buf/samples/sample.rs
@@ -2,10 +2,9 @@
 
 pub mod value;
 
+use std::hash::Hash;
+
 pub use self::value::Value;
-
-use std::{error, fmt, hash::Hash};
-
 use super::Keys;
 
 /// A VCF record genotype sample.
@@ -39,34 +38,5 @@ impl<'g> Sample<'g> {
         self.keys
             .get_index_of(key)
             .and_then(|i| self.values.get(i).map(|value| value.as_ref()))
-    }
-}
-
-/// An error returned when a genotype (`GT`) field value fails to parse.
-#[derive(Clone, Debug, PartialEq)]
-pub enum GenotypeError {
-    /// The genotype field value is invalid.
-    InvalidValue(value::genotype::ParseError),
-    /// The genotype field value type is invalid.
-    ///
-    /// The `GT` field value must be a `String`.
-    InvalidValueType(Option<Value>),
-}
-
-impl error::Error for GenotypeError {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match self {
-            Self::InvalidValue(e) => Some(e),
-            Self::InvalidValueType(_) => None,
-        }
-    }
-}
-
-impl fmt::Display for GenotypeError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::InvalidValue(_) => f.write_str("invalid value"),
-            Self::InvalidValueType(value) => write!(f, "invalid String, got {value:?}"),
-        }
     }
 }
