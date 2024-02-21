15d83a23e10d808f0df26c1990488ec695ebebbb
Michael Macias <zaeleus@gmail.com>
2024-02-21 13:51:20 -0600

    vcf/variant/record_buf/samples/sample/value/array: Add conversion to crate::variant::record::samples::series::value::Array

--- noodles-vcf/src/variant/record_buf/samples/sample/value/array.rs
+++ noodles-vcf/src/variant/record_buf/samples/sample/value/array.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 /// A VCF record genotype field array value.
 #[derive(Clone, Debug, PartialEq)]
 pub enum Array {
@@ -9,4 +11,48 @@ pub enum Array {
     Character(Vec<Option<char>>),
     /// An array of strings.
     String(Vec<Option<String>>),
+}
+
+impl<'a> From<&'a Array> for crate::variant::record::samples::series::value::Array<'a> {
+    fn from(array_buf: &'a Array) -> Self {
+        match array_buf {
+            Array::Integer(values) => Self::Integer(Box::new(Values::new(values))),
+            Array::Float(values) => Self::Float(Box::new(Values::new(values))),
+            Array::Character(values) => Self::Character(Box::new(Values::new(values))),
+            Array::String(values) => Self::String(Box::new(Values::new(values))),
+        }
+    }
+}
+
+struct Values<'a, N>(&'a [Option<N>]);
+
+impl<'a, N> Values<'a, N> {
+    fn new(values: &'a [Option<N>]) -> Self {
+        Self(values)
+    }
+}
+
+impl<'a, N> crate::variant::record::samples::series::value::array::Values<'a, N> for Values<'a, N>
+where
+    N: Copy,
+{
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn iter(&self) -> Box<dyn Iterator<Item = io::Result<Option<N>>> + '_> {
+        Box::new(self.0.iter().copied().map(Ok))
+    }
+}
+
+impl<'a> crate::variant::record::samples::series::value::array::Values<'a, &'a str>
+    for Values<'a, String>
+{
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn iter(&self) -> Box<dyn Iterator<Item = io::Result<Option<&'a str>>> + '_> {
+        Box::new(self.0.iter().map(|s| Ok(s.as_deref())))
+    }
 }
