b53a499e351f797d5081243d43bad1e6d3c873e1
Michael Macias <zaeleus@gmail.com>
2024-03-01 09:48:00 -0600

    vcf/io/writer/record/alternate_bases: Write from AlternateBases trait

--- noodles-vcf/src/io/writer/record/alternate_bases.rs
+++ noodles-vcf/src/io/writer/record/alternate_bases.rs
@@ -1,24 +1,24 @@
 use std::io::{self, Write};
 
 use super::MISSING;
-use crate::variant::{record::AlternateBases as _, record_buf::AlternateBases};
+use crate::variant::record::AlternateBases;
 
-pub(super) fn write_alternate_bases<W>(
-    writer: &mut W,
-    alternate_bases: &AlternateBases,
-) -> io::Result<()>
+pub(super) fn write_alternate_bases<W, B>(writer: &mut W, alternate_bases: &B) -> io::Result<()>
 where
     W: Write,
+    B: AlternateBases,
 {
     if alternate_bases.is_empty() {
         writer.write_all(MISSING)?;
     } else {
-        for (i, allele) in alternate_bases.as_ref().iter().enumerate() {
+        for (i, result) in alternate_bases.iter().enumerate() {
+            let bases = result?;
+
             if i > 0 {
                 write!(writer, ",")?;
             }
 
-            write!(writer, "{allele}")?;
+            write!(writer, "{bases}")?;
         }
     }
 
@@ -28,23 +28,24 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::variant::record_buf::AlternateBases as AlternateBasesBuf;
 
     #[test]
     fn test_write_alternate_bases() -> io::Result<()> {
         let mut buf = Vec::new();
 
         buf.clear();
-        let alternate_bases = AlternateBases::default();
+        let alternate_bases = AlternateBasesBuf::default();
         write_alternate_bases(&mut buf, &alternate_bases)?;
         assert_eq!(buf, b".");
 
         buf.clear();
-        let alternate_bases = AlternateBases::from(vec![String::from("C")]);
+        let alternate_bases = AlternateBasesBuf::from(vec![String::from("C")]);
         write_alternate_bases(&mut buf, &alternate_bases)?;
         assert_eq!(buf, b"C");
 
         buf.clear();
-        let alternate_bases = AlternateBases::from(vec![String::from("C"), String::from("GT")]);
+        let alternate_bases = AlternateBasesBuf::from(vec![String::from("C"), String::from("GT")]);
         write_alternate_bases(&mut buf, &alternate_bases)?;
         assert_eq!(buf, b"C,GT");
 
