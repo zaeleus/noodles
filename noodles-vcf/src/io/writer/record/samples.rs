67e05fe4ea84431f9994b6100a90bbf417ab4b10
Michael Macias <zaeleus@gmail.com>
2024-03-01 15:11:15 -0600

    vcf/io/writer/record/samples/sample: Write from Sample trait

--- noodles-vcf/src/io/writer/record/samples.rs
+++ noodles-vcf/src/io/writer/record/samples.rs
@@ -4,9 +4,16 @@ mod sample;
 use std::io::{self, Write};
 
 use self::{keys::write_keys, sample::write_sample};
-use crate::variant::record_buf::Samples;
+use crate::{
+    variant::{record::Samples as _, record_buf::Samples},
+    Header,
+};
 
-pub(super) fn write_samples<W>(writer: &mut W, genotypes: &Samples) -> io::Result<()>
+pub(super) fn write_samples<W>(
+    writer: &mut W,
+    header: &Header,
+    genotypes: &Samples,
+) -> io::Result<()>
 where
     W: Write,
 {
@@ -14,9 +21,9 @@ where
 
     write_keys(writer, genotypes.keys())?;
 
-    for sample in genotypes.values() {
+    for sample in genotypes.iter() {
         writer.write_all(DELIMITER)?;
-        write_sample(writer, &sample)?;
+        write_sample(writer, header, sample)?;
     }
 
     Ok(())
@@ -30,20 +37,26 @@ mod tests {
     fn test_write_samples() -> Result<(), Box<dyn std::error::Error>> {
         use crate::variant::record_buf::samples::{keys::key, sample::Value, Keys};
 
-        fn t(buf: &mut Vec<u8>, genotypes: &Samples, expected: &[u8]) -> io::Result<()> {
+        fn t(
+            buf: &mut Vec<u8>,
+            header: &Header,
+            genotypes: &Samples,
+            expected: &[u8],
+        ) -> io::Result<()> {
             buf.clear();
-            write_samples(buf, genotypes)?;
+            write_samples(buf, header, genotypes)?;
             assert_eq!(buf, expected);
             Ok(())
         }
 
+        let header = Header::default();
         let mut buf = Vec::new();
 
         let genotypes = Samples::new(
             Keys::try_from(vec![String::from(key::GENOTYPE)])?,
             vec![vec![Some(Value::from("0|0"))]],
         );
-        t(&mut buf, &genotypes, b"GT\t0|0")?;
+        t(&mut buf, &header, &genotypes, b"GT\t0|0")?;
 
         let genotypes = Samples::new(
             Keys::try_from(vec![
@@ -55,7 +68,7 @@ mod tests {
                 vec![Some(Value::from("0/1")), Some(Value::from(8))],
             ],
         );
-        t(&mut buf, &genotypes, b"GT:GQ\t0|0:13\t0/1:8")?;
+        t(&mut buf, &header, &genotypes, b"GT:GQ\t0|0:13\t0/1:8")?;
 
         Ok(())
     }
--- noodles-vcf/src/io/writer/record/samples/sample.rs
+++ noodles-vcf/src/io/writer/record/samples/sample.rs
@@ -2,25 +2,29 @@ use std::io::{self, Write};
 
 use crate::{
     io::writer::record::MISSING,
-    variant::record_buf::samples::{
-        sample::{value::Array, Value},
+    variant::record::samples::{
+        series::{value::Array, Value},
         Sample,
     },
+    Header,
 };
 
-pub(super) fn write_sample<W>(writer: &mut W, sample: &Sample<'_>) -> io::Result<()>
+pub(super) fn write_sample<W, S>(writer: &mut W, header: &Header, sample: S) -> io::Result<()>
 where
     W: Write,
+    S: Sample,
 {
     const DELIMITER: &[u8] = b":";
 
-    for (i, value) in sample.values().iter().enumerate() {
+    for (i, result) in sample.iter(header).enumerate() {
+        let (_, value) = result?;
+
         if i > 0 {
             writer.write_all(DELIMITER)?;
         }
 
         match value {
-            Some(v) => write_value(writer, v)?,
+            Some(v) => write_value(writer, &v)?,
             None => writer.write_all(MISSING)?,
         }
     }
@@ -49,12 +53,12 @@ where
 
     match array {
         Array::Integer(values) => {
-            for (i, v) in values.iter().enumerate() {
+            for (i, result) in values.iter().enumerate() {
                 if i > 0 {
                     writer.write_all(DELIMITER)?;
                 }
 
-                if let Some(n) = v {
+                if let Some(n) = result? {
                     write!(writer, "{n}")?;
                 } else {
                     writer.write_all(MISSING)?;
@@ -62,12 +66,12 @@ where
             }
         }
         Array::Float(values) => {
-            for (i, v) in values.iter().enumerate() {
+            for (i, result) in values.iter().enumerate() {
                 if i > 0 {
                     writer.write_all(DELIMITER)?;
                 }
 
-                if let Some(n) = v {
+                if let Some(n) = result? {
                     write!(writer, "{n}")?;
                 } else {
                     writer.write_all(MISSING)?;
@@ -75,12 +79,12 @@ where
             }
         }
         Array::Character(values) => {
-            for (i, v) in values.iter().enumerate() {
+            for (i, result) in values.iter().enumerate() {
                 if i > 0 {
                     writer.write_all(DELIMITER)?;
                 }
 
-                if let Some(c) = v {
+                if let Some(c) = result? {
                     write!(writer, "{c}")?;
                 } else {
                     writer.write_all(MISSING)?;
@@ -88,12 +92,12 @@ where
             }
         }
         Array::String(values) => {
-            for (i, v) in values.iter().enumerate() {
+            for (i, result) in values.iter().enumerate() {
                 if i > 0 {
                     writer.write_all(DELIMITER)?;
                 }
 
-                if let Some(s) = v {
+                if let Some(s) = result? {
                     writer.write_all(s.as_bytes())?;
                 } else {
                     writer.write_all(MISSING)?;
@@ -111,6 +115,33 @@ mod tests {
 
     #[test]
     fn test_write_value() -> io::Result<()> {
+        struct Values<'a, T>(&'a [Option<T>]);
+
+        impl<'a, T> crate::variant::record::samples::series::value::array::Values<'a, T> for Values<'a, T>
+        where
+            T: Copy,
+        {
+            fn len(&self) -> usize {
+                self.0.len()
+            }
+
+            fn iter(&self) -> Box<dyn Iterator<Item = io::Result<Option<T>>> + '_> {
+                Box::new(self.0.iter().copied().map(Ok))
+            }
+        }
+
+        impl<'a> crate::variant::record::samples::series::value::array::Values<'a, &'a str>
+            for Values<'a, String>
+        {
+            fn len(&self) -> usize {
+                self.0.len()
+            }
+
+            fn iter(&self) -> Box<dyn Iterator<Item = io::Result<Option<&'a str>>> + '_> {
+                Box::new(self.0.iter().map(|s| Ok(s.as_deref())))
+            }
+        }
+
         fn t(buf: &mut Vec<u8>, value: &Value, expected: &[u8]) -> io::Result<()> {
             buf.clear();
             write_value(buf, value)?;
@@ -120,43 +151,80 @@ mod tests {
 
         let mut buf = Vec::new();
 
-        t(&mut buf, &Value::from(8), b"8")?;
-        t(&mut buf, &Value::from(0.333), b"0.333")?;
-        t(&mut buf, &Value::from('n'), b"n")?;
-        t(&mut buf, &Value::from("noodles"), b"noodles")?;
+        t(&mut buf, &Value::Integer(8), b"8")?;
+        t(&mut buf, &Value::Float(0.333), b"0.333")?;
+        t(&mut buf, &Value::Character('n'), b"n")?;
+        t(&mut buf, &Value::String("noodles"), b"noodles")?;
 
-        t(&mut buf, &Value::from(vec![Some(8)]), b"8")?;
-        t(&mut buf, &Value::from(vec![Some(8), Some(13)]), b"8,13")?;
-        t(&mut buf, &Value::from(vec![Some(8), None]), b"8,.")?;
-
-        t(&mut buf, &Value::from(vec![Some(0.333)]), b"0.333")?;
         t(
             &mut buf,
-            &Value::from(vec![Some(0.333), Some(0.667)]),
+            &Value::Array(Array::Integer(Box::new(Values(&[Some(8)])))),
+            b"8",
+        )?;
+        t(
+            &mut buf,
+            &Value::Array(Array::Integer(Box::new(Values(&[Some(8), Some(13)])))),
+            b"8,13",
+        )?;
+        t(
+            &mut buf,
+            &Value::Array(Array::Integer(Box::new(Values(&[Some(8), None])))),
+            b"8,.",
+        )?;
+
+        t(
+            &mut buf,
+            &Value::Array(Array::Float(Box::new(Values(&[Some(0.333)])))),
+            b"0.333",
+        )?;
+        t(
+            &mut buf,
+            &Value::Array(Array::Float(Box::new(Values(&[Some(0.333), Some(0.667)])))),
             b"0.333,0.667",
         )?;
-        t(&mut buf, &Value::from(vec![Some(0.333), None]), b"0.333,.")?;
-
-        t(&mut buf, &Value::from(vec![Some('n')]), b"n")?;
-        t(&mut buf, &Value::from(vec![Some('n'), Some('d')]), b"n,d")?;
-        t(&mut buf, &Value::from(vec![Some('n'), None]), b"n,.")?;
+        t(
+            &mut buf,
+            &Value::Array(Array::Float(Box::new(Values(&[Some(0.333), None])))),
+            b"0.333,.",
+        )?;
 
         t(
             &mut buf,
-            &Value::from(vec![Some(String::from("noodles"))]),
+            &Value::Array(Array::Character(Box::new(Values(&[Some('n')])))),
+            b"n",
+        )?;
+        t(
+            &mut buf,
+            &Value::Array(Array::Character(Box::new(Values(&[Some('n'), Some('d')])))),
+            b"n,d",
+        )?;
+        t(
+            &mut buf,
+            &Value::Array(Array::Character(Box::new(Values(&[Some('n'), None])))),
+            b"n,.",
+        )?;
+
+        t(
+            &mut buf,
+            &Value::Array(Array::String(Box::new(Values(&[Some(String::from(
+                "noodles",
+            ))])))),
             b"noodles",
         )?;
         t(
             &mut buf,
-            &Value::from(vec![
+            &Value::Array(Array::String(Box::new(Values(&[
                 Some(String::from("noodles")),
                 Some(String::from("vcf")),
-            ]),
+            ])))),
             b"noodles,vcf",
         )?;
         t(
             &mut buf,
-            &Value::from(vec![Some(String::from("noodles")), None]),
+            &Value::Array(Array::String(Box::new(Values(&[
+                Some(String::from("noodles")),
+                None,
+            ])))),
             b"noodles,.",
         )?;
 
