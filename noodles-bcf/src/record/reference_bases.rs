5407bd19d360c0f71c90ec91f837a41e68c8818f
Michael Macias <zaeleus@gmail.com>
2024-03-12 10:17:48 -0500

    bcf/record/reference_bases: Implement vcf::variant::record::ReferenceBases

--- noodles-bcf/src/record/reference_bases.rs
+++ noodles-bcf/src/record/reference_bases.rs
@@ -1,3 +1,7 @@
+use std::io;
+
+use noodles_vcf as vcf;
+
 /// BCF record reference bases.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct ReferenceBases<'a>(&'a [u8]);
@@ -11,5 +15,19 @@ impl<'a> ReferenceBases<'a> {
 impl<'a> AsRef<[u8]> for ReferenceBases<'a> {
     fn as_ref(&self) -> &[u8] {
         self.0
+    }
+}
+
+impl<'a> vcf::variant::record::ReferenceBases for ReferenceBases<'a> {
+    fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn iter(&self) -> Box<dyn Iterator<Item = io::Result<u8>> + '_> {
+        Box::new(self.0.iter().copied().map(Ok))
     }
 }
