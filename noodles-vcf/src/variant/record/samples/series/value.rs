a0bd31e32360a6aa1c0633067a2328d03f6fe9f2
Michael Macias <zaeleus@gmail.com>
2024-03-15 11:40:20 -0500

    vcf/variant/record/samples/series/value: Add conversion to buffer

--- noodles-vcf/src/variant/record/samples/series/value.rs
+++ noodles-vcf/src/variant/record/samples/series/value.rs
@@ -2,6 +2,8 @@
 
 pub mod array;
 
+use std::io;
+
 pub use self::array::Array;
 
 /// A variant record samples value.
@@ -17,4 +19,18 @@ pub enum Value<'a> {
     String(&'a str),
     /// An array.
     Array(Array<'a>),
+}
+
+impl<'a> TryFrom<Value<'a>> for crate::variant::record_buf::samples::sample::Value {
+    type Error = io::Error;
+
+    fn try_from(value: Value<'a>) -> Result<Self, Self::Error> {
+        match value {
+            Value::Integer(n) => Ok(Self::Integer(n)),
+            Value::Float(n) => Ok(Self::Float(n)),
+            Value::Character(c) => Ok(Self::Character(c)),
+            Value::String(s) => Ok(Self::String(s.into())),
+            Value::Array(array) => array.try_into().map(Self::Array),
+        }
+    }
 }
