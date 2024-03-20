98938941d2a27a364363ad25a1bfe05cefbc0a2a
Michael Macias <zaeleus@gmail.com>
2024-03-20 16:27:35 -0500

    vcf/variant/record/samples/series/value/genotype: Require Debug

--- noodles-vcf/src/variant/record/samples/series/value/genotype.rs
+++ noodles-vcf/src/variant/record/samples/series/value/genotype.rs
@@ -1,7 +1,7 @@
-use std::io;
+use std::{fmt::Debug, io};
 
 /// A variant record samples genotype value.
-pub trait Genotype {
+pub trait Genotype: Debug {
     /// Returns an iterator over allele position-phasing pairs.
     fn iter(&self) -> Box<dyn Iterator<Item = io::Result<(Option<usize>, u8)>> + '_>;
 }
