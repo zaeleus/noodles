c00a4590ecff67b0dcc89fcad2ddcac18bc30199
Michael Macias <zaeleus@gmail.com>
2024-03-26 10:01:20 -0500

    vcf/io/writer/record/samples/keys: Validate the genotype field is first if present

--- noodles-vcf/src/io/writer/record/samples/keys.rs
+++ noodles-vcf/src/io/writer/record/samples/keys.rs
@@ -1,5 +1,7 @@
 use std::io::{self, Write};
 
+use crate::variant::record::samples::keys::key;
+
 pub(super) fn write_keys<'a, W, I>(writer: &mut W, keys: I) -> io::Result<()>
 where
     W: Write,
@@ -11,6 +13,13 @@ where
         let key = result?;
 
         if i > 0 {
+            if key == key::GENOTYPE {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "GT must be first series",
+                ));
+            }
+
             writer.write_all(DELIMITER)?;
         }
 
@@ -37,6 +46,18 @@ mod tests {
         let keys = [Ok("GT"), Ok("GQ")];
         write_keys(&mut buf, keys.into_iter())?;
         assert_eq!(buf, b"GT:GQ");
+
+        buf.clear();
+        let keys = [Ok("GQ")];
+        write_keys(&mut buf, keys.into_iter())?;
+        assert_eq!(buf, b"GQ");
+
+        buf.clear();
+        let keys = [Ok("GQ"), Ok("GT")];
+        assert!(matches!(
+            write_keys(&mut buf, keys.into_iter()),
+            Err(e) if e.kind() == io::ErrorKind::InvalidInput
+        ));
 
         Ok(())
     }
