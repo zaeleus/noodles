4accefffd7bf2aba3038a4add053b6c511c6b21b
Michael Macias <zaeleus@gmail.com>
2024-03-28 10:51:22 -0500

    vcf/io/writer/record/samples/sample/value/genotype: Add tests

--- noodles-vcf/src/io/writer/record/samples/sample/value/genotype.rs
+++ noodles-vcf/src/io/writer/record/samples/sample/value/genotype.rs
@@ -23,8 +23,8 @@ fn write_phasing<W>(writer: &mut W, phasing: Phasing) -> io::Result<()>
 where
     W: Write,
 {
-    const PHASED: u8 = b'/';
-    const UNPHASED: u8 = b'|';
+    const PHASED: u8 = b'|';
+    const UNPHASED: u8 = b'/';
 
     match phasing {
         Phasing::Phased => writer.write_all(&[PHASED]),
@@ -42,5 +42,88 @@ where
         write!(writer, "{n}")
     } else {
         writer.write_all(&[MISSING])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::variant::record_buf::samples::sample::value::{
+        genotype::Allele, Genotype as GenotypeBuf,
+    };
+
+    #[test]
+    fn test_write_genotype() -> Result<(), Box<dyn std::error::Error>> {
+        let mut buf = Vec::new();
+
+        buf.clear();
+        let genotype = &GenotypeBuf::try_from(vec![Allele::new(Some(0), Phasing::Phased)])?;
+        write_genotype(&mut buf, &genotype)?;
+        assert_eq!(buf, b"0");
+
+        buf.clear();
+        let genotype = &GenotypeBuf::try_from(vec![
+            Allele::new(Some(0), Phasing::Unphased),
+            Allele::new(Some(1), Phasing::Unphased),
+        ])?;
+        write_genotype(&mut buf, &genotype)?;
+        assert_eq!(buf, b"0/1");
+
+        buf.clear();
+        let genotype = &GenotypeBuf::try_from(vec![
+            Allele::new(Some(0), Phasing::Phased),
+            Allele::new(Some(1), Phasing::Phased),
+        ])?;
+        write_genotype(&mut buf, &genotype)?;
+        assert_eq!(buf, b"0|1");
+
+        buf.clear();
+        let genotype = &GenotypeBuf::try_from(vec![
+            Allele::new(Some(0), Phasing::Unphased),
+            Allele::new(Some(1), Phasing::Unphased),
+            Allele::new(Some(2), Phasing::Phased),
+        ])?;
+        write_genotype(&mut buf, &genotype)?;
+        assert_eq!(buf, b"0/1|2");
+
+        buf.clear();
+        let genotype = &GenotypeBuf::try_from(vec![
+            Allele::new(None, Phasing::Unphased),
+            Allele::new(None, Phasing::Unphased),
+        ])?;
+        write_genotype(&mut buf, &genotype)?;
+        assert_eq!(buf, b"./.");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_write_phasing() -> io::Result<()> {
+        let mut buf = Vec::new();
+
+        buf.clear();
+        write_phasing(&mut buf, Phasing::Phased)?;
+        assert_eq!(buf, b"|");
+
+        buf.clear();
+        write_phasing(&mut buf, Phasing::Unphased)?;
+        assert_eq!(buf, b"/");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_write_position() -> io::Result<()> {
+        let mut buf = Vec::new();
+
+        buf.clear();
+        write_position(&mut buf, None)?;
+        assert_eq!(buf, b".");
+
+        buf.clear();
+        write_position(&mut buf, Some(0))?;
+        assert_eq!(buf, b"0");
+
+        Ok(())
     }
 }
