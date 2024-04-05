a70d0d88c4c416cc33bb0e79209d1c159b8b8083
Michael Macias <zaeleus@gmail.com>
2024-04-05 11:39:24 -0500

    vcf/io/writer/record/info/field/value: Move array writer to a module

--- noodles-vcf/src/io/writer/record/info/field/value.rs
+++ noodles-vcf/src/io/writer/record/info/field/value.rs
@@ -1,81 +1,20 @@
+mod array;
+
 use std::io::{self, Write};
 
-use crate::{
-    io::writer::record::MISSING,
-    variant::record::info::field::{value::Array, Value},
-};
+use self::array::write_array;
+use crate::variant::record::info::field::Value;
 
 pub(super) fn write_value<W>(writer: &mut W, value: &Value) -> io::Result<()>
 where
     W: Write,
 {
-    const DELIMITER: &[u8] = b",";
-
     match value {
         Value::Integer(n) => write!(writer, "{n}"),
         Value::Float(n) => write!(writer, "{n}"),
         Value::Flag => Ok(()),
         Value::Character(c) => write!(writer, "{c}"),
         Value::String(s) => writer.write_all(s.as_bytes()),
-        Value::Array(Array::Integer(values)) => {
-            for (i, result) in values.iter().enumerate() {
-                if i > 0 {
-                    writer.write_all(DELIMITER)?;
-                }
-
-                if let Some(n) = result? {
-                    write!(writer, "{n}")?;
-                } else {
-                    writer.write_all(MISSING)?;
-                }
-            }
-
-            Ok(())
-        }
-        Value::Array(Array::Float(values)) => {
-            for (i, result) in values.iter().enumerate() {
-                if i > 0 {
-                    writer.write_all(DELIMITER)?;
-                }
-
-                if let Some(n) = result? {
-                    write!(writer, "{n}")?;
-                } else {
-                    writer.write_all(MISSING)?;
-                }
-            }
-
-            Ok(())
-        }
-        Value::Array(Array::Character(values)) => {
-            for (i, result) in values.iter().enumerate() {
-                if i > 0 {
-                    writer.write_all(DELIMITER)?;
-                }
-
-                if let Some(c) = result? {
-                    write!(writer, "{c}")?;
-                } else {
-                    writer.write_all(MISSING)?;
-                }
-            }
-
-            Ok(())
-        }
-        Value::Array(Array::String(values)) => {
-            for (i, result) in values.iter().enumerate() {
-                if i > 0 {
-                    writer.write_all(DELIMITER)?;
-                }
-
-                if let Some(s) = result? {
-                    writer.write_all(s.as_bytes())?;
-                } else {
-                    writer.write_all(MISSING)?;
-                }
-            }
-
-            Ok(())
-        }
+        Value::Array(array) => write_array(writer, array),
     }
 }
---
+++ noodles-vcf/src/io/writer/record/info/field/value/array.rs
@@ -0,0 +1,73 @@
+use std::io::{self, Write};
+
+use crate::{io::writer::record::MISSING, variant::record::info::field::value::Array};
+
+pub(super) fn write_array<W>(writer: &mut W, array: &Array) -> io::Result<()>
+where
+    W: Write,
+{
+    const DELIMITER: &[u8] = b",";
+
+    match array {
+        Array::Integer(values) => {
+            for (i, result) in values.iter().enumerate() {
+                if i > 0 {
+                    writer.write_all(DELIMITER)?;
+                }
+
+                if let Some(n) = result? {
+                    write!(writer, "{n}")?;
+                } else {
+                    writer.write_all(MISSING)?;
+                }
+            }
+
+            Ok(())
+        }
+        Array::Float(values) => {
+            for (i, result) in values.iter().enumerate() {
+                if i > 0 {
+                    writer.write_all(DELIMITER)?;
+                }
+
+                if let Some(n) = result? {
+                    write!(writer, "{n}")?;
+                } else {
+                    writer.write_all(MISSING)?;
+                }
+            }
+
+            Ok(())
+        }
+        Array::Character(values) => {
+            for (i, result) in values.iter().enumerate() {
+                if i > 0 {
+                    writer.write_all(DELIMITER)?;
+                }
+
+                if let Some(c) = result? {
+                    write!(writer, "{c}")?;
+                } else {
+                    writer.write_all(MISSING)?;
+                }
+            }
+
+            Ok(())
+        }
+        Array::String(values) => {
+            for (i, result) in values.iter().enumerate() {
+                if i > 0 {
+                    writer.write_all(DELIMITER)?;
+                }
+
+                if let Some(s) = result? {
+                    writer.write_all(s.as_bytes())?;
+                } else {
+                    writer.write_all(MISSING)?;
+                }
+            }
+
+            Ok(())
+        }
+    }
+}
