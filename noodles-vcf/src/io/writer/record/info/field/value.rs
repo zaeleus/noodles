93f67916345396221b701094a59ae0d6631fe9d8
Michael Macias <zaeleus@gmail.com>
2024-04-05 11:44:22 -0500

    vcf/io/writer/record/info/field/value: Move string writer to a module

--- noodles-vcf/src/io/writer/record/info/field/value.rs
+++ noodles-vcf/src/io/writer/record/info/field/value.rs
@@ -1,8 +1,9 @@
 mod array;
+mod string;
 
 use std::io::{self, Write};
 
-use self::array::write_array;
+use self::{array::write_array, string::write_string};
 use crate::variant::record::info::field::Value;
 
 pub(super) fn write_value<W>(writer: &mut W, value: &Value) -> io::Result<()>
@@ -14,7 +15,7 @@ where
         Value::Float(n) => write!(writer, "{n}"),
         Value::Flag => Ok(()),
         Value::Character(c) => write!(writer, "{c}"),
-        Value::String(s) => writer.write_all(s.as_bytes()),
+        Value::String(s) => write_string(writer, s),
         Value::Array(array) => write_array(writer, array),
     }
 }
