0421db99d308e70a04b4faf07c80ee45cd442c08
Michael Macias <zaeleus@gmail.com>
2024-04-12 16:13:37 -0500

    sam/examples/reheader: Add self program record

--- noodles-sam/examples/sam_reheader.rs
+++ noodles-sam/examples/sam_reheader.rs
@@ -6,13 +6,32 @@
 
 use std::{env, io};
 
-use noodles_sam as sam;
+use noodles_sam::{
+    self as sam,
+    header::record::value::{
+        map::{builder::BuildError, program::tag, Program},
+        Map,
+    },
+};
 
-fn main() -> io::Result<()> {
+fn build_self_program() -> Result<Map<Program>, BuildError> {
+    let args = env::args().collect::<Vec<_>>().join(" ");
+
+    Map::builder()
+        .insert(tag::NAME, env!("CARGO_BIN_NAME"))
+        .insert(tag::VERSION, env!("CARGO_PKG_VERSION"))
+        .insert(tag::COMMAND_LINE, args)
+        .build()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let src = env::args().nth(1).expect("missing src");
 
     let mut reader = sam::io::reader::Builder::default().build_from_path(src)?;
     let mut header = reader.read_header()?;
+
+    let pg = build_self_program()?;
+    header.programs_mut().add("noodles", pg)?;
 
     header.add_comment("a comment added by noodles-sam");
 
