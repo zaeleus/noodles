2fb7d8d4f8b7dd11af1281e4edbea040b49de413
Michael Macias <zaeleus@gmail.com>
2024-04-12 15:42:16 -0500

    sam/examples/write: Simplify header

--- noodles-sam/examples/sam_write.rs
+++ noodles-sam/examples/sam_write.rs
@@ -4,30 +4,12 @@
 //!
 //! Verify the output by piping to `samtools view --no-PG --with-header`.
 
-use std::{io, num::NonZeroUsize};
+use std::io;
 
 use noodles_sam::{
     self as sam,
     alignment::{io::Write, RecordBuf},
-    header::record::value::{
-        map::{Program, ReferenceSequence},
-        Map,
-    },
-};
-
-const SQ0_LN: NonZeroUsize = match NonZeroUsize::new(8) {
-    Some(length) => length,
-    None => unreachable!(),
-};
-
-const SQ1_LN: NonZeroUsize = match NonZeroUsize::new(13) {
-    Some(length) => length,
-    None => unreachable!(),
-};
-
-const SQ2_LN: NonZeroUsize = match NonZeroUsize::new(21) {
-    Some(length) => length,
-    None => unreachable!(),
+    header::record::value::{map::Program, Map},
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -36,9 +18,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let header = sam::Header::builder()
         .set_header(Default::default())
-        .add_reference_sequence("sq0", Map::<ReferenceSequence>::new(SQ0_LN))
-        .add_reference_sequence("sq1", Map::<ReferenceSequence>::new(SQ1_LN))
-        .add_reference_sequence("sq2", Map::<ReferenceSequence>::new(SQ2_LN))
         .add_program("noodles-sam", Map::<Program>::default())
         .add_comment("an example SAM written by noodles-sam")
         .build();
