6f276afe49dd28a8ef634260aae301ac8dd30b70
Michael Macias <zaeleus@gmail.com>
2024-04-28 20:06:14 -0500

    bgzf/multithreaded_writer/builder: Add examples

--- noodles-bgzf/src/multithreaded_writer/builder.rs
+++ noodles-bgzf/src/multithreaded_writer/builder.rs
@@ -11,18 +11,41 @@ pub struct Builder {
 
 impl Builder {
     /// Sets the compression level.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_bgzf::{multithreaded_writer::Builder, writer::CompressionLevel};
+    /// let builder = Builder::default().set_compression_level(CompressionLevel::default());
+    /// ```
     pub fn set_compression_level(mut self, compression_level: CompressionLevel) -> Self {
         self.compression_level = compression_level;
         self
     }
 
     /// Sets the worker count.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::num::NonZeroUsize;
+    /// use noodles_bgzf::multithreaded_writer::Builder;
+    /// let builder = Builder::default().set_worker_count(NonZeroUsize::MIN);
+    /// ```
     pub fn set_worker_count(mut self, worker_count: NonZeroUsize) -> Self {
         self.worker_count = worker_count;
         self
     }
 
     /// Builds a multithreaded BGZF writer from a writer.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::io;
+    /// use noodles_bgzf::multithreaded_writer::Builder;
+    /// let builder = Builder::default().build_from_writer(io::sink());
+    /// ```
     pub fn build_from_writer<W>(self, writer: W) -> MultithreadedWriter<W>
     where
         W: Write + Send + 'static,
