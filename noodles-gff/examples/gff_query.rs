2d1d38644523f3eeb6ea327cd6eb813b39141f74
Michael Macias <zaeleus@gmail.com>
2024-05-18 19:03:20 -0500

    gff/examples: Add query example

---
+++ noodles-gff/examples/gff_query.rs
@@ -0,0 +1,36 @@
+//! Queries a bgzip-compressed GFF file with a given region.
+//!
+//! The input must have an associated coordinate-sorted index (CSI) in the same directory.
+//!
+//! The result matches the output of `tabix <src> <region>`.
+
+use std::{env, fs::File, io};
+
+use noodles_bgzf as bgzf;
+use noodles_csi as csi;
+use noodles_gff as gff;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = env::args().skip(1);
+    let src = args.next().expect("missing src");
+    let region = args.next().expect("missing region").parse()?;
+
+    let index_src = format!("{src}.csi");
+    let index = csi::read(index_src)?;
+
+    let mut reader = File::open(src)
+        .map(bgzf::Reader::new)
+        .map(gff::io::Reader::new)?;
+
+    let query = reader.query(&index, &region)?;
+
+    let stdout = io::stdout().lock();
+    let mut writer = gff::io::Writer::new(stdout);
+
+    for result in query {
+        let record = result?;
+        writer.write_record(&record)?;
+    }
+
+    Ok(())
+}
