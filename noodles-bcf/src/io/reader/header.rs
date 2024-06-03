6e346533c7004abe9841d763ec4f6c19fbf7acb7
Michael Macias <zaeleus@gmail.com>
2024-06-03 10:24:48 -0500

    bcf/io/reader/header: Add header reader

--- noodles-bcf/src/io/reader/header.rs
+++ noodles-bcf/src/io/reader/header.rs
@@ -1,21 +1,101 @@
-use std::io::{self, BufRead, BufReader, Read};
+use std::io::{self, BufRead, BufReader, Read, Take};
 
 use byteorder::{LittleEndian, ReadBytesExt};
 use noodles_vcf::{self as vcf, header::StringMaps};
+
+#[derive(Clone, Copy, Debug)]
+enum State {
+    Length,
+    Header,
+    Padding,
+    Done,
+}
+
+struct Reader<R> {
+    inner: BufReader<Take<R>>,
+    state: State,
+}
+
+impl<R> Reader<R>
+where
+    R: Read,
+{
+    fn new(inner: R) -> Self {
+        Self {
+            inner: BufReader::new(inner.take(4)),
+            state: State::Length,
+        }
+    }
+}
+
+impl<R> Read for Reader<R>
+where
+    R: Read,
+{
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let mut src = self.fill_buf()?;
+        let amt = src.read(buf)?;
+        self.consume(amt);
+        Ok(amt)
+    }
+}
+
+impl<R> BufRead for Reader<R>
+where
+    R: Read,
+{
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        const NUL: u8 = 0x00;
+
+        loop {
+            match self.state {
+                State::Length => {
+                    let header_len = self.inner.read_u32::<LittleEndian>().map(u64::from)?;
+                    self.inner.get_mut().set_limit(header_len);
+                    self.state = State::Header;
+                }
+                State::Header => {
+                    let src = self.inner.fill_buf()?;
+
+                    if src.is_empty() {
+                        self.state = State::Done;
+                    } else if src[0] == NUL {
+                        self.state = State::Padding;
+                    } else if let Some(i) = src.iter().position(|&b| b == NUL) {
+                        // NOTE: This is a borrowck workaround. See NLL problem case #3.
+                        let src = self.inner.fill_buf()?;
+                        return Ok(&src[..i]);
+                    } else {
+                        // NOTE: This is a borrowck workaround. See NLL problem case #3.
+                        let src = self.inner.fill_buf()?;
+                        return Ok(src);
+                    }
+                }
+                State::Padding => {
+                    io::copy(&mut self.inner, &mut io::sink())?;
+                    self.state = State::Done;
+                }
+                State::Done => return Ok(&[]),
+            }
+        }
+    }
+
+    fn consume(&mut self, amt: usize) {
+        self.inner.consume(amt);
+    }
+}
 
 pub(super) fn read_header<R>(reader: &mut R) -> io::Result<vcf::Header>
 where
     R: Read,
 {
-    let l_text = reader.read_u32::<LittleEndian>().map(u64::from)?;
-
     let mut parser = vcf::header::Parser::default();
     let mut string_maps = StringMaps::default();
 
-    let mut header_reader = BufReader::new(reader.take(l_text));
+    let mut reader = Reader::new(reader);
     let mut buf = Vec::new();
 
-    while read_line(&mut header_reader, &mut buf)? != 0 {
+    while read_line(&mut reader, &mut buf)? != 0 {
         let entry = parser
             .parse_partial(&buf)
             .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
@@ -24,8 +104,6 @@ where
             .insert_entry(&entry)
             .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
     }
-
-    discard_padding(&mut header_reader)?;
 
     let mut header = parser
         .finish()
@@ -40,17 +118,10 @@ fn read_line<R>(reader: &mut R, dst: &mut Vec<u8>) -> io::Result<usize>
 where
     R: BufRead,
 {
-    const NUL: u8 = 0x00;
     const LINE_FEED: u8 = b'\n';
     const CARRIAGE_RETURN: u8 = b'\r';
 
     dst.clear();
-
-    let src = reader.fill_buf()?;
-
-    if src.is_empty() || src[0] == NUL {
-        return Ok(0);
-    }
 
     match reader.read_until(LINE_FEED, dst)? {
         0 => Ok(0),
@@ -65,22 +136,6 @@ where
 
             Ok(n)
         }
-    }
-}
-
-fn discard_padding<R>(reader: &mut R) -> io::Result<()>
-where
-    R: BufRead,
-{
-    loop {
-        let src = reader.fill_buf()?;
-
-        if src.is_empty() {
-            return Ok(());
-        }
-
-        let len = src.len();
-        reader.consume(len);
     }
 }
 
