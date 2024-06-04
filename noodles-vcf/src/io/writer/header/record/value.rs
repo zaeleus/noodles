e39531be535abb711c2dbafdfb4703b92d5b4228
Michael Macias <zaeleus@gmail.com>
2024-06-04 14:35:44 -0500

    vcf/io/writer/header/record/value: Move string writer to a module

--- noodles-vcf/src/io/writer/header/record/value.rs
+++ noodles-vcf/src/io/writer/header/record/value.rs
@@ -1,56 +1,9 @@
 pub(super) mod file_format;
 pub(super) mod map;
+mod string;
 
 pub(super) use self::{
     file_format::write_file_format,
     map::{write_map, write_other_map},
+    string::write_string,
 };
-
-use std::io::{self, Write};
-
-pub(super) fn write_string<W>(writer: &mut W, s: &str) -> io::Result<()>
-where
-    W: Write,
-{
-    // § 1.4 "Meta-information lines" (2024-04-20): "An _unstructured_ meta-information line
-    // consists of [...] a _value_ (which may not be empty and must not start with a '<'
-    // character)..."
-    fn is_valid(s: &str) -> bool {
-        const LESS_THAN_SIGN: char = '<';
-        s.chars().next().is_some_and(|c| c != LESS_THAN_SIGN)
-    }
-
-    if is_valid(s) {
-        writer.write_all(s.as_bytes())
-    } else {
-        Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "invalid unstructured header record value",
-        ))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_write_string() -> io::Result<()> {
-        let mut buf = Vec::new();
-
-        write_string(&mut buf, "ndls")?;
-        assert_eq!(buf, b"ndls");
-
-        assert!(matches!(
-            write_string(&mut buf, ""),
-            Err(e) if e.kind() == io::ErrorKind::InvalidInput
-        ));
-
-        assert!(matches!(
-            write_string(&mut buf, "<"),
-            Err(e) if e.kind() == io::ErrorKind::InvalidInput
-        ));
-
-        Ok(())
-    }
-}
