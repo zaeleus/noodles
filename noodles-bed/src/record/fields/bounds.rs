ae4e49917b6ff16115a91f7fa3814ccacb3e094b
Michael Macias <zaeleus@gmail.com>
2024-06-19 13:02:02 -0500

    bed/record/fields/bounds: Add tests

--- noodles-bed/src/record/fields/bounds.rs
+++ noodles-bed/src/record/fields/bounds.rs
@@ -43,3 +43,30 @@ impl Default for Bounds {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ranges() {
+        let bounds = Bounds::default();
+        assert_eq!(bounds.reference_sequence_name_range(), 0..3);
+        assert_eq!(bounds.feature_start_range(), 3..4);
+        assert_eq!(bounds.feature_end_range(), 4..5);
+    }
+
+    #[test]
+    fn test_get() {
+        let mut bounds = Bounds::default();
+
+        assert!(bounds.get(0).is_none());
+
+        bounds.other_fields_ends.push(6);
+        bounds.other_fields_ends.push(7);
+
+        assert_eq!(bounds.get(0), Some(5..6));
+        assert_eq!(bounds.get(1), Some(6..7));
+        assert!(bounds.get(2).is_none());
+    }
+}
