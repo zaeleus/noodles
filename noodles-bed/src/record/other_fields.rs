c2d4feed970dac0f4c60e2a6e6bc1b270c2009c9
Michael Macias <zaeleus@gmail.com>
2024-07-01 10:44:38 -0500

    bed/record/other_fields: Add iterator

--- noodles-bed/src/record/other_fields.rs
+++ noodles-bed/src/record/other_fields.rs
@@ -1,3 +1,5 @@
+use std::iter;
+
 use super::Fields;
 
 /// BED record other fields.
@@ -21,5 +23,16 @@ impl<'a> OtherFields<'a> {
     /// Returns an other field at the given index.
     pub fn get(&self, i: usize) -> Option<&[u8]> {
         self.0.get(i)
+    }
+
+    /// Returns an iterator over other fields.
+    pub fn iter(&self) -> impl Iterator<Item = &[u8]> {
+        let mut i = 0;
+
+        iter::from_fn(move || {
+            let field = self.get(i)?;
+            i += 1;
+            Some(field)
+        })
     }
 }
