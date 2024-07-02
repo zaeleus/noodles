281b382c0869c7f9f909737361450690b1187b4b
Michael Macias <zaeleus@gmail.com>
2024-07-02 16:01:49 -0500

    bed/io/writer/record/reference_sequence_name: Add validation

--- noodles-bed/src/io/writer/record/reference_sequence_name.rs
+++ noodles-bed/src/io/writer/record/reference_sequence_name.rs
@@ -7,7 +7,24 @@ pub(super) fn write_reference_sequence_name<W>(
 where
     W: Write,
 {
-    writer.write_all(reference_sequence_name)
+    if is_valid(reference_sequence_name) {
+        writer.write_all(reference_sequence_name)
+    } else {
+        Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "invalid reference sequence name",
+        ))
+    }
+}
+
+// 1.5 "BED fields" (2022-01-05): `[[:alnum:]_]{1,255}`.
+fn is_valid(buf: &[u8]) -> bool {
+    fn is_valid_char(b: u8) -> bool {
+        const UNDERSCORE: u8 = b'_';
+        b.is_ascii_alphanumeric() || b == UNDERSCORE
+    }
+
+    matches!(buf.len(), 1..=255) && buf.iter().copied().all(is_valid_char)
 }
 
 #[cfg(test)]
@@ -17,8 +34,17 @@ mod tests {
     #[test]
     fn test_write_reference_sequence_name() -> io::Result<()> {
         let mut buf = Vec::new();
+
+        buf.clear();
         write_reference_sequence_name(&mut buf, b"sq0")?;
         assert_eq!(buf, b"sq0");
+
+        buf.clear();
+        assert!(matches!(
+            write_reference_sequence_name(&mut buf, "🍜".as_bytes()),
+            Err(e) if e.kind() == io::ErrorKind::InvalidInput
+        ));
+
         Ok(())
     }
 }
