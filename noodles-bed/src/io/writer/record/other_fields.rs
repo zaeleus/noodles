47df6fbd77df1b6b7df0a4c86ad71e45149f5e21
Michael Macias <zaeleus@gmail.com>
2024-07-15 15:02:02 -0500

    bed/io/writer/record/other_fields: Accept OtherFields implementations

--- noodles-bed/src/io/writer/record/other_fields.rs
+++ noodles-bed/src/io/writer/record/other_fields.rs
@@ -1,11 +1,12 @@
 use std::io::{self, Write};
 
 use super::write_separator;
-use crate::record::OtherFields;
+use crate::feature::record::OtherFields;
 
-pub(super) fn write_other_fields<W>(writer: &mut W, other_fields: OtherFields<'_>) -> io::Result<()>
+pub(super) fn write_other_fields<W, F>(writer: &mut W, other_fields: &F) -> io::Result<()>
 where
     W: Write,
+    F: OtherFields,
 {
     for field in other_fields.iter() {
         write_separator(writer)?;
