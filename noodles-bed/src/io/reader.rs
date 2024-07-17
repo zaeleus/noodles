04ee46d3c5a1e825899f1558fe1cbce14a5e5581
Michael Macias <zaeleus@gmail.com>
2024-07-17 16:01:49 -0500

    bed/io/reader: Remove Reader::read_line
    Read into a `Record<N>` buffer instead.

--- noodles-bed/src/io/reader.rs
+++ noodles-bed/src/io/reader.rs
@@ -71,76 +71,8 @@ where
         Self { inner }
     }
 
-    /// Reads a raw BED line.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use std::io;
-    /// use noodles_bed as bed;
-    ///
-    /// let data = b"sq0\t8\t13\n";
-    /// let mut reader = bed::io::Reader::new(&data[..]);
-    ///
-    /// let mut buf = String::new();
-    /// reader.read_line(&mut buf)?;
-    ///
-    /// assert_eq!(buf, "sq0\t8\t13");
-    /// # Ok::<_, io::Error>(())
-    /// ```
-    pub fn read_line(&mut self, buf: &mut String) -> io::Result<usize> {
-        read_line(&mut self.inner, buf)
-    }
-
     /// Reads a record.
     pub fn read_record<const N: usize>(&mut self, record: &mut Record<N>) -> io::Result<usize> {
         read_record(&mut self.inner, record)
-    }
-}
-
-fn read_line<R>(reader: &mut R, buf: &mut String) -> io::Result<usize>
-where
-    R: BufRead,
-{
-    const LINE_FEED: char = '\n';
-    const CARRIAGE_RETURN: char = '\r';
-
-    match reader.read_line(buf) {
-        Ok(0) => Ok(0),
-        Ok(n) => {
-            if buf.ends_with(LINE_FEED) {
-                buf.pop();
-
-                if buf.ends_with(CARRIAGE_RETURN) {
-                    buf.pop();
-                }
-            }
-
-            Ok(n)
-        }
-        Err(e) => Err(e),
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_read_line() -> io::Result<()> {
-        fn t(buf: &mut String, mut reader: &[u8], expected: &str) -> io::Result<()> {
-            buf.clear();
-            read_line(&mut reader, buf)?;
-            assert_eq!(buf, expected);
-            Ok(())
-        }
-
-        let mut buf = String::new();
-
-        t(&mut buf, b"noodles\n", "noodles")?;
-        t(&mut buf, b"noodles\r\n", "noodles")?;
-        t(&mut buf, b"noodles", "noodles")?;
-
-        Ok(())
     }
 }
