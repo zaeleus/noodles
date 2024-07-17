a58576e2ca293f13c2ae841283e69d91e038a080
Michael Macias <zaeleus@gmail.com>
2024-07-17 14:06:24 -0500

    bed/record: Implement Default for Record<4>

--- noodles-bed/src/record/fields/bounds.rs
+++ noodles-bed/src/record/fields/bounds.rs
@@ -40,13 +40,22 @@ impl Default for Bounds<3> {
     }
 }
 
+impl Default for Bounds<4> {
+    fn default() -> Self {
+        Self {
+            standard_fields_ends: [3, 4, 5, 6],
+            other_fields_ends: Vec::new(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_ranges() {
-        let bounds = Bounds::default();
+        let bounds = Bounds::<3>::default();
         assert_eq!(bounds.reference_sequence_name_range(), 0..3);
         assert_eq!(bounds.feature_start_range(), 3..4);
         assert_eq!(bounds.feature_end_range(), 4..5);
@@ -54,7 +63,7 @@ mod tests {
 
     #[test]
     fn test_get() {
-        let mut bounds = Bounds::default();
+        let mut bounds = Bounds::<3>::default();
 
         assert!(bounds.get(0).is_none());
 
