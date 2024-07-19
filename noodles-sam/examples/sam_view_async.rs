c6a86e598aeb36cd03fb23149e06af0784fea947
Michael Macias <zaeleus@gmail.com>
2024-07-19 09:47:30 -0500

    sam/examples/view_async: Use records iterator

--- noodles-sam/examples/sam_view_async.rs
+++ noodles-sam/examples/sam_view_async.rs
@@ -24,7 +24,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut writer = sam::AsyncWriter::new(io::stdout());
 
-    let mut records = reader.record_bufs(&header);
+    let mut records = reader.records();
 
     while let Some(record) = records.try_next().await? {
         writer.write_alignment_record(&header, &record).await?;
