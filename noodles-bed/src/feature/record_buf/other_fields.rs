45e1669b50eb459676313288f9a7b990f87cc7a3
Michael Macias <zaeleus@gmail.com>
2024-07-26 13:24:51 -0500

    bed/feature/record_buf/other_fields: Require fields to exist

--- noodles-bed/src/feature/record_buf/other_fields.rs
+++ noodles-bed/src/feature/record_buf/other_fields.rs
@@ -2,16 +2,16 @@ use bstr::{BStr, BString};
 
 /// Feature record other fields.
 #[derive(Clone, Default, Debug, Eq, PartialEq)]
-pub struct OtherFields(Vec<Option<BString>>);
+pub struct OtherFields(Vec<BString>);
 
-impl AsRef<[Option<BString>]> for OtherFields {
-    fn as_ref(&self) -> &[Option<BString>] {
+impl AsRef<[BString]> for OtherFields {
+    fn as_ref(&self) -> &[BString] {
         &self.0
     }
 }
 
-impl AsMut<Vec<Option<BString>>> for OtherFields {
-    fn as_mut(&mut self) -> &mut Vec<Option<BString>> {
+impl AsMut<Vec<BString>> for OtherFields {
+    fn as_mut(&mut self) -> &mut Vec<BString> {
         &mut self.0
     }
 }
