093e963e8d61c8acc6060e5e4b3975a32dcdc43e
Michael Macias <zaeleus@gmail.com>
2024-07-26 14:05:04 -0500

    bed/examples: Add write example

---
+++ noodles-bed/examples/bed_write.rs
@@ -0,0 +1,23 @@
+//! Creates a new BED3 file.
+//!
+//! This writes a single BED3 record to stdout.
+
+use std::io;
+
+use noodles_bed::{self as bed, feature::RecordBuf};
+use noodles_core::Position;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let stdout = io::stdout().lock();
+    let mut writer = bed::io::Writer::new(stdout);
+
+    let record = RecordBuf::<3>::builder()
+        .set_reference_sequence_name("sq0")
+        .set_feature_start(Position::try_from(8)?)
+        .set_feature_end(Position::try_from(13)?)
+        .build();
+
+    writer.write_feature_record(&record)?;
+
+    Ok(())
+}
