cb6d00dd4ed38c9b139150b23daf841d643fb134
Michael Macias <zaeleus@gmail.com>
2024-07-26 10:39:26 -0500

    bed/feature/record: Use byte strings

--- noodles-bed/src/feature/record.rs
+++ noodles-bed/src/feature/record.rs
@@ -4,6 +4,7 @@ mod other_fields;
 
 use std::io;
 
+use bstr::BStr;
 use noodles_core::Position;
 
 pub use self::other_fields::OtherFields;
@@ -14,7 +15,7 @@ pub trait Record {
     fn standard_field_count(&self) -> usize;
 
     /// Returns the reference sequence name.
-    fn reference_sequence_name(&self) -> &[u8];
+    fn reference_sequence_name(&self) -> &BStr;
 
     /// Returns the feature start.
     fn feature_start(&self) -> io::Result<Position>;
@@ -23,7 +24,7 @@ pub trait Record {
     fn feature_end(&self) -> Option<io::Result<Position>>;
 
     /// Returns the name.
-    fn name(&self) -> Option<&[u8]>;
+    fn name(&self) -> Option<&BStr>;
 
     /// Returns the other fields.
     fn other_fields(&self) -> Box<dyn OtherFields + '_>;
