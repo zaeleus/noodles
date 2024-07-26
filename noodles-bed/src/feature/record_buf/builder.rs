dcc554f196c3916fdfdf65755cceae1f617e8a18
Michael Macias <zaeleus@gmail.com>
2024-07-26 14:12:57 -0500

    bed/feature/record_buf/builder: Add builders

--- noodles-bed/src/feature/record_buf/builder.rs
+++ noodles-bed/src/feature/record_buf/builder.rs
@@ -12,9 +12,26 @@ pub struct Builder<const N: usize> {
     feature_start: Option<Position>,
     feature_end: Option<Position>,
     name: Option<BString>,
-    score: Option<u16>,
+    score: u16,
     strand: Option<Strand>,
     other_fields: OtherFields,
+}
+
+impl<const N: usize> Builder<N> {
+    /// Builds a feature record buffer.
+    pub fn build(self) -> RecordBuf<N> {
+        RecordBuf {
+            standard_fields: StandardFields {
+                reference_sequence_name: self.reference_sequence_name,
+                feature_start: self.feature_start.unwrap_or(Position::MIN),
+                feature_end: self.feature_end,
+                name: self.name,
+                score: self.score,
+                strand: self.strand,
+            },
+            other_fields: self.other_fields,
+        }
+    }
 }
 
 impl Builder<3> {
@@ -46,19 +63,134 @@ impl Builder<3> {
     }
 }
 
-impl Builder<3> {
-    /// Builds a BED3 record.
-    pub fn build(self) -> RecordBuf<3> {
-        RecordBuf {
-            standard_fields: StandardFields {
-                reference_sequence_name: self.reference_sequence_name,
-                feature_start: self.feature_start.unwrap_or(Position::MIN),
-                feature_end: self.feature_end,
-                name: None,
-                score: 0,
-                strand: None,
-            },
-            other_fields: self.other_fields,
-        }
+impl Builder<4> {
+    /// Sets the reference sequence name.
+    pub fn set_reference_sequence_name<M>(mut self, reference_sequence_name: M) -> Self
+    where
+        M: Into<BString>,
+    {
+        self.reference_sequence_name = reference_sequence_name.into();
+        self
+    }
+
+    /// Sets the feature start position.
+    pub fn set_feature_start(mut self, start_position: Position) -> Self {
+        self.feature_start = Some(start_position);
+        self
+    }
+
+    /// Sets the feature end position.
+    pub fn set_feature_end(mut self, end_position: Position) -> Self {
+        self.feature_end = Some(end_position);
+        self
+    }
+
+    /// Sets the name.
+    pub fn set_name<M>(mut self, name: M) -> Self
+    where
+        M: Into<BString>,
+    {
+        self.name = Some(name.into());
+        self
+    }
+
+    /// Sets the list of raw optional fields.
+    pub fn set_other_fields(mut self, other_fields: OtherFields) -> Self {
+        self.other_fields = other_fields;
+        self
+    }
+}
+
+impl Builder<5> {
+    /// Sets the reference sequence name.
+    pub fn set_reference_sequence_name<M>(mut self, reference_sequence_name: M) -> Self
+    where
+        M: Into<BString>,
+    {
+        self.reference_sequence_name = reference_sequence_name.into();
+        self
+    }
+
+    /// Sets the feature start position.
+    pub fn set_feature_start(mut self, start_position: Position) -> Self {
+        self.feature_start = Some(start_position);
+        self
+    }
+
+    /// Sets the feature end position.
+    pub fn set_feature_end(mut self, end_position: Position) -> Self {
+        self.feature_end = Some(end_position);
+        self
+    }
+
+    /// Sets the name.
+    pub fn set_name<M>(mut self, name: M) -> Self
+    where
+        M: Into<BString>,
+    {
+        self.name = Some(name.into());
+        self
+    }
+
+    /// Sets the score.
+    pub fn set_score(mut self, score: u16) -> Self {
+        self.score = score;
+        self
+    }
+
+    /// Sets the list of raw optional fields.
+    pub fn set_other_fields(mut self, other_fields: OtherFields) -> Self {
+        self.other_fields = other_fields;
+        self
+    }
+}
+
+impl Builder<6> {
+    /// Sets the reference sequence name.
+    pub fn set_reference_sequence_name<M>(mut self, reference_sequence_name: M) -> Self
+    where
+        M: Into<BString>,
+    {
+        self.reference_sequence_name = reference_sequence_name.into();
+        self
+    }
+
+    /// Sets the feature start position.
+    pub fn set_feature_start(mut self, start_position: Position) -> Self {
+        self.feature_start = Some(start_position);
+        self
+    }
+
+    /// Sets the feature end position.
+    pub fn set_feature_end(mut self, end_position: Position) -> Self {
+        self.feature_end = Some(end_position);
+        self
+    }
+
+    /// Sets the name.
+    pub fn set_name<M>(mut self, name: M) -> Self
+    where
+        M: Into<BString>,
+    {
+        self.name = Some(name.into());
+        self
+    }
+
+    /// Sets the score.
+    pub fn set_score(mut self, score: u16) -> Self {
+        self.score = score;
+        self
+    }
+
+    /// Sets the strand.
+    pub fn set_strand(mut self, strand: Strand) -> Self {
+        self.strand = Some(strand);
+        self
+    }
+
+    /// Sets the list of raw optional fields.
+    pub fn set_other_fields(mut self, other_fields: OtherFields) -> Self {
+        self.other_fields = other_fields;
+        self
     }
 }
