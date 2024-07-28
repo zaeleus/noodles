b65e13fa1f22cb9307d00e6075af8cd54c2ace6f
Michael Macias <zaeleus@gmail.com>
2024-07-28 11:30:32 -0500

    bed/record: Implement Debug for Record<5>

--- noodles-bed/src/record.rs
+++ noodles-bed/src/record.rs
@@ -156,6 +156,19 @@ impl fmt::Debug for Record<4> {
     }
 }
 
+impl fmt::Debug for Record<5> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Record")
+            .field("reference_sequence_name", &self.reference_sequence_name())
+            .field("feature_start", &self.feature_start())
+            .field("feature_end", &self.feature_end())
+            .field("name", &self.name())
+            .field("score", &self.score())
+            .field("other_fields", &self.other_fields())
+            .finish()
+    }
+}
+
 impl Default for Record<3> {
     fn default() -> Self {
         Self(Fields::default())
