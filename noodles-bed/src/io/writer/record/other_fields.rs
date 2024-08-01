5ea46bb2018380aee2e4b84385d98319a1b48526
Michael Macias <zaeleus@gmail.com>
2024-08-01 13:57:01 -0500

    bed/writer/record/other_fields: Add tests

--- noodles-bed/src/io/writer/record/other_fields.rs
+++ noodles-bed/src/io/writer/record/other_fields.rs
@@ -18,3 +18,31 @@ where
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::feature::record_buf::{other_fields::Value, OtherFields as OtherFieldsBuf};
+
+    #[test]
+    fn test_write_other_fields() -> io::Result<()> {
+        let mut buf = Vec::new();
+
+        let mut other_fields = OtherFieldsBuf::default();
+        buf.clear();
+        write_other_fields(&mut buf, &other_fields)?;
+        assert!(buf.is_empty());
+
+        other_fields.as_mut().push(Value::UInt64(8));
+        buf.clear();
+        write_other_fields(&mut buf, &other_fields)?;
+        assert_eq!(buf, b"\t8");
+
+        other_fields.as_mut().push(Value::UInt64(13));
+        buf.clear();
+        write_other_fields(&mut buf, &other_fields)?;
+        assert_eq!(buf, b"\t8\t13");
+
+        Ok(())
+    }
+}
