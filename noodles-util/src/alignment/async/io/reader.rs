25619e9f074009cc07fa647738199686029d53cc
Michael Hall <michael@mbh.sh>
2024-08-07 15:17:46 +1000

    util/alignment/async: add Reader

--- noodles-util/src/alignment/async/io/reader.rs
+++ noodles-util/src/alignment/async/io/reader.rs
@@ -1,12 +1,12 @@
 //! Async alignment reader.
 //! todo add an example
 
-use futures::Stream;
+use futures::{Stream, StreamExt};
 use noodles_bam as bam;
 use noodles_cram as cram;
 use noodles_sam as sam;
-use std::io;
-use tokio::io::AsyncBufRead;
+use std::pin::Pin;
+use tokio::io::{self, AsyncBufRead};
 
 /// An async alignment reader.
 pub enum Reader<R> {
@@ -24,9 +24,14 @@ where
 {
     /// Reads the SAM header
     ///
-    /// todo example usage
-    pub async fn read_header(&mut self) -> tokio::io::Result<sam::Header> {
-        todo!()
+    /// # Examples
+    /// todo once Builder is implemented
+    pub async fn read_header(&mut self) -> io::Result<sam::Header> {
+        match self {
+            Self::Sam(reader) => reader.read_header().await,
+            Self::Bam(reader) => reader.read_header().await,
+            Self::Cram(reader) => reader.read_header().await,
+        }
     }
 
     /// Returns an iterator over records starting from the current stream position.
@@ -35,6 +40,27 @@ where
         &'r mut self,
         header: &'h sam::Header,
     ) -> impl Stream<Item = io::Result<Box<dyn sam::alignment::Record>>> + 'r {
-        todo!()
+        #[allow(clippy::type_complexity)]
+        let records: Pin<
+            Box<dyn Stream<Item = io::Result<Box<dyn sam::alignment::Record>>>>,
+        > = match self {
+            Self::Sam(reader) => Box::pin(
+                reader
+                    .records()
+                    .map(|result| result.map(|r| Box::new(r) as Box<dyn sam::alignment::Record>)),
+            ),
+            Self::Bam(reader) => Box::pin(
+                reader
+                    .records()
+                    .map(|result| result.map(|r| Box::new(r) as Box<dyn sam::alignment::Record>)),
+            ),
+            Self::Cram(reader) => Box::pin(
+                reader
+                    .records(header)
+                    .map(|result| result.map(|r| Box::new(r) as Box<dyn sam::alignment::Record>)),
+            ),
+        };
+
+        records
     }
 }
