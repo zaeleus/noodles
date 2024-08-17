391a5800f039d807c1def31753d978efcd158774
Michael Macias <zaeleus@gmail.com>
2024-08-17 16:10:57 -0500

    bcf/record/codec/decoder/quality_score: Add tests

--- noodles-bcf/src/record/codec/decoder/quality_score.rs
+++ noodles-bcf/src/record/codec/decoder/quality_score.rs
@@ -14,3 +14,25 @@ pub(crate) fn read_qual(src: &mut &[u8]) -> io::Result<Option<f32>> {
         )),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_qual() -> io::Result<()> {
+        let mut src = &[0x01, 0x00, 0x80, 0x7f][..];
+        assert!(read_qual(&mut src)?.is_none());
+
+        let mut src = &[0x00, 0x00, 0x00, 0x00][..];
+        assert_eq!(read_qual(&mut src)?, Some(0.0));
+
+        let mut src = &[0x02, 0x00, 0x80, 0x7f][..];
+        assert!(matches!(
+            read_qual(&mut src),
+            Err(e) if e.kind() == io::ErrorKind::InvalidInput
+        ));
+
+        Ok(())
+    }
+}
