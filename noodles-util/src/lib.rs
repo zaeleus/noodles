174ad3c98092728a6be3feabb346619ebd14a5ad
Michael Macias <zaeleus@gmail.com>
2024-08-21 13:27:46 -0500

    util: Remove intra-doc links
    These modules are not always enabled.

--- noodles-util/src/lib.rs
+++ noodles-util/src/lib.rs
@@ -1,7 +1,7 @@
 #![warn(missing_docs)]
 
 //! **noodles-util** are utilities for working with noodles. Currently, this consists of a unified
-//! interface for reading and writing [alignment] (BAM/CRAM/SAM) and [variant] (VCF/BCF) data.
+//! interface for reading and writing alignment (BAM/CRAM/SAM) and variant (VCF/BCF) data.
 
 #[cfg(feature = "alignment")]
 pub mod alignment;
