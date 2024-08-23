75775efd756d84d793d1aa8d12ca799220d6ab0a
Michael Macias <zaeleus@gmail.com>
2024-08-23 15:36:44 -0500

    bed/feature/record/other_fields/value: Add conversion to value buffer

--- noodles-bed/src/feature/record/other_fields/value.rs
+++ noodles-bed/src/feature/record/other_fields/value.rs
@@ -14,3 +14,15 @@ pub enum Value<'a> {
     /// A byte string.
     String(&'a BStr),
 }
+
+impl<'a> From<Value<'a>> for crate::feature::record_buf::other_fields::Value {
+    fn from(value: Value<'a>) -> Self {
+        match value {
+            Value::Int64(n) => Self::Int64(n),
+            Value::UInt64(n) => Self::UInt64(n),
+            Value::Float64(n) => Self::Float64(n),
+            Value::Character(b) => Self::Character(b),
+            Value::String(s) => Self::String(s.into()),
+        }
+    }
+}
