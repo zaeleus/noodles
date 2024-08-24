54e6d70158d2a22c1b1668fec84722fccb6b9004
Michael Macias <zaeleus@gmail.com>
2024-08-24 15:58:47 -0500

    bed/feature/record_buf/convert: Add conversion from BED6+ feature record

--- noodles-bed/src/feature/record_buf/convert.rs
+++ noodles-bed/src/feature/record_buf/convert.rs
@@ -52,7 +52,7 @@ impl RecordBuf<4> {
 }
 
 impl RecordBuf<5> {
-    /// Converts a BED4+ feature record to a BED4+ record buffer.
+    /// Converts a BED5+ feature record to a BED5+ record buffer.
     pub fn try_from_feature_record<R>(record: &R) -> io::Result<Self>
     where
         R: Record<5>,
@@ -71,6 +71,40 @@ impl RecordBuf<5> {
 
         if let Some(score) = record.score().transpose()? {
             builder = builder.set_score(score);
+        }
+
+        let values: Vec<_> = record.other_fields().iter().map(Value::from).collect();
+        let other_fields = OtherFields::from(values);
+        builder = builder.set_other_fields(other_fields);
+
+        Ok(builder.build())
+    }
+}
+
+impl RecordBuf<6> {
+    /// Converts a BED6+ feature record to a BED6+ record buffer.
+    pub fn try_from_feature_record<R>(record: &R) -> io::Result<Self>
+    where
+        R: Record<6>,
+    {
+        let mut builder = RecordBuf::<6>::builder()
+            .set_reference_sequence_name(record.reference_sequence_name())
+            .set_feature_start(record.feature_start()?);
+
+        if let Some(feature_end) = record.feature_end().transpose()? {
+            builder = builder.set_feature_end(feature_end);
+        }
+
+        if let Some(Some(name)) = record.name() {
+            builder = builder.set_name(name);
+        }
+
+        if let Some(score) = record.score().transpose()? {
+            builder = builder.set_score(score);
+        }
+
+        if let Some(Some(strand)) = record.strand().transpose()? {
+            builder = builder.set_strand(strand);
         }
 
         let values: Vec<_> = record.other_fields().iter().map(Value::from).collect();
