2215def26dc67f136654644cbf47b7d9acf6b7e6
Michael Macias <zaeleus@gmail.com>
2024-08-24 15:57:41 -0500

    bed/feature/record_buf/convert: Add conversion from BED5+ feature record

--- noodles-bed/src/feature/record_buf/convert.rs
+++ noodles-bed/src/feature/record_buf/convert.rs
@@ -50,3 +50,33 @@ impl RecordBuf<4> {
         Ok(builder.build())
     }
 }
+
+impl RecordBuf<5> {
+    /// Converts a BED4+ feature record to a BED4+ record buffer.
+    pub fn try_from_feature_record<R>(record: &R) -> io::Result<Self>
+    where
+        R: Record<5>,
+    {
+        let mut builder = RecordBuf::<5>::builder()
+            .set_reference_sequence_name(record.reference_sequence_name())
+            .set_feature_start(record.feature_start()?);
+
+        if let Some(feature_end) = record.feature_end().transpose()? {
+            builder = builder.set_feature_end(feature_end);
+        }
+
+        if let Some(Some(name)) = record.name() {
+            builder = builder.set_name(name);
+        }
+
+        if let Some(score) = record.score().transpose()? {
+            builder = builder.set_score(score);
+        }
+
+        let values: Vec<_> = record.other_fields().iter().map(Value::from).collect();
+        let other_fields = OtherFields::from(values);
+        builder = builder.set_other_fields(other_fields);
+
+        Ok(builder.build())
+    }
+}
