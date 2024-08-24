b448a6ae97ba353c22ae4b04b4ac142feece6f32
Michael Macias <zaeleus@gmail.com>
2024-08-24 15:55:33 -0500

    bed/feature/record_buf/convert: Add conversion from BED4+ feature record

--- noodles-bed/src/feature/record_buf/convert.rs
+++ noodles-bed/src/feature/record_buf/convert.rs
@@ -24,3 +24,29 @@ impl RecordBuf<3> {
         Ok(builder.build())
     }
 }
+
+impl RecordBuf<4> {
+    /// Converts a BED4+ feature record to a BED4+ record buffer.
+    pub fn try_from_feature_record<R>(record: &R) -> io::Result<Self>
+    where
+        R: Record<4>,
+    {
+        let mut builder = RecordBuf::<4>::builder()
+            .set_reference_sequence_name(record.reference_sequence_name())
+            .set_feature_start(record.feature_start()?);
+
+        if let Some(feature_end) = record.feature_end().transpose()? {
+            builder = builder.set_feature_end(feature_end);
+        }
+
+        if let Some(Some(name)) = record.name() {
+            builder = builder.set_name(name);
+        }
+
+        let values: Vec<_> = record.other_fields().iter().map(Value::from).collect();
+        let other_fields = OtherFields::from(values);
+        builder = builder.set_other_fields(other_fields);
+
+        Ok(builder.build())
+    }
+}
