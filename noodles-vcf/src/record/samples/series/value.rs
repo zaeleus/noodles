38cd349f94bca51f54dd0324c29554421d54c347
Michael Macias <zaeleus@gmail.com>
2024-09-13 11:26:51 -0500

    vcf/record/samples/series/value: Add tests

--- noodles-vcf/src/record/samples/series/value.rs
+++ noodles-vcf/src/record/samples/series/value.rs
@@ -7,7 +7,10 @@ use std::io;
 pub use self::genotype::Genotype;
 use crate::{
     io::reader::record_buf::value::percent_decode,
-    variant::record::samples::series::{value::Array, Value},
+    variant::record::samples::{
+        keys::key,
+        series::{value::Array, Value},
+    },
     Header,
 };
 
@@ -16,10 +19,7 @@ pub(crate) fn parse_value<'a>(
     header: &Header,
     key: &str,
 ) -> io::Result<Option<Value<'a>>> {
-    use crate::{
-        header::record::value::map::format::{definition::definition, Number, Type},
-        variant::record::samples::keys::key,
-    };
+    use crate::header::record::value::map::format::{definition::definition, Number, Type};
 
     const MISSING: &str = ".";
 
@@ -108,4 +108,113 @@ fn parse_character_array_value(src: &str) -> io::Result<Value<'_>> {
 
 fn parse_string_array_value(src: &str) -> io::Result<Value<'_>> {
     Ok(Value::Array(Array::String(Box::new(src))))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        header::record::value::{
+            map::{
+                format::{Number, Type},
+                Format,
+            },
+            Map,
+        },
+        variant::{
+            record::samples::series::value::genotype::Phasing,
+            record_buf::samples::sample::{value::genotype::Allele, Value as ValueBuf},
+        },
+    };
+
+    #[test]
+    fn test_parse_value() -> io::Result<()> {
+        fn t(s: &str, header: &Header, key: &str, expected: Option<ValueBuf>) -> io::Result<()> {
+            let actual = parse_value(s, header, key)
+                .and_then(|result| result.map(ValueBuf::try_from).transpose())?;
+
+            assert_eq!(actual, expected);
+
+            Ok(())
+        }
+
+        #[rustfmt::skip]
+        let header = Header::builder()
+            .add_format("I32", Map::<Format>::new(Number::Count(1), Type::Integer, ""))
+            .add_format("F32", Map::<Format>::new(Number::Count(1), Type::Float, ""))
+            .add_format("CHAR", Map::<Format>::new(Number::Count(1), Type::Character, ""))
+            .add_format("STRING", Map::<Format>::new(Number::Count(1), Type::String, ""))
+            .add_format("I32_ARRAY", Map::<Format>::new(Number::Count(2), Type::Integer, ""))
+            .add_format("F32_ARRAY", Map::<Format>::new(Number::Count(2), Type::Float, ""))
+            .add_format("CHAR_ARRAY", Map::<Format>::new(Number::Count(2), Type::Character, ""))
+            .add_format("STRING_ARRAY", Map::<Format>::new(Number::Count(2), Type::String, ""))
+            .add_format("I32_INVALID", Map::<Format>::new(Number::Count(0), Type::Integer, ""))
+            .build();
+
+        t(".", &header, "I32", None)?;
+        t("8", &header, "I32", Some(ValueBuf::from(8)))?;
+
+        t(".", &header, "F32", None)?;
+        t("0", &header, "F32", Some(ValueBuf::from(0.0)))?;
+
+        t(".", &header, "CHAR", None)?;
+        t("n", &header, "CHAR", Some(ValueBuf::from('n')))?;
+
+        t(".", &header, "STRING", None)?;
+        t("ndls", &header, "STRING", Some(ValueBuf::from("ndls")))?;
+
+        t(".", &header, "I32_ARRAY", None)?;
+        t(
+            "8,.",
+            &header,
+            "I32_ARRAY",
+            Some(ValueBuf::from(vec![Some(8), None])),
+        )?;
+
+        t(".", &header, "F32_ARRAY", None)?;
+        t(
+            "0,.",
+            &header,
+            "F32_ARRAY",
+            Some(ValueBuf::from(vec![Some(0.0), None])),
+        )?;
+
+        t(".", &header, "CHAR_ARRAY", None)?;
+        t(
+            "n,.",
+            &header,
+            "CHAR_ARRAY",
+            Some(ValueBuf::from(vec![Some('n'), None])),
+        )?;
+
+        t(".", &header, "STRING_ARRAY", None)?;
+        t(
+            "n,.",
+            &header,
+            "STRING_ARRAY",
+            Some(ValueBuf::from(vec![Some(String::from("n")), None])),
+        )?;
+
+        t(".", &header, key::GENOTYPE, None)?;
+        t(
+            "0/0",
+            &header,
+            key::GENOTYPE,
+            Some(ValueBuf::Genotype(
+                [
+                    Allele::new(Some(0), Phasing::Unphased),
+                    Allele::new(Some(0), Phasing::Unphased),
+                ]
+                .into_iter()
+                .collect(),
+            )),
+        )?;
+
+        assert!(matches!(
+            dbg!(parse_value("0", &header, "I32_INVALID")),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+
+        Ok(())
+    }
 }
