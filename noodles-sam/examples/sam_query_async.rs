ec1718504c6ec9f4957c47ebde73133b5b655b52
Michael Macias <zaeleus@gmail.com>
2024-09-19 10:45:05 -0500

    sam/examples/query_async: Handle querying unmapped region

--- noodles-sam/examples/sam_query_async.rs
+++ noodles-sam/examples/sam_query_async.rs
@@ -5,9 +5,9 @@
 //!
 //! The result matches the output of `samtools view <src> <region>`.
 
-use std::{env, path::PathBuf};
+use std::{env, path::PathBuf, pin::Pin};
 
-use futures::TryStreamExt;
+use futures::{Stream, TryStreamExt};
 use noodles_bgzf as bgzf;
 use noodles_csi as csi;
 use noodles_sam as sam;
@@ -15,6 +15,8 @@ use tokio::{
     fs::File,
     io::{self, AsyncWriteExt},
 };
+
+const UNMAPPED: &str = "*";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -31,12 +33,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let header = reader.read_header().await?;
 
     let index = csi::r#async::read(src.with_extension("sam.csi")).await?;
-    let region = raw_region.parse()?;
-    let mut query = reader.query(&header, &index, &region)?;
+
+    let mut records: Pin<Box<dyn Stream<Item = io::Result<sam::Record>>>> =
+        if raw_region == UNMAPPED {
+            reader.query_unmapped(&index).await.map(Box::pin)?
+        } else {
+            let region = raw_region.parse()?;
+            reader.query(&header, &index, &region).map(Box::pin)?
+        };
 
     let mut writer = sam::r#async::io::Writer::new(io::stdout());
 
-    while let Some(record) = query.try_next().await? {
+    while let Some(record) = records.try_next().await? {
         writer.write_alignment_record(&header, &record).await?;
     }
 
