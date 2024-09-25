6bfbf4ba8444e76d41e4d5c4454d9a8f09da5d9a
Michael Macias <zaeleus@gmail.com>
2024-09-25 12:05:51 -0500

    vcf/io/writer/header/record/value/map/other: Add test

--- noodles-vcf/src/io/writer/header/record/value/map/other.rs
+++ noodles-vcf/src/io/writer/header/record/value/map/other.rs
@@ -9,3 +9,22 @@ where
 {
     write_other_fields(writer, other.other_fields())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_write_other() -> Result<(), Box<dyn std::error::Error>> {
+        let mut buf = Vec::new();
+
+        buf.clear();
+        let map = Map::<Other>::builder()
+            .insert("noodles".parse()?, "vcf")
+            .build()?;
+        write_other(&mut buf, &map)?;
+        assert_eq!(buf, br#",noodles="vcf""#);
+
+        Ok(())
+    }
+}
