c8ed93a176f0b9ba1fa25176b1fe1b02478c9b5f
Michael Macias <zaeleus@gmail.com>
2024-11-08 09:55:30 -0600

    sam/record/data/field/tag: Split first chunk into an array reference

--- noodles-sam/src/record/data/field/tag.rs
+++ noodles-sam/src/record/data/field/tag.rs
@@ -3,19 +3,22 @@ use std::io;
 use crate::alignment::record::data::field::Tag;
 
 pub(super) fn parse_tag(src: &mut &[u8]) -> io::Result<Tag> {
-    if src.len() < 2 {
-        return Err(io::Error::from(io::ErrorKind::UnexpectedEof));
-    }
-
-    let (buf, rest) = src.split_at(2);
-
-    // SAFETY: `buf` is 2 bytes.
-    let raw_tag: [u8; 2] = buf.try_into().unwrap();
-    let tag = Tag::from(raw_tag);
-
+    let ([a, b], rest) =
+        split_first_chunk(src).ok_or_else(|| io::Error::from(io::ErrorKind::UnexpectedEof))?;
+    let tag = Tag::new(*a, *b);
     *src = rest;
-
     Ok(tag)
+}
+
+// TODO: Use `slice::split_first_chunk` when the MSRV is raised to or above Rust 1.77.0.
+fn split_first_chunk<const N: usize>(src: &[u8]) -> Option<(&[u8; N], &[u8])> {
+    if src.len() < N {
+        None
+    } else {
+        // SAFETY: `src.len` >= `N`.
+        let (head, tail) = src.split_at(N);
+        <&[u8; N]>::try_from(head).ok().map(|chunk| (chunk, tail))
+    }
 }
 
 #[cfg(test)]
