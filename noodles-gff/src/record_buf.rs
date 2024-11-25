ca0e91f09b25a7a9748ae0baeb1e825c7ed7a406
Michael Macias <zaeleus@gmail.com>
2024-11-25 11:37:14 -0600

    gff/record_buf: Remove formatter

--- noodles-gff/src/record_buf.rs
+++ noodles-gff/src/record_buf.rs
@@ -181,42 +181,6 @@ impl Default for RecordBuf {
     }
 }
 
-impl fmt::Display for RecordBuf {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{seqid}\t{source}\t{ty}\t{start}\t{end}",
-            seqid = self.reference_sequence_name(),
-            source = self.source(),
-            ty = self.ty(),
-            start = self.start(),
-            end = self.end(),
-        )?;
-
-        if let Some(score) = self.score() {
-            write!(f, "\t{score}")?;
-        } else {
-            write!(f, "\t{MISSING_FIELD}")?;
-        }
-
-        write!(f, "\t{}", self.strand())?;
-
-        if let Some(phase) = self.phase() {
-            write!(f, "\t{phase}")?;
-        } else {
-            write!(f, "\t{MISSING_FIELD}")?;
-        }
-
-        if self.attributes().is_empty() {
-            write!(f, "\t{MISSING_FIELD}")?;
-        } else {
-            write!(f, "\t{}", self.attributes())?;
-        }
-
-        Ok(())
-    }
-}
-
 /// An error returned when a raw GFF record fails to parse.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum ParseError {
@@ -355,12 +319,6 @@ fn parse_reference_sequence_name(s: &str) -> Result<String, ParseError> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_fmt() {
-        let record = RecordBuf::default();
-        assert_eq!(record.to_string(), ".\t.\t.\t1\t1\t.\t.\t.\t.");
-    }
 
     #[test]
     fn test_from_str() -> Result<(), Box<dyn std::error::Error>> {
