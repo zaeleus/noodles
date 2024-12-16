3545466a3625c6e237f84b2b669c53f895c86422
Michael Macias <zaeleus@gmail.com>
2024-12-16 11:21:40 -0600

    bam/async/io/reader/header: Move reference sequences reader to a module

--- noodles-bam/src/async/io/reader/header.rs
+++ noodles-bam/src/async/io/reader/header.rs
@@ -1,16 +1,10 @@
-use std::num::NonZeroUsize;
+mod reference_sequences;
 
-use bstr::BString;
-use noodles_sam::{
-    self as sam,
-    header::{
-        record::value::{map::ReferenceSequence, Map},
-        ReferenceSequences,
-    },
-};
+use noodles_sam as sam;
 use tokio::io::{self, AsyncBufRead, AsyncBufReadExt, AsyncRead, AsyncReadExt, BufReader};
 
-use crate::io::reader::{bytes_with_nul_to_bstring, header::reference_sequences_eq};
+use self::reference_sequences::read_reference_sequences;
+use crate::io::reader::header::reference_sequences_eq;
 
 pub(super) async fn read_header<R>(reader: &mut R) -> io::Result<sam::Header>
 where
@@ -101,50 +95,10 @@ where
     }
 }
 
-async fn read_reference_sequences<R>(reader: &mut R) -> io::Result<ReferenceSequences>
-where
-    R: AsyncRead + Unpin,
-{
-    let n_ref = reader.read_u32_le().await.and_then(|n| {
-        usize::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-    })?;
-
-    let mut reference_sequences = ReferenceSequences::with_capacity(n_ref);
-
-    for _ in 0..n_ref {
-        let (name, reference_sequence) = read_reference_sequence(reader).await?;
-        reference_sequences.insert(name, reference_sequence);
-    }
-
-    Ok(reference_sequences)
-}
-
-async fn read_reference_sequence<R>(reader: &mut R) -> io::Result<(BString, Map<ReferenceSequence>)>
-where
-    R: AsyncRead + Unpin,
-{
-    let l_name = reader.read_u32_le().await.and_then(|n| {
-        usize::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-    })?;
-
-    let mut c_name = vec![0; l_name];
-    reader.read_exact(&mut c_name).await?;
-
-    let name = bytes_with_nul_to_bstring(&c_name)?;
-
-    let l_ref = reader.read_u32_le().await.and_then(|len| {
-        usize::try_from(len)
-            .and_then(NonZeroUsize::try_from)
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-    })?;
-
-    let reference_sequence = Map::<ReferenceSequence>::new(l_ref);
-
-    Ok((name, reference_sequence))
-}
-
 #[cfg(test)]
 mod tests {
+    use std::num::NonZeroUsize;
+
     use noodles_sam::header::record::value::{
         map::{self, header::Version},
         Map,
@@ -178,28 +132,6 @@ mod tests {
             .set_header(Map::<map::Header>::new(Version::new(1, 6)))
             .add_reference_sequence("sq0", Map::<map::ReferenceSequence>::new(SQ0_LN))
             .build();
-
-        assert_eq!(actual, expected);
-
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn test_read_reference_sequences() -> io::Result<()> {
-        let data = [
-            0x01, 0x00, 0x00, 0x00, // n_ref = 1
-            0x04, 0x00, 0x00, 0x00, // ref[0].l_name = 4
-            0x73, 0x71, 0x30, 0x00, // ref[0].name = "sq0\x00"
-            0x08, 0x00, 0x00, 0x00, // ref[0].l_ref = 8
-        ];
-
-        let mut reader = &data[..];
-        let actual = read_reference_sequences(&mut reader).await?;
-
-        let expected: ReferenceSequences =
-            [(BString::from("sq0"), Map::<ReferenceSequence>::new(SQ0_LN))]
-                .into_iter()
-                .collect();
 
         assert_eq!(actual, expected);
 
