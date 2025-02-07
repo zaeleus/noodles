0b9bab6a4dcc5ef1a1250e12215b35fd9e7a4d8b
Michael Macias <zaeleus@gmail.com>
2025-02-07 11:27:31 -0600

    cram/io/writer/container/block/compression_method: Extract encoder

--- noodles-cram/src/io/writer/container/block/compression_method.rs
+++ noodles-cram/src/io/writer/container/block/compression_method.rs
@@ -11,7 +11,12 @@ pub(super) fn write_compression_method<W>(
 where
     W: Write,
 {
-    let n = match compression_method {
+    let n = encode(compression_method);
+    writer.write_u8(n)
+}
+
+fn encode(compression_method: CompressionMethod) -> u8 {
+    match compression_method {
         CompressionMethod::None => 0,
         CompressionMethod::Gzip => 1,
         CompressionMethod::Bzip2 => 2,
@@ -21,9 +26,7 @@ where
         CompressionMethod::AdaptiveArithmeticCoding => 6,
         CompressionMethod::Fqzcomp => 7,
         CompressionMethod::NameTokenizer => 8,
-    };
-
-    writer.write_u8(n)
+    }
 }
 
 #[cfg(test)]
@@ -32,33 +35,22 @@ mod tests {
 
     #[test]
     fn test_write_compression_method() -> io::Result<()> {
-        fn t(
-            buf: &mut Vec<u8>,
-            compression_method: CompressionMethod,
-            expected: &[u8],
-        ) -> io::Result<()> {
-            buf.clear();
-            write_compression_method(buf, compression_method)?;
-            assert_eq!(buf, expected);
-            Ok(())
-        }
-
         let mut buf = Vec::new();
-
-        t(&mut buf, CompressionMethod::None, &[0x00])?;
-        t(&mut buf, CompressionMethod::Gzip, &[0x01])?;
-        t(&mut buf, CompressionMethod::Bzip2, &[0x02])?;
-        t(&mut buf, CompressionMethod::Lzma, &[0x03])?;
-        t(&mut buf, CompressionMethod::Rans4x8, &[0x04])?;
-        t(&mut buf, CompressionMethod::RansNx16, &[0x05])?;
-        t(
-            &mut buf,
-            CompressionMethod::AdaptiveArithmeticCoding,
-            &[0x06],
-        )?;
-        t(&mut buf, CompressionMethod::Fqzcomp, &[0x07])?;
-        t(&mut buf, CompressionMethod::NameTokenizer, &[0x08])?;
-
+        write_compression_method(&mut buf, CompressionMethod::None)?;
+        assert_eq!(buf, [0x00]);
         Ok(())
+    }
+
+    #[test]
+    fn test_encode() {
+        assert_eq!(encode(CompressionMethod::None), 0);
+        assert_eq!(encode(CompressionMethod::Gzip), 1);
+        assert_eq!(encode(CompressionMethod::Bzip2), 2);
+        assert_eq!(encode(CompressionMethod::Lzma), 3);
+        assert_eq!(encode(CompressionMethod::Rans4x8), 4);
+        assert_eq!(encode(CompressionMethod::RansNx16), 5);
+        assert_eq!(encode(CompressionMethod::AdaptiveArithmeticCoding), 6);
+        assert_eq!(encode(CompressionMethod::Fqzcomp), 7);
+        assert_eq!(encode(CompressionMethod::NameTokenizer), 8);
     }
 }
