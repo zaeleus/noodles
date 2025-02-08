4d431e5ad889cc6f74d907dbba667cc437e61e83
Michael Macias <zaeleus@gmail.com>
2025-02-08 16:02:42 -0600

    cram/writer/container/block/content_type: Extract encoder

--- noodles-cram/src/io/writer/container/block/content_type.rs
+++ noodles-cram/src/io/writer/container/block/content_type.rs
@@ -8,16 +8,19 @@ pub(super) fn write_content_type<W>(writer: &mut W, content_type: ContentType) -
 where
     W: Write,
 {
-    let n = match content_type {
+    let n = encode(content_type);
+    writer.write_u8(n)
+}
+
+fn encode(content_type: ContentType) -> u8 {
+    match content_type {
         ContentType::FileHeader => 0,
         ContentType::CompressionHeader => 1,
         ContentType::SliceHeader => 2,
         ContentType::Reserved => 3,
         ContentType::ExternalData => 4,
         ContentType::CoreData => 5,
-    };
-
-    writer.write_u8(n)
+    }
 }
 
 #[cfg(test)]
@@ -26,22 +29,19 @@ mod tests {
 
     #[test]
     fn test_write_content_type() -> io::Result<()> {
-        fn t(buf: &mut Vec<u8>, content_type: ContentType, expected: &[u8]) -> io::Result<()> {
-            buf.clear();
-            write_content_type(buf, content_type)?;
-            assert_eq!(buf, expected);
-            Ok(())
-        }
-
         let mut buf = Vec::new();
-
-        t(&mut buf, ContentType::FileHeader, &[0x00])?;
-        t(&mut buf, ContentType::CompressionHeader, &[0x01])?;
-        t(&mut buf, ContentType::SliceHeader, &[0x02])?;
-        t(&mut buf, ContentType::Reserved, &[0x03])?;
-        t(&mut buf, ContentType::ExternalData, &[0x04])?;
-        t(&mut buf, ContentType::CoreData, &[0x05])?;
-
+        write_content_type(&mut buf, ContentType::FileHeader)?;
+        assert_eq!(buf, [0x00]);
         Ok(())
+    }
+
+    #[test]
+    fn test_encode() {
+        assert_eq!(encode(ContentType::FileHeader), 0);
+        assert_eq!(encode(ContentType::CompressionHeader), 1);
+        assert_eq!(encode(ContentType::SliceHeader), 2);
+        assert_eq!(encode(ContentType::Reserved), 3);
+        assert_eq!(encode(ContentType::ExternalData), 4);
+        assert_eq!(encode(ContentType::CoreData), 5);
     }
 }
