a62a3c5d82c9dc2dcd0a5e3ad191ee802920f32f
Michael Macias <zaeleus@gmail.com>
2025-02-12 12:49:48 -0600

    cram/io/reader/container/compression_header/tag_encodings: Extract inner reader

--- noodles-cram/src/io/reader/container/compression_header/tag_encodings.rs
+++ noodles-cram/src/io/reader/container/compression_header/tag_encodings.rs
@@ -16,15 +16,19 @@ pub fn get_tag_encodings(src: &mut Bytes) -> io::Result<TagEncodings> {
 
     let mut buf = src.split_to(data_len);
 
-    let map_len = get_itf8(&mut buf).and_then(|n| {
+    let len = get_itf8(&mut buf).and_then(|n| {
         usize::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
     })?;
 
-    let mut encodings = TagEncodings::with_capacity(map_len);
+    get_tag_encodings_inner(&mut buf, len)
+}
 
-    for _ in 0..map_len {
-        let key = get_itf8(&mut buf)?;
-        let encoding = get_encoding_for_byte_array_codec(&mut buf)?;
+fn get_tag_encodings_inner(src: &mut Bytes, len: usize) -> io::Result<TagEncodings> {
+    let mut encodings = TagEncodings::with_capacity(len);
+
+    for _ in 0..len {
+        let key = get_itf8(src)?;
+        let encoding = get_encoding_for_byte_array_codec(src)?;
         encodings.insert(key, encoding);
     }
 
