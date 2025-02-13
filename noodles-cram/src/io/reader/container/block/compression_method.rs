e04b72fb7fced29dfd83ef900ca52ea988e7c7b6
Michael Macias <zaeleus@gmail.com>
2025-02-13 10:12:54 -0600

    cram/io/reader/container/block/compression_method: Extract decoder

--- noodles-cram/src/io/reader/container/block/compression_method.rs
+++ noodles-cram/src/io/reader/container/block/compression_method.rs
@@ -8,10 +8,12 @@ pub(super) fn get_compression_method<B>(src: &mut B) -> io::Result<CompressionMe
 where
     B: Buf,
 {
-    let n = src
-        .try_get_u8()
-        .map_err(|e| io::Error::new(io::ErrorKind::UnexpectedEof, e))?;
+    src.try_get_u8()
+        .map_err(|e| io::Error::new(io::ErrorKind::UnexpectedEof, e))
+        .and_then(decode)
+}
 
+fn decode(n: u8) -> io::Result<CompressionMethod> {
     match n {
         0 => Ok(CompressionMethod::None),
         1 => Ok(CompressionMethod::Gzip),
@@ -35,31 +37,33 @@ mod tests {
 
     #[test]
     fn test_get_compression_method() -> io::Result<()> {
-        fn t(mut src: &[u8], expected: CompressionMethod) -> io::Result<()> {
-            let actual = get_compression_method(&mut src)?;
-            assert_eq!(actual, expected);
-            Ok(())
-        }
+        assert_eq!(
+            get_compression_method(&mut &[0x00][..])?,
+            CompressionMethod::None
+        );
 
-        t(&[0x00], CompressionMethod::None)?;
-        t(&[0x01], CompressionMethod::Gzip)?;
-        t(&[0x02], CompressionMethod::Bzip2)?;
-        t(&[0x03], CompressionMethod::Lzma)?;
-        t(&[0x04], CompressionMethod::Rans4x8)?;
-        t(&[0x05], CompressionMethod::RansNx16)?;
-        t(&[0x06], CompressionMethod::AdaptiveArithmeticCoding)?;
-        t(&[0x07], CompressionMethod::Fqzcomp)?;
-        t(&[0x08], CompressionMethod::NameTokenizer)?;
-
-        let mut src = &[][..];
         assert!(matches!(
-            get_compression_method(&mut src),
+            get_compression_method(&mut &[][..]),
             Err(e) if e.kind() == io::ErrorKind::UnexpectedEof
         ));
 
-        let mut src = &[0x09][..];
+        Ok(())
+    }
+
+    #[test]
+    fn test_decode() -> io::Result<()> {
+        assert_eq!(decode(0)?, CompressionMethod::None);
+        assert_eq!(decode(1)?, CompressionMethod::Gzip);
+        assert_eq!(decode(2)?, CompressionMethod::Bzip2);
+        assert_eq!(decode(3)?, CompressionMethod::Lzma);
+        assert_eq!(decode(4)?, CompressionMethod::Rans4x8);
+        assert_eq!(decode(5)?, CompressionMethod::RansNx16);
+        assert_eq!(decode(6)?, CompressionMethod::AdaptiveArithmeticCoding);
+        assert_eq!(decode(7)?, CompressionMethod::Fqzcomp);
+        assert_eq!(decode(8)?, CompressionMethod::NameTokenizer);
+
         assert!(matches!(
-            get_compression_method(&mut src),
+            decode(9),
             Err(e) if e.kind() == io::ErrorKind::InvalidData
         ));
 
