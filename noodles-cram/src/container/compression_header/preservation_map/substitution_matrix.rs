2fe382a84b76cf07522a446bb4581e36f8bfd668
Michael Macias <zaeleus@gmail.com>
2025-02-17 13:44:04 -0600

    cram/container/compression_header/preservation_map/substitution_matrix: Move encoder to writer

--- noodles-cram/src/container/compression_header/preservation_map/substitution_matrix.rs
+++ noodles-cram/src/container/compression_header/preservation_map/substitution_matrix.rs
@@ -55,34 +55,6 @@ impl Default for SubstitutionMatrix {
     }
 }
 
-impl From<SubstitutionMatrix> for [u8; 5] {
-    fn from(substitution_matrix: SubstitutionMatrix) -> Self {
-        <[u8; 5]>::from(&substitution_matrix)
-    }
-}
-
-impl From<&SubstitutionMatrix> for [u8; 5] {
-    fn from(substitution_matrix: &SubstitutionMatrix) -> Self {
-        let mut encoded_substitution_matrix = [0; 5];
-
-        for (read_bases, codes) in substitution_matrix
-            .substitutions
-            .iter()
-            .zip(encoded_substitution_matrix.iter_mut())
-        {
-            let mut index_base_pairs: Vec<_> = read_bases.iter().enumerate().collect();
-            index_base_pairs.sort_by_key(|p| p.1);
-
-            for (i, _) in index_base_pairs {
-                *codes <<= 2;
-                *codes |= i as u8;
-            }
-        }
-
-        encoded_substitution_matrix
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -103,21 +75,5 @@ mod tests {
         assert_eq!(matrix.find_code(Base::C, Base::G), 0b01);
         assert_eq!(matrix.find_code(Base::G, Base::C), 0b10);
         assert_eq!(matrix.find_code(Base::T, Base::C), 0b11);
-    }
-
-    #[test]
-    fn test_from_substitution_matrix_for_5_byte_array() {
-        let matrix = SubstitutionMatrix {
-            substitutions: [
-                [Base::T, Base::G, Base::C, Base::N],
-                [Base::A, Base::G, Base::T, Base::N],
-                [Base::N, Base::A, Base::C, Base::T],
-                [Base::G, Base::N, Base::A, Base::C],
-                [Base::C, Base::G, Base::T, Base::A],
-            ],
-        };
-
-        assert_eq!(<[u8; 5]>::from(&matrix), [0x93, 0x1b, 0x6c, 0xb1, 0xc6]);
-        assert_eq!(<[u8; 5]>::from(matrix), [0x93, 0x1b, 0x6c, 0xb1, 0xc6]);
     }
 }
--- noodles-cram/src/io/writer/container/compression_header/preservation_map/substitution_matrix.rs
+++ noodles-cram/src/io/writer/container/compression_header/preservation_map/substitution_matrix.rs
@@ -4,7 +4,9 @@ use std::io::{self, Write};
 
 use self::frequencies::Frequencies;
 use crate::{
-    container::compression_header::preservation_map::SubstitutionMatrix,
+    container::compression_header::preservation_map::{
+        substitution_matrix::Base, SubstitutionMatrix,
+    },
     io::writer::{record::Feature, Record},
 };
 
@@ -15,8 +17,29 @@ pub(super) fn write_substitution_matrix<W>(
 where
     W: Write,
 {
-    let buf = <[u8; 5]>::from(substitution_matrix);
+    let buf = encode(substitution_matrix);
     writer.write_all(&buf)
+}
+
+fn encode(substitution_matrix: &SubstitutionMatrix) -> [u8; 5] {
+    let mut buf = [0; 5];
+    let mut index_bases = [(0, Base::N); 4];
+
+    for (bases, codes) in substitution_matrix.substitutions.iter().zip(&mut buf) {
+        for ((i, base), index_base) in bases.iter().enumerate().zip(&mut index_bases) {
+            *index_base = (i, *base);
+        }
+
+        index_bases.sort_by_key(|(_, base)| *base);
+
+        for (i, _) in &index_bases {
+            *codes <<= 2;
+            // SAFETY: `i < 4`.
+            *codes |= *i as u8;
+        }
+    }
+
+    buf
 }
 
 pub(super) fn build_substitution_matrix(records: &[Record]) -> SubstitutionMatrix {
@@ -36,4 +59,25 @@ pub(super) fn build_substitution_matrix(records: &[Record]) -> SubstitutionMatri
     }
 
     frequencies.into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encode() {
+        // § 10.6.4 "Mapped reads: Substitution Matrix Format" (2024-09-04)
+        let substitution_matrix = SubstitutionMatrix {
+            substitutions: [
+                [Base::T, Base::C, Base::G, Base::N], // A
+                [Base::G, Base::A, Base::T, Base::N], // C
+                [Base::C, Base::T, Base::A, Base::N], // G
+                [Base::A, Base::G, Base::C, Base::N], // T
+                [Base::A, Base::C, Base::G, Base::T], // N
+            ],
+        };
+
+        assert_eq!(encode(&substitution_matrix), [0x63, 0x4b, 0x87, 0x27, 0x1b]);
+    }
 }
