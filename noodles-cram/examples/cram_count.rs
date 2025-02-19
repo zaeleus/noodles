573c3f43436151045384cc58b9f8cdd5ce322288
Michael Macias <zaeleus@gmail.com>
2025-02-19 13:17:24 -0600

    cram/examples/count: Sum container record counts

--- noodles-cram/examples/cram_count.rs
+++ noodles-cram/examples/cram_count.rs
@@ -1,44 +1,22 @@
 //! Counts the number of records in a CRAM file.
 //!
-//! Note that this example counts each successfully read record and not by summing the number of
-//! records field in each container or slice header.
-//!
 //! The result matches the output of `samtools view --count <src>`.
 
 use std::{env, io};
 
 use noodles_cram::{self as cram, io::reader::Container};
-use noodles_fasta as fasta;
 
 fn main() -> io::Result<()> {
     let src = env::args().nth(1).expect("missing src");
 
     let mut reader = cram::io::reader::Builder::default().build_from_path(src)?;
-    let header = reader.read_header()?;
-
-    let reference_sequence_repository = fasta::Repository::default();
+    reader.read_header()?;
 
     let mut container = Container::default();
     let mut n = 0;
 
     while reader.read_container(&mut container)? != 0 {
-        let compression_header = container.compression_header()?;
-
-        for result in container.slices() {
-            let slice = result?;
-
-            let (core_data_src, external_data_srcs) = slice.decode_blocks()?;
-
-            let records = slice.records(
-                reference_sequence_repository.clone(),
-                &header,
-                &compression_header,
-                &core_data_src,
-                &external_data_srcs,
-            )?;
-
-            n += records.len();
-        }
+        n += container.header().record_count();
     }
 
     println!("{n}");
--- noodles-cram/examples/cram_count_async.rs
+++ noodles-cram/examples/cram_count_async.rs
@@ -1,14 +1,10 @@
 //! Counts the number of records in a CRAM file.
 //!
-//! Note that this example counts each successfully read record and not by summing the number of
-//! records field in each container or slice header.
-//!
 //! The result matches the output of `samtools view --count <src>`.
 
 use std::env;
 
 use noodles_cram::{self as cram, io::reader::Container};
-use noodles_fasta as fasta;
 use tokio::{fs::File, io};
 
 #[tokio::main]
@@ -16,31 +12,13 @@ async fn main() -> io::Result<()> {
     let src = env::args().nth(1).expect("missing src");
 
     let mut reader = File::open(src).await.map(cram::r#async::io::Reader::new)?;
-    let header = reader.read_header().await?;
-
-    let reference_sequence_repository = fasta::Repository::default();
+    reader.read_header().await?;
 
     let mut container = Container::default();
     let mut n = 0;
 
     while reader.read_container(&mut container).await? != 0 {
-        let compression_header = container.compression_header()?;
-
-        for result in container.slices() {
-            let slice = result?;
-
-            let (core_data_src, external_data_srcs) = slice.decode_blocks()?;
-
-            let records = slice.records(
-                reference_sequence_repository.clone(),
-                &header,
-                &compression_header,
-                &core_data_src,
-                &external_data_srcs,
-            )?;
-
-            n += records.len();
-        }
+        n += container.header().record_count();
     }
 
     println!("{n}");
