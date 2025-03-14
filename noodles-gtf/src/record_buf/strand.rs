4387db3572b990a8398f2e86cc4ec14bcd9b51c4
Michael Macias <zaeleus@gmail.com>
2025-03-14 10:04:56 -0500

    gtf/record_buf/strand: Remove formatter

--- noodles-gtf/src/record_buf/strand.rs
+++ noodles-gtf/src/record_buf/strand.rs
@@ -11,21 +11,6 @@ pub enum Strand {
     Reverse,
 }
 
-impl AsRef<str> for Strand {
-    fn as_ref(&self) -> &str {
-        match self {
-            Self::Forward => "+",
-            Self::Reverse => "-",
-        }
-    }
-}
-
-impl fmt::Display for Strand {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(self.as_ref())
-    }
-}
-
 /// An error returned when a raw GTF record strand fails to parse.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum ParseError {
@@ -62,12 +47,6 @@ impl FromStr for Strand {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_fmt() {
-        assert_eq!(Strand::Forward.to_string(), "+");
-        assert_eq!(Strand::Reverse.to_string(), "-");
-    }
 
     #[test]
     fn test_from_str() -> Result<(), ParseError> {
