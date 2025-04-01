a70ed08ef8fc1f8b53f47e1fa4479fe87e68bbd3
Michael Macias <zaeleus@gmail.com>
2025-04-01 14:21:18 -0500

    bam/record/data/field/tag: Split input

--- noodles-bam/src/record/data/field/tag.rs
+++ noodles-bam/src/record/data/field/tag.rs
@@ -1,11 +1,15 @@
-use std::io::{self, Read};
+use std::io;
 
 use noodles_sam::alignment::record::data::field::Tag;
 
 pub(crate) fn decode_tag(src: &mut &[u8]) -> io::Result<Tag> {
-    let mut buf = [0; 2];
-    src.read_exact(&mut buf)?;
-    Ok(Tag::from(buf))
+    let Some((buf, rest)) = src.split_first_chunk() else {
+        return Err(io::Error::from(io::ErrorKind::UnexpectedEof));
+    };
+
+    *src = rest;
+
+    Ok(Tag::from(*buf))
 }
 
 #[cfg(test)]
