6f5bfb2585636fba02bddf27d9fd48474f9a763f
Michael Macias <zaeleus@gmail.com>
2025-04-01 14:35:56 -0500

    cram/io/reader/container/slice/records/data: Add endianness to function names

--- noodles-cram/src/io/reader/container/slice/records/data.rs
+++ noodles-cram/src/io/reader/container/slice/records/data.rs
@@ -13,11 +13,11 @@ pub(super) fn read_value(src: &[u8], ty: Type) -> io::Result<Value<'_>> {
         Type::Character => read_u8(src).map(Value::Character),
         Type::Int8 => read_u8(src).map(|n| Value::Int8(n as i8)),
         Type::UInt8 => read_u8(src).map(Value::UInt8),
-        Type::Int16 => read_u16(src).map(|n| Value::Int16(n as i16)),
-        Type::UInt16 => read_u16(src).map(Value::UInt16),
-        Type::Int32 => read_u32(src).map(|n| Value::Int32(n as i32)),
-        Type::UInt32 => read_u32(src).map(Value::UInt32),
-        Type::Float => read_f32(src).map(Value::Float),
+        Type::Int16 => read_u16_le(src).map(|n| Value::Int16(n as i16)),
+        Type::UInt16 => read_u16_le(src).map(Value::UInt16),
+        Type::Int32 => read_u32_le(src).map(|n| Value::Int32(n as i32)),
+        Type::UInt32 => read_u32_le(src).map(Value::UInt32),
+        Type::Float => read_f32_le(src).map(Value::Float),
         Type::String => read_string(src).map(Value::String),
         Type::Hex => read_string(src).map(Value::Hex),
         Type::Array => read_array(src).map(Value::Array),
@@ -31,19 +31,19 @@ fn read_u8(src: &[u8]) -> io::Result<u8> {
         .ok_or_else(|| io::Error::from(io::ErrorKind::UnexpectedEof))
 }
 
-fn read_u16(src: &[u8]) -> io::Result<u16> {
+fn read_u16_le(src: &[u8]) -> io::Result<u16> {
     src.split_first_chunk()
         .map(|(buf, _)| u16::from_le_bytes(*buf))
         .ok_or_else(|| io::Error::from(io::ErrorKind::UnexpectedEof))
 }
 
-fn read_u32(src: &[u8]) -> io::Result<u32> {
+fn read_u32_le(src: &[u8]) -> io::Result<u32> {
     src.split_first_chunk()
         .map(|(buf, _)| u32::from_le_bytes(*buf))
         .ok_or_else(|| io::Error::from(io::ErrorKind::UnexpectedEof))
 }
 
-fn read_f32(src: &[u8]) -> io::Result<f32> {
+fn read_f32_le(src: &[u8]) -> io::Result<f32> {
     src.split_first_chunk()
         .map(|(buf, _)| f32::from_le_bytes(*buf))
         .ok_or_else(|| io::Error::from(io::ErrorKind::UnexpectedEof))
