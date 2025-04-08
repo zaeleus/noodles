aaeaa9fa4cf55d65aa37c1c387e85380a4508c97
Michael Macias <zaeleus@gmail.com>
2025-04-08 10:14:09 -0500

    cram/io/bit_reader: Limit read length

--- noodles-cram/src/io/bit_reader.rs
+++ noodles-cram/src/io/bit_reader.rs
@@ -1,6 +1,7 @@
 use std::io;
 
 const U8_BITS: usize = 8;
+const MAX_READ_LENGTH: u32 = 31;
 
 pub struct BitReader<'c> {
     src: &'c [u8],
@@ -22,6 +23,13 @@ impl<'c> BitReader<'c> {
     }
 
     fn read_u32(&mut self, len: u32) -> io::Result<u32> {
+        if len > MAX_READ_LENGTH {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "invalid read length",
+            ))?;
+        }
+
         let mut n = 0;
 
         for _ in 0..len {
@@ -59,9 +67,16 @@ mod tests {
     fn test_read_u32() -> io::Result<()> {
         let src = [0b11001111, 0b01000000];
         let mut reader = BitReader::new(&src[..]);
+
         assert_eq!(reader.read_u32(4)?, 0b1100);
         assert_eq!(reader.read_u32(2)?, 0b11);
         assert_eq!(reader.read_u32(6)?, 0b110100);
+
+        assert!(matches!(
+            reader.read_u32(32),
+            Err(e) if e.kind() == io::ErrorKind::InvalidInput
+        ));
+
         Ok(())
     }
 }
