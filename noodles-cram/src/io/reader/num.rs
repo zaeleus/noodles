63f4d0cbf9bf7af0711124190bf4cfa479988a35
Michael Macias <zaeleus@gmail.com>
2025-07-19 18:15:42 -0500

    cram/io/reader/num: Use constants

--- noodles-cram/src/io/reader/num.rs
+++ noodles-cram/src/io/reader/num.rs
@@ -2,7 +2,10 @@ mod itf8;
 mod ltf8;
 mod vlq;
 
-use std::io::{self, Read};
+use std::{
+    io::{self, Read},
+    mem,
+};
 
 pub use self::{
     itf8::{read_itf8, read_itf8_as},
@@ -14,7 +17,7 @@ pub(crate) fn read_u8<R>(reader: &mut R) -> io::Result<u8>
 where
     R: Read,
 {
-    let mut buf = [0; 1];
+    let mut buf = [0; mem::size_of::<u8>()];
     reader.read_exact(&mut buf)?;
     Ok(buf[0])
 }
@@ -23,7 +26,7 @@ fn read_u16_be<R>(reader: &mut R) -> io::Result<u16>
 where
     R: Read,
 {
-    let mut buf = [0; 2];
+    let mut buf = [0; mem::size_of::<u16>()];
     reader.read_exact(&mut buf)?;
     Ok(u16::from_be_bytes(buf))
 }
@@ -32,7 +35,7 @@ fn read_u24_be<R>(reader: &mut R) -> io::Result<u32>
 where
     R: Read,
 {
-    let mut buf = [0; 4];
+    let mut buf = [0; mem::size_of::<u32>()];
     reader.read_exact(&mut buf[1..])?;
     Ok(u32::from_be_bytes(buf))
 }
@@ -41,7 +44,7 @@ pub(crate) fn read_i32_le<R>(reader: &mut R) -> io::Result<i32>
 where
     R: Read,
 {
-    let mut buf = [0; 4];
+    let mut buf = [0; mem::size_of::<i32>()];
     reader.read_exact(&mut buf)?;
     Ok(i32::from_le_bytes(buf))
 }
@@ -50,7 +53,7 @@ fn read_u32_be<R>(reader: &mut R) -> io::Result<u32>
 where
     R: Read,
 {
-    let mut buf = [0; 4];
+    let mut buf = [0; mem::size_of::<u32>()];
     reader.read_exact(&mut buf)?;
     Ok(u32::from_be_bytes(buf))
 }
@@ -59,7 +62,7 @@ pub(crate) fn read_u32_le<R>(reader: &mut R) -> io::Result<u32>
 where
     R: Read,
 {
-    let mut buf = [0; 4];
+    let mut buf = [0; mem::size_of::<u32>()];
     reader.read_exact(&mut buf)?;
     Ok(u32::from_le_bytes(buf))
 }
