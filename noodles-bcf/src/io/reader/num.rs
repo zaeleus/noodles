c2479e7e17f61263a0e843767b911763b53c18bd
Michael Macias <zaeleus@gmail.com>
2025-08-11 13:03:14 -0500

    bcf/io/reader/num: Add little-endian f32 reader

--- noodles-bcf/src/io/reader/num.rs
+++ noodles-bcf/src/io/reader/num.rs
@@ -29,3 +29,12 @@ where
     reader.read_exact(&mut buf)?;
     Ok(u32::from_le_bytes(buf))
 }
+
+pub(crate) fn read_f32_le<R>(reader: &mut R) -> io::Result<f32>
+where
+    R: Read,
+{
+    let mut buf = [0; mem::size_of::<f32>()];
+    reader.read_exact(&mut buf)?;
+    Ok(f32::from_le_bytes(buf))
+}
--- noodles-bcf/src/record/codec/decoder/quality_score.rs
+++ noodles-bcf/src/record/codec/decoder/quality_score.rs
@@ -1,11 +1,11 @@
 use std::io;
 
-use byteorder::{LittleEndian, ReadBytesExt};
+use crate::io::reader::num::read_f32_le;
 
 pub(crate) fn read_qual(src: &mut &[u8]) -> io::Result<Option<f32>> {
     use crate::record::codec::value::Float;
 
-    match src.read_f32::<LittleEndian>().map(Float::from)? {
+    match read_f32_le(src).map(Float::from)? {
         Float::Value(n) => Ok(Some(n)),
         Float::Missing => Ok(None),
         qual => Err(io::Error::new(
