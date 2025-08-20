b041a1a7b124fa36a64f75d0c4e67708ac7e8830
Michael Macias <zaeleus@gmail.com>
2025-08-20 11:10:21 -0500

    sam/examples/write_async: Sync example with blocking example

--- noodles-sam/examples/sam_write_async.rs
+++ noodles-sam/examples/sam_write_async.rs
@@ -4,32 +4,12 @@
 //!
 //! Verify the output by piping to `samtools view --no-PG --with-header`.
 
-use std::num::NonZeroUsize;
-
 use noodles_sam::{
     self as sam,
     alignment::RecordBuf,
-    header::record::value::{
-        Map,
-        map::{Program, ReferenceSequence},
-    },
+    header::record::value::{Map, map::Program},
 };
 use tokio::io;
-
-const SQ0_LN: NonZeroUsize = match NonZeroUsize::new(8) {
-    Some(length) => length,
-    None => unreachable!(),
-};
-
-const SQ1_LN: NonZeroUsize = match NonZeroUsize::new(13) {
-    Some(length) => length,
-    None => unreachable!(),
-};
-
-const SQ2_LN: NonZeroUsize = match NonZeroUsize::new(21) {
-    Some(length) => length,
-    None => unreachable!(),
-};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -37,9 +17,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let header = sam::Header::builder()
         .set_header(Default::default())
-        .add_reference_sequence("sq0", Map::<ReferenceSequence>::new(SQ0_LN))
-        .add_reference_sequence("sq1", Map::<ReferenceSequence>::new(SQ1_LN))
-        .add_reference_sequence("sq2", Map::<ReferenceSequence>::new(SQ2_LN))
         .add_program("noodles-sam", Map::<Program>::default())
         .add_comment("an example SAM written by noodles-sam")
         .build();
