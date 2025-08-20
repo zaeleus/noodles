379026278acda447d76e5edee123ef193facdefd
Michael Macias <zaeleus@gmail.com>
2025-08-20 11:29:56 -0500

    sam/io/writer/record/data/field/ty: Extract encoder

--- noodles-sam/src/io/writer/record/data/field/ty.rs
+++ noodles-sam/src/io/writer/record/data/field/ty.rs
@@ -2,28 +2,25 @@ use std::io::{self, Write};
 
 use crate::alignment::record::data::field::Type;
 
-pub fn write_type<W>(writer: &mut W, mut ty: Type) -> io::Result<()>
+pub fn write_type<W>(writer: &mut W, ty: Type) -> io::Result<()>
 where
     W: Write,
 {
-    if matches!(
-        ty,
-        Type::Int8 | Type::UInt8 | Type::Int16 | Type::UInt16 | Type::Int32 | Type::UInt32
-    ) {
-        ty = Type::Int32;
-    }
+    let c = encode(ty);
+    writer.write_all(&[c])
+}
 
-    let c = match ty {
+// § 1.5 "The alignment section: optional fields" (2024-11-06): "In an optional field, `TYPE` is a
+// single case-sensitive letter which defines the format of `VALUE`: `[AifZHB]`."
+fn encode(ty: Type) -> u8 {
+    match ty {
         Type::Character => b'A',
-        Type::Int32 => b'i',
+        Type::Int8 | Type::UInt8 | Type::Int16 | Type::UInt16 | Type::Int32 | Type::UInt32 => b'i',
         Type::Float => b'f',
         Type::String => b'Z',
         Type::Hex => b'H',
         Type::Array => b'B',
-        _ => unreachable!(),
-    };
-
-    writer.write_all(&[c])
+    }
 }
 
 #[cfg(test)]
@@ -32,27 +29,24 @@ mod tests {
 
     #[test]
     fn test_write_type() -> io::Result<()> {
-        fn t(buf: &mut Vec<u8>, ty: Type, expected: &[u8]) -> io::Result<()> {
-            buf.clear();
-            write_type(buf, ty)?;
-            assert_eq!(buf, expected);
-            Ok(())
-        }
-
         let mut buf = Vec::new();
-
-        t(&mut buf, Type::Character, b"A")?;
-        t(&mut buf, Type::Int8, b"i")?;
-        t(&mut buf, Type::UInt8, b"i")?;
-        t(&mut buf, Type::Int16, b"i")?;
-        t(&mut buf, Type::UInt16, b"i")?;
-        t(&mut buf, Type::Int32, b"i")?;
-        t(&mut buf, Type::UInt32, b"i")?;
-        t(&mut buf, Type::Float, b"f")?;
-        t(&mut buf, Type::String, b"Z")?;
-        t(&mut buf, Type::Hex, b"H")?;
-        t(&mut buf, Type::Array, b"B")?;
-
+        write_type(&mut buf, Type::Character)?;
+        assert_eq!(buf, b"A");
         Ok(())
+    }
+
+    #[test]
+    fn test_encode() {
+        assert_eq!(encode(Type::Character), b'A');
+        assert_eq!(encode(Type::Int8), b'i');
+        assert_eq!(encode(Type::UInt8), b'i');
+        assert_eq!(encode(Type::Int16), b'i');
+        assert_eq!(encode(Type::UInt16), b'i');
+        assert_eq!(encode(Type::Int32), b'i');
+        assert_eq!(encode(Type::UInt32), b'i');
+        assert_eq!(encode(Type::Float), b'f');
+        assert_eq!(encode(Type::String), b'Z');
+        assert_eq!(encode(Type::Hex), b'H');
+        assert_eq!(encode(Type::Array), b'B');
     }
 }
