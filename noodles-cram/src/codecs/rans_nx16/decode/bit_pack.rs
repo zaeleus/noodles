bb37116add51bc45ef87f364b1b63e638fd82e34
Michael Macias <zaeleus@gmail.com>
2025-09-26 13:04:44 -0500

    cram/codecs/rans_nx16/decode/bit_pack: Add unpacker

--- noodles-cram/src/codecs/rans_nx16/decode/bit_pack.rs
+++ noodles-cram/src/codecs/rans_nx16/decode/bit_pack.rs
@@ -33,61 +33,57 @@ pub fn read_context<'a>(
     Ok((context, len))
 }
 
-pub fn decode(
-    src: &[u8],
-    Context {
-        symbol_count: n_sym,
-        mapping_table: p,
-        uncompressed_size: len,
-    }: &Context<'_>,
-) -> io::Result<Vec<u8>> {
-    let mut dst = vec![0; *len];
-    let mut j = 0;
+pub fn decode(src: &[u8], ctx: &Context<'_>) -> io::Result<Vec<u8>> {
+    let mapping_table = ctx.mapping_table;
 
-    if n_sym.get() <= 1 {
-        dst.fill(p[0]);
-    } else if n_sym.get() <= 2 {
-        let mut v = 0;
+    let mut dst = vec![0; ctx.uncompressed_size];
 
-        for (i, b) in dst.iter_mut().enumerate() {
-            if i % 8 == 0 {
-                v = src[j];
-                j += 1;
-            }
-
-            *b = p[usize::from(v & 0x01)];
-            v >>= 1;
+    match ctx.symbol_count.get() {
+        1 => dst.fill(mapping_table[0]),
+        2 => unpack(src, mapping_table, 8, &mut dst),
+        3..=4 => unpack(src, mapping_table, 4, &mut dst),
+        5..=16 => unpack(src, mapping_table, 2, &mut dst),
+        n => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("expected bit pack symbol count to be <= 16, got {n}"),
+            ));
         }
-    } else if n_sym.get() <= 4 {
-        let mut v = 0;
-
-        for (i, b) in dst.iter_mut().enumerate() {
-            if i % 4 == 0 {
-                v = src[j];
-                j += 1;
-            }
-
-            *b = p[usize::from(v & 0x03)];
-            v >>= 2;
-        }
-    } else if n_sym.get() <= 16 {
-        let mut v = 0;
-
-        for (i, b) in dst.iter_mut().enumerate() {
-            if i % 2 == 0 {
-                v = src[j];
-                j += 1;
-            }
-
-            *b = p[usize::from(v & 0x0f)];
-            v >>= 4;
-        }
-    } else {
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            format!("expected n_sym to be <= 16, got {n_sym}"),
-        ));
     }
 
     Ok(dst)
+}
+
+fn unpack(src: &[u8], mapping_table: &[u8], chunk_size: usize, dst: &mut [u8]) {
+    const BITS: usize = u8::BITS as usize;
+
+    let shift = BITS / chunk_size;
+    let mask = (1 << shift) - 1;
+
+    for (mut s, chunk) in src.iter().copied().zip(dst.chunks_mut(chunk_size)) {
+        for d in chunk {
+            *d = mapping_table[usize::from(s & mask)];
+            s >>= shift;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_unpack() {
+        let mut dst = [0; 4];
+        unpack(&[0b00001100], b"nd", 8, &mut dst);
+        assert_eq!(&dst, b"nndd");
+
+        let mut dst = [0; 4];
+        unpack(&[0b11100100], b"ndls", 4, &mut dst);
+        assert_eq!(&dst, b"ndls");
+
+        let mut dst = [0; 2];
+        unpack(&[0b00100000], b"nodles", 2, &mut dst);
+        assert_eq!(&dst, b"nd");
+    }
 }
