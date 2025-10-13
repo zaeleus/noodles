c7a617776453daa9b326e10ee6545f2a4a14fa75
Michael Macias <zaeleus@gmail.com>
2025-10-13 10:55:12 -0500

    cram/codecs/rans_nx16/decode/bit_pack: Move context to a module

--- noodles-cram/src/codecs/rans_nx16/decode/bit_pack.rs
+++ noodles-cram/src/codecs/rans_nx16/decode/bit_pack.rs
@@ -1,37 +1,9 @@
-use std::{io, num::NonZero};
+mod context;
 
-use crate::io::reader::num::{read_u8, read_uint7_as};
+use std::io;
 
-pub struct Context<'a> {
-    pub symbol_count: NonZero<usize>,
-    pub mapping_table: &'a [u8],
-    pub uncompressed_size: usize,
-}
-
-pub fn read_context<'a>(
-    src: &mut &'a [u8],
-    uncompressed_size: usize,
-) -> io::Result<(Context<'a>, usize)> {
-    let symbol_count = read_u8(src).and_then(|n| {
-        NonZero::try_from(usize::from(n)).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-    })?;
-
-    let (mapping_table, rest) = src
-        .split_at_checked(symbol_count.get())
-        .ok_or_else(|| io::Error::from(io::ErrorKind::UnexpectedEof))?;
-
-    *src = rest;
-
-    let len = read_uint7_as(src)?;
-
-    let context = Context {
-        symbol_count,
-        mapping_table,
-        uncompressed_size,
-    };
-
-    Ok((context, len))
-}
+use self::context::Context;
+pub use self::context::read_context;
 
 pub fn decode(src: &[u8], ctx: &Context<'_>) -> io::Result<Vec<u8>> {
     let mapping_table = ctx.mapping_table;
---
+++ noodles-cram/src/codecs/rans_nx16/decode/bit_pack/context.rs
@@ -0,0 +1,34 @@
+use std::{io, num::NonZero};
+
+use crate::io::reader::num::{read_u8, read_uint7_as};
+
+pub struct Context<'a> {
+    pub symbol_count: NonZero<usize>,
+    pub mapping_table: &'a [u8],
+    pub uncompressed_size: usize,
+}
+
+pub fn read_context<'a>(
+    src: &mut &'a [u8],
+    uncompressed_size: usize,
+) -> io::Result<(Context<'a>, usize)> {
+    let symbol_count = read_u8(src).and_then(|n| {
+        NonZero::try_from(usize::from(n)).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+    })?;
+
+    let (mapping_table, rest) = src
+        .split_at_checked(symbol_count.get())
+        .ok_or_else(|| io::Error::from(io::ErrorKind::UnexpectedEof))?;
+
+    *src = rest;
+
+    let len = read_uint7_as(src)?;
+
+    let context = Context {
+        symbol_count,
+        mapping_table,
+        uncompressed_size,
+    };
+
+    Ok((context, len))
+}
