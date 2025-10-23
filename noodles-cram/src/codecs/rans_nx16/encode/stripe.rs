1ec313802b6a8da98a18c601da6549523c1f7dcb
Michael Macias <zaeleus@gmail.com>
2025-10-23 10:35:19 -0500

    cram/codecs/rans_nx16/encode/stripe: Add compressed sizes writer

--- noodles-cram/src/codecs/rans_nx16/encode/stripe.rs
+++ noodles-cram/src/codecs/rans_nx16/encode/stripe.rs
@@ -29,11 +29,7 @@ pub(super) fn rans_encode_stripe(src: &[u8]) -> io::Result<Vec<u8>> {
 
     let mut dst = Vec::new();
     write_chunk_count(&mut dst, compressed_chunks.len())?;
-
-    for chunk in &compressed_chunks {
-        let clen = chunk.len() as u32;
-        write_uint7(&mut dst, clen)?;
-    }
+    write_compressed_sizes(&mut dst, &compressed_chunks)?;
 
     for chunk in &compressed_chunks {
         dst.write_all(chunk)?;
@@ -59,6 +55,17 @@ fn write_chunk_count(dst: &mut Vec<u8>, chunk_count: usize) -> io::Result<()> {
         u8::try_from(chunk_count).map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
 
     write_u8(dst, n)?;
+
+    Ok(())
+}
+
+fn write_compressed_sizes(dst: &mut Vec<u8>, chunks: &[Vec<u8>]) -> io::Result<()> {
+    for chunk in chunks {
+        let n = u32::try_from(chunk.len())
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+
+        write_uint7(dst, n)?;
+    }
 
     Ok(())
 }
@@ -92,6 +99,15 @@ mod tests {
             Err(e) if e.kind() == io::ErrorKind::InvalidInput
         ));
 
+        Ok(())
+    }
+
+    #[test]
+    fn test_write_compressed_sizes() -> io::Result<()> {
+        let mut dst = Vec::new();
+        let compressed_chunks = [Vec::from(b"abcde"), Vec::from(b"ABCD"), Vec::from(b"1234")];
+        write_compressed_sizes(&mut dst, &compressed_chunks)?;
+        assert_eq!(dst, [0x05, 0x04, 0x04]);
         Ok(())
     }
 }
