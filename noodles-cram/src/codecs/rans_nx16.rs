ec2534ab06df1560de0acaaa8298b43a3e8760a0
Michael Macias <zaeleus@gmail.com>
2025-11-05 09:50:23 -0600

    cram/codecs/rans_nx16: Add self tests

--- noodles-cram/src/codecs/rans_nx16.rs
+++ noodles-cram/src/codecs/rans_nx16.rs
@@ -8,3 +8,33 @@ pub use self::flags::Flags;
 pub(crate) use self::{decode::decode, encode::encode};
 
 const ALPHABET_SIZE: usize = 256;
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+
+    use super::*;
+
+    #[test]
+    fn test_self() -> io::Result<()> {
+        let src = b"noodles";
+
+        let options = [
+            Flags::empty(),
+            Flags::ORDER,
+            Flags::N32,
+            Flags::STRIPE,
+            Flags::CAT,
+            // Flags::RLE,
+            Flags::PACK,
+        ];
+
+        for flags in options {
+            let compressed_data = encode(flags, src)?;
+            let uncompressed_data = decode(&compressed_data, src.len())?;
+            assert_eq!(uncompressed_data, src);
+        }
+
+        Ok(())
+    }
+}
