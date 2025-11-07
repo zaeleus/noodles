53308e4fd60b9455a7d50b88d5f22e17be800ce3
Michael Macias <zaeleus@gmail.com>
2025-11-07 10:04:02 -0600

    bam/async/io/reader/query: Use async CSI query reader

--- noodles-bam/src/async/io/reader.rs
+++ noodles-bam/src/async/io/reader.rs
@@ -383,7 +383,7 @@ where
         let chunks = index.query(reference_sequence_id, region.interval())?;
 
         Ok(Query::new(
-            self,
+            self.get_mut(),
             chunks,
             reference_sequence_id,
             region.interval(),
--- noodles-bam/src/async/io/reader/query.rs
+++ noodles-bam/src/async/io/reader/query.rs
@@ -1,32 +1,23 @@
-use std::vec;
-
 use futures::{Stream, stream};
 use noodles_bgzf as bgzf;
 use noodles_core::region::Interval;
+use noodles_csi as csi;
 use noodles_csi::binning_index::index::reference_sequence::bin::Chunk;
 use tokio::io::{self, AsyncRead, AsyncSeek};
 
 use super::Reader;
 use crate::{Record, io::reader::query::intersects};
 
-enum State {
-    Seek,
-    Read(bgzf::VirtualPosition),
-    Done,
-}
-
 /// An async reader over records of an async BAM reader that intersects a given region.
 ///
-/// This is created by calling [`super::Reader::query`].
+/// This is created by calling [`Reader::query`].
 pub struct Query<'r, R>
 where
     R: AsyncRead + AsyncSeek,
 {
-    inner: &'r mut Reader<bgzf::r#async::io::Reader<R>>,
-    chunks: vec::IntoIter<Chunk>,
+    reader: Reader<csi::r#async::io::Query<'r, R>>,
     reference_sequence_id: usize,
     interval: Interval,
-    state: State,
 }
 
 impl<'r, R> Query<'r, R>
@@ -34,17 +25,15 @@ where
     R: AsyncRead + AsyncSeek + Unpin,
 {
     pub(super) fn new(
-        inner: &'r mut Reader<bgzf::r#async::io::Reader<R>>,
+        reader: &'r mut bgzf::r#async::io::Reader<R>,
         chunks: Vec<Chunk>,
         reference_sequence_id: usize,
         interval: Interval,
     ) -> Self {
         Self {
-            inner,
-            chunks: chunks.into_iter(),
+            reader: Reader::from(csi::r#async::io::Query::new(reader, chunks)),
             reference_sequence_id,
             interval,
-            state: State::Seek,
         }
     }
 
@@ -77,29 +66,13 @@ where
     /// ```
     pub async fn read_record(&mut self, record: &mut Record) -> io::Result<usize> {
         loop {
-            match self.state {
-                State::Seek => {
-                    self.state = match self.chunks.next() {
-                        Some(chunk) => {
-                            self.inner.get_mut().seek(chunk.start()).await?;
-                            State::Read(chunk.end())
-                        }
-                        None => State::Done,
-                    };
-                }
-                State::Read(chunk_end) => match self.inner.read_record(record).await? {
-                    0 => self.state = State::Seek,
-                    n => {
-                        if self.inner.get_ref().virtual_position() >= chunk_end {
-                            self.state = State::Seek;
-                        }
-
-                        if intersects(record, self.reference_sequence_id, self.interval)? {
-                            return Ok(n);
-                        }
+            match self.reader.read_record(record).await? {
+                0 => return Ok(0),
+                n => {
+                    if intersects(record, self.reference_sequence_id, self.interval)? {
+                        return Ok(n);
                     }
-                },
-                State::Done => return Ok(0),
+                }
             }
         }
     }
