5740af63ff8d4e75be3cd5fdbf3bb36e9f0da113
Michael Macias <zaeleus@gmail.com>
2025-11-24 11:36:29 -0600

    csi/io/indexed_reader: Add examples

--- noodles-csi/src/async/io/indexed_reader.rs
+++ noodles-csi/src/async/io/indexed_reader.rs
@@ -18,6 +18,16 @@ where
     I: BinningIndex,
 {
     /// Creates an async indexed reader.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_csi as csi;
+    /// use tokio::io;
+    ///
+    /// let index = csi::Index::default();
+    /// let reader = csi::r#async::io::IndexedReader::new(io::empty(), index);
+    /// ```
     pub fn new(inner: R, index: I) -> Self {
         Self {
             inner: bgzf::r#async::io::Reader::new(inner),
@@ -26,21 +36,65 @@ where
     }
 
     /// Returns a reference to the underlying reader.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_csi as csi;
+    /// use tokio::io;
+    ///
+    /// let index = csi::Index::default();
+    /// let reader = csi::r#async::io::IndexedReader::new(io::empty(), index);
+    /// let inner = reader.get_ref();
+    /// ```
     pub fn get_ref(&self) -> &bgzf::r#async::io::Reader<R> {
         &self.inner
     }
 
     /// Returns a mutable reference to the underlying reader.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_csi as csi;
+    /// use tokio::io;
+    ///
+    /// let index = csi::Index::default();
+    /// let mut reader = csi::r#async::io::IndexedReader::new(io::empty(), index);
+    /// let inner = reader.get_mut();
+    /// ```
     pub fn get_mut(&mut self) -> &mut bgzf::r#async::io::Reader<R> {
         &mut self.inner
     }
 
     /// Returns the underlying reader.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_csi as csi;
+    /// use tokio::io;
+    ///
+    /// let index = csi::Index::default();
+    /// let reader = csi::r#async::io::IndexedReader::new(io::empty(), index);
+    /// let inner = reader.into_inner();
+    /// ```
     pub fn into_inner(self) -> bgzf::r#async::io::Reader<R> {
         self.inner
     }
 
     /// Returns the associated index.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use noodles_csi as csi;
+    /// use tokio::io;
+    ///
+    /// let index = csi::Index::default();
+    /// let reader = csi::r#async::io::IndexedReader::new(io::empty(), index);
+    /// let index = reader.index();
+    /// ```
     pub fn index(&self) -> &I {
         &self.index
     }
@@ -52,6 +106,31 @@ where
     I: BinningIndex,
 {
     /// Returns a stream over records that intersects the given region.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    /// use futures::TryStreamExt;
+    /// use noodles_csi as csi;
+    /// use tokio::fs::File;
+    ///
+    /// let index = csi::r#async::fs::read("sample.bed.gz.csi").await?;
+    ///
+    /// let mut reader = File::open("sample.bed.gz")
+    ///     .await
+    ///     .map(|f| csi::r#async::io::IndexedReader::new(f, index))?;
+    ///
+    /// let region = "sq0:8-13".parse()?;
+    /// let mut query = reader.query(&region)?;
+    ///
+    /// while let Some(record) = query.try_next().await? {
+    ///     // ...
+    /// }
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn query(&mut self, region: &Region) -> io::Result<impl Stream<Item = io::Result<Record>>> {
         let header = self
             .index
--- noodles-csi/src/io/indexed_reader.rs
+++ noodles-csi/src/io/indexed_reader.rs
@@ -18,6 +18,15 @@ where
     I: BinningIndex,
 {
     /// Creates an indexed reader.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::io;
+    /// use noodles_csi as csi;
+    /// let index = csi::Index::default();
+    /// let reader = csi::io::IndexedReader::new(io::empty(), index);
+    /// ```
     pub fn new(inner: R, index: I) -> Self {
         Self {
             inner: bgzf::io::Reader::new(inner),
@@ -26,21 +35,65 @@ where
     }
 
     /// Returns a reference to the underlying reader.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::io;
+    /// use noodles_csi as csi;
+    ///
+    /// let index = csi::Index::default();
+    /// let reader = csi::io::IndexedReader::new(io::empty(), index);
+    /// let inner = reader.get_ref();
+    /// ```
     pub fn get_ref(&self) -> &bgzf::io::Reader<R> {
         &self.inner
     }
 
     /// Returns a mutable reference to the underlying reader.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::io;
+    /// use noodles_csi as csi;
+    ///
+    /// let index = csi::Index::default();
+    /// let mut reader = csi::io::IndexedReader::new(io::empty(), index);
+    /// let inner = reader.get_mut();
+    /// ```
     pub fn get_mut(&mut self) -> &mut bgzf::io::Reader<R> {
         &mut self.inner
     }
 
     /// Returns the underlying reader.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::io;
+    /// use noodles_csi as csi;
+    ///
+    /// let index = csi::Index::default();
+    /// let reader = csi::io::IndexedReader::new(io::empty(), index);
+    /// let inner = reader.into_inner();
+    /// ```
     pub fn into_inner(self) -> bgzf::io::Reader<R> {
         self.inner
     }
 
     /// Returns the associated index.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::io;
+    /// use noodles_csi as csi;
+    ///
+    /// let index = csi::Index::default();
+    /// let reader = csi::io::IndexedReader::new(io::empty(), index);
+    /// let index = reader.index();
+    /// ```
     pub fn index(&self) -> &I {
         &self.index
     }
@@ -52,6 +105,26 @@ where
     I: BinningIndex,
 {
     /// Returns an iterator over records that intersects the given region.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # use std::fs::File;
+    /// use noodles_csi as csi;
+    ///
+    /// let index = csi::fs::read("sample.bed.gz.csi")?;
+    /// let mut reader = File::open("sample.bed.gz")
+    ///     .map(|f| csi::io::IndexedReader::new(f, index))?;
+    ///
+    /// let region = "sq0:8-13".parse()?;
+    /// let query = reader.query(&region)?;
+    ///
+    /// for result in query {
+    ///     let record = result?;
+    ///     // ...
+    /// }
+    /// # Ok::<_, Box<dyn std::error::Error>>(())
+    /// ```
     pub fn query<'r>(
         &'r mut self,
         region: &'r Region,
