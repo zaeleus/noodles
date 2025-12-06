4821ace4ec7844d044d798e6c5f80d851cb38464
Michael Macias <zaeleus@gmail.com>
2025-12-06 14:42:15 -0600

    vcf/io/reader/query: Merge Reader into Query

--- noodles-vcf/src/io/reader/query.rs
+++ noodles-vcf/src/io/reader/query.rs
@@ -4,48 +4,17 @@ use noodles_bgzf as bgzf;
 use noodles_core::region::Interval;
 use noodles_csi::{self as csi, binning_index::index::reference_sequence::bin::Chunk};
 
+use super::Reader;
 use crate::{Header, Record, variant::Record as _};
-
-struct Reader<'r, R> {
-    inner: super::Reader<csi::io::Query<'r, R>>,
-    reference_sequence_name: Vec<u8>,
-    interval: Interval,
-}
-
-impl<'r, R> Reader<'r, R>
-where
-    R: bgzf::io::BufRead + bgzf::io::Seek,
-{
-    fn new(
-        reader: &'r mut R,
-        chunks: Vec<Chunk>,
-        reference_sequence_name: Vec<u8>,
-        interval: Interval,
-    ) -> Self {
-        Self {
-            inner: super::Reader::new(csi::io::Query::new(reader, chunks)),
-            reference_sequence_name,
-            interval,
-        }
-    }
-
-    fn read_record(&mut self, header: &Header, record: &mut Record) -> io::Result<usize> {
-        next_record(
-            &mut self.inner,
-            record,
-            header,
-            &self.reference_sequence_name,
-            self.interval,
-        )
-    }
-}
 
 /// An iterator over records of a VCF reader that intersects a given region.
 ///
 /// This is created by calling [`super::Reader::query`].
 pub struct Query<'r, 'h, R> {
-    reader: Reader<'r, R>,
+    reader: Reader<csi::io::Query<'r, R>>,
     header: &'h Header,
+    reference_sequence_name: Vec<u8>,
+    interval: Interval,
     record: Record,
 }
 
@@ -54,15 +23,17 @@ where
     R: bgzf::io::BufRead + bgzf::io::Seek,
 {
     pub(super) fn new(
-        reader: &'r mut R,
+        inner: &'r mut R,
         chunks: Vec<Chunk>,
         reference_sequence_name: Vec<u8>,
         interval: Interval,
         header: &'h Header,
     ) -> Self {
         Self {
-            reader: Reader::new(reader, chunks, reference_sequence_name, interval),
+            reader: Reader::new(csi::io::Query::new(inner, chunks)),
             header,
+            reference_sequence_name,
+            interval,
             record: Record::default(),
         }
     }
@@ -75,7 +46,13 @@ where
     type Item = io::Result<Record>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.reader.read_record(self.header, &mut self.record) {
+        match next_record(
+            &mut self.reader,
+            &mut self.record,
+            self.header,
+            &self.reference_sequence_name,
+            self.interval,
+        ) {
             Ok(0) => None,
             Ok(_) => Some(Ok(self.record.clone())),
             Err(e) => Some(Err(e)),
