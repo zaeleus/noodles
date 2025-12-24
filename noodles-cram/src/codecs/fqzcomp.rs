662d469c365855007fd0d3d4b616da132472f0d2
Michael Macias <zaeleus@gmail.com>
2025-12-24 09:04:10 -0600

    cram/codecs/fqzcomp: Move models to a module

--- noodles-cram/src/codecs/fqzcomp.rs
+++ noodles-cram/src/codecs/fqzcomp.rs
@@ -1,30 +1,10 @@
 mod decode;
 mod encode;
+mod models;
 mod parameters;
 
+use self::models::Models;
 pub use self::{decode::decode, encode::encode};
-
-use super::aac::Model;
-
-struct Models {
-    len: Vec<Model>,
-    qual: Vec<Model>,
-    dup: Model,
-    rev: Model,
-    sel: Model,
-}
-
-impl Models {
-    fn new(max_sym: u8, max_sel: u8) -> Models {
-        Self {
-            len: vec![Model::new(u8::MAX); 4],
-            qual: vec![Model::new(max_sym); 1 << 16],
-            dup: Model::new(1),
-            rev: Model::new(1),
-            sel: Model::new(max_sel),
-        }
-    }
-}
 
 #[cfg(test)]
 mod tests {
---
+++ noodles-cram/src/codecs/fqzcomp/models.rs
@@ -0,0 +1,21 @@
+use crate::codecs::aac::Model;
+
+pub struct Models {
+    pub len: Vec<Model>,
+    pub qual: Vec<Model>,
+    pub dup: Model,
+    pub rev: Model,
+    pub sel: Model,
+}
+
+impl Models {
+    pub fn new(max_sym: u8, max_sel: u8) -> Models {
+        Self {
+            len: vec![Model::new(u8::MAX); 4],
+            qual: vec![Model::new(max_sym); 1 << 16],
+            dup: Model::new(1),
+            rev: Model::new(1),
+            sel: Model::new(max_sel),
+        }
+    }
+}
