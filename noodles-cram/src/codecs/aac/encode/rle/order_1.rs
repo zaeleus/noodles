29f626db1fd3564a1ef002dd9e7cab7437a24ca5
Michael Macias <zaeleus@gmail.com>
2026-01-15 09:30:02 -0600

    cram/codecs/aac/encode/rle/order_1: Iterate over source

--- noodles-cram/src/codecs/aac/encode/rle/order_1.rs
+++ noodles-cram/src/codecs/aac/encode/rle/order_1.rs
@@ -6,42 +6,52 @@ use crate::codecs::aac::{
     rle::{CONTINUE, CONTINUE_CONTEXT, INITIAL_CONTEXT, MODEL_COUNT, MODEL_SYMBOL_COUNT},
 };
 
+const NUL: u8 = 0x00;
+
 pub(super) fn encode(src: &[u8], dst: &mut Vec<u8>) -> io::Result<()> {
     let symbol_count = count_symbols(src);
     write_symbol_count(dst, symbol_count)?;
 
-    let mut model_lit = vec![Model::new(symbol_count); symbol_count.get()];
-    let mut model_run = vec![Model::new(MODEL_SYMBOL_COUNT); MODEL_COUNT];
+    let mut models = vec![Model::new(symbol_count); symbol_count.get()];
+    let mut rle_models = vec![Model::new(MODEL_SYMBOL_COUNT); MODEL_COUNT];
 
-    let mut range_coder = RangeCoder::default();
+    let mut coder = RangeCoder::default();
 
-    let mut i = 0;
-    let mut last = 0;
+    let mut iter = src.iter().peekable();
+    let mut prev_sym = NUL;
 
-    while i < src.len() {
-        let sym = src[i];
-        model_lit[last].encode(dst, &mut range_coder, sym)?;
+    while let Some(&sym) = iter.next() {
+        models[usize::from(prev_sym)].encode(dst, &mut coder, sym)?;
 
-        let mut run = src[i + 1..].iter().position(|&s| s != sym).unwrap_or(0);
-        i += run + 1;
+        let mut len = 0;
 
-        let mut rctx = usize::from(sym);
-        last = usize::from(sym);
-
-        let mut part = run.min(CONTINUE);
-        model_run[rctx].encode(dst, &mut range_coder, part as u8)?;
-        rctx = INITIAL_CONTEXT;
-        run -= part;
-
-        while part == CONTINUE {
-            part = run.min(CONTINUE);
-            model_run[rctx].encode(dst, &mut range_coder, part as u8)?;
-            rctx = CONTINUE_CONTEXT;
-            run -= part;
+        while let Some(&&s) = iter.peek()
+            && s == sym
+        {
+            len += 1;
+            iter.next();
         }
+
+        let mut rle_model = &mut rle_models[usize::from(sym)];
+
+        let mut n = len.min(CONTINUE);
+        // SAFETY: `n <= CONTINUE`.
+        rle_model.encode(dst, &mut coder, n as u8)?;
+        len -= n;
+        rle_model = &mut rle_models[INITIAL_CONTEXT];
+
+        while n == CONTINUE {
+            n = len.min(CONTINUE);
+            // SAFETY: `n <= CONTINUE`.
+            rle_model.encode(dst, &mut coder, n as u8)?;
+            len -= n;
+            rle_model = &mut rle_models[CONTINUE_CONTEXT];
+        }
+
+        prev_sym = sym;
     }
 
-    range_coder.range_encode_end(dst)?;
+    coder.range_encode_end(dst)?;
 
     Ok(())
 }
