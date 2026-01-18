e14dccc379ce9d57dba665182d4eef2540879a56
Michael Macias <zaeleus@gmail.com>
2026-01-18 11:48:49 -0600

    cram/codecs/aac/encode/order_1: Add NUL constant

--- noodles-cram/src/codecs/aac/encode/order_1.rs
+++ noodles-cram/src/codecs/aac/encode/order_1.rs
@@ -4,6 +4,7 @@ use super::{count_symbols, write_symbol_count};
 use crate::codecs::aac::{Model, RangeCoder};
 
 const CONTEXT_SIZE: usize = 2;
+const NUL: u8 = 0x00;
 
 pub(super) fn encode(src: &[u8], dst: &mut Vec<u8>) -> io::Result<()> {
     let symbol_count = count_symbols(src);
@@ -12,7 +13,7 @@ pub(super) fn encode(src: &[u8], dst: &mut Vec<u8>) -> io::Result<()> {
     let mut models = vec![Model::new(symbol_count); symbol_count.get()];
     let mut coder = RangeCoder::default();
 
-    models[0].encode(dst, &mut coder, src[0])?;
+    models[usize::from(NUL)].encode(dst, &mut coder, src[0])?;
 
     for syms in src.windows(CONTEXT_SIZE) {
         let (prev_sym, sym) = (usize::from(syms[0]), syms[1]);
