e3db25bbd6eb213e2def12ac988697419e1c72ea
Michael Macias <zaeleus@gmail.com>
2026-01-18 11:46:27 -0600

    cram/codecs/aac/encode/order_1: Add context size constant

--- noodles-cram/src/codecs/aac/encode/order_1.rs
+++ noodles-cram/src/codecs/aac/encode/order_1.rs
@@ -3,6 +3,8 @@ use std::io;
 use super::{count_symbols, write_symbol_count};
 use crate::codecs::aac::{Model, RangeCoder};
 
+const CONTEXT_SIZE: usize = 2;
+
 pub(super) fn encode(src: &[u8], dst: &mut Vec<u8>) -> io::Result<()> {
     let symbol_count = count_symbols(src);
     write_symbol_count(dst, symbol_count)?;
@@ -13,7 +15,7 @@ pub(super) fn encode(src: &[u8], dst: &mut Vec<u8>) -> io::Result<()> {
 
     models[0].encode(dst, &mut range_coder, src[0])?;
 
-    for window in src.windows(2) {
+    for window in src.windows(CONTEXT_SIZE) {
         let sym_0 = usize::from(window[0]);
         let sym_1 = window[1];
         models[sym_0].encode(dst, &mut range_coder, sym_1)?;
