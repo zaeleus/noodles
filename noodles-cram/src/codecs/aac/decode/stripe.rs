abb45de5e7d8fa7ddb52f211088634f41bb9257d
Michael Macias <zaeleus@gmail.com>
2026-01-27 10:11:15 -0600

    cram/codecs/aac/decode/stripe: Add chunk count reader

--- noodles-cram/src/codecs/aac/decode/stripe.rs
+++ noodles-cram/src/codecs/aac/decode/stripe.rs
@@ -3,20 +3,20 @@ use std::io::{self, Read};
 use crate::io::reader::num::{read_u8, read_uint7_as};
 
 pub(super) fn decode(src: &mut &[u8], len: usize) -> io::Result<Vec<u8>> {
-    let n = read_u8(src).map(usize::from)?;
-    let mut clens: Vec<usize> = Vec::with_capacity(n);
+    let chunk_count = read_chunk_count(src)?;
+    let mut clens: Vec<usize> = Vec::with_capacity(chunk_count);
 
-    for _ in 0..n {
+    for _ in 0..chunk_count {
         let clen = read_uint7_as(src)?;
         clens.push(clen);
     }
 
-    let mut t = Vec::with_capacity(n);
+    let mut t = Vec::with_capacity(chunk_count);
 
     for (j, clen) in clens.iter().enumerate() {
-        let mut ulen = len / n;
+        let mut ulen = len / chunk_count;
 
-        if len % n > j {
+        if len % chunk_count > j {
             ulen += 1;
         }
 
@@ -28,6 +28,10 @@ pub(super) fn decode(src: &mut &[u8], len: usize) -> io::Result<Vec<u8>> {
     }
 
     Ok(transpose(&t, len))
+}
+
+fn read_chunk_count(src: &mut &[u8]) -> io::Result<usize> {
+    read_u8(src).map(usize::from)
 }
 
 fn transpose<T>(chunks: &[T], uncompressed_size: usize) -> Vec<u8>
