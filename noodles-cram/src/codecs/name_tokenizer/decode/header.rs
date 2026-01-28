60afd301c371ba4f1c0f279f7ebcdada5c7ea87a
Michael Macias <zaeleus@gmail.com>
2026-01-28 10:22:23 -0600

    cram/codecs/name_tokenizer/decode/header: Read from byte buffer

--- noodles-cram/src/codecs/name_tokenizer/decode/header.rs
+++ noodles-cram/src/codecs/name_tokenizer/decode/header.rs
@@ -1,20 +1,17 @@
-use std::io::{self, Read};
+use std::io;
 
 use crate::io::reader::num::{read_u8, read_u32_le};
 
-pub(super) fn read_header<R>(reader: &mut R) -> io::Result<(usize, usize, bool)>
-where
-    R: Read,
-{
-    let ulen = read_u32_le(reader).and_then(|n| {
+pub(super) fn read_header(src: &mut &[u8]) -> io::Result<(usize, usize, bool)> {
+    let ulen = read_u32_le(src).and_then(|n| {
         usize::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
     })?;
 
-    let n_names = read_u32_le(reader).and_then(|n| {
+    let n_names = read_u32_le(src).and_then(|n| {
         usize::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
     })?;
 
-    let use_arith = read_u8(reader)? == 1;
+    let use_arith = read_u8(src)? == 1;
 
     Ok((ulen, n_names, use_arith))
 }
