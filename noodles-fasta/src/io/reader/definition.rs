33ea411f8a691ec2a96f04cdd85d77169d8c164b
Michael Macias <zaeleus@gmail.com>
2026-05-01 16:44:41 -0500

    fasta/io/reader/definition: Add parser
    This follows the same behavior as `Definition::from_str`.

--- noodles-fasta/src/io/reader/definition.rs
+++ noodles-fasta/src/io/reader/definition.rs
@@ -8,3 +8,75 @@ where
 {
     read_line(reader, buf)
 }
+
+#[allow(dead_code)]
+fn parse_definition(mut src: &[u8]) -> io::Result<(&[u8], &[u8])> {
+    if src.is_empty() {
+        return Err(io::Error::new(io::ErrorKind::InvalidData, "empty input"));
+    }
+
+    consume_prefix(&mut src)?;
+    let name = parse_name(&mut src)?;
+    let description = src.trim_ascii();
+
+    Ok((name, description))
+}
+
+fn consume_prefix(src: &mut &[u8]) -> io::Result<()> {
+    const PREFIX: u8 = b'>';
+
+    let b = src
+        .split_off_first()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "missing prefix"))?;
+
+    if *b == PREFIX {
+        Ok(())
+    } else {
+        Err(io::Error::new(io::ErrorKind::InvalidData, "invalid prefix"))
+    }
+}
+
+fn parse_name<'a>(src: &mut &'a [u8]) -> io::Result<&'a [u8]> {
+    let i = src
+        .iter()
+        .position(|&b| b.is_ascii_whitespace())
+        .unwrap_or(src.len());
+
+    // SAFETY: `i <= src.len()`.
+    let name = src.split_off(..i).unwrap();
+
+    if name.is_empty() {
+        Err(io::Error::new(io::ErrorKind::InvalidData, "missing name"))
+    } else {
+        Ok(name)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_definition() -> io::Result<()> {
+        assert_eq!(parse_definition(&b">sq0"[..])?, (&b"sq0"[..], &b""[..]));
+        assert_eq!(
+            parse_definition(&b">sq0  LN:13"[..])?,
+            (&b"sq0"[..], &b"LN:13"[..])
+        );
+
+        assert!(matches!(
+            parse_definition(b""),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+        assert!(matches!(
+            parse_definition(b"sq0"),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+        assert!(matches!(
+            parse_definition(b">"),
+            Err(e) if e.kind() == io::ErrorKind::InvalidData
+        ));
+
+        Ok(())
+    }
+}
