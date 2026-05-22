afbf8a66bab14b3872af17f3dfab9907a41abc4a
Michael Macias <zaeleus@gmail.com>
2026-05-22 10:50:50 -0500

    cram/io/reader/container/slice/records/external_data_readers: Add tests

--- noodles-cram/src/io/reader/container/slice/records/external_data_readers.rs
+++ noodles-cram/src/io/reader/container/slice/records/external_data_readers.rs
@@ -36,3 +36,42 @@ impl<'c> ExternalDataReaders<'c> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let readers = ExternalDataReaders::new();
+        assert!(readers.low_readers.iter().all(|reader| reader.is_none()));
+        assert!(readers.high_readers.is_empty());
+    }
+
+    #[test]
+    fn test_insert() {
+        let mut readers = ExternalDataReaders::new();
+
+        readers.insert(0, b"z");
+        readers.insert(0, b"a");
+        readers.insert(LOW_READER_COUNT_I32 - 1, b"b");
+        readers.insert(LOW_READER_COUNT_I32, b"c");
+
+        assert_eq!(readers.low_readers[0], Some(&b"a"[..]));
+        assert_eq!(readers.low_readers[63], Some(&b"b"[..]));
+        assert_eq!(readers.high_readers.get(&64), Some(&&b"c"[..]));
+    }
+
+    #[test]
+    fn test_get_mut() {
+        let mut readers = ExternalDataReaders::new();
+
+        readers.insert(0, b"a");
+        readers.insert(LOW_READER_COUNT_I32 - 1, b"b");
+        readers.insert(LOW_READER_COUNT_I32, b"c");
+
+        assert_eq!(readers.get_mut(&0), Some(&mut &b"a"[..]));
+        assert_eq!(readers.get_mut(&63), Some(&mut &b"b"[..]));
+        assert_eq!(readers.get_mut(&64), Some(&mut &b"c"[..]));
+    }
+}
