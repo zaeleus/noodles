4bfdb13b038d07d2eddfa1dba5390adc30c8f608
Michael Macias <zaeleus@gmail.com>
2026-05-22 10:41:42 -0500

    cram/io/reader/container/slice/records/external_data_readers: Add constant for low reader count

--- noodles-cram/src/io/reader/container/slice/records/external_data_readers.rs
+++ noodles-cram/src/io/reader/container/slice/records/external_data_readers.rs
@@ -2,8 +2,11 @@ use std::collections::HashMap;
 
 use crate::container::block;
 
+const LOW_READER_COUNT: usize = 64;
+const LOW_READER_COUNT_I32: i32 = LOW_READER_COUNT as i32;
+
 pub struct ExternalDataReaders<'c> {
-    low_readers: [Option<&'c [u8]>; 64],
+    low_readers: [Option<&'c [u8]>; LOW_READER_COUNT],
     high_readers: HashMap<block::ContentId, &'c [u8]>,
 }
 
@@ -17,7 +20,7 @@ impl<'c> ExternalDataReaders<'c> {
 
     pub fn insert(&mut self, id: block::ContentId, reader: &'c [u8]) {
         match id {
-            i @ 0..=63 => {
+            i @ 0..LOW_READER_COUNT_I32 => {
                 self.low_readers[i as usize] = Some(reader);
             }
             _ => {
@@ -28,13 +31,13 @@ impl<'c> ExternalDataReaders<'c> {
 
     pub fn get_mut(&mut self, id: &block::ContentId) -> Option<&mut &'c [u8]> {
         match *id {
-            i @ 0..=63 => self.low_readers[i as usize].as_mut(),
+            i @ 0..LOW_READER_COUNT_I32 => self.low_readers[i as usize].as_mut(),
             _ => self.high_readers.get_mut(id),
         }
     }
 }
 
-fn init_low_readers<'c>() -> [Option<&'c [u8]>; 64] {
+fn init_low_readers<'c>() -> [Option<&'c [u8]>; LOW_READER_COUNT] {
     [
         None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
         None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
