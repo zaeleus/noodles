4955cbae2334be141c5ee42f27b86913dda6c4fb
Michael Macias <zaeleus@gmail.com>
2026-05-25 14:36:38 -0500

    fasta/fai/io/reader: Handle other newlines for read_line

--- noodles-fasta/src/fai/io/reader.rs
+++ noodles-fasta/src/fai/io/reader.rs
@@ -116,7 +116,44 @@ fn read_line<R>(reader: &mut R, buf: &mut String) -> io::Result<usize>
 where
     R: BufRead,
 {
-    let result = reader.read_line(buf);
-    buf.pop();
-    result
+    const LINE_FEED: char = '\n';
+    const CARRIAGE_RETURN: char = '\r';
+
+    match reader.read_line(buf)? {
+        0 => Ok(0),
+        n => {
+            if buf.ends_with(LINE_FEED) {
+                buf.pop();
+
+                if buf.ends_with(CARRIAGE_RETURN) {
+                    buf.pop();
+                }
+            }
+
+            Ok(n)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_line() -> io::Result<()> {
+        fn t(buf: &mut String, mut src: &[u8], expected: &str) -> io::Result<()> {
+            buf.clear();
+            read_line(&mut src, buf)?;
+            assert_eq!(buf, expected);
+            Ok(())
+        }
+
+        let mut buf = String::new();
+
+        t(&mut buf, b"noodles\n", "noodles")?;
+        t(&mut buf, b"noodles\r\n", "noodles")?;
+        t(&mut buf, b"noodles", "noodles")?;
+
+        Ok(())
+    }
 }
