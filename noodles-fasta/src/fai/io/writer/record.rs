a7e8a655fcc570d5a679983e6c30f65912039681
Michael Macias <zaeleus@gmail.com>
2026-06-08 11:15:39 -0500

    fasta/fai/io/writer/record: Add test

--- noodles-fasta/src/fai/io/writer/record.rs
+++ noodles-fasta/src/fai/io/writer/record.rs
@@ -17,3 +17,25 @@ where
         line_width = record.line_width(),
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use std::num::NonZero;
+
+    use super::*;
+
+    #[test]
+    fn test_write_record() -> io::Result<()> {
+        let mut buf = Vec::new();
+
+        let line_base_count = const { NonZero::new(80).unwrap() };
+        let line_width = const { NonZero::new(81).unwrap() };
+        let record = Record::new("sq0", 13, 5, line_base_count, line_width);
+
+        write_record(&mut buf, &record)?;
+
+        assert_eq!(buf, b"sq0\t13\t5\t80\t81\n");
+
+        Ok(())
+    }
+}
