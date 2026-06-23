601ac77914b898a9a10f3ea00f3488552f36b7bc
Michael Macias <zaeleus@gmail.com>
2026-06-23 13:46:27 -0500

    sam/header/parser/context: Note how the allow duplicate tags option is toggled

--- noodles-sam/src/header/parser/context.rs
+++ noodles-sam/src/header/parser/context.rs
@@ -20,6 +20,8 @@ impl Default for Context {
 impl From<Version> for Context {
     fn from(version: Version) -> Self {
         Self {
+            // SAM 1.6 no longer allows duplicate tags. See § 1.3 "The header section" (2025-08-12):
+            // "Within each (non-`@CO`) header line, no field tag may appear more than once..."
             allow_duplicate_tags: version < Version::new(1, 6),
         }
     }
