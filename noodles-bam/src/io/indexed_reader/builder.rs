167b6e03a519fd8461fc872c36c5a50804116cbb
Michael Macias <zaeleus@gmail.com>
2026-09-22 10:41:25 -0500

    bam/io/indexed_reader/builder: Use crate::fs::read_associated_index

--- noodles-bam/src/io/indexed_reader/builder.rs
+++ noodles-bam/src/io/indexed_reader/builder.rs
@@ -1,15 +1,13 @@
 use std::{
-    ffi::{OsStr, OsString},
     fs::File,
     io::{self, Read},
-    path::{Path, PathBuf},
+    path::Path,
 };
 
 use noodles_bgzf as bgzf;
-use noodles_csi::{self as csi, BinningIndex};
+use noodles_csi::BinningIndex;
 
 use super::IndexedReader;
-use crate::bai;
 
 /// An indexed BAM reader builder.
 #[derive(Default)]
@@ -55,7 +53,7 @@ impl Builder {
 
         let index = match self.index {
             Some(index) => index,
-            None => read_associated_index(src)?,
+            None => crate::fs::read_associated_index(src).map(Box::new)?,
         };
 
         let file = File::open(src)?;
@@ -83,52 +81,5 @@ impl Builder {
             .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "missing index"))?;
 
         Ok(IndexedReader::new(reader, index))
-    }
-}
-
-fn read_associated_index<P>(src: P) -> io::Result<Box<dyn BinningIndex>>
-where
-    P: AsRef<Path>,
-{
-    let src = src.as_ref();
-
-    match bai::fs::read(build_index_src(src, "bai")) {
-        Ok(index) => Ok(Box::new(index)),
-        Err(e) if e.kind() == io::ErrorKind::NotFound => {
-            let index = csi::fs::read(build_index_src(src, "csi"))?;
-            Ok(Box::new(index))
-        }
-        Err(e) => Err(e),
-    }
-}
-
-fn build_index_src<P, S>(src: P, ext: S) -> PathBuf
-where
-    P: AsRef<Path>,
-    S: AsRef<OsStr>,
-{
-    push_ext(src.as_ref().into(), ext)
-}
-
-fn push_ext<S>(path: PathBuf, ext: S) -> PathBuf
-where
-    S: AsRef<OsStr>,
-{
-    let mut s = OsString::from(path);
-    s.push(".");
-    s.push(ext);
-    PathBuf::from(s)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_push_ext() {
-        assert_eq!(
-            push_ext(PathBuf::from("sample.bam"), "bai"),
-            PathBuf::from("sample.bam.bai")
-        );
     }
 }
