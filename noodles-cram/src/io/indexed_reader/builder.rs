3567c45b8e250428866f93cc534b5d44312f8b65
Michael Macias <zaeleus@gmail.com>
2026-09-22 10:51:17 -0500

    cram/io/indexed_reader/builder: Use crate::fs::read_associated_index

--- noodles-cram/src/io/indexed_reader/builder.rs
+++ noodles-cram/src/io/indexed_reader/builder.rs
@@ -1,8 +1,7 @@
 use std::{
-    ffi::{OsStr, OsString},
     fs::File,
     io::{self, Read},
-    path::{Path, PathBuf},
+    path::Path,
 };
 
 use noodles_fasta as fasta;
@@ -70,8 +69,7 @@ impl Builder {
         let src = src.as_ref();
 
         if self.index.is_none() {
-            let index_src = build_index_src(src);
-            self.index = crai::fs::read(index_src).map(Some)?;
+            self.index = crate::fs::read_associated_index(src).map(Some)?;
         }
 
         let file = File::open(src)?;
@@ -105,36 +103,5 @@ impl Builder {
             .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "missing index"))?;
 
         Ok(IndexedReader { inner, index })
-    }
-}
-
-fn build_index_src<P>(src: P) -> PathBuf
-where
-    P: AsRef<Path>,
-{
-    const EXT: &str = "crai";
-    push_ext(src.as_ref().into(), EXT)
-}
-
-fn push_ext<S>(path: PathBuf, ext: S) -> PathBuf
-where
-    S: AsRef<OsStr>,
-{
-    let mut s = OsString::from(path);
-    s.push(".");
-    s.push(ext);
-    PathBuf::from(s)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_build_index_src() {
-        assert_eq!(
-            build_index_src("sample.cram"),
-            PathBuf::from("sample.cram.crai")
-        );
     }
 }
