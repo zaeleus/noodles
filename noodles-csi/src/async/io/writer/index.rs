625a46bd6e681b2f83b942df02849fb9bd80c18b
Michael Macias <zaeleus@gmail.com>
2026-09-24 09:15:04 -0500

    csi/io/writer/index: Extract depth writer

--- noodles-csi/src/async/io/writer/index.rs
+++ noodles-csi/src/async/io/writer/index.rs
@@ -19,8 +19,7 @@ where
     let min_shift = i32::from(index.min_shift());
     writer.write_i32_le(min_shift).await?;
 
-    let depth = i32::from(index.depth());
-    writer.write_i32_le(depth).await?;
+    write_depth(writer, index.depth()).await?;
 
     write_aux(writer, index.header()).await?;
     write_reference_sequences(writer, index.depth(), index.reference_sequences()).await?;
@@ -30,4 +29,37 @@ where
     }
 
     Ok(())
+}
+
+async fn write_depth<W>(writer: &mut W, depth: u8) -> io::Result<()>
+where
+    W: AsyncWrite + Unpin,
+{
+    let n = i32::from(depth);
+    writer.write_i32_le(n).await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_write_depth() -> io::Result<()> {
+        async fn t(buf: &mut Vec<u8>, depth: u8, expected: &[u8]) -> io::Result<()> {
+            buf.clear();
+            write_depth(buf, depth).await?;
+            assert_eq!(buf, expected);
+            Ok(())
+        }
+
+        let mut buf = Vec::new();
+
+        t(&mut buf, 0, &[0x00, 0x00, 0x00, 0x00]).await?;
+        t(&mut buf, 5, &[0x05, 0x00, 0x00, 0x00]).await?;
+        t(&mut buf, 9, &[0x09, 0x00, 0x00, 0x00]).await?;
+
+        t(&mut buf, 10, &[0x0a, 0x00, 0x00, 0x00]).await?; // FIXME
+
+        Ok(())
+    }
 }
--- noodles-csi/src/io/writer/index.rs
+++ noodles-csi/src/io/writer/index.rs
@@ -21,8 +21,7 @@ where
     let min_shift = i32::from(index.min_shift());
     write_i32_le(writer, min_shift)?;
 
-    let depth = i32::from(index.depth());
-    write_i32_le(writer, depth)?;
+    write_depth(writer, index.depth())?;
 
     write_aux(writer, index.header())?;
     write_reference_sequences(writer, index.depth(), index.reference_sequences())?;
@@ -32,4 +31,37 @@ where
     }
 
     Ok(())
+}
+
+fn write_depth<W>(writer: &mut W, depth: u8) -> io::Result<()>
+where
+    W: Write,
+{
+    let n = i32::from(depth);
+    write_i32_le(writer, n)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_write_depth() -> io::Result<()> {
+        fn t(buf: &mut Vec<u8>, depth: u8, expected: &[u8]) -> io::Result<()> {
+            buf.clear();
+            write_depth(buf, depth)?;
+            assert_eq!(buf, expected);
+            Ok(())
+        }
+
+        let mut buf = Vec::new();
+
+        t(&mut buf, 0, &[0x00, 0x00, 0x00, 0x00])?;
+        t(&mut buf, 5, &[0x05, 0x00, 0x00, 0x00])?;
+        t(&mut buf, 9, &[0x09, 0x00, 0x00, 0x00])?;
+
+        t(&mut buf, 10, &[0x0a, 0x00, 0x00, 0x00])?; // FIXME
+
+        Ok(())
+    }
 }
